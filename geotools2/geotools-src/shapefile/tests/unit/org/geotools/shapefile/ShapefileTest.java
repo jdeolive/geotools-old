971a7dfa530429c9a771f51c0219a250bb6dfec5
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-02-13 10:31:25 +0000

    unit test for Shapefile reader basic at the moment
    git-svn-id: http://svn.osgeo.org/geotools/trunk@46 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/shapefile/tests/unit/org/geotools/shapefile/ShapefileTest.java
@@ -0,0 +1,63 @@
+/*
+ * ShapefileTest.java
+ * JUnit based test
+ *
+ * Created on 12 February 2002, 21:27
+ */
+
+package org.geotools.shapefile;
+
+import junit.framework.*;
+import java.net.*;
+import cmp.LEDataStream.*;
+import com.vividsolutions.jts.geom.*;
+import java.io.*;
+
+/**
+ *
+ * @author James Macgill
+ */
+public class ShapefileTest extends TestCase {
+    
+    public ShapefileTest(java.lang.String testName) {
+        super(testName);
+    }
+    
+    public static void main(java.lang.String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+    
+    public static Test suite() {
+        TestSuite suite = new TestSuite(ShapefileTest.class);
+        return suite;
+    }
+    
+    public void testLoadingStatePop() {
+        
+        String dataFolder = System.getProperty("dataFolder");
+        try{
+            URL url = new URL("file:///"+dataFolder+"/statePop.shp");
+            System.out.println("Testing ability to load "+url);
+            URLConnection uc = url.openConnection();
+            BufferedInputStream in = new BufferedInputStream(uc.getInputStream());
+            
+            LEDataInputStream sfile = new LEDataInputStream(in);
+            GeometryCollection shapes = Shapefile.read(sfile, new GeometryFactory());
+            assertEquals("Number of Geometries loaded incorect",49,shapes.getNumGeometries());
+            //Geometry bounds = shapes.getEnvelope();
+            //bounds.
+            //System.out.println(""+bounds.getMinX()+" "+bounds.getMinY()+" "+bounds.getMaxX()+" "+bounds.getMaxY()); 
+        }
+        catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+            fail("Load failed because of exception "+e.toString());
+        }
+    }
+    
+    // Add test methods here, they have to start with 'test' name.
+    // for example:
+    // public void testHello() {}
+    
+    
+}
