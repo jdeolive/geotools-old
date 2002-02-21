65cda5ede62ba7412bfb5223893e6ff157e0b52d
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-02-21 17:40:28 +0000

    first working version
    git-svn-id: http://svn.osgeo.org/geotools/trunk@63 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/proj4j/src/org/geotools/proj4j/Misc.java
+++ geotools2/geotools-src/proj4j/src/org/geotools/proj4j/Misc.java
@@ -32,69 +32,93 @@ public class Misc {
     //static final int MAX_WORK = 64;
     static final String sym = "NnEeSsWw";
     static final double[] vm = {
-	.0174532925199433,
-	.0002908882086657216,
-	.0000048481368110953599
+        .0174532925199433,
+        .0002908882086657216,
+        .0000048481368110953599
     };
     public static double dmsToR(String is){
-	int n, nl;
-	String work;
-	double v, tv;
-
-	//if (rs.length!=0)
-	//	rs[0] = is;
-	/* copy sting into work space */
+        int n=0, nl;
+        String work;
+        double v, tv;
+        String sign,s;
+        boolean signFlag=true;
+        
+        //if (rs.length!=0)
+        //	rs[0] = is;
+        /* copy sting into work space */
         work = is.trim();
-	StringTokenizer tok = new StringTokenizer(work,"Dd\'\"Rr+-",true);
-        String sign = tok.nextToken();
-	if (sign == "+" || sign == "-");
-	else sign = "+";
-	for (v = 0., nl = 0 ; nl < 3 ; nl = n + 1 ) {
-                String s = tok.nextToken();
-                if(s==".")break;
-                //if its not a digit then what?
-                try{
-                    tv= Double.parseDouble(s);
-                }
-                catch(NumberFormatException nfe){
-                    return Double.MAX_VALUE;
-                }
+        System.out.println("Input string was "+work);
+        StringTokenizer tok = new StringTokenizer(work,"Dd\'\"Rr+-"+sym,true);
+        s = tok.nextToken();
+        System.out.println("First tok ="+s+":");
+        if (!(s.equals( "+") || s.equals("-"))){
+            sign = "+";
+            signFlag=false;
+        }else{
+            sign=s;
+            signFlag=true;
+        }
+        for (v = 0., nl = 0 ; nl < 3 ; nl = n+1  ) {
+            if(signFlag) {
+                s = tok.nextToken();
+            }else{
+                signFlag=true;
+            }
+            
+            //if its not a digit then what?
+            try{
+                System.out.println("about to parse "+s);
+                tv= Double.parseDouble(s);
+            }
+            catch(NumberFormatException nfe){
+                System.out.println("unable to parse number:"+s+":");
+                return Double.MAX_VALUE;
+            }
+            if(tok.hasMoreTokens()){
                 s = tok.nextToken();//should be a seperator;
-                if(s.equalsIgnoreCase("d")) {n=0; break;}
-                else if(s.equals("\'")){ n=1; break ;}
-                else if(s.equals("\"")){ n=2; break ;}
+                if(s.equalsIgnoreCase("d")) {n=0;}
+                else if(s.equals("\'")){ n=1;}
+                else if(s.equals("\"")){ n=2;}
                 else if(s.equalsIgnoreCase("r")){
-			if (nl!=0) {
-				//pj_errno = -16;
-				return Double.MAX_VALUE;
-			}
-
-			s = tok.nextToken();
-			v = tv;
-			n = 4;
-                        continue;
-                }
-                else{
-                    v += tv * vm[nl];
+                    if (nl!=0) {
+                        //pj_errno = -16;
+                        System.out.println("wrongly placed R: nl="+nl+" s="+s);
+                        return Double.MAX_VALUE;
+                    }
+                    
+                    
+                    v = tv;
                     n = 4;
                     continue;
-		}
-		//if (n < nl) {
-			//pj_errno = -16;
-		//	return Double.MAX_VALUE;
-		//}
-		//v += tv * vm[n];
-		//++s;
-	}
-		/* postfix sign */
-        char end = work.charAt(work.length());
+                }else{
+                    // it must be a direction character
+                    v+=tv * vm[n];
+                    break;
+                }
+            }else{
+                v+=tv * vm[n];
+                break;
+            }
+            
+            v += tv * vm[n];
+            
+            //if (n < nl) {
+            //pj_errno = -16;
+            //	return Double.MAX_VALUE;
+            //}
+            //v += tv * vm[n];
+            //++s;
+        }
+        /* postfix sign */
+        char end = work.charAt(work.length()-1);
         if(sym.indexOf(end)>=0){
             sign = (sym.indexOf(end)>=4)?"-" : "+";
         }
-	if (sign == "-")
-		v = -v;
-	return v;
-}
+        if (sign.equals("-")){
+            v = -v;
+        }
+        return v;
+    }
     
     
 }
