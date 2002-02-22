45ce5b09bb0e926c3cf9d0231de07d83ca355825
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-02-22 10:17:37 +0000

    inital set of contants
    git-svn-id: http://svn.osgeo.org/geotools/trunk@71 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/proj4j/src/org/geotools/proj4j/Constants.java
@@ -0,0 +1,27 @@
+/*
+ * Constants.java
+ *
+ * Created on 22 February 2002, 01:24
+ */
+
+package org.geotools.proj4j;
+
+/**
+ *
+ * @author  James Macgill
+ */
+public interface Constants {
+    public static final double SIXTH = .1666666666666666667; /* 1/6 */
+    public static final double RA4 = .04722222222222222222; /* 17/360 */
+    public static final double RA6 = .02215608465608465608; /* 67/3024 */
+    public static final double RV4 =.06944444444444444444; /* 5/72 */
+    public static final double RV6 = .04243827160493827160; /* 55/1296 */
+    
+     public static final double HALFPI	=	1.5707963267948966;
+    public static final double FORTPI	=	0.78539816339744833;
+    public static final double PI	=	3.14159265358979323846;
+    public static final double TWOPI	=	6.2831853071795864769;
+    public static final double EPS      =       1.0e-12;
+
+}
+
