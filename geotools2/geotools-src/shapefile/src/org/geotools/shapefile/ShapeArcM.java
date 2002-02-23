1c8b58e7a417ab53d5e2c622fc00e99e673a450c
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-02-23 23:40:33 +0000

    commented out to ensure it compiles
    git-svn-id: http://svn.osgeo.org/geotools/trunk@93 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/shapefile/src/org/geotools/shapefile/ShapeArcM.java
+++ geotools2/geotools-src/shapefile/src/org/geotools/shapefile/ShapeArcM.java
@@ -14,10 +14,10 @@ public class ShapeArcM  {
     public int getShapeType(){
         return Shapefile.ARC_M;
     }
-    
+    /*
     public int getLength(){
         return (44+(4*numParts)+16+(16*numParts));
     }
-
+    */
   
 }
