6e30aded6f59af7f5eefccc01d3943ff849c8cbd
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-03-13 18:02:12 +0000

    moved from datasource package
    git-svn-id: http://svn.osgeo.org/geotools/trunk@203 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/core/src/org/geotools/featuretable/IndexException.java
@@ -0,0 +1,12 @@
+package org.geotools.featuretable;
+
+/** Thrown when there is an error rebuilding an index
+ */
+public class IndexException extends Exception
+{
+	public IndexException(String msg)
+	{
+		super(msg);
+	}
+}
+
