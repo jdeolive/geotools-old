2218000acc81b170aac6600fdb89de7af2178516
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-03-13 17:46:42 +0000

    converted to interface
    git-svn-id: http://svn.osgeo.org/geotools/trunk@199 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/datasource/Feature.java
+++ geotools2/geotools-src/core/src/org/geotools/datasource/Feature.java
@@ -1,16 +1,12 @@
 package org.geotools.datasource;
 
+import com.vividsolutions.jts.geom.Geometry;
 /** Represents one row of a table. A Feature is a primary Object (can be any Object, usually a Geography)
  * plus any attributes which are associated with the Object.
- * Usually, the primary is at offset 0 in "row".
- * The column names are there for help, usually a reference to the same String[] as is returned
- * from DataSource.getColumnNames().
  */
-public class Feature
-{
-	/** The row of Objects this Feature represents */
-	public Object [] row;
-	/** The names of the columns for this Feature */
-	public String [] columnNames;
+public interface Feature {
+    public Object[] getAttributes();
+    public String[] getAttributeNames();
+    public Geometry getGeometry();
 }
 
