4cc7e1663fd40c326e3dfebdcf3e0ce8681731cd
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-03-13 17:42:31 +0000

    cleaned  interface
    git-svn-id: http://svn.osgeo.org/geotools/trunk@196 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/datasource/DataSource.java
+++ geotools2/geotools-src/core/src/org/geotools/datasource/DataSource.java
@@ -1,22 +1,25 @@
 package org.geotools.datasource;
 
-import java.util.*;
 
-/** The source of data for Features. Shapefiles, database, etc. are reference through this interface.
+
+/** The source of data for Features. Shapefiles, database, etc. are referenced through this interface.
+ *  @version $Id: DataSource.java,v 1.4 2002/03/13 17:42:31 ianturton Exp $
  */
 public interface DataSource
 {
 	/** Loads Feature rows for the given Extent from the datasource
-	 */
-	public List load(Extent ex) throws DataSourceException;
+   * @param ft featureTable to load features into
+   * @param ex an extent defining which features to load - null means all features
+   * @throws DataSourceException if anything goes wrong
+   */
+	public void importFeatures(FeatureTable ft, Extent ex) throws DataSourceException;
 	
 	/** Saves the given features to the datasource
-	 */
-	public void save(List features) throws DataSourceException;
-	
-	/** gets the Column names (used by FeatureTable) for this DataSource
-	 */
-	public String [] getColumnNames();
+   * @param ft feature table to get features from
+   * @param ex extent to define which features to write - null means all
+   * @throws DataSourceException if anything goes wrong or if exporting is not supported
+   */
+	public void exportFeatures(FeatureTable ft, Extent ex) throws DataSourceException;
 	
 	/** Stops this DataSource from loading
 	 */
