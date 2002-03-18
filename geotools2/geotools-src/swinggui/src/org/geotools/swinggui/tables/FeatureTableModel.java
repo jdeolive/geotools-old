43f948ad8e78db5c7136e0f30028bbf147708ed4
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-03-18 17:40:32 +0000

    First shot at a TableModel for jtables that wraps around FeatureTables
    git-svn-id: http://svn.osgeo.org/geotools/trunk@222 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/swinggui/src/org/geotools/swinggui/tables/FeatureTableModel.java
@@ -0,0 +1,76 @@
+/*
+ * FeatureTableModel.java
+ *
+ * Created on March 18, 2002, 4:06 PM
+ */
+
+package org.geotools.swinggui.tables;
+import org.geotools.featuretable.FeatureTable;
+import org.geotools.datasource.*;
+
+/**
+ * An implementaion of swings TableModel which allows FeatureTables to be displayed.
+ * TODO: It would be excelent if there were custom cell renderers available for Geometry types
+ * @author James Macgill
+ */
+public class FeatureTableModel extends javax.swing.table.AbstractTableModel implements javax.swing.table.TableModel {
+    /** Holds the FeatureTable that will be represented by this model
+     */    
+    protected FeatureTable featureTable;
+    
+    
+    /** Creates a new instance of FeatureTableModel */
+    public FeatureTableModel() {
+    }
+
+    /** Set which featuretable to represent
+     * @param featureTable The feature table to represent.  This could fire a Table Structure Changed event.
+     */    
+    public void setFeatureTable(FeatureTable featureTable){
+        this.featureTable = featureTable;
+        this.fireTableStructureChanged();
+    }
+    //TODO:This assumes that all features have the same number of attributes, which may not be true
+    /** The number of columns in the feature table
+     * Note, for the moment this is determined by the first feature.
+     * @return the number of columns in this feature table
+     */    
+    public int getColumnCount() {
+        if(featureTable==null)return 0;
+        Feature features[] = featureTable.getFeatures();
+        if(features.length==0)return 0;
+        return features[0].getAttributes().length;
+    }
+    
+    /** gets the row count for the feature table
+     * @return the number of features in featuretable
+     */    
+    public int getRowCount() {
+        if(featureTable==null)return 0;
+        return featureTable.getFeatures().length;
+    }
+    
+    /** Gets the name of a specified column
+     * @param col The index of the column to get the name of.
+     * @return The name of 'col'
+     */    
+    public String getColumnName(int col){
+        if(featureTable==null)return null;
+        Feature features[] = featureTable.getFeatures();
+        if(features.length==0)return null;
+        return features[0].getAttributeNames()[col];
+    }
+        
+    
+    /** Get the value stored in a specifed cell.
+     * In this case, row=Feature and col=Attribute
+     * @param row the row number
+     * @param col the column number
+     * @return the value in the specifed cell
+     */    
+    public Object getValueAt(int row, int col) {
+        Feature features[] = featureTable.getFeatures();
+        return features[row].getAttributes()[col];
+    }
+    
+}
