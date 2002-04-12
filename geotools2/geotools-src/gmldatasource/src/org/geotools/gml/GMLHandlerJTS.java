e8f94ec3e8cd3510bd188ef04fda1be1880046a0
robhranac <robhranac@e5c1c795-43da-0310-a71f-fac65c449510>
2002-04-12 17:23:13 +0000

    cleaned up the documentation
    git-svn-id: http://svn.osgeo.org/geotools/trunk@298 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/gmldatasource/src/org/geotools/gml/GMLHandlerJTS.java
+++ geotools2/geotools-src/gmldatasource/src/org/geotools/gml/GMLHandlerJTS.java
@@ -1,5 +1,4 @@
 /*
- * This code is released under the Apache license, availible at the root GML4j directory.
  */
 package org.geotools.gml;
 
@@ -14,19 +13,17 @@ import com.vividsolutions.jts.geom.*;
 /**
  * LEVEL3 saxGML4j GML handler: Gets JTS objects.
  *
- * <p>This filter simply seperates and passes GML events to a GMLGeometryFilter.  The main
- * simplification that it performs is to pass along coordinates as an abstracted CoordType,
- * regardless of their notation in the GML (Coord vs. Coordinates).</p>
+ * <p>This handler must be implemented by the parent of a GMLFilterGeometry filter, in order to handle the 
+ * JTS objects passed to it from the child.</p>
  * 
  * @author Rob Hranac, Vision for New York
  * @version alpha, 12/01/01
- *
  */
 public interface GMLHandlerJTS extends XMLReader {
 
 
 		/**
-		 * Sets content handler and passes along start warning.
+		 * Recieves OGC simple feature type geometry from parent.
 		 */
 		public void geometry(Geometry geometry);
 
