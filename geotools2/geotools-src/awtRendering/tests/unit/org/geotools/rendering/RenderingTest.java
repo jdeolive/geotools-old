58ef3bee7fd257b002a6f835cacc28465f7658d8
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-04-20 12:10:56 +0000

    Quick test for the AWT renderer
    git-svn-id: http://svn.osgeo.org/geotools/trunk@314 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/awtRendering/tests/unit/org/geotools/rendering/RenderingTest.java
+++ geotools2/geotools-src/awtRendering/tests/unit/org/geotools/rendering/RenderingTest.java
@@ -3,7 +3,7 @@
  * JUnit based test
  *
  * Created on April 12, 2002, 1:18 PM
- */                
+ */
 
 package org.geotools.rendering;
 import org.geotools.renderer.*;
@@ -15,16 +15,19 @@ import org.geotools.styling.*;
 import org.geotools.map.*;
 import java.util.*;
 import junit.framework.*;
+import java.awt.Frame;
+import java.awt.Panel;
+
 
 /**
  *
  * @author jamesm
- */                                
+ */
 public class RenderingTest extends TestCase {
     
     public RenderingTest(java.lang.String testName) {
         super(testName);
-    }        
+    }
     
     public static void main(java.lang.String[] args) {
         junit.textui.TestRunner.run(suite());
@@ -34,27 +37,23 @@ public class RenderingTest extends TestCase {
         TestSuite suite = new TestSuite(RenderingTest.class);
         return suite;
     }
-
-    public void testSimpleRender()throws java.io.IOException {
+    
+    public void testSimpleRender()throws Exception {
         //same as the datasource test, load in some features into a table
-
-        // Request extent
-        EnvelopeExtent ex = new EnvelopeExtent(0, 360, 0, 180.0);
-        Feature feature = new DefaultFeature();
-        GeometryFactory geomFac = new GeometryFactory();
-        Coordinate[] coordinates = new Coordinate[7];
-        coordinates[0] = new Coordinate(0.0d,0.0d);
-        coordinates[1] = new Coordinate(1.0d,0.0d);
-        coordinates[2] = new Coordinate(1.0d,1.0d);
-        coordinates[3] = new Coordinate(2.0d,1.0d);
-        coordinates[4] = new Coordinate(2.0d,2.0d);
-        coordinates[5] = new Coordinate(3.0d,2.0d);
-        coordinates[6] = new Coordinate(3.0d,3.0d);
-        LineString line = geomFac.createLineString(coordinates);
         
-        feature.setAttributes(new Object[]{line});
+        // Request extent
+        EnvelopeExtent ex = new EnvelopeExtent(0, 15, 0, 15);
+        Feature lineFeature = new DefaultFeature();
+        
+        GeometryFactory geomFac = new GeometryFactory();
+        LineString line = makeSampleLineString(geomFac);
+        lineFeature.setAttributes(new Object[]{line});
+        Feature polygonFeature = new DefaultFeature();
+        
+        polygonFeature.setAttributes(new Object[]{makeSamplePolygon(geomFac)});
         MemoryDataSource datasource = new MemoryDataSource();
-        datasource.addFeature(feature);
+        datasource.addFeature(lineFeature);
+        datasource.addFeature(polygonFeature);
         
         FeatureTable ft = new DefaultFeatureTable(datasource);
         
@@ -63,8 +62,12 @@ public class RenderingTest extends TestCase {
         //The following is complex, and should be built from
         //an SLD document and not by hand
         LineSymbolizer linesym = new DefaultLineSymbolizer();
+        DefaultPolygonSymbolizer polysym = new DefaultPolygonSymbolizer();
+        DefaultFill myFill = new DefaultFill();
+        myFill.setColor("#ff0000");
+        polysym.setFill(myFill);
         DefaultRule rule = new DefaultRule();
-        rule.setSymbolizers(new Symbolizer[]{linesym});
+        rule.setSymbolizers(new Symbolizer[]{linesym,polysym});
         DefaultFeatureTypeStyle fts = new DefaultFeatureTypeStyle();
         fts.setRules(new Rule[]{rule});
         
@@ -72,8 +75,52 @@ public class RenderingTest extends TestCase {
         style.setFeatureTypeStyles(new FeatureTypeStyle[]{fts});
         
         map.addFeatureTable(ft,style);
-        Renderer renderer = new org.geotools.renderer.AWTRenderer();
-        map.render(renderer,ex);//and finaly try and draw it!
+        AWTRenderer renderer = new org.geotools.renderer.AWTRenderer();
+        Frame frame = new Frame();
+        Panel p = new Panel();
+        frame.add(p);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+        renderer.setOutput(p);
+        map.render(renderer,ex.getBounds());//and finaly try and draw it!
+        Thread.sleep(5000);
+    }
+    
+    private LineString makeSampleLineString(final GeometryFactory geomFac) {
+        Coordinate[] linestringCoordinates = new Coordinate[7];
+        linestringCoordinates[0] = new Coordinate(0.0d,0.0d);
+        linestringCoordinates[1] = new Coordinate(1.0d,0.0d);
+        linestringCoordinates[2] = new Coordinate(1.0d,1.0d);
+        linestringCoordinates[3] = new Coordinate(2.0d,1.0d);
+        linestringCoordinates[4] = new Coordinate(2.0d,2.0d);
+        linestringCoordinates[5] = new Coordinate(3.0d,2.0d);
+        linestringCoordinates[6] = new Coordinate(3.0d,3.0d);
+        LineString line = geomFac.createLineString(linestringCoordinates);
+        
+        return line;
+    }
+    
+    private com.vividsolutions.jts.geom.Polygon makeSamplePolygon(final GeometryFactory geomFac) {
+        Coordinate[] polygonCoordinates = new Coordinate[10];
+        polygonCoordinates[0] = new Coordinate(2,2);
+        polygonCoordinates[1] = new Coordinate(1,4);
+        polygonCoordinates[2] = new Coordinate(1,6);
+        polygonCoordinates[3] = new Coordinate(2,7);
+        polygonCoordinates[4] = new Coordinate(4,6);
+        polygonCoordinates[5] = new Coordinate(6,7);
+        polygonCoordinates[6] = new Coordinate(8,6);
+        polygonCoordinates[7] = new Coordinate(8,4);
+        polygonCoordinates[8] = new Coordinate(6,2);
+        polygonCoordinates[9] = new Coordinate(2,2);
+        try{
+        LinearRing ring = geomFac.createLinearRing(polygonCoordinates);
+        com.vividsolutions.jts.geom.Polygon polyg = geomFac.createPolygon(ring,null);
+        return polyg;
+        }
+        catch(TopologyException te){
+           fail("Error creating sample polygon for testing "+te);
+        }
+        return null;
     }
     
     class MemoryDataSource implements DataSource{
@@ -84,7 +131,7 @@ public class RenderingTest extends TestCase {
          */
         public void stopLoading() {
             //do nothing
-        }        
+        }
         
         /** Loads Feature rows for the given Extent from the datasource
          * @param ft featureTable to load features into
@@ -95,10 +142,10 @@ public class RenderingTest extends TestCase {
             for(int i=0;i<features.size();i++){
                 Feature f = (Feature)features.elementAt(i);
                 //if(ex.containsFeature(f)){
-                    ft.addFeature(f);
+                ft.addFeature(f);
                 //}
             }
-        }        
+        }
         
         /** Saves the given features to the datasource
          * @param ft feature table to get features from
@@ -114,6 +161,6 @@ public class RenderingTest extends TestCase {
         }
         
     }
-        
-
+    
+    
 }
