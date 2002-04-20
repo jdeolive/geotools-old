cea9c8ee968a98cd93729702d65a8ca68a964d47
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-04-20 12:10:15 +0000

    First semi working version Can render simple polygons and lines
    git-svn-id: http://svn.osgeo.org/geotools/trunk@313 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/awtRendering/src/org/geotools/renderer/AWTRenderer.java
+++ geotools2/geotools-src/awtRendering/src/org/geotools/renderer/AWTRenderer.java
@@ -36,75 +36,124 @@ import com.vividsolutions.jts.geom.*;
 import java.awt.*;
 
 public class AWTRenderer implements org.geotools.renderer.Renderer {
-    private Graphics g;
+    private Graphics graphics;
+    private Component component;
     private double scaleDenominator;
+    
     /** Creates a new instance of AWTRenderer */
     public AWTRenderer() {
     }
     
-    public void setOutput(Graphics graphics){
-        g = graphics;
+   public void setOutput(Component comp){
+       graphics = comp.getGraphics();
+       component = comp;
     }
     
-    public void render(Feature features[], Extent e,Style s){
+    public void render(Feature features[], Envelope e,Style s){
+        if(graphics==null) return;
+        System.out.println("renderering "+features.length+" features");
+        GeometryTransformer transform = new GeometryTransformer(new AffineTransformer(e,component.getBounds()));
         FeatureTypeStyle[] featureStylers = s.getFeatureTypeStyles();
-        for(int i=0;i<featureStylers.length;i++){
-            FeatureTypeStyle fts = featureStylers[i];
-            for(int j=0;j<features.length;j++){
-                Feature feature = features[j];
-                if(feature.getTypeName().equalsIgnoreCase(fts.getFeatureTypeName())){
-                    //this styler is for this type of feature
-                    //now find which rule applies
-                    Rule[] rules = fts.getRules();
-                    for(int k=0;k<rules.length;k++){
-                        //does this rule apply?
-                        if(rules[k].getMinScaleDenominator()<scaleDenominator && rules[k].getMaxScaleDenominator()>scaleDenominator){
-                            //yes it does
-                            //this gives us a list of symbolizers
-                            Symbolizer[] symbolizers = rules[k].getSymbolizers();
-                            //HACK: now this gets a little tricky...
-                            //HACK: each symbolizer could be a point, line, text, raster or polygon symboliser
-                            //HACK: but, if need be, a line symboliser can symbolise a polygon
-                            //HACK: this code ingores this potential problem for the moment
-                            for(int m =0;m<symbolizers.length;m++){
-                                System.out.println("Using symbolizer "+symbolizers[m]);
-                                if (symbolizers[m] instanceof PolygonSymbolizer){
-                                    renderPolygon(feature,(PolygonSymbolizer)symbolizers[m]);
-                                }
-                                else if(symbolizers[m] instanceof LineSymbolizer){
-                                    renderLine(feature,(LineSymbolizer)symbolizers[m]);
-                                }
-                                //else if...
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        processStylers(features, transform, featureStylers);
+    }
+
+    private void processStylers(final Feature[] features, final GeometryTransformer transform, final FeatureTypeStyle[] featureStylers) {
+      for(int i=0;i<featureStylers.length;i++){
+          FeatureTypeStyle fts = featureStylers[i];
+          for(int j=0;j<features.length;j++){
+              Feature feature = features[j];
+              if(feature.getTypeName().equalsIgnoreCase(fts.getFeatureTypeName())){
+                  //this styler is for this type of feature
+                  //now find which rule applies
+                  Rule[] rules = fts.getRules();
+                  for(int k=0;k<rules.length;k++){
+                      //does this rule apply?
+                      if(rules[k].getMinScaleDenominator()<scaleDenominator && rules[k].getMaxScaleDenominator()>scaleDenominator){
+                          //yes it does
+                          //this gives us a list of symbolizers
+                          Symbolizer[] symbolizers = rules[k].getSymbolizers();
+                          //HACK: now this gets a little tricky...
+                          //HACK: each symbolizer could be a point, line, text, raster or polygon symboliser
+                          //HACK: but, if need be, a line symboliser can symbolise a polygon
+                          //HACK: this code ingores this potential problem for the moment
+                          processSymbolizers(transform, feature, symbolizers);
+                      }
+                  }
+              }
+          }
+      }
+    }
+
+    private void processSymbolizers(final GeometryTransformer transform, final Feature feature, final Symbolizer[] symbolizers) {
+      for(int m =0;m<symbolizers.length;m++){
+          System.out.println("Using symbolizer "+symbolizers[m]);
+          if (symbolizers[m] instanceof PolygonSymbolizer){
+              renderPolygon(feature,(PolygonSymbolizer)symbolizers[m],transform);
+          }
+          else if(symbolizers[m] instanceof LineSymbolizer){
+              renderLine(feature,(LineSymbolizer)symbolizers[m],transform);
+          }
+          //else if...
+      }
     }
     
-    private void renderPolygon(Feature feature, PolygonSymbolizer symbolizer){
+    private void renderPolygon(Feature feature, PolygonSymbolizer symbolizer,GeometryTransformer transform){
+      
+        
         org.geotools.styling.Stroke stroke = symbolizer.getStroke();
-        String geomName = symbolizer.geometryPropertyName();
-        Geometry geom;
-        if(geomName==null){
-            geom = feature.getGeometry();
-        }
-        else{
-            String names[] =  feature.getAttributeNames();
-            for(int i=0;i<names.length;i++){
-                if(names[i].equalsIgnoreCase(geomName)){
-                    geom=(Geometry)feature.getAttributes()[i];
-                }
-            }
-        }
         Fill fill = symbolizer.getFill();
+        String geomName = symbolizer.geometryPropertyName();
+        Geometry geom = findGeometry(feature,geomName);
+        Geometry scaled = transform.transformGeometry(geom);
+        
+        
+        Coordinate[] coords = scaled.getCoordinates();
+        int[][] points = extractPointArrays(coords);
+        graphics.setColor(Color.decode(fill.getColor()));
+        graphics.fillPolygon(points[0],points[1],points[0].length);
+        graphics.setColor(Color.decode(stroke.getColor()));
+        graphics.drawPolygon(points[0],points[1],points[0].length);
+        
         System.out.println("Rendering a polygon with an outline colour of "+stroke.getColor()+
             "and a fill colour of "+fill.getColor());
     }
+
+    private int[][] extractPointArrays(final Coordinate[] coords) {
+      int[][] points = new int[2][coords.length];
+      int ypoints[] = new int[coords.length];
+      for(int i=0;i<coords.length;i++){
+          points[0][i]=(int)coords[i].x;
+          points[1][i]=(int)coords[i].y;
+      }
+
+      return points;
+    }
     
-    private void renderLine(Feature feature, LineSymbolizer symbolizer){
+    private void renderLine(Feature feature, LineSymbolizer symbolizer,GeometryTransformer transform){
         org.geotools.styling.Stroke stroke = symbolizer.getStroke();
+        String geomName = symbolizer.geometryPropertyName();
+        Geometry geom = findGeometry(feature, geomName);
+        Geometry scaled = transform.transformGeometry(geom);
+        Coordinate[] coords = scaled.getCoordinates();
+        int points[][] = extractPointArrays(coords);
+        graphics.setColor(Color.decode(stroke.getColor()));
+        graphics.drawPolyline(points[0],points[1],coords.length);
         System.out.println("Rendering a line with a colour of "+stroke.getColor());
+    }
+
+    private Geometry findGeometry(final Feature feature, final String geomName) {
+      Geometry geom = null;
+      if(geomName==null){
+          geom = feature.getGeometry();
+      }
+      else{
+          String names[] =  feature.getAttributeNames();
+          for(int i=0;i<names.length;i++){
+              if(names[i].equalsIgnoreCase(geomName)){
+                  geom=(Geometry)feature.getAttributes()[i];
+              }
+          }
+      }
+      return geom;
     }
 }
