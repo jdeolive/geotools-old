0cfcb0c8af63f71419a6b421d6017c4b91268b77
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-04-29 13:49:36 +0000

    works using gmlGeomertyDataSource
    git-svn-id: http://svn.osgeo.org/geotools/trunk@353 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/gmldatasource/tests/unit/org/geotools/gml/GmlSuite.java
+++ geotools2/geotools-src/gmldatasource/tests/unit/org/geotools/gml/GmlSuite.java
@@ -5,7 +5,7 @@ package org.geotools.gml;
  * JUnit based test
  *
  * Created on 04 March 2002, 16:09
- */                
+ */
 import org.geotools.datasource.*;
 import org.geotools.datasource.extents.*;
 import org.geotools.featuretable.*;
@@ -20,7 +20,7 @@ import junit.framework.*;
 /**
  *
  * @author ian
- */                                
+ */
 public class GmlSuite extends TestCase {
     
     static int NTests = 6;
@@ -29,7 +29,7 @@ public class GmlSuite extends TestCase {
     
     public GmlSuite(java.lang.String testName) {
         super(testName);
-    }        
+    }
     
     public static void main(java.lang.String[] args) {
         junit.textui.TestRunner.run(suite());
@@ -39,14 +39,15 @@ public class GmlSuite extends TestCase {
         TestSuite suite = new TestSuite(GmlSuite.class);
         return suite;
     }
-
-    public void testDataSource() {
+    
+        
+    public void testDataSource() throws Exception{
         System.out.println("testDataSource");
         try{
             String dataFolder = System.getProperty("dataFolder");
             URL url = new URL("file:///"+dataFolder+"/testGML7.gml");
             System.out.println("Testing ability to load "+url+" as datasource");
-            GMLDataSource ds = new GMLDataSource(url);
+            GMLGeometryDataSource ds = new GMLGeometryDataSource(url.toString());
             
             table = new DefaultFeatureTable(ds);
             
@@ -56,15 +57,15 @@ public class GmlSuite extends TestCase {
             r.setBounds(new com.vividsolutions.jts.geom.Envelope(-100, 100, 0, 100.0));
             
             //table.requestExtent(r);
-            try {
+            //try {
                 //fi = new SimpleIndex(table, "LONGITUDE");
                 
                 table.getFeatures(r);
                 
-            }catch(DataSourceException exp) {
-                System.out.println("Exception requesting Extent : "+exp.getClass().getName()+" : "+exp.getMessage());
-                exp.printStackTrace();
-            }
+            //}catch(DataSourceException exp) {
+             //   System.out.println("Exception requesting Extent : "+exp.getClass().getName()+" : "+exp.getMessage());
+             //   exp.printStackTrace();
+            //}
             
             assertEquals(5,table.getFeatures().length);
             // TODO: add more tests here
@@ -81,63 +82,9 @@ public class GmlSuite extends TestCase {
         }
     }
     
-    public void testRead() throws Exception {
-        System.out.println("testRead");
-        String[] results = {"Point","LineString","LinearRing",
-        "Polygon","Polygon","Point","Point","Point"};
-        int r=0;
-        for(int j=1;j<=NTests;j++){
-            //System.out.println("******* TEST NUMBER "+j+" ***********");
-            String dataFolder = System.getProperty("dataFolder");
-            URL url = new URL("file:///"+dataFolder+"/testGML"+j+".gml");
-            //System.out.println("Testing ability to read "+url);
-            java.io.BufferedReader in = new java.io.BufferedReader(new java.io.InputStreamReader(url.openStream()));
-            GMLReader gmlr = new GMLReader(in);
-            
-            com.vividsolutions.jts.geom.GeometryCollection gc = gmlr.read();
-            com.vividsolutions.jts.geom.GeometryCollectionIterator gci = new com.vividsolutions.jts.geom.GeometryCollectionIterator(gc);
-            int i=0;
-            Geometry g = (Geometry)gci.next(); // eat the first geomcollection
-            while(gci.hasNext()){
-                g=(Geometry)gci.next();
-                assertEquals(results[r++],g.getGeometryType());
-                //System.out.println("Geometry["+(i++)+"] = "+g.getGeometryType());
-                if(!g.getGeometryType().equalsIgnoreCase("GeometryCollection")){
-                    //System.out.println("Coordinates:");
-                    com.vividsolutions.jts.geom.Coordinate[] c = g.getCoordinates();
-                    //for(int k=0;k<c.length;k++){
-                    //System.out.println("\t "+c[k].toString());
-                    //}
-                }
-            }
-            //System.out.println("********* END TEST "+j+" **********");
-        }
-        // now a more complex test - using testGML7
-        String dataFolder = System.getProperty("dataFolder");
-        URL url = new URL("file:///"+dataFolder+"/testGML7.gml");
-        java.io.BufferedReader in = new java.io.BufferedReader(new java.io.InputStreamReader(url.openStream()));
-        GMLReader gmlr = new GMLReader(in);
-        
-        com.vividsolutions.jts.geom.GeometryCollection gc = gmlr.read();
-        com.vividsolutions.jts.geom.GeometryCollectionIterator gci = new com.vividsolutions.jts.geom.GeometryCollectionIterator(gc);
-        int i=0;
-        Geometry g = (Geometry)gci.next(); // eat the first geomcollection
-        com.vividsolutions.jts.geom.Polygon box =(com.vividsolutions.jts.geom.Polygon)gci.next();
-        com.vividsolutions.jts.geom.Polygon poly = (com.vividsolutions.jts.geom.Polygon)gci.next();
-        Point p1 = (Point)gci.next();
-        Point p2 = (Point)gci.next();
-        Point p3 = (Point)gci.next();
-        assertEquals(true,box.equals(poly.getEnvelope()));
-        assertEquals(false,poly.contains(p1));
-        assertEquals(true,poly.contains(p2));
-        assertEquals(false,poly.contains(p3));
-        assertEquals(1,poly.getNumInteriorRing());
-        
-    }
-    
     // Add test methods here, they have to start with 'test' name.
-    // for example: 
+    // for example:
     // public void testHello() {}
-
-
+    
+    
 }
