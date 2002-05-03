53c41565a721931d53eeaa9c192339cf272aabc8
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-05-03 16:30:37 +0000

    improved unit tests - now checks a geometry load of all (?) types
    git-svn-id: http://svn.osgeo.org/geotools/trunk@377 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/gmldatasource/tests/unit/org/geotools/gml/GmlSuite.java
+++ geotools2/geotools-src/gmldatasource/tests/unit/org/geotools/gml/GmlSuite.java
@@ -40,14 +40,61 @@ public class GmlSuite extends TestCase {
         return suite;
     }
     
-        
-    public void testDataSource() throws Exception{
-        System.out.println("testDataSource");
+    public void testGeometryDataSource() throws Exception{
+        System.out.println("Testing basic Geometry Loaders");
+        int[] results = {1,1,1,1,2,2,5,1,1};
+        for(int i=0;i<results.length;i++){
+            String file = "testGML"+(i+1)+".gml";
+            System.out.println("testing "+file);
+            useGeometryDataSource(file,results[i]);
+        }
+    }
+    public void useGeometryDataSource(String file, int result) throws Exception{
+        //System.out.println("testDataSource");
         try{
             String dataFolder = System.getProperty("dataFolder");
-            URL url = new URL("file:///"+dataFolder+"/testGML7.gml");
-            System.out.println("Testing ability to load "+url+" as datasource");
+            URL url = new URL("file:///"+dataFolder+"/"+file);
+            System.out.println("Testing ability to load "+file+" using Geometrydatasource");
             GMLGeometryDataSource ds = new GMLGeometryDataSource(url.toString());
+            
+            table = new DefaultFeatureTable(ds);
+            
+            
+            
+            EnvelopeExtent r = new EnvelopeExtent();
+            r.setBounds(new com.vividsolutions.jts.geom.Envelope(-100, 100, 0, 100.0));
+            
+            //table.requestExtent(r);
+            //try {
+                //fi = new SimpleIndex(table, "LONGITUDE");
+                
+                table.getFeatures(r);
+                
+            //}catch(DataSourceException exp) {
+             //   System.out.println("Exception requesting Extent : "+exp.getClass().getName()+" : "+exp.getMessage());
+             //   exp.printStackTrace();
+            //}
+            
+            assertEquals(result,table.getFeatures().length);
+            // TODO: add more tests here
+            Feature[] features = table.getFeatures();
+            System.out.println("Got "+ features.length + " features");
+            for(int i=0;i<features.length;i++){
+                
+                System.out.println("Feature: "+features[i].toString());
+            }
+        }catch(Exception e){
+            System.out.println(e);
+            e.printStackTrace();
+            fail("Load failed because of exception "+e.toString());
+        }
+    }
+    public void testGMLDataSource()throws Exception{
+        try{
+            String dataFolder = System.getProperty("dataFolder");
+            URL url = new URL("file:///"+dataFolder+"/testGML7Features.gml");
+            System.out.println("Testing ability to load "+url+" as Feature datasource");
+            DataSource ds = new GMLDataSource(url);
             
             table = new DefaultFeatureTable(ds);
             
@@ -70,10 +117,10 @@ public class GmlSuite extends TestCase {
             assertEquals(5,table.getFeatures().length);
             // TODO: add more tests here
             Feature[] features = table.getFeatures();
-            
+            System.out.println("Got "+ features.length + " features");
             for(int i=0;i<features.length;i++){
                 
-                System.out.println("Feature: "+features[i].getGeometry().toString());
+                System.out.println("Feature: "+features[i].toString());
             }
         }catch(Exception e){
             System.out.println(e);
