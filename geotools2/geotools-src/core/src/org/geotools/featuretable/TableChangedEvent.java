5408803f66fe0a3685f93abd28bd7a8e56c6ccbe
robhranac <robhranac@e5c1c795-43da-0310-a71f-fac65c449510>
2002-05-14 21:37:17 +0000

    revamped feature model
    git-svn-id: http://svn.osgeo.org/geotools/trunk@407 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/featuretable/TableChangedEvent.java
+++
@@ -1,9 +0,0 @@
-package org.geotools.featuretable;
-import java.util.EventObject;
-public class TableChangedEvent extends java.util.EventObject {
-    // TODO: potential for reason codes here later
-    public TableChangedEvent(Object source){
-        super(source);
-    }
-}
-
--- geotools2/geotools-src/core/src/org/geotools/featuretable/TableChangedListener.java
+++
@@ -1,8 +0,0 @@
-package org.geotools.featuretable;
-
-public interface TableChangedListener extends java.util.EventListener {
-    /** Gets called when a TableChangedEvent is fired
-     */
-    public void tableChanged(TableChangedEvent tce);
-}
-
