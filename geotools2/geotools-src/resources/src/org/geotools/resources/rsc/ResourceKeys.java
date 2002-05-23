09406f0b62a9d6f48403576a9810e32a35ad5f7b
desruisseaux <desruisseaux@e5c1c795-43da-0310-a71f-fac65c449510>
2002-05-23 22:27:09 +0000

    Automatically generated files for localization
    git-svn-id: http://svn.osgeo.org/geotools/trunk@514 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/resources/src/org/geotools/resources/rsc/ResourceKeys.java
@@ -0,0 +1,37 @@
+/*
+ * SEAGIS - An OpenSource implementation of OpenGIS specification
+ *          (C) 2001, Institut de Recherche pour le D‰veloppement
+ *
+ *          THIS IS AN AUTOMATICALLY GENERATED FILE. DO NOT EDIT!
+ *          Generated with: net.seagis.resources.ResourceCompiler
+ */
+package org.geotools.resources.rsc;
+
+
+/**
+ * Resource keys. This interface is used when compiling sources, but
+ * no dependencies to <code>ResourceKeys</code> should appear in any
+ * resulting class files.  Since Java compiler inline final integers
+ * values, using long identifiers will not bloat constant pools of
+ * classes compiled against the interface, providing that no class
+ * implements this interface.
+ *
+ * @see net.seagis.resources.ResourceBundle
+ * @see net.seagis.resources.ResourceCompiler
+ */
+public interface ResourceKeys
+{
+    public static final int CANCEL                            =     0;
+    public static final int OK                                =     1;
+    public static final int RESET                             =     2;
+
+    public static final int ERROR_BAD_RANGE_$2                =     3;
+    public static final int ERROR_LINE_TOO_LONG_$3            =     4;
+    public static final int ERROR_LINE_TOO_SHORT_$2           =     5;
+    public static final int ERROR_NOT_AN_INTEGER_$1           =     6;
+    public static final int ERROR_NOT_COMPARABLE_CLASS_$1     =     7;
+    public static final int ERROR_NULL_FORMAT_$2              =     8;
+    public static final int ERROR_OPERATION_ALREADY_BOUNDS_$1 =     9;
+    public static final int ERROR_PARSE_EXCEPTION_$2          =    10;
+    public static final int ERROR_UNPARSABLE_NUMBER_$1        =    11;
+}
