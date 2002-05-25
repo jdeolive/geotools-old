61aa34cf27fb5cd94b0052c2c60021ddf96c7d12
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-05-25 17:51:42 +0000

    Resolved all remaining checkstyle issues
    git-svn-id: http://svn.osgeo.org/geotools/trunk@531 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/algorithms/src/org/geotools/algorithms/GeometryProperties.java
+++ geotools2/geotools-src/algorithms/src/org/geotools/algorithms/GeometryProperties.java
@@ -6,14 +6,38 @@
 
 package org.geotools.algorithms;
 
-import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.geom.Geometry;
 
 /**
  *
  * @author  jamesm
  */
 public interface GeometryProperties {
+    
+    /**
+     * Cacluclate and return the area of the specified geometry.<br>
+     * For Polygons this is the total area inside the external ring less
+     * the total of any contained by interior rings.  GeometryCollections
+     * (including MultiPolygons) are ittereted through so the result is the
+     * sum of all polygons anywhere within the collection.
+     * Any geometry other than Polgyon or a collection returns 0;
+     *
+     * @param g The Geometry to calculate the area of.
+     * @return The total area of the Geometry 
+     */
     public double getArea(Geometry g);
+    
+    /**
+     * Cacluclate and return the perimeter of the specified geometry.<br>
+     * For Polygons this is the total length of the exterior ring and all
+     * internal rings.  For LineStrings the total line length is returned.
+     * GeometryCollections are ittereted through so the result is the
+     * sum of all Polygon and Line geometries anywhere within the collection.
+     * Any point geometries return a value of 0;
+     *
+     * @param g The Geometry to calculate the area of.
+     * @return The total area of the Geometry 
+     */
     public double getPerimeter(Geometry g);
 }
 
--- geotools2/geotools-src/algorithms/src/org/geotools/algorithms/RobustGeometryProperties.java
+++ geotools2/geotools-src/algorithms/src/org/geotools/algorithms/RobustGeometryProperties.java
@@ -6,7 +6,13 @@
 
 package org.geotools.algorithms;
 
-import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.Coordinate;
 
 /** 
  * Robust implementation of the GeometryProperties interface
@@ -15,7 +21,7 @@ import com.vividsolutions.jts.geom.*;
  *
  *
  * @author andyt
- * @version $Revision: 1.3 $ $Date: 2002/03/12 12:02:04 $
+ * @version $Revision: 1.4 $ $Date: 2002/05/25 17:51:42 $
  */
 public class RobustGeometryProperties implements org.geotools.algorithms.GeometryProperties {
 
@@ -30,7 +36,7 @@ public class RobustGeometryProperties implements org.geotools.algorithms.Geometr
         double perimeter = 0.0d;
         int numberOfGeometries1 = geometryCollection1.getNumGeometries();
         // Go through geometryCollection1 and sum areas of component geometries
-        for (int i = 0; i < numberOfGeometries1; i ++) {
+        for (int i = 0; i < numberOfGeometries1; i++) {
             area += getArea(geometryCollection1.getGeometryN(i));
         }
         return area;
@@ -38,53 +44,69 @@ public class RobustGeometryProperties implements org.geotools.algorithms.Geometr
 
     /**
      * Returns
-     * @param geometryCollection1 The GeometryCollection for which the perimeter is
+     * @param geometryCollection The GeometryCollection for which the perimeter is
      * calulated
      * @return the perimeter of a GeometryCollection
      */
-    protected double getPerimeter(GeometryCollection geometryCollection1) {
+    protected double getPerimeter(GeometryCollection geometryCollection) {
         double perimeter = 0.0d;
-        int numberOfGeometries1 = geometryCollection1.getNumGeometries();
-        // Go through geometryCollection1 and sum perimeters of component 
+        int numberOfGeometries = geometryCollection.getNumGeometries();
+        // Go through geometryCollection and sum perimeters of component 
         // geometries
-        for (int i = 0; i < numberOfGeometries1; i ++) {
-            perimeter += getPerimeter(geometryCollection1.getGeometryN(i));
+        for (int i = 0; i < numberOfGeometries; i++) {
+            perimeter += getPerimeter(geometryCollection.getGeometryN(i));
         }
         return perimeter;
     }
 
-    /**
-     * Returns the area of the geometry
+   /**
+     * Cacluclate and return the area of the specified geometry.<br>
+     * For Polygons this is the total area inside the external ring less
+     * the total of any contained by interior rings.  GeometryCollections
+     * (including MultiPolygons) are ittereted through so the result is the
+     * sum of all polygons anywhere within the collection.
+     * Any geometry other than Polgyon or a collection returns 0;
+     *
+     * @param geometry The Geometry to calculate the area of.
+     * @return The total area of the Geometry 
      */
-    public double getArea(Geometry geometry1) {
+    public double getArea(Geometry geometry) {
         double area = 0.0d;
-        if (geometry1 instanceof GeometryCollection) {
-            area += getArea((GeometryCollection) geometry1);
-        } else if (geometry1 instanceof MultiPolygon) {
-            area += getArea((MultiPolygon) geometry1);
-        } else if (geometry1 instanceof Polygon) {
-            area += getArea((Polygon) geometry1);
+        if (geometry instanceof GeometryCollection) {
+            area += getArea((GeometryCollection) geometry);
+        } else if (geometry instanceof MultiPolygon) {
+            area += getArea((MultiPolygon) geometry);
+        } else if (geometry instanceof Polygon) {
+            area += getArea((Polygon) geometry);
         } else {
             area += 0.0d;
         }
         return area;
     }
 
-    /**
-     * Returns the perimeter of the geometry
+   /**
+     * Cacluclate and return the perimeter of the specified geometry.<br>
+     * For Polygons this is the total length of the exterior ring and all
+     * internal rings.  For LineStrings the total line length is returned.
+     * GeometryCollections are ittereted through so the result is the
+     * sum of all Polygon and Line geometries anywhere within the collection.
+     * Any point geometries return a value of 0;
+     *
+     * @param geometry The Geometry to calculate the area of.
+     * @return The total area of the Geometry 
      */
-    public double getPerimeter(Geometry geometry1) {
+    public double getPerimeter(Geometry geometry) {
         double perimeter = 0.0d;
-        if (geometry1 instanceof GeometryCollection) {
-            perimeter += getPerimeter((GeometryCollection) geometry1);
-        } else if (geometry1 instanceof MultiPolygon) {
-            perimeter += getPerimeter((MultiPolygon) geometry1);
-        } else if (geometry1 instanceof Polygon) {
-            perimeter += getPerimeter((Polygon) geometry1);
-        } else if (geometry1 instanceof MultiLineString) {
-            perimeter += getPerimeter((MultiLineString) geometry1);
-        } else if (geometry1 instanceof LineString) {
-            perimeter += getPerimeter((LineString) geometry1);
+        if (geometry instanceof GeometryCollection) {
+            perimeter += getPerimeter((GeometryCollection) geometry);
+        } else if (geometry instanceof MultiPolygon) {
+            perimeter += getPerimeter((MultiPolygon) geometry);
+        } else if (geometry instanceof Polygon) {
+            perimeter += getPerimeter((Polygon) geometry);
+        } else if (geometry instanceof MultiLineString) {
+            perimeter += getPerimeter((MultiLineString) geometry);
+        } else if (geometry instanceof LineString) {
+            perimeter += getPerimeter((LineString) geometry);
         } else {
             perimeter += 0.0d;
         }
@@ -93,77 +115,88 @@ public class RobustGeometryProperties implements org.geotools.algorithms.Geometr
     
     /**
      * Returns the area of a MultiPolygon
-     * @param multiPolygon1 - the MultiPolygon for which the area is calculated
+     * @param multiPolygon - the MultiPolygon for which the area is calculated
+     * @return Total area of all polygons in multiPolgyon
      */
-    protected double getArea(MultiPolygon multiPolygon1) {
+    protected double getArea(MultiPolygon multiPolygon) {
         double area = 0.0d;
-        int numberOfGeometries1 = multiPolygon1.getNumGeometries();
-        for (int i = 0; i < numberOfGeometries1; i ++) {
-            area += getArea(multiPolygon1.getGeometryN(i));
+        int numberOfGeometries = multiPolygon.getNumGeometries();
+        for (int i = 0; i < numberOfGeometries; i++) {
+            area += getArea(multiPolygon.getGeometryN(i));
         }
         return area;
     }
     
     /**
      * Returns the perimeter of a MultiPolygon
-     * @param multiPolygon1 - the MultiPolygon for which the perimeter is 
+     * @param multiPolygon - the MultiPolygon for which the perimeter is 
      * calculated
+     * @return Total perimeter of all polygons in the multiPolygon
      */
-    protected double getperimeter(MultiPolygon multiPolygon1) {
+    protected double getperimeter(MultiPolygon multiPolygon) {
         double perimeter = 0.0d;
-        int numberOfGeometries1 = multiPolygon1.getNumGeometries();
-        for (int i = 0; i < numberOfGeometries1; i ++) {
-            perimeter += getPerimeter(multiPolygon1.getGeometryN(i));
+        int numberOfGeometries = multiPolygon.getNumGeometries();
+        for (int i = 0; i < numberOfGeometries; i++) {
+            perimeter += getPerimeter(multiPolygon.getGeometryN(i));
         }
         return perimeter;
     }
 
     /**
      * Returns the area of a Polygon
-     * @param polygon1 - the Polygon for which the area is calculated
+     * @param polygon - the Polygon for which the area is calculated
+     * @return The area of the polgyon
      */
-    protected double getArea(Polygon polygon1) {
+    protected double getArea(Polygon polygon) {
         double area = 0.0d;
         double interiorArea = 0.0d;
-        Coordinate[] exteriorRingCoordinates = polygon1.getExteriorRing().getCoordinates();
+        Coordinate[] exteriorRingCoordinates = polygon.getExteriorRing().getCoordinates();
         int numberOfExteriorRingCoordinates = exteriorRingCoordinates.length;
         // Calculate the boundingBox of polygon1 aligned with the axes x and y
         double minx = Double.POSITIVE_INFINITY;
         double maxx = Double.NEGATIVE_INFINITY;
         double miny = Double.POSITIVE_INFINITY;
         double maxy = Double.NEGATIVE_INFINITY;
-        for (int i = 0; i < numberOfExteriorRingCoordinates; i ++) {
-            minx = Math.min(minx,exteriorRingCoordinates[i].x);
-            maxx = Math.max(maxx,exteriorRingCoordinates[i].x);
-            miny = Math.min(miny,exteriorRingCoordinates[i].y);
-            maxy = Math.max(maxy,exteriorRingCoordinates[i].y);
+        for (int i = 0; i < numberOfExteriorRingCoordinates; i++) {
+            minx = Math.min(minx, exteriorRingCoordinates[i].x);
+            maxx = Math.max(maxx, exteriorRingCoordinates[i].x);
+            miny = Math.min(miny, exteriorRingCoordinates[i].y);
+            maxy = Math.max(maxy, exteriorRingCoordinates[i].y);
         }
-        //Calculate area of each trapezoid formed by droping lines from each pair of coordinates in exteriorRingCoorinates to the x-axis.
+        //Calculate area of each trapezoid formed by droping lines from 
+        //each pair of coordinates in exteriorRingCoorinates to the x-axis.
         //x[i]<x[i-1] will contribute a negative area
-        for (int i = 0; i < (numberOfExteriorRingCoordinates - 1); i ++) {
-            area += (((exteriorRingCoordinates[i+1].x - minx) - (exteriorRingCoordinates[i].x - minx)) * (((exteriorRingCoordinates[i+1].y - miny) + (exteriorRingCoordinates[i].y - miny)) / 2d));
+        for (int i = 0; i < (numberOfExteriorRingCoordinates - 1); i++) {
+            area += (((exteriorRingCoordinates[i + 1].x - minx) - 
+                    (exteriorRingCoordinates[i].x - minx)) * 
+                    (((exteriorRingCoordinates[i + 1].y - miny) + 
+                    (exteriorRingCoordinates[i].y - miny)) / 2d));
         }
         area = Math.abs(area);
-        //Calculate area of each trapezoid formed by droping lines from each pair of coordinates in interiorRingCoorinates to the x-axis.
-        int numberOfInteriorRings = polygon1.getNumInteriorRing();
+        //Calculate area of each trapezoid formed by droping lines
+        //from each pair of coordinates in interiorRingCoorinates to the x-axis.
+        int numberOfInteriorRings = polygon.getNumInteriorRing();
         int numberOfInteriorRingCoordinates;
         Coordinate[] interiorRingCoordinates;
-        for (int i = 0; i < numberOfInteriorRings; i ++) {
+        for (int i = 0; i < numberOfInteriorRings; i++) {
             interiorArea = 0.0d;
-            interiorRingCoordinates = polygon1.getInteriorRingN(i).getCoordinates();
+            interiorRingCoordinates = polygon.getInteriorRingN(i).getCoordinates();
             numberOfInteriorRingCoordinates = interiorRingCoordinates.length;
             minx = Double.POSITIVE_INFINITY;
             maxx = Double.NEGATIVE_INFINITY;
             miny = Double.POSITIVE_INFINITY;
             maxy = Double.NEGATIVE_INFINITY;
-            for (int j = 0; j < numberOfInteriorRingCoordinates; j ++) {
-                minx = Math.min(minx,interiorRingCoordinates[j].x);
-                maxx = Math.max(maxx,interiorRingCoordinates[j].x);
-                miny = Math.min(miny,interiorRingCoordinates[j].y);
-                maxy = Math.max(maxy,interiorRingCoordinates[j].y);
+            for (int j = 0; j < numberOfInteriorRingCoordinates; j++) {
+                minx = Math.min(minx, interiorRingCoordinates[j].x);
+                maxx = Math.max(maxx, interiorRingCoordinates[j].x);
+                miny = Math.min(miny, interiorRingCoordinates[j].y);
+                maxy = Math.max(maxy, interiorRingCoordinates[j].y);
             }
-            for (int j = 0; j < (numberOfInteriorRingCoordinates - 1); j ++) {
-                interiorArea += (((interiorRingCoordinates[j+1].x - minx) - (interiorRingCoordinates[j].x - minx)) * (((interiorRingCoordinates[j+1].y - miny) + (interiorRingCoordinates[j].y - miny)) / 2d));
+            for (int j = 0; j < (numberOfInteriorRingCoordinates - 1); j++) {
+                interiorArea += (((interiorRingCoordinates[j + 1].x - minx) - 
+                                (interiorRingCoordinates[j].x - minx)) * 
+                                (((interiorRingCoordinates[j + 1].y - miny) + 
+                                (interiorRingCoordinates[j].y - miny)) / 2d));
             }
             area -= Math.abs(interiorArea);
         }
@@ -172,43 +205,46 @@ public class RobustGeometryProperties implements org.geotools.algorithms.Geometr
 
     /**
      * Returns the perimeter of a Polygon
-     * @param polygon1 - the Polygon for which the perimeter is calculated
+     * @param polygon - the Polygon for which the perimeter is calculated
+     * @return The perimeter of the polygon
      */
-    protected double getPerimeter(Polygon polygon1) {
+    protected double getPerimeter(Polygon polygon) {
         double perimeter = 0.0d;
-        LineString lineString1 = polygon1.getExteriorRing();
-        perimeter += getPerimeter(lineString1);
-        int numberOfHoles = polygon1.getNumInteriorRing();
-        for (int i = 0; i < numberOfHoles; i ++) {
-            perimeter += getPerimeter(polygon1.getInteriorRingN(i));
+        LineString lineString = polygon.getExteriorRing();
+        perimeter += getPerimeter(lineString);
+        int numberOfHoles = polygon.getNumInteriorRing();
+        for (int i = 0; i < numberOfHoles; i++) {
+            perimeter += getPerimeter(polygon.getInteriorRingN(i));
         }
         return perimeter;
     }
 
     /**
      * Returns the perimeter of a MultiLineString
-     * @param multiLineString1 - the MultiLineString for which the perimeter is
+     * @param multiLineString - the MultiLineString for which the perimeter is
      * calculated
+     * @return the total perimter (length) of the lines in multiLineString
      */
-    protected double getPerimeter(MultiLineString multiLineString1) {
+    protected double getPerimeter(MultiLineString multiLineString) {
         double perimeter = 0.0d;
-        int numberOfGeometries1 = multiLineString1.getNumGeometries();
-        for (int i = 0; i < numberOfGeometries1; i ++) {
-            perimeter += getPerimeter(multiLineString1.getGeometryN(i));
+        int numberOfGeometries = multiLineString.getNumGeometries();
+        for (int i = 0; i < numberOfGeometries; i++) {
+            perimeter += getPerimeter(multiLineString.getGeometryN(i));
         }
         return perimeter;
     }
 
     /**
      * Returns the perimeter of a LineString
-     * @param lineString1 - the LineString for which the perimeter is calculated
+     * @param lineString - the LineString for which the perimeter is calculated
+     * @return the perimeter (length) of hte lineString
      */
-    protected double getPerimeter(LineString lineString1) {
+    protected double getPerimeter(LineString lineString) {
         double perimeter = 0.0d;
-        int numberOfPoints1 = lineString1.getNumPoints();
-        Coordinate[] coordinates1 = lineString1.getCoordinates();
-        for (int i = 0; i < (numberOfPoints1 - 1); i ++) {
-            perimeter += coordinates1[i].distance(coordinates1[i + 1]);
+        int numberOfPoints = lineString.getNumPoints();
+        Coordinate[] coordinates = lineString.getCoordinates();
+        for (int i = 0; i < (numberOfPoints - 1); i++) {
+            perimeter += coordinates[i].distance(coordinates[i + 1]);
         }
         return perimeter;
     }
