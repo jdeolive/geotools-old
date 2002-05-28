d6bdb60d4e997ac83a7f56d046d942fa87c1cfba
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-05-28 09:50:41 +0000

    a holder for graphic objects needed during styling
    git-svn-id: http://svn.osgeo.org/geotools/trunk@550 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/defaultcore/src/org/geotools/styling/DefaultGraphic.java
@@ -0,0 +1,75 @@
+/*
+ * DefaultGraphic.java
+ *
+ * Created on 23 May 2002, 14:56
+ */
+
+package org.geotools.styling;
+
+/**
+ *
+ * @author  iant
+ */
+public class DefaultGraphic implements org.geotools.styling.Graphic {
+    
+    /** Creates a new instance of DefaultGraphic */
+    public DefaultGraphic(String url) {
+        // TODO: do something with this 
+    }
+    
+    /**
+     * Privides a list of external graphics which can be used to represent this graphic.
+     * Each one should be an equivelent representation but in a different format.
+     * If none are provied, or if none of the formats are supported then the list of Marks should be used instead.
+     */
+    public ExternalGraphic[] getExternalGraphics() {
+        return null; //TODO provide reall return
+    }
+    
+    /**
+     * Provides a list of suitible marks which can be used to represent this graphic.
+     * These should only be used if not ExternalGraphic is provided, or if none of the external graphics formats
+     * are supported.
+     *
+     * @return An array of marks to use when displaying this Graphic.  By default a "square" with 50% gray fill and black outline with
+     * a size of 6 pixels (unless a size is specifed) is provided.
+     */
+    public Mark[] getMarks() {
+        return null; //TODO provide reall return
+    }
+    
+    /**
+     * This specifies the level of translucency to use when rendering the graphic.<br>
+     * The value is encoded as a floating-point value between 0.0 and 1.0 with 0.0
+     * representing totaly transparent and 1.0 representing totaly opaque, with a linear scale of
+     * translucency for intermediate values.<br>
+     * For example, "0.65" would represent 65% opacity.  The default value is 1.0 (opaque)
+     *
+     * @return The opacity of the Graphic, where 0.0 is completly transparent and 1.0 is completly opaque.
+     */
+    public double getOpacity() {
+        return 0.0; //TODO provide reall return
+    }
+    
+    /**
+     * This parameter defines the rotation of a graphic in the clockwise direction about its center point in decimal degrees.  The value encoded as a floating point number.
+     *
+     * @return The angle of rotation in decimal degrese, Negative values represent counter-clockwise rotation.  The default is 0.0 (no rotation)
+     */
+    public double getRotation() {
+        return 0.0; //TODO provide reall return
+    }
+    
+    /**
+     * This paramteter gives the absolute size of teh graphic in pixels encoded as a floating point number.<p>
+     * The default size of an image format (such as GIFD) is the inferent size of the image.  The default size of a format
+     * without an inherent size (such as SVG) is deffined to be 16 pixels in height and the corresponding aspect in width.
+     * If a size is specifed, the height of the graphic will be scaled to that size and the corresponding aspect will be used for the width.
+     *
+     * @return The size of the graphic, the default is context specific.  Negative values are not possible.
+     */
+    public double getSize() {
+        return 0.0; //TODO provide reall return
+    }
+    
+}
