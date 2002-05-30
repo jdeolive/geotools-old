c7432335be286703347cd8e31847c19022e7485d
desruisseaux <desruisseaux@e5c1c795-43da-0310-a71f-fac65c449510>
2002-05-30 23:16:17 +0000

    Added localized strings for 'cts-coordtrans' package
    git-svn-id: http://svn.osgeo.org/geotools/trunk@592 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/cts-coordtrans/src/org/geotools/resources/cts/Resources_en.java
@@ -0,0 +1,54 @@
+/*
+ * Geotools - OpenSource mapping toolkit
+ * (C) 2002, Centre for Computational Geography
+ * (C) 2001, Institut de Recherche pour le Développement
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ * Contacts:
+ *     UNITED KINGDOM: James Macgill
+ *             mailto:j.macgill@geog.leeds.ac.uk
+ *
+ *     FRANCE: Surveillance de l'Environnement Assistée par Satellite
+ *             Institut de Recherche pour le Développement / US-Espace
+ *             mailto:seasnet@teledetection.fr
+ *
+ *     CANADA: Observatoire du Saint-Laurent
+ *             Institut Maurice-Lamontagne
+ *             mailto:osl@osl.gc.ca
+ */
+package org.geotools.resources.cts;
+
+
+/**
+ * English language resource.
+ *
+ * @version 1.0
+ * @author Martin Desruisseaux
+ */
+public class Resources_en extends Resources {
+    /**
+     * The resource file, using UTF-8 encoding.
+     */
+    static final String FILEPATH = "org/geotools/resources/cts/resources_en.utf";
+    
+    /**
+     * Construct resources for English.
+     */
+    public Resources_en() {
+        super(FILEPATH);
+    }
+}
