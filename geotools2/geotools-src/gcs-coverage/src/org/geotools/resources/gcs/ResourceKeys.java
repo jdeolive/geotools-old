f88cbf54abeb7c08427745ec5294d428524d3be9
desruisseaux <desruisseaux@e5c1c795-43da-0310-a71f-fac65c449510>
2002-06-02 19:38:31 +0000

    Initial localized ressources
    git-svn-id: http://svn.osgeo.org/geotools/trunk@633 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/gcs-coverage/src/org/geotools/resources/gcs/ResourceKeys.java
@@ -0,0 +1,86 @@
+/*
+ * Geotools - OpenSource mapping toolkit
+ * (C) 2002, Centre for Computational Geography
+ * (C) 2001, Institut de Recherche pour le Développement
+ *
+ *     THIS IS AN AUTOMATICALLY GENERATED FILE. DO NOT EDIT!
+ *     Generated with: org.geotools.resources.ResourceCompiler
+ */
+package org.geotools.resources.gcs;
+
+
+/**
+ * Resource keys. This interface is used when compiling sources, but
+ * no dependencies to <code>ResourceKeys</code> should appear in any
+ * resulting class files.  Since Java compiler inlines final integer
+ * values, using long identifiers will not bloat constant pools of
+ * classes compiled against the interface, provided that no class
+ * implements this interface.
+ *
+ * @see org.geotools.resources.ResourceBundle
+ * @see org.geotools.resources.ResourceCompiler
+ */
+public interface ResourceKeys
+{
+    public static final int BLACK                                =     0;
+    public static final int BLUE                                 =     1;
+    public static final int CLASS                                =     2;
+    public static final int CODEC_GRID                           =     3;
+    public static final int CODEC_MATRIX                         =     4;
+    public static final int CODEC_RAW                            =     5;
+    public static final int CYAN                                 =     6;
+    public static final int DEFAULT_VALUE                        =     7;
+    public static final int FILE_$1                              =     8;
+    public static final int FILE_POSITION_$2                     =     9;
+    public static final int GRAY                                 =    10;
+    public static final int GREEN                                =    11;
+    public static final int HUE                                  =    12;
+    public static final int LIGHTNESS                            =    13;
+    public static final int LINE_$1                              =    14;
+    public static final int MAGENTA                              =    15;
+    public static final int NAME                                 =    16;
+    public static final int NODATA                               =    17;
+    public static final int PALETTE                              =    18;
+    public static final int POINT_COUNT_$3                       =    19;
+    public static final int RED                                  =    20;
+    public static final int SATURATION                           =    21;
+    public static final int TRANSPARENCY                         =    22;
+    public static final int UNDEFINED                            =    23;
+    public static final int UNTITLED                             =    24;
+    public static final int YELLOW                               =    25;
+
+    public static final int ERROR_AMBIGIOUS_AXIS_LENGTH          =    26;
+    public static final int ERROR_BAD_BAND_NUMBER_$1             =    27;
+    public static final int ERROR_BAD_COEFFICIENT_$2             =    28;
+    public static final int ERROR_BAD_COORDINATE_$1              =    29;
+    public static final int ERROR_BAD_GRID_RANGE_$1              =    30;
+    public static final int ERROR_CANT_REPROJECT_$1              =    31;
+    public static final int ERROR_CANT_TRANSFORM_ENVELOPE        =    32;
+    public static final int ERROR_DUPLICATED_PROPERTY_$1         =    33;
+    public static final int ERROR_EMPTY_ENVELOPE                 =    34;
+    public static final int ERROR_ENVELOPE_MISMATCH              =    35;
+    public static final int ERROR_FILE_HAS_TOO_FEW_DATA          =    36;
+    public static final int ERROR_INCOMPATIBLE_COORDINATE_SYSTEM =    37;
+    public static final int ERROR_MISMATCHED_ARRAY_LENGTH        =    38;
+    public static final int ERROR_MISMATCHED_DIMENSION_$2        =    39;
+    public static final int ERROR_NEGATIVE_COLUMN_$2             =    40;
+    public static final int ERROR_NOT_A_GRID                     =    41;
+    public static final int ERROR_NO_IMAGE_INPUT                 =    42;
+    public static final int ERROR_NO_IMAGE_READER                =    43;
+    public static final int ERROR_NO_TRANSFORM2D_AVAILABLE       =    44;
+    public static final int ERROR_NUMBER_OF_BANDS_MISMATCH_$2    =    45;
+    public static final int ERROR_OPERATION_ALREADY_BOUND_$1     =    46;
+    public static final int ERROR_OPERATION_NOT_FOUND_$1         =    47;
+    public static final int ERROR_POINT_OUTSIDE_COVERAGE_$1      =    48;
+    public static final int ERROR_QUALITATIVE_CATEGORY_$1        =    49;
+    public static final int ERROR_RANGE_OVERLAP_$4               =    50;
+    public static final int ERROR_RGB_OUT_OF_RANGE_$1            =    51;
+    public static final int ERROR_UNDEFINED_PROPERTY             =    52;
+    public static final int ERROR_UNDEFINED_PROPERTY_$1          =    53;
+    public static final int ERROR_UNEXPECTED_IMAGE_SIZE          =    54;
+    public static final int ERROR_UNKNOW_INTERPOLATION_$1        =    55;
+    public static final int ERROR_UNKNOW_PARAMETER_NAME_$1       =    56;
+    public static final int ERROR_UNSPECIFIED_COORDINATE_SYSTEM  =    57;
+    public static final int ERROR_UNSPECIFIED_IMAGE_SIZE         =    58;
+    public static final int ERROR_UNSUPPORTED_DATA_TYPE          =    59;
+}
