d2d8c40ebec692478833405d430cd301f424b996
loxnard <loxnard@e5c1c795-43da-0310-a71f-fac65c449510>
2002-06-04 16:09:34 +0000

    Added cvs Id token + licence statement
    git-svn-id: http://svn.osgeo.org/geotools/trunk@675 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/styling/Style.java
+++ geotools2/geotools-src/core/src/org/geotools/styling/Style.java
@@ -1,14 +1,28 @@
 /*
- * Style.java
+ *    Geotools - OpenSource mapping toolkit
+ *    (C) 2002, Centre for Computational Geography
  *
- * Created on March 27, 2002, 1:14 PM
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; 
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *    
  */
 
 package org.geotools.styling;
 
 /**
- *
- * @author  jamesm
+ * @version $Id: Style.java,v 1.2 2002/06/04 16:09:34 loxnard Exp $
+ * @author James Macgill
  */
 public interface Style {
     
