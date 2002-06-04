33e4f2d096c26d3bfd3cd4aa1e3a5c96ba551d1a
loxnard <loxnard@e5c1c795-43da-0310-a71f-fac65c449510>
2002-06-04 14:27:01 +0000

    Misc. JavaDoc + added licence statement
    git-svn-id: http://svn.osgeo.org/geotools/trunk@659 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/data/Extent.java
+++ geotools2/geotools-src/core/src/org/geotools/data/Extent.java
@@ -1,3 +1,23 @@
+/*
+ *    Geotools - OpenSource mapping toolkit
+ *    (C) 2002, Centre for Computational Geography
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 package org.geotools.data;
 
 
@@ -5,12 +25,12 @@ import org.geotools.feature.Feature;
 
 /**
  * Represents the extent of a loaded Feature set.
- * @author ray
+ * @author Ray Gallagher
  */
 public interface Extent{
         /**
-         * Gets the Extent which represents the intersection between this and
-         * another.
+         * Gets the Extent which represents the intersection between this
+         * Extent and other.
          * @param other The extent to test against.
          * @return An Extent representing the intersecting area between two
          * Extents (null if there is no overlap).
