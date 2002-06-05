66a01ad4272bbcb8f8632fa760ee06c9c42a3792
loxnard <loxnard@e5c1c795-43da-0310-a71f-fac65c449510>
2002-06-05 13:08:00 +0000

    Added licence statement and cvs id tag
    git-svn-id: http://svn.osgeo.org/geotools/trunk@718 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/filter/src/org/geotools/filter/ExpressionAttribute.java
+++ geotools2/geotools-src/filter/src/org/geotools/filter/ExpressionAttribute.java
@@ -1,6 +1,23 @@
 /*
- * This code is released under the Apache license, availible at the root GML4j directory.
+ *    Geotools - OpenSource mapping toolkit
+ *    (C) 2002, Centre for Computational Geography
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; 
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *    
  */
+
 package org.geotools.filter;
 
 import org.geotools.data.*;
@@ -10,10 +27,10 @@ import org.geotools.feature.*;
  * Defines a complex filter (could also be called logical filter).
  *
  * This filter holds one or more filters together and relates
- * them logically in an internally defined.
+ * them logically in an internally defined manner.
  *
+ * @version $Id: ExpressionAttribute.java,v 1.4 2002/06/05 13:08:00 loxnard Exp $
  * @author Rob Hranac, Vision for New York
- * @version alpha, 12/01/01
  */
 public class ExpressionAttribute extends ExpressionDefault {
 
@@ -77,7 +94,7 @@ public class ExpressionAttribute extends ExpressionDefault {
                 throw new MalformedFilterException(ife.toString());
             }
         // Check to make sure that attribute conforms to advertised type before 
-        //  returning
+        // returning
         if( ((tempAttribute instanceof Double) && 
              (expressionType == ATTRIBUTE_DOUBLE)) || 
             ((tempAttribute instanceof Integer) && 
