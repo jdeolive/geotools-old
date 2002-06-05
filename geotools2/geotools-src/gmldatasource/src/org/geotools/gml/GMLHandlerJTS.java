1ad3854a427f234c7307e8cc9adc6f1ce9733bd8
loxnard <loxnard@e5c1c795-43da-0310-a71f-fac65c449510>
2002-06-05 11:14:24 +0000

    Added licence statement and cvs id tag
    git-svn-id: http://svn.osgeo.org/geotools/trunk@709 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/gmldatasource/src/org/geotools/gml/GMLHandlerJTS.java
+++ geotools2/geotools-src/gmldatasource/src/org/geotools/gml/GMLHandlerJTS.java
@@ -1,16 +1,33 @@
 /*
+ *    Geotools - OpenSource mapping toolkit
+ *    (C) 2002, Centre for Computational Geography
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; 
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *    
  */
+
 package org.geotools.gml;
 
 /**
  * LEVEL3 saxGML4j GML handler: Gets JTS objects.
  *
  * <p>This handler must be implemented by the parent of a GMLFilterGeometry
- * filter, in order to handle the
- * JTS objects passed to it from the child.</p>
+ * filter in order to handle the JTS objects passed to it from the child.</p>
  *
+ * @version $Id: GMLHandlerJTS.java,v 1.5 2002/06/05 11:14:24 loxnard Exp $
  * @author Rob Hranac, Vision for New York
- * @version alpha, 12/01/01
  */
 public interface GMLHandlerJTS extends org.xml.sax.ContentHandler {
     
