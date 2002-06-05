4c842f9b48028174ea88c38ac51610571366d3c0
loxnard <loxnard@e5c1c795-43da-0310-a71f-fac65c449510>
2002-06-05 11:09:06 +0000

    Added licence statement and cvs id tag
    git-svn-id: http://svn.osgeo.org/geotools/trunk@708 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/gmldatasource/src/org/geotools/gml/GMLHandlerGeometry.java
+++ geotools2/geotools-src/gmldatasource/src/org/geotools/gml/GMLHandlerGeometry.java
@@ -1,6 +1,23 @@
 /*
- * This code is released under the Apache license, availible at the root GML4j directory.
+ *    Geotools - OpenSource mapping toolkit
+ *    (C) 2002, Centre for Computational Geography
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; 
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *    
  */
+
 package org.geotools.gml;
 
 import java.util.*;
@@ -14,40 +31,40 @@ import com.vividsolutions.jts.geom.*;
 /**
  * LEVEL2 saxGML4j GML handler: Gets basic alerts from GMLFilterDocument.
  *
- * <p>This handler is required for any parent of a GMLFilterDocument filter.  It recieves
- * basic element notifications and coordinates.</p>
+ * <p>This handler is required for any parent of a GMLFilterDocument filter.
+ * It receives basic element notifications and coordinates.</p>
  * 
+ * @version $Id: GMLHandlerGeometry.java,v 1.4 2002/06/05 11:09:06 loxnard Exp $
  * @author Rob Hranac, Vision for New York
- * @version alpha, 12/01/01
  */
 public interface GMLHandlerGeometry extends ContentHandler {
 
 
 		/**
-		 * Recieves a geometry start element from the parent.
+		 * Receives a geometry start element from the parent.
 		 */
 		public abstract void geometryStart(String localName, Attributes atts) throws SAXException;
 
 
 		/**
-		 * Recieves a geometry end element from the parent.
+		 * Receives a geometry end element from the parent.
 		 */
 		public abstract void geometryEnd(String localName) throws SAXException;
 
 
 		/**
-		 * Recieves a geometry sub element from the parent.
+		 * Receives a geometry sub element from the parent.
 		 */
 		public abstract void geometrySub(String localName) throws SAXException;
 
 
 		/**
-		 * Recieves a finished coordinate from the parent (2-valued).
+		 * Receives a finished coordinate from the parent (2-valued).
 		 */
 		public abstract void gmlCoordinates( double x, double y )	throws SAXException;
 
 		/**
-		 * Recieves a finished coordinate from the parent (3-valued).
+		 * Receives a finished coordinate from the parent (3-valued).
 		 */
 		public abstract void gmlCoordinates( double x, double y, double z ) throws SAXException;
 
