764f4f2004a34b322fa805130e152496558c5290
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-09 16:33:07 +0000

    fixed problem with non interactive images returning before loaded.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@939 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/Java2DRendering/src/org/geotools/renderer/ImageLoader.java
+++ geotools2/geotools-src/Java2DRendering/src/org/geotools/renderer/ImageLoader.java
@@ -31,18 +31,29 @@ public class ImageLoader implements Runnable{
     URL location;
     boolean waiting = true;
     private void add(URL location, boolean interactive){
+        
+        int localId = imageID;
         this.location = location;
+        //_log.debug("adding image, interactive? "+interactive);
         Thread t = new Thread(this);
         t.start();
         if(interactive){
+            //_log.debug("fast return");
             return;
         }else{
+            waiting=true;
             while(waiting){
+                //_log.debug("waiting..."+waiting);
                 try{
                     Thread.sleep(500);
                 } catch (InterruptedException e){
                 }
             }
+            //_log.debug(""+localId+" complete?: "+((tracker.statusID(localId,true)&tracker.COMPLETE) == tracker.COMPLETE));
+            //_log.debug(""+localId+" abort?: "+((tracker.statusID(localId,true)&tracker.ABORTED) == tracker.ABORTED));
+            //_log.debug(""+localId+" error?: "+((tracker.statusID(localId,true)&tracker.ERRORED) == tracker.ERRORED));
+            //_log.debug(""+localId+" loading?: "+((tracker.statusID(localId,true)&tracker.LOADING) == tracker.LOADING));
+            //_log.debug(""+localId+"slow return "+waiting);
             return;
         }
             
@@ -50,11 +61,13 @@ public class ImageLoader implements Runnable{
     
     public BufferedImage get(URL location,boolean interactive){
         if(images.containsKey(location)){
-            _log.debug("found it ");
+            //_log.debug("found it ");
             return (BufferedImage)images.get(location);
         }else{
-            images.put(location,null);
-            _log.debug("adding "+location);
+            if(!interactive){
+                images.put(location,null);
+            }
+            //_log.debug("adding "+location);
             add(location,interactive);
             return (BufferedImage)images.get(location);
         }
@@ -78,7 +91,7 @@ public class ImageLoader implements Runnable{
         try{
             while((tracker.statusID(myID,true)&tracker.LOADING)!=0){
                 tracker.waitForID(myID,500);
-                
+                //_log.debug(""+myID+"loading - waiting....");
             }
         } catch (InterruptedException ie){
         }
@@ -87,21 +100,26 @@ public class ImageLoader implements Runnable{
         
         
         if(state==tracker.ERRORED){
-            _log.debug("Il: ERRORED");
+            _log.debug(""+myID+" Error loading");
             images.remove(location);
             waiting = false;
             return;
         }
         
         if((state&tracker.COMPLETE) == tracker.COMPLETE){
-            
+            //_log.debug(""+myID+"completed load");
             int iw = img.getWidth(obs);
             int ih = img.getHeight(obs);
-            BufferedImage bi = new BufferedImage(iw, ih, BufferedImage.TYPE_INT_RGB);
+            BufferedImage bi = new BufferedImage(iw, ih, BufferedImage.TYPE_INT_ARGB);
             Graphics2D big = bi.createGraphics();
             big.drawImage(img,0,0,obs);
             images.put(location,bi);
+        
+            waiting = false;
+            return;
         }
+        
+        _log.debug(""+myID+" whoops - some other outcome "+state);
         waiting = false;
         return;
     }
