860df77a4cc3de3a1606f4ca3792f43d2911da54
loxnard <loxnard@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-11 18:10:16 +0000

    Updated javadocs.  Added licence statement and ID.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@995 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/defaultcore/src/org/geotools/styling/DefaultAnchorPoint.java
+++ geotools2/geotools-src/defaultcore/src/org/geotools/styling/DefaultAnchorPoint.java
@@ -1,15 +1,29 @@
 /*
- * DefaultAnchorPoint.java
+ *    Geotools - OpenSource mapping toolkit
+ *    (C) 2002, Centre for Computational Geography
  *
- * Created on 03 July 2002, 13:16
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 
 package org.geotools.styling;
 
 import org.geotools.filter.*;
 /**
- *
- * @author  iant
+ * @version $Id: DefaultAnchorPoint.java,v 1.3 2002/07/11 18:09:02 loxnard Exp $
+ * @author Ian Turton, CCG
  */
 public class DefaultAnchorPoint implements AnchorPoint {
     private static org.apache.log4j.Logger _log =
@@ -18,12 +32,12 @@ public class DefaultAnchorPoint implements AnchorPoint {
     private Expression anchorPointY = null;
     /** Creates a new instance of DefaultAnchorPoint */
     public DefaultAnchorPoint() {
-        try{
+        try {
             anchorPointX = new org.geotools.filter.ExpressionLiteral(new Double(0.0));
             anchorPointY = new org.geotools.filter.ExpressionLiteral(new Double(0.5));
         } catch (org.geotools.filter.IllegalFilterException ife){
-            _log.fatal("Failed to build defaultAnchorPoint: "+ife);
-            System.err.println("Failed to build defaultAnchorPoint: "+ife);
+            _log.fatal("Failed to build defaultAnchorPoint: " + ife);
+            System.err.println("Failed to build defaultAnchorPoint: " + ife);
         }
     }
     
--- geotools2/geotools-src/defaultcore/src/org/geotools/styling/DefaultDisplacement.java
+++ geotools2/geotools-src/defaultcore/src/org/geotools/styling/DefaultDisplacement.java
@@ -1,14 +1,28 @@
 /*
- * DefaultDisplacement.java
+ *    Geotools - OpenSource mapping toolkit
+ *    (C) 2002, Centre for Computational Geography
  *
- * Created on 03 July 2002, 13:19
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 
 package org.geotools.styling;
 import org.geotools.filter.*;
 /**
- *
- * @author  iant
+ * @version $Id: DefaultDisplacement.java,v 1.2 2002/07/11 18:10:16 loxnard Exp $
+ * @author Ian Turton, CCG
  */
 public class DefaultDisplacement implements Displacement {
     private static org.apache.log4j.Logger _log =
@@ -17,12 +31,12 @@ public class DefaultDisplacement implements Displacement {
     private Expression displacementY = null;
     /** Creates a new instance of DefaultDisplacement */
     public DefaultDisplacement() {
-        try{
+        try {
             displacementX = new org.geotools.filter.ExpressionLiteral(new Integer(0));
             displacementY = new org.geotools.filter.ExpressionLiteral(new Integer(0));
         } catch (org.geotools.filter.IllegalFilterException ife){
-            _log.fatal("Failed to build defaultDisplacement: "+ife);
-            System.err.println("Failed to build defaultDisplacement: "+ife);
+            _log.fatal("Failed to build defaultDisplacement: " + ife);
+            System.err.println("Failed to build defaultDisplacement: " + ife);
         }
     }
  
