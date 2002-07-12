e5cd7cc0c4417c2bfbfb692fe196fd375910e850
loxnard <loxnard@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-12 15:22:43 +0000

    Removed redundant public modifiers + updated javadocs
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1031 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/map/AreaOfInterestModel.java
+++ geotools2/geotools-src/core/src/org/geotools/map/AreaOfInterestModel.java
@@ -6,15 +6,13 @@ package org.geotools.map;
  * The Tools classes process key and mouse actions, and the Renderers handle
  * displaying of the data.
  *
- * @version $Id: AreaOfInterestModel.java,v 1.1 2002/07/10 22:26:11 camerons Exp $
+ * @version $Id: AreaOfInterestModel.java,v 1.2 2002/07/12 15:22:43 loxnard Exp $
  * @author Cameron Shorter
  * 
  */
 
-import java.util.Vector;
 import org.opengis.cs.*;
 import com.vividsolutions.jts.geom.Envelope;
-import javax.swing.event.EventListenerList;
 import org.geotools.map.events.*;
 
 public interface AreaOfInterestModel {
@@ -23,18 +21,18 @@ public interface AreaOfInterestModel {
      * Register interest in receiving an AreaOfInterestChangedEvent.
      * @param ecl The object to notify when AreaOfInterest has changed.
      */
-    public void addAreaOfInterestChangedListener(
+    void addAreaOfInterestChangedListener(
             AreaOfInterestChangedListener ecl);
 
     /**
      * Remove interest in receiving an AreaOfInterestChangedEvent.
      * @param ecl The object to stop sending AreaOfInterestChanged Events.
      */
-    public void removeAreaOfInterestChangedListener(
+    void removeAreaOfInterestChangedListener(
             AreaOfInterestChangedListener ecl);
 
  
-    public void setAreaOfInterest(
+    void setAreaOfInterest(
             Envelope areaOfInterest,
             CS_CoordinateSystem coordinateSystem);
 
@@ -44,12 +42,12 @@ public interface AreaOfInterestModel {
      * units of the coordinate system.
      * For instance, if a map zooms to the left by half a map width,
      * then deltaMinX=-0.5, deltaMaxX=-0.5, deltaMinY=0, deltaMaxY=0.
-     * @param deltaX1 The relative change in the bottom left X coordinate.
-     * @param deltaY1 The relative change in the bottom left Y coordinate.
-     * @param deltaX1 The relative change in the top right X coordinate.
-     * @param deltaX1 The relative change in the top right Y coordinate.
+     * @param deltaMinX The relative change in the bottom left X coordinate.
+     * @param deltaMinY The relative change in the bottom left Y coordinate.
+     * @param deltaMaxX The relative change in the top right X coordinate.
+     * @param deltaMaxY The relative change in the top right Y coordinate.
      */
-    public void changeRelativeAreaOfInterest(
+    void changeRelativeAreaOfInterest(
             float deltaMinX,
             float deltaMaxX,
             float deltaMinY,
@@ -58,10 +56,10 @@ public interface AreaOfInterestModel {
     /**
      * Set the coordinateSystem.
      */
-    public void setCoordinateSystem(CS_CoordinateSystem coordinateSystem);
+    void setCoordinateSystem(CS_CoordinateSystem coordinateSystem);
 
     /**
      * Get the coordinateSystem.
      */
-    public CS_CoordinateSystem getCoordinateSystem();
+    CS_CoordinateSystem getCoordinateSystem();
 }
