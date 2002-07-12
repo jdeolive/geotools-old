819f2f3eaf789f2ecb6e4f6bf04cff31060d7afa
loxnard <loxnard@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-12 15:04:20 +0000

    Updated javadocs.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1028 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/shapefile/src/org/geotools/shapefile/ShapefileHeader.java
+++ geotools2/geotools-src/shapefile/src/org/geotools/shapefile/ShapefileHeader.java
@@ -23,11 +23,11 @@ package org.geotools.shapefile;
 import com.vividsolutions.jts.geom.Envelope;
 
 /**
- * @version $Id: ShapefileHeader.java,v 1.5 2002/06/05 13:01:32 loxnard Exp $
+ * @version $Id: ShapefileHeader.java,v 1.6 2002/07/12 15:04:20 loxnard Exp $
  * @author James Macgill, CCG
  */
 public class ShapefileHeader{
-    private final static boolean DEBUG=true;
+    private static final boolean DEBUG = true;
     private int fileCode = -1;
     private int fileLength = -1;
     private int indexLength = -1;
@@ -39,26 +39,30 @@ public class ShapefileHeader{
     public ShapefileHeader(cmp.LEDataStream.LEDataInputStream file) throws java.io.IOException {
         file.setLittleEndianMode(false);
         fileCode = file.readInt();
-        if(DEBUG)System.out.println("Sfh->Filecode "+fileCode);
-        if ( fileCode != Shapefile.SHAPEFILE_ID )
-            System.err.println("Sfh->WARNING filecode "+fileCode+" not a match for documented shapefile code "+Shapefile.SHAPEFILE_ID);
-        
-        for(int i=0;i<5;i++){
+        if (DEBUG) {
+            System.out.println("Sfh->Filecode " + fileCode);
+        }
+        if (fileCode != Shapefile.SHAPEFILE_ID) {
+            System.err.println("Sfh->WARNING filecode " + fileCode + " not a match for documented shapefile code " + Shapefile.SHAPEFILE_ID);
+        }        
+        for (int i = 0; i < 5; i++){
             int tmp = file.readInt();
-            if(DEBUG)System.out.println("Sfh->blank "+tmp);
+            if (DEBUG) {
+                System.out.println("Sfh->blank " + tmp);
+            }
         }
         fileLength = file.readInt();
         
         file.setLittleEndianMode(true);
-        version=file.readInt();
-        shapeType=file.readInt();
+        version = file.readInt();
+        shapeType = file.readInt();
        
         //read in and store the bounding box
         double[] coords = new double[4];
-        for(int i = 0;i<4;i++){
+        for (int i = 0; i < 4; i++){
             coords[i]=file.readDouble();
         }
-        bounds = new Envelope(coords[0],coords[2],coords[1],coords[3]);
+        bounds = new Envelope(coords[0], coords[2], coords[1], coords[3]);
         
         //skip remaining unused bytes
         file.setLittleEndianMode(false);//well they may not be unused forever...
@@ -73,12 +77,12 @@ public class ShapefileHeader{
         fileCode = Shapefile.SHAPEFILE_ID;
         bounds = geometries.getEnvelopeInternal();
         fileLength = 0;
-        for(int i=0;i<numShapes;i++){
-            fileLength+=handle.getLength(geometries.getGeometryN(i));
-            fileLength+=4;//for each header
+        for (int i = 0; i < numShapes; i++){
+            fileLength += handle.getLength(geometries.getGeometryN(i));
+            fileLength += 4;//for each header
         }
-        fileLength+=50;//space used by this, the main header
-        indexLength = 50+(4*numShapes);
+        fileLength += 50;//space used by this, the main header
+        indexLength = 50 + (4 * numShapes);
     }
     
     public void setFileLength(int fileLength){
@@ -91,33 +95,35 @@ public class ShapefileHeader{
         int pos = 0;
         file.setLittleEndianMode(false);
         file.writeInt(fileCode);
-        pos+=4;
-        for(int i=0;i<5;i++){
+        pos += 4;
+        for (int i = 0; i < 5; i++){
             file.writeInt(0);//Skip unused part of header
-            pos+=4;
+            pos += 4;
         }
         file.writeInt(fileLength);
-        pos+=4;
+        pos += 4;
         file.setLittleEndianMode(true);
         file.writeInt(version);
-        pos+=4;
+        pos += 4;
         file.writeInt(shapeType);
-        pos+=4;
+        pos += 4;
         //write the bounding box
         file.writeDouble(bounds.getMinX());
         file.writeDouble(bounds.getMinY());
         file.writeDouble(bounds.getMaxX());
         file.writeDouble(bounds.getMaxY());
-        pos+=8*4;
+        pos += 8 * 4;
         
         //skip remaining unused bytes
         //file.setLittleEndianMode(false);//well they may not be unused forever...
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++){
             file.writeDouble(0.0);//Skip unused part of header
-            pos+=8;
+            pos += 8;
         }
         
-        if(DEBUG)System.out.println("Sfh->Position "+pos);
+        if (DEBUG) {
+            System.out.println("Sfh->Position " + pos);
+        }
     }
     
     /*public void writeToIndex(LEDataOutputStream file)throws IOException {
@@ -164,7 +170,7 @@ public class ShapefileHeader{
     }
     
     public String toString()  {
-        String res = new String("Sf-->type "+fileCode+" size "+fileLength+" version "+ version + " Shape Type "+shapeType);
+        String res = new String("Sf-->type " + fileCode + " size " + fileLength + " version " + version + " Shape Type " + shapeType);
         return res;
     }
 }
