f9f823956ad902dba0a0726405f4ceeeb6b13f63
camerons <camerons@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-13 06:03:01 +0000

    Initial limited version
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1062 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/gui/src/org/geotools/gui/tools/PanTool.java
@@ -0,0 +1,41 @@
+package org.geotools.gui.tools;
+
+import java.awt.event.MouseEvent;
+import javax.swing.JComponent;
+import javax.swing.event.MouseInputAdapter;
+import org.geotools.gui.swing.MapPane;
+import org.geotools.map.*;
+
+public class PanTool extends MouseInputAdapter {
+
+    // Internal variables
+    private JComponent mapPane;
+    private AreaOfInterestModel areaOfInterestModel;
+    
+    public PanTool(MapPane mapPane, AreaOfInterestModel areaOfInterestModel){
+        this.mapPane=mapPane;
+        this.areaOfInterestModel=areaOfInterestModel;
+
+        // Register for MapPane Mouse events
+        this.mapPane.addMouseListener(this);
+        this.mapPane.addMouseMotionListener(this);
+    }
+
+    /**
+     * Set up Click/Pan.
+     * Pan the map so that the new extent has the click point in the middle
+     * of the map.
+     */
+    public void MouseClicked(MouseEvent e) {
+        areaOfInterestModel.changeRelativeAreaOfInterest(
+            (float)(e.getX()-mapPane.getWidth())/mapPane.getWidth(),
+            (float)(e.getX()-mapPane.getWidth())/mapPane.getWidth(),
+            (float)(e.getY()-mapPane.getHeight())/mapPane.getHeight(),
+            (float)(e.getY()-mapPane.getHeight())/mapPane.getHeight());
+    }
+
+    /*
+     * Set up Click and Drap Pan.
+     */
+    //public void ...
+}
