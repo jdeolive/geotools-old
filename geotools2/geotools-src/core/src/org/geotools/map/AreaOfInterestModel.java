a37971ed41ac55b176f3ae535f32cf4424b5c868
camerons <camerons@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-13 20:53:47 +0000

    Added javadocs
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1086 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/map/AreaOfInterestModel.java
+++ geotools2/geotools-src/core/src/org/geotools/map/AreaOfInterestModel.java
@@ -1,20 +1,19 @@
 package org.geotools.map;
 
+import org.opengis.cs.CS_CoordinateSystem;
+import com.vividsolutions.jts.geom.Envelope;
+import org.geotools.map.events.AreaOfInterestChangedListener;
+
 /**
- * AreaOfInterestModel stores AreaOfInterest associated with a geographic map.
+ * AreaOfInterestModel stores AreaOfInterest associated with a geographic
+ * map.
  * Geotools uses a Model-View-Control (MVC) design to control maps.
  * The Tools classes process key and mouse actions, and the Renderers handle
  * displaying of the data.
  *
- * @version $Id: AreaOfInterestModel.java,v 1.2 2002/07/12 15:22:43 loxnard Exp $
  * @author Cameron Shorter
- * 
+ * @version $Id: AreaOfInterestModel.java,v 1.3 2002/07/13 20:53:47 camerons Exp $
  */
-
-import org.opengis.cs.*;
-import com.vividsolutions.jts.geom.Envelope;
-import org.geotools.map.events.*;
-
 public interface AreaOfInterestModel {
     
     /**
@@ -31,7 +30,11 @@ public interface AreaOfInterestModel {
     void removeAreaOfInterestChangedListener(
             AreaOfInterestChangedListener ecl);
 
- 
+    /**
+     * Set a new AreaOfInterest and trigger an AreaOfInterestEvent.
+     * @param areaOfInterest The new areaOfInterest.
+     * @param coordinateSystem The coordinate system being using by this model.
+     */
     void setAreaOfInterest(
             Envelope areaOfInterest,
             CS_CoordinateSystem coordinateSystem);
@@ -55,11 +58,13 @@ public interface AreaOfInterestModel {
 
     /**
      * Set the coordinateSystem.
+     * @param coordinateSystem The coordinate system being using by this model.
      */
     void setCoordinateSystem(CS_CoordinateSystem coordinateSystem);
 
     /**
      * Get the coordinateSystem.
+     * @return The coordinate system being using by this model.
      */
     CS_CoordinateSystem getCoordinateSystem();
 }
