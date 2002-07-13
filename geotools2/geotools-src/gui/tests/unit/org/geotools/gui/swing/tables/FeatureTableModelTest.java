c2c29870a9490128b4d6cda3677ab68daa6679ca
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-13 14:08:01 +0000

    made test able to find sample data when run via maven
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1073 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/gui/tests/unit/org/geotools/gui/swing/tables/FeatureTableModelTest.java
+++ geotools2/geotools-src/gui/tests/unit/org/geotools/gui/swing/tables/FeatureTableModelTest.java
@@ -40,6 +40,11 @@ public class FeatureTableModelTest extends TestCase {
 
     public void testDisplay() {
         String dataFolder = System.getProperty("dataFolder");
+         if(dataFolder==null){
+            //then we are being run by maven
+            dataFolder = System.getProperty("basedir");
+            dataFolder+="/tests/unit/testData";
+        }
         try{
             URL url = new URL("file:///"+dataFolder+"/statepop.shp");
             System.out.println("Testing ability to load "+url);
