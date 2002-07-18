3f12631deb2c45fb5feb1919527519be1fa39ef4
robhranac <robhranac@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-18 16:12:00 +0000

    modifications based on jul 16 IRC discussion
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1146 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/data/DataSource.java
+++ geotools2/geotools-src/core/src/org/geotools/data/DataSource.java
@@ -19,58 +19,131 @@
  */
 package org.geotools.data;
 
-
+import org.geotools.filter.Filter;
+import org.geotools.feature.AttributeType;
 import org.geotools.feature.FeatureCollection;
+
 /**
  * The source of data for Features. Shapefiles, databases, etc. are referenced
  * through this interface.
- * @version $Id: DataSource.java,v 1.4 2002/07/12 15:06:42 loxnard Exp $
+ *
+ * @version $Id: DataSource.java,v 1.5 2002/07/18 16:12:00 robhranac Exp $
  * @author Ray Gallagher
+ * @author Rob Hranac, TOPP
  */
 public interface DataSource {
     
+
+    /**************************************************
+     * Feature retrieval methods.                     *
+     **************************************************/
+
     /**
-     * Loads Feature rows for the given Extent from the datasource.
-     * @param ft featureTable to load features into
-     * @param ex an extent defining which features to load - null means all
-     * features
-     * @throws DataSourceException if anything goes wrong
+     * Loads features from the datasource into the passed collection, based on
+     * the passed filter.  Note that all data sources must support this method
+     * at a minimum.
+     *
+     * @param collection The collection to put the features into.
+     * @param filter An OpenGIS filter; specifies which features to retrieve.
+     * @throws DataSourceException For all data source errors.
      */
-    void importFeatures(FeatureCollection ft, Extent ex)
-           throws DataSourceException;
+    void getFeatures(FeatureCollection collection, Filter filter)
+        throws DataSourceException;
     
     /**
-     * Saves the given features to the datasource.
-     * @param ft featureTable to get features from
-     * @param ex an extent defining which features to write - null means all
-     * @throws DataSourceException if anything goes wrong or if exporting is
-     * not supported
+     * Loads features from the datasource into the returned collection, based on
+     * the passed filter.
+     *
+     * @param filter An OpenGIS filter; specifies which features to retrieve.
+     * @return Collection The collection to put the features into.
+     * @throws DataSourceException For all data source errors.
      */
-    void exportFeatures(FeatureCollection ft, Extent ex)
-           throws DataSourceException;
+    FeatureCollection getFeatures(Filter filter)
+        throws DataSourceException;
     
+
+    /**************************************************
+     * Data source modification methods.              *
+     **************************************************/
+
+    /**
+     * Adds all features from the passed feature collection to the datasource.
+     *
+     * @param collection The collection from which to add the features.
+     * @throws DataSourceException If anything goes wrong or if exporting is
+     * not supported.
+     */
+    void addFeatures(FeatureCollection collection)
+        throws DataSourceException;
+    
+    /**
+     * Removes all of the features specificed by the passed filter from the
+     * collection.
+     *
+     * @param filter An OpenGIS filter; specifies which features to remove.
+     * @throws DataSourceException If anything goes wrong or if deleting is
+     * not supported.
+     */
+    void removeFeatures(Filter filter)
+        throws DataSourceException;
+    
+    /**
+     * Modifies the passed attribute types with the passed objects in all
+     * features that correspond to the passed OGS filter.
+     *
+     * @param type The attributes to modify.
+     * @param value The values to put in the attribute types.
+     * @param filter An OGC filter to note which attributes to modify.
+     * @throws DataSourceException If modificaton is not supported, if
+     * the attribute and object arrays are not eqaul length, or if the object
+     * types do not match the attribute types.
+     */
+    void modifyFeatures(AttributeType[] type, Object[] value, Filter filter)
+        throws DataSourceException;
+    
+    /**
+     * Modifies the passed attribute types with the passed objects in all
+     * features that correspond to the passed OGS filter.  A convenience
+     * method for single attribute modifications.
+     *
+     * @param type The attributes to modify.
+     * @param value The values to put in the attribute types.
+     * @param filter An OGC filter to note which attributes to modify.
+     * @throws DataSourceException If modificaton is not supported, if
+     * the object type do not match the attribute type.
+     */
+    void modifyFeatures(AttributeType type, Object value, Filter filter)
+        throws DataSourceException;
+    
+
+    /**************************************************
+      Data source utility methods.
+     **************************************************/
+
     /**
      * Stops this DataSource from loading.
      */
-    void stopLoading();
+    void abortLoading();
     
     /**
-     * Gets the extent of this datasource using the default speed of 
+     * Gets the bounding box of this datasource using the default speed of 
      * this datasource as set by the implementer. 
-     * @return the extent of the datasource or null if unknown and too
+     *
+     * @return The bounding box of the datasource or null if unknown and too
      * expensive for the method to calculate.
      */
-     Extent getExtent();
+    Filter getBbox();
     
     /**
-     * Gets the extent of this datasource using the speed of 
+     * Gets the bounding box of this datasource using the speed of 
      * this datasource as set by the parameter.
-     * @param speed if true then a quick (and possibly dirty) estimate of
+     *
+     * @param speed If true then a quick (and possibly dirty) estimate of
      * the extent is returned. If false then a slow but accurate extent
      * will be returned
-     * @return the extent of the datasource or null if unknown and too
+     * @return The extent of the datasource or null if unknown and too
      * expensive for the method to calculate.
      */
-    Extent getExtent(boolean speed);
+    Filter getBbox(boolean speed);
 }
 
