34bb6666bd5d9d02227f1b9a1708b9b0276823a9
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-19 13:09:49 +0000

    a convienence wrapper around literal expression for BBOXes constructed from a JTS Envelope
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1156 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/defaultcore/src/org/geotools/filter/BBoxExpression.java
@@ -0,0 +1,44 @@
+/*
+ * BBoxExpression.java
+ *
+ * Created on 19 July 2002, 11:39
+ */
+
+package org.geotools.filter;
+
+/**
+ *
+ * @author  iant
+ */
+
+import com.vividsolutions.jts.geom.*;
+
+public class BBoxExpression extends org.geotools.filter.ExpressionLiteral {
+    private GeometryFactory gfac = new GeometryFactory();
+    /** Creates a new instance of BBoxExpression */
+    public BBoxExpression() throws IllegalFilterException{
+        this(new Envelope());
+    }
+    public BBoxExpression(Envelope env) throws IllegalFilterException{
+        expressionType = ExpressionDefault.LITERAL_GEOMETRY;
+        setBounds(env);
+    }
+    public void setBounds(Envelope env) throws IllegalFilterException{
+        
+        Coordinate[] c = new Coordinate[5];
+        c[0] = new Coordinate(env.getMinX(), env.getMinY());
+        c[1] = new Coordinate(env.getMinX(), env.getMaxY());
+        c[2] = new Coordinate(env.getMaxX(), env.getMaxY());
+        c[3] = new Coordinate(env.getMaxX(), env.getMinY());
+        c[4] = new Coordinate(env.getMinX(), env.getMinY());
+        LinearRing r = null;
+        try{
+            r = gfac.createLinearRing(c);
+        } catch (TopologyException e){
+            throw new IllegalFilterException(e.toString());
+        }
+        
+        super.setLiteral(gfac.createPolygon(r, null));
+        
+    }
+}
