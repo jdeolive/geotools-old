9fd3fee3db39c83d0e3391536a00229168acb0a7
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-22 20:08:09 +0000

    fixed typos, now compiles
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1197 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/filter/src/org/geotools/filter/XMLEncoder.java
+++ geotools2/geotools-src/filter/src/org/geotools/filter/XMLEncoder.java
@@ -1,35 +1,52 @@
 /*
- * XMLEncoder.java
+ *    Geotools - OpenSource mapping toolkit
+ *    (C) 2002, Centre for Computational Geography
  *
- * Created on 22 July 2002, 17:12
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; 
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *    
  */
 
 package org.geotools.filter;
 
-import org.geotools.filter;
-
+import org.apache.log4j.Logger;
 /**
  *
  * @author  jamesm
  */
 public class XMLEncoder implements org.geotools.filter.FilterVisitor {
     
+     private static Logger log = Logger.getLogger("filter");
+
+    
     /** Creates a new instance of XMLEncoder */
     public XMLEncoder() {
     }
     
     public void visit(Filter filter) {
-        //unknown filter type!
+        log.warn("exporting unknown filter type");
     }
     
     public void visit(LogicFilter filter){
-        
+        log.debug("exporting LogicFilter");
     }
-    public void visit(ComparisonFilter filter){
-        
+    
+    public void visit(CompareFilter filter){
+        log.debug("exporting ComparisonFilter");
     }
     
     public void visit(GeometryFilter filter){
-        
+        log.debug("exporting GeometryFilter");
     }
 }
