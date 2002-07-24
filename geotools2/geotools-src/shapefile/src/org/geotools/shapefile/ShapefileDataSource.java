2921f54511d1e0d2a68304c78773721c866f0d67
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-24 14:50:30 +0000

    added extra todos formated code added extra javadocs
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1231 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/shapefile/src/org/geotools/shapefile/ShapefileDataSource.java
+++ geotools2/geotools-src/shapefile/src/org/geotools/shapefile/ShapefileDataSource.java
@@ -4,7 +4,7 @@
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; 
+ *    License as published by the Free Software Foundation;
  *    version 2.1 of the License.
  *
  *    This library is distributed in the hope that it will be useful,
@@ -15,27 +15,33 @@
  *    You should have received a copy of the GNU Lesser General Public
  *    License along with this library; if not, write to the Free Software
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *    
+ *
  */
 
 package org.geotools.shapefile;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
 
 import org.geotools.data.*;
 import org.geotools.feature.*;
 import org.geotools.filter.*;
-import org.geotools.datasource.extents.*;
 import com.vividsolutions.jts.geom.*;
 
 /**
- * @version $Id: ShapefileDataSource.java,v 1.12 2002/07/20 13:30:12 jmacgill Exp $
+ * @version $Id: ShapefileDataSource.java,v 1.13 2002/07/24 14:50:30 jmacgill Exp $
  * @author James Macgill, CCG
+ * @task TODO: add support for reading dbf file
+ * @task TODO: add support for the optional spatial index files to improve
+ *             loading of sub regions
  */
 public class ShapefileDataSource implements org.geotools.data.DataSource {
-    Shapefile shapefile;
+    /**
+     * The shapefile object that this datasouce connects to
+     **/
+    private Shapefile shapefile;
+    
     /** Creates a new instance of ShapefileDataSource. */
     public ShapefileDataSource(Shapefile shapefile) {
         this.shapefile = shapefile;
@@ -47,14 +53,6 @@ public class ShapefileDataSource implements org.geotools.data.DataSource {
     public String[] getColumnNames() {
         return new String[]{"Geometry"};
     }
-    
-    
-   
- 
-    
- 
-    
-    
     
     /** Stops this DataSource from loading.
      */
@@ -69,7 +67,7 @@ public class ShapefileDataSource implements org.geotools.data.DataSource {
      * @task TODO: Implement addFeatures method
      */
     public void addFeatures(FeatureCollection collection) throws DataSourceException {
-         throw new DataSourceException("Removal of features is not yet supported by this datasource");
+        throw new DataSourceException("Removal of features is not yet supported by this datasource");
     }
     
     /** Gets the bounding box of this datasource using the default speed of
@@ -95,8 +93,9 @@ public class ShapefileDataSource implements org.geotools.data.DataSource {
         return getBbox();
     }
     
-    /** Loads features from the datasource into the returned collection, based on
-     * the passed filter.
+    /** 
+     * Loads features from the datasource into the returned collection, based
+     * on the passed filter.
      *
      * @param filter An OpenGIS filter; specifies which features to retrieve.
      * @return Collection The collection to put the features into.
@@ -104,54 +103,53 @@ public class ShapefileDataSource implements org.geotools.data.DataSource {
      */
     public FeatureCollection getFeatures(Filter filter) throws DataSourceException {
         FeatureCollectionDefault fc = new FeatureCollectionDefault();
-        getFeatures(fc,filter);
+        getFeatures(fc, filter);
         return fc;
     }
     
-    /** Loads features from the datasource into the passed collection, based on
-     * the passed filter.  Note that all data sources must support this method
-     * at a minimum.
+    /** 
+     * Loads features from the datasource into the passed collection, based
+     * on the passed filter.  Note that all data sources must support this
+     * method at a minimum.
      *
      * @param collection The collection to put the features into.
      * @param filter An OpenGIS filter; specifies which features to retrieve.
      * @throws DataSourceException For all data source errors.
      */
     public void getFeatures(FeatureCollection collection, Filter filter) throws DataSourceException {
-        try{
-                GeometryCollection shapes = shapefile.read(new GeometryFactory());
-                List features = new ArrayList();
-                Geometry typical = shapes.getGeometryN(0);
-                AttributeType geometryAttribute = new AttributeTypeDefault(Shapefile.getShapeTypeDescription(Shapefile.getShapeType(typical)), Geometry.class);
-                
-                FeatureType shapefileType = new FeatureTypeFlat(geometryAttribute);
-                System.out.println("schema is " + shapefileType);
-                FeatureFactory fac = new FeatureFactory(shapefileType);
-                int count = shapes.getNumGeometries();
-                //Feature[] features = new Feature[count];
-                for(int i = 0; i < count; i++){
-                    //Feature feat = new FlatFeature();
-                    
-                    Object [] row = new Object[1];
-                    row[0] = (Geometry) shapes.getGeometryN(i);
-                    System.out.println("adding geometry" + row[0]);
-                    Feature feature = fac.create(row);
-                    if (filter.contains(feature)){
-                        collection.addFeatures(new Feature[]{feature});
-                    }
+        try {
+            GeometryCollection shapes = shapefile.read(new GeometryFactory());
+            List features = new ArrayList();
+            Geometry typical = shapes.getGeometryN(0);
+            AttributeType geometryAttribute = new AttributeTypeDefault(Shapefile.getShapeTypeDescription(Shapefile.getShapeType(typical)), Geometry.class);
+            
+            FeatureType shapefileType = new FeatureTypeFlat(geometryAttribute);
+            System.out.println("schema is " + shapefileType);
+            FeatureFactory fac = new FeatureFactory(shapefileType);
+            int count = shapes.getNumGeometries();
+            //Feature[] features = new Feature[count];
+            for (int i = 0; i < count; i++){
+                Object [] row = new Object[1];
+                row[0] = (Geometry) shapes.getGeometryN(i);
+                System.out.println("adding geometry" + row[0]);
+                Feature feature = fac.create(row);
+                if (filter.contains(feature)){
+                    collection.addFeatures(new Feature[]{feature});
                 }
             }
-            catch (IOException ioe){
-                throw new DataSourceException("IO Exception loading data : " + ioe.getMessage());
-            }
-            catch (ShapefileException se){
-                throw new DataSourceException("Shapefile Exception loading data : " + se.getMessage());
-            }
-            catch (TopologyException te){
-                throw new DataSourceException("Topology Exception loading data : " + te.getMessage());
-            }
-            catch (IllegalFeatureException ife){
-                throw new DataSourceException("Illegal Feature Exception loading data : " + ife.getMessage());
-            }    
+        }
+        catch (IOException ioe){
+            throw new DataSourceException("IO Exception loading data : " + ioe.getMessage());
+        }
+        catch (ShapefileException se){
+            throw new DataSourceException("Shapefile Exception loading data : " + se.getMessage());
+        }
+        catch (TopologyException te){
+            throw new DataSourceException("Topology Exception loading data : " + te.getMessage());
+        }
+        catch (IllegalFeatureException ife){
+            throw new DataSourceException("Illegal Feature Exception loading data : " + ife.getMessage());
+        }
     }
     
     /** Modifies the passed attribute types with the passed objects in all
@@ -181,7 +179,7 @@ public class ShapefileDataSource implements org.geotools.data.DataSource {
      * @task TODO: Implement support for modification of feature (multi attribute)
      */
     public void modifyFeatures(AttributeType[] type, Object[] value, Filter filter) throws DataSourceException {
-               throw new DataSourceException("Modification of features is not yet supported by this datasource");
+        throw new DataSourceException("Modification of features is not yet supported by this datasource");
     }
     
     /** Removes all of the features specificed by the passed filter from the
@@ -193,7 +191,7 @@ public class ShapefileDataSource implements org.geotools.data.DataSource {
      * @task TODO: Implement support for removal of features
      */
     public void removeFeatures(Filter filter) throws DataSourceException {
-               throw new DataSourceException("Removal of features is not yet supported by this datasource");
+        throw new DataSourceException("Removal of features is not yet supported by this datasource");
     }
     
 }
