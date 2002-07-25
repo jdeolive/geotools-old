e2d5ed5462bcfd618cb5ab5e0264c386c96966cc
omyar <omyar@e5c1c795-43da-0310-a71f-fac65c449510>
2002-07-25 16:34:37 +0000

    Updated the test for mapInfoDataSource to be consistent with the newer version of the class
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1254 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/mapinfo/tests/unittest/org/geotools/mapinfo/testMapInfoDataSource.java
+++ geotools2/geotools-src/mapinfo/tests/unittest/org/geotools/mapinfo/testMapInfoDataSource.java
@@ -1,5 +1,4 @@
 package unittest.org.geotools.mapinfo;
-
 import org.geotools.mapinfo.*;
 import org.geotools.data.*;
 import org.geotools.feature.*;
@@ -7,12 +6,12 @@ import org.geotools.feature.*;
 import junit.framework.*;
 
 import java.io.*;
+import java.util.*;
 
 import com.vividsolutions.jts.geom.*;
 
 public class testMapInfoDataSource extends TestCase
 {
-	FeatureCollection fc;
 	MapInfoDataSource dsMapInfo;
 	
 	public testMapInfoDataSource(String name)
@@ -24,50 +23,27 @@ public class testMapInfoDataSource extends TestCase
 		System.out.println("Running");
 		junit.textui.TestRunner.run(testMapInfoDataSource.class);
 	}
-	
+
 	public void setUp()
 	{
 		dsMapInfo = new MapInfoDataSource();
-		try
-		{
-			dsMapInfo.setSource(new File("D:\\geotools\\Task6\\MapInfo\\Arable_Land.MIF"));
-		}
-		catch(FileNotFoundException fnfexp)
-		{
-			fail ("FileNotFound Exception : "+fnfexp.getMessage());
-		}
-		catch(IOException ioexp)
-		{
-			fail ("IO Exception : "+ioexp.getMessage());
-		}
-		catch(MIFObjectTypeException mifexp)
-		{
-			fail ("FileNotFound Exception : "+mifexp.getMessage());
-		}
-		fc = new FeatureCollectionDefault(dsMapInfo);
 	}
-	
-	public void testReadDataSource()
+
+	public void testLoad()
 	{
-		System.out.println("testReadDataSource()");
-		fc.setExtent(new DummyExtent());
 		try
 		{
-			dsMapInfo.importFeatures(fc, new DummyExtent());
-			System.out.println("Read "+fc.getFeatures().length+" features");
-			
-			Feature [] features = fc.getFeatures();
-			
-			for (int i=0;i<10;i++)
-			{
-				Polygon poly = (Polygon)features[i].getDefaultGeometry();
-				System.out.println("Poly : "+poly.toString());
-			}
+			// Load file
+			String miffile = "D:\\geotools\\Task6\\test.mif";
+			Vector objects = dsMapInfo.readMifMid(miffile);
+			System.out.println("Read "+objects.size()+" object");
 		}
 		catch(DataSourceException dsexp)
 		{
-			fail("DataSource Exception : "+dsexp.getMessage());
+			dsexp.printStackTrace();
+			fail("Exception : "+dsexp.getMessage());
 		}
 	}
+	
 }
 
