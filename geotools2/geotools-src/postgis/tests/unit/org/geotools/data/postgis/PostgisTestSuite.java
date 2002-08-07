56cd91c6d7ad5173aa1dfca59e6283f90a4105e8
robhranac <robhranac@e5c1c795-43da-0310-a71f-fac65c449510>
2002-08-07 18:31:50 +0000

    minor changes, including naming for data source
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1374 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/postgis/tests/unit/org/geotools/data/postgis/PostgisTestSuite.java
+++ geotools2/geotools-src/postgis/tests/unit/org/geotools/data/postgis/PostgisTestSuite.java
@@ -1,10 +1,11 @@
 package org.geotools.data.postgis;
 
+import java.util.*;
 import junit.framework.*;
-import org.apache.log4j.Category;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 import com.vividsolutions.jts.geom.*;
-import java.util.*;
 import org.geotools.data.*;
 import org.geotools.feature.*;
 import org.geotools.datasource.extents.*;
@@ -12,7 +13,7 @@ import org.geotools.datasource.extents.*;
 public class PostgisTestSuite extends TestCase {
 
     /** Standard logging instance */
-    private static Category _log = Category.getInstance(PostgisTestSuite.class.getName());
+    private static Logger _log = Logger.getLogger(PostgisTestSuite.class);
 
     DataSource postgis = null;
 
@@ -32,7 +33,31 @@ public class PostgisTestSuite extends TestCase {
         suite.addTestSuite(PostgisTest.class);
         return suite;
     }
-    
 
-    
+    /*    
+    public void setUp() {
+        _log.info("creating postgis connection...");
+        PostgisConnection db = new PostgisConnection ("feathers.leeds.ac.uk","5432","postgis_test"); 
+        _log.info("created new db connection");
+        db.setLogin("postgis_ro","postgis_ro");
+        _log.info("set the login");
+        postgis = new DataSourcePostgis(db, "testset");
+        _log.info("created new datasource");
+    }
+
+
+    public void testImport() {
+        _log.info("starting type enforcement tests...");
+        try {
+            postgis.importFeatures(collection, new EnvelopeExtent());
+        }
+        catch(DataSourceException e) {
+            _log.info("...threw data source exception");      
+            this.fail("...threw data source exception");  
+        }
+        assertEquals(18, collection.getFeatures().length);
+        _log.info("...ending type enforcement tests");
+    }
+    */
+
 }
