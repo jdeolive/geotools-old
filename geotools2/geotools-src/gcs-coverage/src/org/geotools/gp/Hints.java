b3c9d67356db7254bd375d518e9837cd58e3ba61
desruisseaux <desruisseaux@e5c1c795-43da-0310-a71f-fac65c449510>
2002-08-08 18:38:03 +0000

    Moved RenderingHints from Operation
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1385 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/gcs-coverage/src/org/geotools/gp/Hints.java
@@ -0,0 +1,117 @@
+/*
+ * Geotools - OpenSource mapping toolkit
+ * (C) 2002, Centre for Computational Geography
+ * (C) 2001, Institut de Recherche pour le Développement
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ * Contacts:
+ *     UNITED KINGDOM: James Macgill
+ *             mailto:j.macgill@geog.leeds.ac.uk
+ *
+ *     FRANCE: Surveillance de l'Environnement Assistée par Satellite
+ *             Institut de Recherche pour le Développement / US-Espace
+ *             mailto:seasnet@teledetection.fr
+ *
+ *     CANADA: Observatoire du Saint-Laurent
+ *             Institut Maurice-Lamontagne
+ *             mailto:osl@osl.gc.ca
+ *
+ *    This package contains documentation from OpenGIS specifications.
+ *    OpenGIS consortium's work is fully acknowledged here.
+ */
+package org.geotools.gp;
+
+// J2SE dependencies
+import java.awt.RenderingHints;
+import java.util.Locale;
+
+// JAI dependencies
+import javax.media.jai.JAI;
+
+// Geotools Dependencies
+import org.geotools.ct.CoordinateTransformation;
+import org.geotools.ct.CoordinateTransformationFactory;
+
+
+/**
+ * A set {@link RenderingHints} keys for grid coverage operation. Hints are memorized by
+ * {@link GridCoverageProcessor}, which formard them to {@link Operation#doOperation} at
+ * every invocation. Rendering hints can be used to control some low-level details, like
+ * the {@link JAI} instance to use when performing operation. Operations may use the
+ * hints or ignore them.
+ * <br><br>
+ * For example, if a user want to use the <code>"Resample"</code> operation with
+ * a custom {@link CoordinateTransformation}, he should first create its own
+ * {@link CoordinateTransformationFactory} implementation. Then, he can create
+ * a {@link GridCoverageProcessor} with its factory as a rendering hint:
+ *
+ * <blockquote><pre>
+ * CoordinateTransformationFactory myFactory = <FONT FACE="Arial">...</FONT>
+ * RenderingHints hints = new RenderingHints(Hints.{@link #COORDINATE_TRANSFORMATION_FACTORY}, myFactory);
+ * GridCoverageProcessor processor = new GridCoverageProcessor(hints);
+ * </pre></blockquote>
+ *
+ * @version $Id: Hints.java,v 1.1 2002/08/08 18:38:03 desruisseaux Exp $
+ * @author Martin Desruisseaux
+ */
+public final class Hints extends RenderingHints.Key {
+    /**
+     * Key for setting a {@link JAI} instance other than the default one when
+     * JAI operation must be performed at rendering time.
+     */
+    public static final RenderingHints.Key JAI_INSTANCE =
+            new Hints(0, JAI.class);
+
+    /**
+     * Key for setting a {@link CoordinateTransformationFactory} object other
+     * than the default one when coordinate transformations must be performed
+     * at rendering time.
+     */
+    public static final RenderingHints.Key COORDINATE_TRANSFORMATION_FACTORY =
+            new Hints(1, CoordinateTransformationFactory.class);
+
+    /**
+     * Base class of all values for this key.
+     *
+     * @task TODO: We could use only the class name (as a string)
+     *             here in order to defer class loading.
+     */
+    private final Class valueClass;
+
+    /**
+     * Construct a new key.
+     *
+     * @param id An ID. Must be unique for all instances of {@link Key}.
+     * @param valueClass Base class of all valid values.
+     */
+    private Hints(final int id, final Class valueClass) {
+        super(id);
+        this.valueClass = valueClass;
+    }
+
+    /**
+     * Returns <code>true</code> if the specified object is a valid
+     * value for this Key.
+     *
+     * @param  value The object to test for validity.
+     * @return <code>true</code> if the value is valid;
+     *         <code>false</code> otherwise.
+     */
+    public boolean isCompatibleValue(final Object value) {
+        return (value != null) && valueClass.isAssignableFrom(value.getClass());
+    }
+}
