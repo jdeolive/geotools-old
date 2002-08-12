564715b29e32f313a91a35239cb9ee7a235be175
desruisseaux <desruisseaux@e5c1c795-43da-0310-a71f-fac65c449510>
2002-08-12 18:18:28 +0000

    Added test for inverse transform
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1420 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/gcs-coverage/tests/unit/org/geotools/gc/LocalizationGridTest.java
+++ geotools2/geotools-src/gcs-coverage/tests/unit/org/geotools/gc/LocalizationGridTest.java
@@ -51,7 +51,7 @@ import junit.framework.TestSuite;
 /**
  * Test the {@link GridLocalization} implementation.
  *
- * @version $Id: LocalizationGridTest.java,v 1.4 2002/08/08 18:36:07 desruisseaux Exp $
+ * @version $Id: LocalizationGridTest.java,v 1.5 2002/08/12 18:18:28 desruisseaux Exp $
  * @author Remi Eve
  * @author Martin Desruisseaux
  */
@@ -69,66 +69,61 @@ public class LocalizationGridTest extends TestCase {
     /**
      * The "real world" coordinates of the image.
      */
-    private static final double[][] REAL_WORLD_VALUE =  
-                                                {{74.27344,   -37.882812,    /* First line. */
-                                                  72.69531,   -38.375,    
-                                                  71.38281,   -38.765625, 
-                                                  70.25,      -39.085938, 
-                                                  69.25781,   -39.359375, 
-                                                  68.375,    -39.585938}, 
-                                                 {74.27344,   -37.875,       /* Second line. */
-                                                  72.69531,   -38.367188, 
-                                                  71.375,     -38.757812, 
-                                                  70.25,      -39.078125, 
-                                                  69.25781,   -39.34375,  
-                                                  68.36719,  -39.578125},   
-                                                 {74.265625,  -37.867188,    /* third line. */
-                                                  72.6875,    -38.359375,  
-                                                  71.375,     -38.75,      
-                                                  70.24219,   -39.070312,  
-                                                  69.25,      -39.335938,  
-                                                  68.36719,  -39.570312},  
-                                                 {74.25781,   -37.859375,    /* fourth line. */
-                                                  72.6875,    -38.351562,  
-                                                  71.36719,   -38.742188,  
-                                                  70.234375,  -39.0625,    
-                                                  69.24219,   -39.328125,  
-                                                  68.359375, -39.5625},   
-                                                 {74.25781,   -37.851562,    /* fifth line. */
-                                                  72.67969,   -38.34375,   
-                                                  71.359375,  -38.734375,  
-                                                  70.234375,  -39.054688,  
-                                                  69.24219,   -39.320312,  
-                                                  68.359375, -39.546875},  
-                                                 {74.25,      -37.84375,     /* sixth line. */
-                                                  72.671875,  -38.335938,  
-                                                  71.359375,  -38.726562,  
-                                                  70.22656,   -39.039062,  
-                                                  69.234375,  -39.3125,    
-                                                  68.35156,   -39.539062}};                                                  
-                                                  
-    /**
-     * Offset of x and y coordinates in an "real world" entry.
-     */                                                  
-    private static final int X_OFFSET = LocalizationGridTransform2D.X_OFFSET,
-                             Y_OFFSET = LocalizationGridTransform2D.Y_OFFSET;
-    
-    /**
-     * Length of an "real world" entry in the grid.
-     */
-    private static final int REAL_WORLD_LENGTH = LocalizationGridTransform2D.CP_LENGTH;
+    private static final double[][] GRID_DATA =  
+        {{74.273440,   -37.882812,    /* First line. */
+          72.695310,   -38.375000,
+          71.382810,   -38.765625,
+          70.250000,   -39.085938,
+          69.257810,   -39.359375,
+          68.375000,   -39.585938}, 
+         {74.273440,   -37.875000,    /* Second line. */
+          72.695310,   -38.367188,
+          71.375000,   -38.757812,
+          70.250000,   -39.078125,
+          69.257810,   -39.343750,
+          68.367190,   -39.578125},
+         {74.265625,   -37.867188,    /* third line. */
+          72.687500,   -38.359375,
+          71.375000,   -38.750000,
+          70.242190,   -39.070312,
+          69.250000,   -39.335938,
+          68.367190,   -39.570312},
+         {74.257810,   -37.859375,    /* fourth line. */
+          72.687500,   -38.351562,
+          71.367190,   -38.742188,
+          70.234375,   -39.062500,
+          69.242190,   -39.328125,
+          68.359375,   -39.562500},
+         {74.257810,   -37.851562,    /* fifth line. */
+          72.679690,   -38.343750,
+          71.359375,   -38.734375,
+          70.234375,   -39.054688,
+          69.242190,   -39.320312,
+          68.359375,   -39.546875},
+         {74.250000,   -37.843750,    /* sixth line. */
+          72.671875,   -38.335938,
+          71.359375,   -38.726562,
+          70.226560,   -39.039062,
+          69.234375,   -39.312500,
+          68.351560,   -39.539062}};
        
     /**
-     * Epsilon between expected "real world" and compute "real world"
+     * Maximal error between expected "real world" and computed "real world"
      * coordinates when direct transformation is used.
      */
-    private static final double epsDirectTransform = 0.001;
+    private static final double EPS = 1E-9;
     
     /**
-     * Epsilon between expected "real world" and compute "real world"
-     * coordinates when affine transformation is used.
+     * Maximal error between expected "real world" and computed "real world"
+     * coordinates when the fitted affine transformation is used.
      */
-    private static final double epsAffineTransform = 0.4;
+    private static final double FIT_TOLERANCE = 0.4;
+
+    /**
+     * The error tolerance for inverse transformations. Since we use an iterative
+     * algorithm, the error may not be as small as we would like...
+     */
+    private static final double INVERSE_TOLERANCE = 0.25;
 
     /**
      * Returns the test suite.
@@ -146,93 +141,111 @@ public class LocalizationGridTest extends TestCase {
 
     /**
      * Set up common objects used for all tests.
+     * This implementation construct a default localization grid.
      */
     protected void setUp() throws Exception {
         super.setUp();
         random = new Random();
-        
-        // Construct a grid with real word associates. 
-        grid = new LocalizationGrid(REAL_WORLD_VALUE[0].length/REAL_WORLD_LENGTH,
-                                    REAL_WORLD_VALUE.length);
-        for (int indiceLine = 0; indiceLine < REAL_WORLD_VALUE.length; indiceLine++) {
-            for (int indicePoint = 0 ; indicePoint < REAL_WORLD_VALUE[0].length/REAL_WORLD_LENGTH ; indicePoint ++) {
-                grid.setLocalizationPoint(indicePoint,
-                                          indiceLine, 
-                                          REAL_WORLD_VALUE[indiceLine][indicePoint*REAL_WORLD_LENGTH + X_OFFSET],
-                                          REAL_WORLD_VALUE[indiceLine][indicePoint*REAL_WORLD_LENGTH + Y_OFFSET]);
+
+        final int width  = GRID_DATA[0].length/2;
+        final int height = GRID_DATA.length;
+        grid = new LocalizationGrid(width, height);
+        for (int j=0; j<height; j++) {
+            final double[] line = GRID_DATA[j];
+            assertEquals("Grid is not square", width*2, line.length);
+            int offset = 0;
+            for (int i=0; i<width; i++) {
+                grid.setLocalizationPoint(i, j, line[offset++], line[offset++]);
             }
         }
     }
+
+    /**
+     * Test some grid properties.
+     */
+    public void testProperties() {
+        assertTrue( grid.isMonotonic(false));
+        assertTrue(!grid.isMonotonic(true));
+        assertTrue(!grid.isNaN());
+    }
+
+    /**
+     * Returns an array with grid or "real world" coordinates.
+     *
+     * @param real <code>true</code> to gets the "real world" coordinates,
+     *            or <code>false</code> to gets the grid coordinates.
+     */
+    private double[] getGridCoordinates(final boolean real) {
+        final int     width  = GRID_DATA[0].length/2;
+        final int     height = GRID_DATA.length;
+        final double[] array = new double[width*height*2];
+        int offset = 0;
+        for (int j=0; j<height; j++) {
+            final double[] line = GRID_DATA[j];
+            assertEquals("Grid is not square", width*2, line.length);
+            for (int i=0; i<width; i++) {
+                array[offset++] = (real) ? line[i*2+0] : i;
+                array[offset++] = (real) ? line[i*2+1] : j;
+            }
+        }
+        assertEquals("Grid is not square", width*height*2, offset);
+        return array;
+    }
+
+    /**
+     * Compare a transformed array with the expected grid or "real world" coordinates.
+     *
+     * @param array The array to compare.
+     * @param real  <code>true</code> to compare with the "real world" coordinates,
+     *              or <code>false</code> to compare with the grid coordinates.
+     * @param eps   The maximal error accepted in comparaisons.
+     */
+    private void compare(final double[] array, final boolean real, final double eps) {
+        final int width  = GRID_DATA[0].length/2;
+        final int height = GRID_DATA.length;
+        int offset = 0;
+        for (int j=0; j<height; j++) {
+            final double[] line = GRID_DATA[j];
+            assertEquals("Grid is not square", width*2, line.length);
+            for (int i=0; i<width; i++) {                
+                assertEquals(real ? line[i*2+0] : i, array[offset++], eps);
+                assertEquals(real ? line[i*2+1] : j, array[offset++], eps);
+            }
+        }
+        assertEquals("Grid is not square", width*height*2, offset);
+    }
     
     /**
-     * Test direct transformation from pixel coordinate system to "real world" 
-     * coordinate system by using the localization grid.
+     * Test direct transformation from grid coordinates to "real world" 
+     * coordinates using the localization grid.
      */
     public void testDirectTransform() throws TransformException {
-        // Construct and set up an array containing index of points to transform in "real 
-        // world" coordinates.
-        final double[] src = new double[REAL_WORLD_VALUE.length * REAL_WORLD_VALUE[0].length],
-                       dst = new double[src.length];
-        
-        for (int indiceLine = 0; indiceLine < REAL_WORLD_VALUE.length ; indiceLine++) {
-            for (int indicePoint = 0 ; indicePoint < REAL_WORLD_VALUE[0].length/REAL_WORLD_LENGTH ; indicePoint ++) {
-                src[indiceLine*REAL_WORLD_VALUE[0].length + indicePoint*REAL_WORLD_LENGTH + X_OFFSET] = indicePoint;
-                src[indiceLine*REAL_WORLD_VALUE[0].length +indicePoint*REAL_WORLD_LENGTH + Y_OFFSET] = indiceLine;
-            }
-        }
-        
-        // Transform point localized by their index in point localized by a "real world"
-        // coordinates.
-        grid.getMathTransform().transform(src, 0, dst, 0, src.length / REAL_WORLD_LENGTH);
-        
-        // Test and compare the "real world" compute with the tranformation and the 
-        // "real world" expected.
-        for (int indiceLine = 0; indiceLine < REAL_WORLD_VALUE.length ; indiceLine++) {
-            for (int indicePoint = 0 ; indicePoint < REAL_WORLD_VALUE[0].length/REAL_WORLD_LENGTH ; indicePoint ++) {                
-                assertEquals(REAL_WORLD_VALUE[indiceLine][indicePoint*REAL_WORLD_LENGTH + X_OFFSET], 
-                             dst[indiceLine*REAL_WORLD_VALUE[0].length + indicePoint*REAL_WORLD_LENGTH + X_OFFSET], 
-                             epsDirectTransform);
-                assertEquals(REAL_WORLD_VALUE[indiceLine][indicePoint*REAL_WORLD_LENGTH + Y_OFFSET], 
-                             dst[indiceLine*REAL_WORLD_VALUE[0].length + indicePoint*REAL_WORLD_LENGTH + Y_OFFSET], 
-                             epsDirectTransform);
-            }
-        }
+        final double[] array = getGridCoordinates(false);
+        grid.getMathTransform().transform(array, 0, array, 0, array.length/2);
+        compare(array, true, EPS);
     }
     
     
     /**
-     * Test affine tranformation for the whole grid by comparing the "real world"
-     * coordinates expected to the "real world" coordinates expected.
+     * Test affine tranformation for the whole grid by comparing the
+     * expected "real world" to the approximated coordinates. Since
+     * the affine transform is fitted using least-squares method, the
+     * transformation is approximative.
      */
     public void testAffineTransform() {
-        // Construct and set up an array containing index of points to transform in "real 
-        // world" coordinates.
-        final double[] src = new double[REAL_WORLD_VALUE.length * REAL_WORLD_VALUE[0].length],
-                       dst = new double[src.length];
-
-        for (int indiceLine = 0; indiceLine < REAL_WORLD_VALUE.length ; indiceLine++) {
-            for (int indicePoint = 0 ; indicePoint < REAL_WORLD_VALUE[0].length/REAL_WORLD_LENGTH ; indicePoint ++) {
-                src[indiceLine*REAL_WORLD_VALUE[0].length +indicePoint*REAL_WORLD_LENGTH + X_OFFSET] = indicePoint;
-                src[indiceLine*REAL_WORLD_VALUE[0].length +indicePoint*REAL_WORLD_LENGTH + Y_OFFSET] = indiceLine;
-            }
-        }
-        
-        // Transform point localized by their index in point localized by a "real world"
-        // coordinates.
-        grid.getAffineTransform().transform(src, 0, dst, 0, src.length / REAL_WORLD_LENGTH);
-        
-        // Test and compare the "real world" compute with the affine transformation and the 
-        // "real world" expected.
-        for (int indiceLine = 0; indiceLine < REAL_WORLD_VALUE.length ; indiceLine++) {
-            for (int indicePoint = 0 ; indicePoint < REAL_WORLD_VALUE[0].length/REAL_WORLD_LENGTH ; indicePoint ++) {                
-                assertEquals(REAL_WORLD_VALUE[indiceLine][indicePoint*REAL_WORLD_LENGTH + X_OFFSET], 
-                             dst[indiceLine*REAL_WORLD_VALUE[0].length + indicePoint*REAL_WORLD_LENGTH + X_OFFSET], 
-                             epsAffineTransform);
-                assertEquals(REAL_WORLD_VALUE[indiceLine][indicePoint*REAL_WORLD_LENGTH + Y_OFFSET], 
-                             dst[indiceLine*REAL_WORLD_VALUE[0].length + indicePoint*REAL_WORLD_LENGTH + Y_OFFSET], 
-                             epsAffineTransform);
-            }
-        }
+        final double[] array = getGridCoordinates(false);
+        grid.getAffineTransform().transform(array, 0, array, 0, array.length/2);
+        compare(array, true, FIT_TOLERANCE);
+    }
+    
+    /**
+     * Test inverse transformation from "real world" coordinates
+     * to grid coordinates coordinate using the localization grid.
+     */
+    public void testInverseTransform () throws TransformException {
+        final double[] array = getGridCoordinates(true);
+        grid.getMathTransform().inverse().transform(array, 0, array, 0, array.length/2);
+        compare(array, false, INVERSE_TOLERANCE);
     }
 
     /**
