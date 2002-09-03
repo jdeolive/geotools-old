9cb9614b8c6f0562a20674e4261d8eb83702d41e
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-09-03 17:01:13 +0000

    added comments
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1552 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/data/DataSourceException.java
+++ geotools2/geotools-src/core/src/org/geotools/data/DataSourceException.java
@@ -17,27 +17,31 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-
 package org.geotools.data;
 
-/** 
+/**
  * Thrown when there is an error in a datasource.
- * @version $Id: DataSourceException.java,v 1.4 2002/07/29 16:53:36 ianturton Exp $
+ * 
+ * @version $Id: DataSourceException.java,v 1.5 2002/09/03 17:01:13 ianturton Exp $
  * @author Ray Gallagher
  */
-public class DataSourceException extends Exception{
-    
+public class DataSourceException extends Exception {
     /**
      * Constructs a new instance of DataSourceException
+     * 
      * @param msg A message explaining the exception
      */
     public DataSourceException(String msg) {
         super(msg);
     }
-    
-    public DataSourceException(String msg, Throwable exp){
-        super(msg,exp);
-    }
-    
-}
 
+    /**
+     * Constructs a new instance of DataSourceException 
+     * 
+     * @param msg A message explaining the exception
+     * @param exp the throwable object which caused this exception
+     */
+    public DataSourceException(String msg, Throwable exp) {
+        super(msg, exp);
+    }
+}
