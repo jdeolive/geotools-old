55b4dde04421cbf0450dd5bc2316a41b9e995b67
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-10-25 12:16:11 +0000

    changed from M??FunctionImpl
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1751 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/defaultcore/src/org/geotools/filter/MinFunction.java
@@ -0,0 +1,54 @@
+/*
+ * MinFunction.java
+ *
+ * Created on 28 July 2002, 16:03
+ */
+
+package org.geotools.filter;
+
+import org.geotools.feature.Feature;
+
+/**
+ *
+ * @author  James
+ */
+public class MinFunction extends FunctionExpressionImpl implements FunctionExpression {
+    
+    Expression a,b;
+    Expression[] args;
+    /** Creates a new instance of MinFunction */
+    protected MinFunction() { 
+    }
+    
+    
+    
+    /** Returns a value for this expression.
+     *
+     * @param feature Specified feature to use when returning value.
+     * @return Value of the feature object.
+     */
+    public Object getValue(Feature feature) {
+        double first = ((Number)a.getValue(feature)).doubleValue();
+        double second = ((Number)b.getValue(feature)).doubleValue();
+        return new Double(Math.min(first,second));
+    }
+    
+    public int getArgCount() {
+        return 2;
+    }
+    
+    public String getName() {
+        return "Min";
+    }
+    
+    public void setArgs(Expression[] args) {
+        a = args[0];
+        b = args[1];
+        this.args=args;
+    }
+    
+    public Expression[] getArgs() {
+        return args;
+    }
+    
+}
