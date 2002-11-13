ad724a75cb5c6100582073c2ae87cf11552c6095
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2002-11-13 12:37:17 +0000

    example of a  maven system for building self executing jars
    git-svn-id: http://svn.osgeo.org/geotools/trunk@1856 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/utils/src/org/geotools/utils/Foo2Bar.java
@@ -0,0 +1,30 @@
+/*
+ * Foo2Bar.java
+ *
+ * Created on 12 November 2002, 22:17
+ */
+
+package org.geotools.utils;
+
+import org.geotools.shapefile.Shapefile;
+
+/**
+ *
+ * @author  James
+ */
+public class Foo2Bar {
+    
+    /** Creates a new instance of Foo2Bar */
+    public Foo2Bar() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Foo is now Bar");
+        Shapefile shp;
+        System.out.println("shp is " + shp);
+    }
+    
+}
