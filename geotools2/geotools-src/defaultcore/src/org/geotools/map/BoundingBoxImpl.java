deb9d027859e88509545430dd605df6b7badc949
camerons <camerons@e5c1c795-43da-0310-a71f-fac65c449510>
2002-12-21 00:42:25 +0000

    Now uses CS_CoordinateSystem instead of CoordinateSystem, plus reused the code a little.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@2037 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/defaultcore/src/org/geotools/map/BoundingBoxImpl.java
+++ geotools2/geotools-src/defaultcore/src/org/geotools/map/BoundingBoxImpl.java
@@ -28,27 +28,33 @@ package org.geotools.map;
  * Extent and CoordinateSystem are cloned during construction and when returned.
  * This is to ensure only this class can change their values.
  *
- * @version $Id: BoundingBoxImpl.java,v 1.1 2002/12/20 10:19:00 camerons Exp $
+ * @version $Id: BoundingBoxImpl.java,v 1.2 2002/12/21 00:42:25 camerons Exp $
  * @author Cameron Shorter
  * 
  */
 
 import java.lang.Cloneable;
 import java.lang.IllegalArgumentException;
+import java.rmi.RemoteException;
 import java.util.Vector;
 import java.util.EventObject;
+import java.util.logging.Logger;
 import com.vividsolutions.jts.geom.Envelope;
 import javax.swing.event.EventListenerList;
+import org.geotools.cs.Adapters;
 import org.geotools.cs.CoordinateSystem;
-//import org.geotools.map.BoundingBox;
+import org.geotools.map.BoundingBox;
 import org.geotools.map.events.*;
+import org.opengis.cs.CS_CoordinateSystem;
 
-public class BoundingBoxImpl { // implements BoundingBox{
+public class BoundingBoxImpl implements BoundingBox{
     
-    private Envelope areaOfInterest;
+    private Envelope bBox;
     private CoordinateSystem coordinateSystem;
     private EventListenerList listenerList = new EventListenerList();
-   
+    private Adapters adapters = Adapters.getDefault();
+    private static final Logger LOGGER = Logger.getLogger("org.geotools.map.BoundingBoxImpl");
+
     /**
      * Initialise the model.
      * @param bbox The extent associated with this class.
@@ -56,14 +62,10 @@ public class BoundingBoxImpl { // implements BoundingBox{
      * @throws IllegalArgumentException if an argument is <code>null</code>.
      */
     public BoundingBoxImpl(
-            Envelope bbox,
-            CoordinateSystem coordinateSystem) throws IllegalArgumentException
+        Envelope bbox,
+        CS_CoordinateSystem coordinateSystem) throws IllegalArgumentException
     {
-        if ((bbox==null) || (coordinateSystem==null)){
-            throw new IllegalArgumentException();
-        }
-        this.areaOfInterest = new Envelope(bbox);
-        this.coordinateSystem = coordinateSystem;
+        this.setAreaOfInterest(bbox,coordinateSystem);
     }
     
     /**
@@ -127,16 +129,38 @@ public class BoundingBoxImpl { // implements BoundingBox{
      * @param coordinateSystem The coordinate system being using by this model.
      * @throws IllegalArgumentException if an argument is <code>null</code>.
      */
-    public void setAreaOfInterest(
+    private void setAreaOfInterest(
             Envelope bbox,
             CoordinateSystem coordinateSystem) throws IllegalArgumentException
     {
         if ((bbox==null) || (coordinateSystem==null) || bbox.isNull()){
             throw new IllegalArgumentException();
         }
-        this.areaOfInterest = new Envelope(bbox);
+        this.bBox = new Envelope(bbox);
         this.coordinateSystem = coordinateSystem;
         fireAreaOfInterestChangedListener();
+    }
+    
+    /**
+     * Set a new AreaOfInterest and trigger an AreaOfInterestEvent.
+     * Note that this is the only method to change coordinateSystem.  A
+     * <code>setCoordinateSystem</code> method is not provided to ensure
+     * this class is not dependant on transform classes.
+     * @param bbox The new areaOfInterest.
+     * @param coordinateSystem The coordinate system being using by this model.
+     * @throws IllegalArgumentException if an argument is <code>null</code>.
+     */
+    public void setAreaOfInterest(
+        Envelope bbox,
+        CS_CoordinateSystem coordinateSystem) throws IllegalArgumentException
+    {
+        try{
+            setAreaOfInterest(bbox,adapters.wrap(coordinateSystem));
+        } catch (RemoteException e) {
+            LOGGER.warning(
+                "RemoteException converted to IllegalArgumentException");
+            throw new IllegalArgumentException();
+        }
     }
     
     /**
@@ -145,9 +169,9 @@ public class BoundingBoxImpl { // implements BoundingBox{
      * @throws IllegalArgumentException if an argument is <code>null</code>.
      */
     public void setAreaOfInterest(
-            Envelope areaOfInterest)
+            Envelope bbox) throws IllegalArgumentException
     {
-        setAreaOfInterest(areaOfInterest,this.coordinateSystem);
+        setAreaOfInterest(bbox,this.coordinateSystem);
     }
     
     /**
@@ -155,14 +179,14 @@ public class BoundingBoxImpl { // implements BoundingBox{
      * @return Current AreaOfInterest
      */
     public Envelope getAreaOfInterest(){
-        return new Envelope(areaOfInterest);
+        return new Envelope(bBox);
     }
 
     /**
      * Get the coordinateSystem.
      */
-    public CoordinateSystem getCoordinateSystem() {
-        return this.coordinateSystem;
+    public CS_CoordinateSystem getCoordinateSystem() {
+        return adapters.export(this.coordinateSystem);
     }
 
     /*
@@ -171,15 +195,15 @@ public class BoundingBoxImpl { // implements BoundingBox{
      */
     public Object clone() {
         return new BoundingBoxImpl(
-            this.areaOfInterest,
-            this.coordinateSystem);
+            this.bBox,
+            adapters.export(this.coordinateSystem));
     }
     
     /**
      * Show the Envelope extent.
      */
     public String toString() {
-        return "("+areaOfInterest.getMinX()+","+areaOfInterest.getMinY()+"),"
-            +"("+areaOfInterest.getMaxX()+","+areaOfInterest.getMaxY()+")";
+        return "("+bBox.getMinX()+","+bBox.getMinY()+"),"
+            +"("+bBox.getMaxX()+","+bBox.getMaxY()+")";
     }
 }
