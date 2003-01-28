c6e5619c39098655d71664fe7ddd84c367c02b39
desruisseaux <desruisseaux@e5c1c795-43da-0310-a71f-fac65c449510>
2003-01-28 16:11:32 +0000

    Moved figures to the 'renderer' module
    git-svn-id: http://svn.osgeo.org/geotools/trunk@2196 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/resources/src/org/geotools/resources/XAffineTransform.java
+++ geotools2/geotools-src/resources/src/org/geotools/resources/XAffineTransform.java
@@ -317,7 +317,7 @@ public abstract class XAffineTransform extends AffineTransform {
     /**
      * Returns the scale factor <var>x</var> by cancelling the effect of an
      * eventual rotation. This factor is calculated by
-     * <IMG src="{@docRoot}/org/geotools/gui/swing/doc-files/scaleX0.png">.
+     * <IMG src="{@docRoot}/org/geotools/renderer/j2d/doc-files/scaleX0.png">.
      */
     public static double getScaleX0(final AffineTransform zoom) {
         return XMath.hypot(zoom.getScaleX(), zoom.getShearX());
@@ -326,7 +326,7 @@ public abstract class XAffineTransform extends AffineTransform {
     /**
      * Returns the scale factor <var>y</var> by cancelling the effect of an
      * eventual rotation. This factor is calculated by
-     * <IMG src="{@docRoot}/org/geotools/gui/swing/doc-files/scaleY0.png">.
+     * <IMG src="{@docRoot}/org/geotools/renderer/j2d/doc-files/scaleY0.png">.
      */
     public static double getScaleY0(final AffineTransform zoom) {
         return XMath.hypot(zoom.getScaleY(), zoom.getShearY());
