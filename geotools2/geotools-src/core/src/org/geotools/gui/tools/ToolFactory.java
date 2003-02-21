2bc8816916c930f742c53aa550f12ad12f06e788
camerons <camerons@e5c1c795-43da-0310-a71f-fac65c449510>
2003-02-21 19:24:09 +0000

    Initial version
    git-svn-id: http://svn.osgeo.org/geotools/trunk@2347 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/core/src/org/geotools/gui/tools/ToolFactory.java
@@ -0,0 +1,68 @@
+package org.geotools.gui.tools;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+/**
+ * Factory for constructing Tool classes.
+ * @version $Id: ToolFactory.java,v 1.1 2003/02/21 19:24:09 camerons Exp $
+ * @author Cameron Shorter
+ */
+public abstract class ToolFactory {
+    /**
+     * The logger 
+     */
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.geotools.gui.tools.ToolFactory");
+
+    private static ToolFactory factory=null;
+
+    /**
+     * Create an instance of the factory.
+     * @return An instance of ToolFactory, or null if ToolFactory could not
+     * be created.
+     */
+    public static ToolFactory createFactory(){
+        if (factory==null){
+            String factoryClass = System.getProperty("ToolFactoryImpl");
+            LOGGER.fine("loaded property = " + factoryClass);
+            if(factoryClass != null && factoryClass != ""){
+                factory = createFactory(factoryClass); 
+            }
+            if (factory==null) {
+                factory = createFactory(
+                    "org.geotools.gui.tools.ToolFactoryImpl");
+            }
+        }
+        return factory;
+    }
+
+    /**
+     * Create an instance of the factory.
+     * @return An instance of the Factory, or null if the Factory could not
+     * be created.
+     * @param factoryClass The name of the factory, eg:
+     * "org.geotools.gui.tools.ToolFactoryImpl".
+     */
+    public static ToolFactory createFactory(String factoryClass) {
+        try{
+            return factory = (ToolFactory)Class.forName(
+                    factoryClass).newInstance();
+        } catch (ClassNotFoundException e){
+            LOGGER.warning("createFactory failed to find implementation "
+                    + factoryClass+ " , "+e);
+        } catch (InstantiationException e){
+            LOGGER.warning("createFactory failed to insantiate implementation "
+                    + factoryClass+" , "+e);
+        } catch (IllegalAccessException e){
+            LOGGER.warning("createStyleFactory failed to access implementation "
+                    + factoryClass+" , "+e);
+        }
+        return null;
+    }
+
+    /**
+     * Create an instance of PanTool.
+     */
+    public abstract PanTool createPanTool();
+}
