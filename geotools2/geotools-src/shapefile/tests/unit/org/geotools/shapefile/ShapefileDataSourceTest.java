9c72a2ecfe67d38bc4270922e958bf9fff977df5
ianturton <ianturton@e5c1c795-43da-0310-a71f-fac65c449510>
2003-03-21 18:12:05 +0000

    added some tests of the datafile read
    git-svn-id: http://svn.osgeo.org/geotools/trunk@2545 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/shapefile/tests/unit/org/geotools/shapefile/ShapefileDataSourceTest.java
+++ geotools2/geotools-src/shapefile/tests/unit/org/geotools/shapefile/ShapefileDataSourceTest.java
@@ -79,17 +79,30 @@ public class ShapefileDataSourceTest extends TestCase {
         try{
             URL url = new URL("file:////"+dataFolder+"/statepop.shp");
             System.out.println("Testing ability to load "+url);
-            Shapefile shapefile = new Shapefile(url);
-            url = new URL("file:////"+dataFolder+"/statepop");
+            //Shapefile shapefile = new Shapefile(url);
+            //url = new URL("file:////"+dataFolder+"/statepop");
             //DbaseFileReader dbf = new DbaseFileReader(url.getFile());
-            ShapefileDataSource datasource = new ShapefileDataSource(shapefile);
+            ShapefileDataSource datasource = new ShapefileDataSource(url);
             FeatureCollection table = new FeatureCollectionDefault();
             table.setDataSource(datasource);
             EnvelopeExtent r = new EnvelopeExtent();
             r.setBounds(new Envelope(-180, 180, -90, 90));
             Feature[] features = table.getFeatures(r);
             System.out.println("No features loaded = "+features.length);
+            assertEquals("Number of Features loaded",49,features.length);
             System.out.println(features[0].getSchema().toString());
+            FeatureType schema = features[0].getSchema();
+            assertNotNull(schema.getDefaultGeometry());
+            System.out.println("default Geom "+schema.getDefaultGeometry());
+            assertEquals("Number of Attributes",253,schema.getAttributeTypes().length);
+            try{
+                assertEquals("Value of statename is wrong",features[0].getAttribute("STATE_NAME"),"Illinois");
+                assertEquals("Value of land area is wrong",((Double)features[0].getAttribute("LAND_KM")).doubleValue(),143986.61,0.001);
+            }catch (IllegalFeatureException ife){
+                System.out.println(ife);
+                ife.printStackTrace();
+                fail("Load failed because of exception "+ife.toString());
+            }
         }
         catch(DataSourceException e){
             System.out.println(e);
