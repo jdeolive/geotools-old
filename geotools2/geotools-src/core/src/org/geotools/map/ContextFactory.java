98bd237d82cc3c47dcd1bf1b641b7709dd03e200
camerons <camerons@e5c1c795-43da-0310-a71f-fac65c449510>
2003-03-22 10:16:16 +0000

    Initial version
    git-svn-id: http://svn.osgeo.org/geotools/trunk@2556 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/core/src/org/geotools/map/ContextFactory.java
@@ -0,0 +1,126 @@
+package org.geotools.map;
+
+import com.vividsolutions.jts.geom.Envelope;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import org.geotools.data.DataSource;
+import org.geotools.gui.tools.AbstractTool;
+import org.geotools.styling.Style;
+import org.opengis.cs.CS_CoordinateSystem;
+
+/**
+ * An implementation of ContextFactory to be used to construct context
+ * classes.  It should not be called directly.  Instead it should be
+ * created from ContextFactory, and ContextFactory methods should be
+ * called instead.
+ */
+public abstract class ContextFactory {
+    /**
+     * The logger 
+     */
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.geotools.gui.tools.ContextFactory");
+
+    private static ContextFactory factory=null;
+
+    /**
+     * Create an instance of the factory.
+     * @return An instance of ContextFactory, or null if ContextFactory
+     * could not be created.
+     */
+    public static ContextFactory createFactory(){
+        if (factory==null){
+            String factoryClass = System.getProperty("ContextFactoryImpl");
+            LOGGER.fine("loaded property = " + factoryClass);
+            if(factoryClass != null && factoryClass != ""){
+                factory = createFactory(factoryClass); 
+            }
+            if (factory==null) {
+                factory = createFactory(
+                    "org.geotools.gui.tools.ContextFactoryImpl");
+            }
+        }
+        return factory;
+    }
+
+    /**
+     * Create an instance of the factory.
+     * @return An instance of the Factory, or null if the Factory could not
+     * be created.
+     * @param factoryClass The name of the factory, eg:
+     * "org.geotools.gui.tools.ContextFactoryImpl".
+     */
+    public static ContextFactory createFactory(String factoryClass) {
+        try{
+            return factory = (ContextFactory)Class.forName(
+                    factoryClass).newInstance();
+        } catch (ClassNotFoundException e){
+            LOGGER.warning("createFactory failed to find implementation "
+                    + factoryClass+ " , "+e);
+        } catch (InstantiationException e){
+            LOGGER.warning("createFactory failed to insantiate implementation "
+                    + factoryClass+" , "+e);
+        } catch (IllegalAccessException e){
+            LOGGER.warning("createStyleFactory failed to access implementation "
+                    + factoryClass+" , "+e);
+        }
+        return null;
+    }
+
+    /**
+     * Create a BoundingBox.
+     * @param bbox The extent associated with this class.
+     * @param coordinateSystem The coordinate system associated with this
+     * class.
+     * @throws IllegalArgumentException if an argument is <code>null</code>.
+     */
+    public abstract BoundingBox createBoundingBox(
+        Envelope bbox,
+        CS_CoordinateSystem coordinateSystem)
+        throws IllegalArgumentException;
+
+    /**
+     * Create a Context.
+     * @param bbox The extent associated with this class.
+     * @param layerList The list of layers associated with this context.
+     * @param title The name of this context.  Must be set.
+     * @param _abstract A description of this context.  Optional, set to
+     * null if none exists.
+     * @param keywords An array of keywords to be used when searching for
+     * this context.  Optional, set to null if none exists.
+     * @param contactInformation Contact details for the person who created
+     * this context.  Optional, set to null if none exists.
+     * @throws IllegalArgumentException if an argument is <code>null</code>.
+     */
+    public abstract Context createContext(
+        BoundingBox bbox,
+        LayerList layerList,
+        String title,
+        String _abstract,
+        String[] keywords,
+        String contactInformation) throws IllegalArgumentException;
+    
+    /** Creates a Layer.
+     * @param dataSource The dataSource to query in order to get features for
+     * this layer.
+     * @param style The style to use when rendering features associated with
+     * this layer.
+     * @throws IllegalArgumentException if an argument is <code>null</code>.
+     */
+    public abstract Layer createLayer(
+        DataSource dataSource,
+        Style style) throws IllegalArgumentException;
+
+    /**
+     * Create a LayerList.
+     */
+    public abstract LayerList createLayerList();
+
+    /**
+     * Creates a SelectedTool.
+     * @param tool The selected tool.
+     * @throws IllegalArgumentException if an argument is <code>null</code>.
+     */
+    public abstract SelectedTool createSelectedContext(AbstractTool tool)
+        throws IllegalArgumentException;
+}
---
+++ geotools2/geotools-src/defaultcore/src/org/geotools/map/ContextFactoryImpl.java
@@ -0,0 +1,102 @@
+package org.geotools.map;
+
+import com.vividsolutions.jts.geom.Envelope;
+import org.geotools.data.DataSource;
+import org.geotools.gui.tools.AbstractTool;
+import org.geotools.styling.Style;
+import org.opengis.cs.CS_CoordinateSystem;
+
+/**
+ * An implementation of ContextFactory to be used to construct context
+ * classes.  It should not be called directly.  Instead it should be
+ * created from ContextFactory, and ContextFactory methods should be
+ * called instead.
+ */
+public class ContextFactoryImpl extends ContextFactory {
+
+    /**
+     * Create an instance of ContextFactoryImpl.  Note that this constructor
+     * should only be called from ContextFactory.
+     */
+    protected void ContextFactoryImpl() {
+    }
+
+    /**
+     * Create a BoundingBox.
+     * @param bbox The extent associated with this class.
+     * @param coordinateSystem The coordinate system associated with this
+     * class.
+     * @throws IllegalArgumentException if an argument is <code>null</code>.
+     */
+    public BoundingBox createBoundingBox(
+        Envelope bbox,
+        CS_CoordinateSystem coordinateSystem)
+        throws IllegalArgumentException
+    {
+        return new BoundingBoxImpl(bbox,coordinateSystem);
+    }
+
+    /**
+     * Create a Context.
+     * @param bbox The extent associated with this class.
+     * @param layerList The list of layers associated with this context.
+     * @param title The name of this context.  Must be set.
+     * @param _abstract A description of this context.  Optional, set to
+     * null if none exists.
+     * @param keywords An array of keywords to be used when searching for
+     * this context.  Optional, set to null if none exists.
+     * @param contactInformation Contact details for the person who created
+     * this context.  Optional, set to null if none exists.
+     * @throws IllegalArgumentException if an argument is <code>null</code>.
+     */
+    public Context createContext(
+        BoundingBox bbox,
+        LayerList layerList,
+        String title,
+        String _abstract,
+        String[] keywords,
+        String contactInformation) throws IllegalArgumentException
+    {
+        return new ContextImpl(
+           bbox, 
+           layerList,
+           title,
+           _abstract,
+           keywords,
+           contactInformation);
+    }
+    
+    /** Creates a Layer.
+     * @param dataSource The dataSource to query in order to get features for
+     * this layer.
+     * @param style The style to use when rendering features associated with
+     * this layer.
+     * @throws IllegalArgumentException if an argument is <code>null</code>.
+     */
+    public Layer createLayer(
+        DataSource dataSource,
+        Style style) throws IllegalArgumentException
+    {
+        return new LayerImpl(
+            dataSource,
+            style);
+    }
+
+    /**
+     * Create a LayerList.
+     */
+    public LayerList createLayerList() {
+        return new LayerListImpl();
+    }
+
+    /**
+     * Creates a SelectedTool.
+     * @param tool The selected tool.
+     * @throws IllegalArgumentException if an argument is <code>null</code>.
+     */
+    public SelectedTool createSelectedContext(AbstractTool tool)
+        throws IllegalArgumentException
+    {
+        return new SelectedToolImpl(tool);
+    }
+}
