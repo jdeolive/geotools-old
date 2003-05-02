1046bdf3a8bd487d499261dde5cc3097df69a294
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2003-05-02 17:28:00 +0000

    Initial commit
    git-svn-id: http://svn.osgeo.org/geotools/trunk@2790 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/gtopo30/tests/unit/org/geotools/coverage/GTOPO30Suite.java
@@ -0,0 +1,28 @@
+package org.geotools.coverage;
+
+import junit.framework.*;
+
+
+/**
+ *
+ * @author aaime
+ */
+public class GTOPO30Suite extends TestCase {
+  
+  public GTOPO30Suite(String testName) {
+    super(testName);
+  }
+  
+  public static void main(java.lang.String[] args) {
+    junit.textui.TestRunner.run(suite());
+  }
+  
+  public static Test suite() {
+    TestSuite suite = new TestSuite("All GTOPO30DataSource Tests");
+    
+    suite.addTestSuite(GT30HeaderTest.class);
+    suite.addTestSuite(GT30StatTest.class);
+    
+    return suite;
+  }
+}
---
+++ geotools2/geotools-src/gtopo30/tests/unit/org/geotools/coverage/TestCaseSupport.java
@@ -0,0 +1,47 @@
+/*
+ * TestCaseSupport.java
+ *
+ * Created on April 30, 2003, 12:16 PM
+ */
+
+package org.geotools.coverage;
+
+import java.io.InputStream;
+import junit.framework.*;
+import java.net.*;
+import java.nio.channels.ReadableByteChannel;
+
+/**
+ *
+ * @author  Ian Schneider
+ */
+public abstract class TestCaseSupport extends TestCase {
+
+  /** Creates a new instance of TestCaseSupport */
+  public TestCaseSupport(String name) {
+    super(name);
+  }
+  
+  protected URL getTestResource(String name) {
+    URL r = TestCaseSupport.class.getResource("/testData/" + name);
+    if (r == null)
+      throw new RuntimeException("Could not locate resource : " + name);
+    return r;
+  }
+  
+  protected InputStream getTestResourceAsStream(String name) {
+    InputStream in = TestCaseSupport.class.getResourceAsStream("/testData/" + name);
+    if (in == null)
+      throw new RuntimeException("Could not locate resource : " + name);
+    return in;
+  }
+  
+  protected ReadableByteChannel getTestResourceChannel(String name) {
+    return java.nio.channels.Channels.newChannel(getTestResourceAsStream(name));
+  }
+  
+  public static Test suite(Class c) {
+    return new TestSuite(c);
+  }
+  
+}
