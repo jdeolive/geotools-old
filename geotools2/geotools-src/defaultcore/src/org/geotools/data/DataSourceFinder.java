63a99e44c60e81d96778d680a4690b98b0f66ef5
jmacgill <jmacgill@e5c1c795-43da-0310-a71f-fac65c449510>
2003-05-16 20:18:54 +0000

    made into a singleton documented and reformated
    git-svn-id: http://svn.osgeo.org/geotools/trunk@2918 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/defaultcore/src/org/geotools/data/DataSourceFinder.java
+++ geotools2/geotools-src/defaultcore/src/org/geotools/data/DataSourceFinder.java
@@ -1,49 +1,87 @@
 /*
- * GeometryFactory.java
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
  *
- * Created on February 12, 2003, 2:36 PM
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
  */
-
 package org.geotools.data;
 
-import java.util.logging.Logger;
-import java.util.Map;
-import java.util.Iterator;
-import org.geotools.data.DataSource;
-import org.geotools.data.DataSourceFactorySpi;
 import sun.misc.Service;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Logger;
 
 
-/**
- *
- * @author  jamesm
+/*
+ * DataSourceFinder.java
+ * Provides methods which enable programs to find all available datasource implementations.
+ * In order to be located by this finder datasources must provide an implementation of
+ * the DataSourceFactorySpi interface. In addition to implementing this
+ * interface datasouces should have a services file:
+ * META-INF/services/org.geotools.data.DataSourceFactorySpi The file should
+ * contain a single line which gives the full name of the implementing class.
+ * e.g. org.geotools.data.mytype.MyTypeDataSourceFacotry
  */
 public class DataSourceFinder {
-    
-    /** The logger for the data module.  */
+    /** The logger for the data module. */
     private static final Logger LOGGER = Logger.getLogger("org.geotools.data");
 
-    /** Creates a new instance of DataSourceFactory */
-    public DataSourceFinder() {
+    private DataSourceFinder() {
     }
-    
-     public static DataSource getDataSource(Map params) throws DataSourceException
-    {
-        Iterator ps = Service.providers(DataSourceFactorySpi.class); 
+
+    /**
+     * Checks each available datasource implementation in turn and returns the
+     * first one which claims to support the resource identified by the params
+     * object.
+     *
+     * @param params A Map object which contains a defenition of the resource
+     *        to connect to. for file based resources the property 'url'
+     *        should be set within this Map.
+     *
+     * @return The first datasource which claims to process the required
+     *         resource, returns null if none can be found.
+     *
+     * @throws DataSourceException If a sutiable loader can be found, but it
+     *         can not be attached to the specified resource without errors.
+     */
+    public static DataSource getDataSource(Map params)
+        throws DataSourceException {
+        Iterator ps = Service.providers(DataSourceFactorySpi.class);
         LOGGER.finer("Available Data Sources:");
-        while(ps.hasNext()){
-            DataSourceFactorySpi fac = (DataSourceFactorySpi)ps.next();
+
+        while (ps.hasNext()) {
+            DataSourceFactorySpi fac = (DataSourceFactorySpi) ps.next();
             LOGGER.finer(fac.getDescription());
-            if(fac.canProcess(params)){
+
+            if (fac.canProcess(params)) {
                 return fac.createDataSource(params);
             }
         }
+
         return null;
     }
-     
-     public static Iterator getAvailableDataSources()
-    {
-        return Service.providers(DataSourceFactorySpi.class); 
-    } 
-}
 
+    /**
+     * Finds all implemtaions of DataSourceFactory which have registered using
+     * the services mechanism.
+     *
+     * @return An iterator over all discovered datasources which have
+     *         registered factories
+     *
+     * @task REVISIT: Currently uses sun.misc classes which may not always be
+     *       available, we can probably work round this.
+     */
+    public static Iterator getAvailableDataSources() {
+        return Service.providers(DataSourceFactorySpi.class);
+    }
+}
