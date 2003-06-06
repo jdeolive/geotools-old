d283f876bfa1c684815d29be454124e455aeb9b4
ianschneider <ianschneider@e5c1c795-43da-0310-a71f-fac65c449510>
2003-06-06 21:12:26 +0000

    *** empty log message ***
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3039 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/pickle/test/PickleTest.java
@@ -0,0 +1,46 @@
+import org.geotools.data.shapefile.ShapefileDataSource;
+import org.geotools.data.pickle.PickleDataSource;
+import org.geotools.feature.*;
+
+public class PickleTest {
+
+  public static final void main(String[] args) throws Exception {
+    String tmpDir = System.getProperty("java.io.tmpdir");
+    java.net.URL url = new java.net.URL("file:" + args[0]);
+    java.net.URL tmpShp = new java.net.URL("file:" + tmpDir + "/tmpshp_delete_me.shp");
+    java.util.logging.Logger.getLogger("org.geotools.data.shapefile").setLevel(java.util.logging.Level.OFF);
+    for (int i = 0, ii = 3; i < ii; i++) {
+      long time = System.currentTimeMillis();
+      org.geotools.data.shapefile.ShapefileDataSource sds = new org.geotools.data.shapefile.ShapefileDataSource(url);
+      FeatureCollection fc = sds.getFeatures();
+      System.out.println("read shapefile in " + (System.currentTimeMillis() - time));
+      
+      time = System.currentTimeMillis();
+      sds = new org.geotools.data.shapefile.ShapefileDataSource(tmpShp);
+      sds.setFeatures(fc);
+      System.out.println("wrote shapefile in " + (System.currentTimeMillis() - time));
+      
+      time = System.currentTimeMillis();
+      PickleDataSource pds = new PickleDataSource(new File(tmpDir), "junkyCrap");
+      pds.setFeatures(fc);
+      System.out.println("pickled in " + (System.currentTimeMillis() - time));
+  
+      time = System.currentTimeMillis();
+      fc = pds.getFeatures();
+      System.out.println("unpickled in " + (System.currentTimeMillis() - time));
+      
+      // uncomment to test serial access
+      
+//      long total = 0;
+//      pds = new PickleDataSource(new File("/tmp"), "junkyCrap");
+//      for (int j = 0, jj = fc.size(); j < jj; j++) {
+//        
+//        time = System.currentTimeMillis();
+//        if (pds.getFeature(j) == null)
+//          System.out.println("FAILURE");
+//        total += System.currentTimeMillis() - time;
+//      }
+//      System.out.println("random access average (" + fc.size() + ") "  + (double)total / fc.size());
+    }
+  }
+}
