b6b6bb06decb04e490507da19ddd7a767e8a3fdd
seangeo <seangeo@e5c1c795-43da-0310-a71f-fac65c449510>
2003-06-13 00:41:38 +0000

    Changed setRule method to replace the exist rules instead of add to them.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3057 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/defaultcore/src/org/geotools/styling/FeatureTypeStyleImpl.java
+++ geotools2/geotools-src/defaultcore/src/org/geotools/styling/FeatureTypeStyleImpl.java
@@ -22,7 +22,7 @@ package org.geotools.styling;
 import java.util.*;
 
 /**
- * @version $Id: FeatureTypeStyleImpl.java,v 1.5 2003/05/16 21:10:20 jmacgill Exp $
+ * @version $Id: FeatureTypeStyleImpl.java,v 1.6 2003/06/13 00:41:38 seangeo Exp $
  * @author James Macgill
  */
 public class FeatureTypeStyleImpl implements org.geotools.styling.FeatureTypeStyle {
@@ -33,42 +33,42 @@ public class FeatureTypeStyleImpl implements org.geotools.styling.FeatureTypeSty
     String abstractStr = "abstract";
     /** Creates a new instance of DefaultFeatureTypeStyle */
     protected FeatureTypeStyleImpl() {
-        
+
     }
     protected FeatureTypeStyleImpl(Rule[] rules) {
         setRules(rules);
     }
-    
+
     public String getFeatureTypeName() {
         return featureTypeName;
     }
-    
+
     public Rule[] getRules() {
         return (Rule[]) ruleList.toArray(new Rule[0]);
     }
-    
+
     public String[] getSemantecTypeIdentifiers() {
         return new String[]{"generic:geometry"};//HACK: - generic catch all identifier
     }
     public void setSemantecTypeIdentifiers(String[] types) {
         // since these are defined yet we can ignore it
     }
-    
+
     public void setRules(Rule[] rules){
-        //ruleList.add( Arrays.asList(rules));
+		ruleList.clear();
         for(int i=0;i<rules.length;i++){
             addRule(rules[i]);
         }
     }
-    
+
     public void addRule(Rule rule){
         ruleList.add(rule);
     }
-    
+
     public void setFeatureTypeName(String name) {
         featureTypeName = name;
     }
-    
+
     /**
      * Getter for property abstractStr.
      * @return Value of property abstractStr.
@@ -76,7 +76,7 @@ public class FeatureTypeStyleImpl implements org.geotools.styling.FeatureTypeSty
     public java.lang.String getAbstract() {
         return abstractStr;
     }
-    
+
     /**
      * Setter for property abstractStr.
      * @param abstractStr New value of property abstractStr.
@@ -84,7 +84,7 @@ public class FeatureTypeStyleImpl implements org.geotools.styling.FeatureTypeSty
     public void setAbstract(java.lang.String abstractStr) {
         this.abstractStr = abstractStr;
     }
-    
+
     /**
      * Getter for property name.
      * @return Value of property name.
@@ -92,7 +92,7 @@ public class FeatureTypeStyleImpl implements org.geotools.styling.FeatureTypeSty
     public java.lang.String getName() {
         return name;
     }
-    
+
     /**
      * Setter for property name.
      * @param name New value of property name.
@@ -100,7 +100,7 @@ public class FeatureTypeStyleImpl implements org.geotools.styling.FeatureTypeSty
     public void setName(java.lang.String name) {
         this.name = name;
     }
-    
+
     /**
      * Getter for property title.
      * @return Value of property title.
@@ -108,7 +108,7 @@ public class FeatureTypeStyleImpl implements org.geotools.styling.FeatureTypeSty
     public java.lang.String getTitle() {
         return title;
     }
-    
+
     /**
      * Setter for property title.
      * @param title New value of property title.
@@ -116,5 +116,5 @@ public class FeatureTypeStyleImpl implements org.geotools.styling.FeatureTypeSty
     public void setTitle(java.lang.String title) {
         this.title = title;
     }
-    
+
 }
