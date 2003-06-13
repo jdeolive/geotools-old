2a6d314c14adc5cfff6026dd23285a5302a33b6d
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2003-06-13 18:26:16 +0000

    SLD support for j2d, basic implementation
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3064 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/renderer/src/org/geotools/renderer/j2d/RenderedLayerFactory.java
+++ geotools2/geotools-src/renderer/src/org/geotools/renderer/j2d/RenderedLayerFactory.java
@@ -1,77 +1,316 @@
 /*
- * Geotools 2 - OpenSource mapping toolkit
- * (C) 2003, Geotools Project Managment Committee (PMC)
- * (C) 2003, Institut de Recherche pour le Développement
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *
- * Contacts:
- *     UNITED KINGDOM: James Macgill
- *             mailto:j.macgill@geog.leeds.ac.uk
- *
- *     FRANCE: Surveillance de l'Environnement Assistée par Satellite
- *             Institut de Recherche pour le Développement / US-Espace
- *             mailto:seasnet@teledetection.fr
  */
 package org.geotools.renderer.j2d;
 
-// J2SE dependencies
-import java.awt.Paint;
 
 // JTS dependencies
 import com.vividsolutions.jts.geom.sfs.SFSGeometry;
+import org.geotools.ct.TransformException;
 
 // Geotools dependencies
-import org.geotools.styling.Style;
 import org.geotools.feature.Feature;
-import org.geotools.ct.TransformException;
+import org.geotools.feature.IllegalFeatureException;
+import org.geotools.filter.Filter;
+import org.geotools.gc.GridCoverage;
 import org.geotools.renderer.geom.JTSGeometries;
+import org.geotools.renderer.style.SLDStyleFactory;
+import org.geotools.renderer.style.Style;
+import org.geotools.styling.FeatureTypeStyle;
+import org.geotools.styling.LineSymbolizer;
+import org.geotools.styling.PointSymbolizer;
+import org.geotools.styling.PolygonSymbolizer;
+import org.geotools.styling.RasterSymbolizer;
+import org.geotools.styling.Rule;
+import org.geotools.styling.Symbolizer;
+import org.geotools.styling.TextSymbolizer;
+import org.geotools.util.RangeSet;
+
+// J2SE dependencies
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.media.jai.util.Range;
 
 
 /**
  * A factory creating {@link RenderedLayer}s from {@link Feature}s and {@link Style}s.
  *
- * @version $Id: RenderedLayerFactory.java,v 1.4 2003/05/28 10:21:46 desruisseaux Exp $
  * @author Martin Desruisseaux
- * @author <--- add your name --->
+ * @author Andrea Aime
+ * @version $Id: RenderedLayerFactory.java,v 1.5 2003/06/13 18:26:16 aaime Exp $
  */
 public class RenderedLayerFactory {
+    private static final Logger LOGGER = Logger.getLogger("org.geotools.renderer.j2d");
+
+    // prepare the style factory that will convert SLD styles into resolved styles
+    private static SLDStyleFactory styleFactory = new SLDStyleFactory();
+
     /**
-     * Create a rendered layer from the specified feature and style.
+     * Create an array of rendered layers from the specified feature and style.
      *
-     * @param  feature The feature.
-     * @param  style   The style to apply.
-     * @return The rendered layer for the specified feature and style.
+     * @param features The feature.
+     * @param SLDStyle The style to apply.
+     *
+     * @return The rendered layer array for the specified feature and style.
+     *
      * @throws TransformException if a transformation was required and failed.
      */
-    public RenderedLayer create(final Feature feature, final Style style) throws TransformException
-    {
-        /*
-         * Case 1: Draw a geometry collection. This block create an 'GeometryCollection' object,
-         *         which can contains an arbitrary number of geometric shapes. All geometries should
-         *         have the same 'z' value. For example, an 'isoline' could be the 50 meters isobath.
-         */
-        final JTSGeometries geometries = new JTSGeometries(/* put the CS here (default to geographic) */);
-        for (int i=0; i<5 /* put the number of geometry here */; i++) {
-            final SFSGeometry geometry = null; // Get a geometry here. It can be a SFSGeometryCollection.
-            geometries.add(geometry);
+    public RenderedLayer[] create(final Feature[] features,
+        final org.geotools.styling.Style SLDStyle) throws TransformException {
+        
+        // ... the list that will contain all generated rendered layers
+        List renderedLayers = new ArrayList();
+
+        // ... and the first geometric layer
+        JTSGeometries geometries = new JTSGeometries();
+
+        // process the styles in order
+        FeatureTypeStyle[] featureStylers = SLDStyle.getFeatureTypeStyles();
+
+        for (int i = 0; i < featureStylers.length; i++) {
+            FeatureTypeStyle fts = featureStylers[i];
+
+            // Prepare the else features map. Since we are preprocessing, we have to consider
+            // what the elseFilters may catch at each scale, so we build a set of scale ranges
+            // in which the elseFilter should apply by subtraction: we start with the full
+            // floating point range and subtract the ranges covered by filters, if the
+            // range set becomes empty, the feature is removed from the elseFeature map,
+            // otherwise a Geometry is created from each remaining scale range
+            LinkedHashMap elseFeatureRanges = new LinkedHashMap(features.length);
+
+            for (int j = 0; j < features.length; j++) {
+                RangeSet rs = new RangeSet(Double.class);
+                rs.add(new Range(Double.class, new Double(0),
+                        new Double(Double.MAX_VALUE)));
+                elseFeatureRanges.put(features[j], rs);
+            }
+
+            // process the rules for the current feature type style
+            Rule[] rules = fts.getRules();
+
+            for (int j = 0; j < rules.length; j++) {
+                // if this rule is not an else filter
+                if (!rules[j].hasElseFilter()) {
+                    Filter filter = rules[j].getFilter();
+                    Symbolizer[] symbolizers = rules[j].getSymbolizers();
+                    List ruleFeatures = new ArrayList();
+                    Range ruleRange = buildRuleRange(rules[j]);
+                    String ftsTypeName = fts.getFeatureTypeName();
+
+                    // get all the features that must be rendered according to
+                    // this rule and update the elseFeatures ranges (remove the scale range
+                    // covered by this rule, if the else feature range is empty, remove it from
+                    // the map
+                    for (int k = 0; k < features.length; k++) {
+                        Feature feature = features[k];
+
+                        if (featureMatching(feature, ftsTypeName, filter)) {
+                            ruleFeatures.add(feature);
+
+                            RangeSet rs = (RangeSet) elseFeatureRanges.get(feature);
+
+                            if (rs != null) {
+                                rs.remove(ruleRange);
+
+                                if (rs.isEmpty()) {
+                                    elseFeatureRanges.remove(feature);
+                                }
+                            }
+                        }
+                    }
+
+                    // now process the symbolizers on the catched features
+                    for (Iterator it = ruleFeatures.iterator(); it.hasNext();) {
+                        Feature feature = (Feature) it.next();
+                        geometries = processSymbolizers(feature, symbolizers, ruleRange,
+                                renderedLayers, geometries);
+                    }
+                }
+            }
+
+            // if some feature is not catched by the rules, see if some else rule applies
+            if (elseFeatureRanges.size() > 0) {
+                Set elseFeatures = elseFeatureRanges.keySet();
+
+                for (int j = 0; j < rules.length; j++) {
+                    if (rules[j].hasElseFilter()) { // if this rule is an else filter
+
+                        Range ruleRange = buildRuleRange(rules[j]);
+                        Symbolizer[] symbolizers = rules[j].getSymbolizers();
+
+                        // for each feature get the list of the ranges that are not catched by rules
+                        // and compare with the elseRule range
+                        for (Iterator featureIt = elseFeatures.iterator(); featureIt.hasNext();) {
+                            Feature feature = (Feature) featureIt.next();
+                            RangeSet rs = (RangeSet) elseFeatureRanges.get(feature);
+
+                            for (Iterator rangeIt = rs.iterator(); rangeIt.hasNext();) {
+                                Range featureRange = (Range) rangeIt.next();
+                                Range finalRange = featureRange.intersect(ruleRange);
+
+                                if ((finalRange != null) && !finalRange.isEmpty()) {
+                                    geometries = processSymbolizers(feature, symbolizers,
+                                            finalRange, renderedLayers, geometries);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-        final RenderedGeometries layer = new RenderedGeometries(geometries);
-        layer.setContour   (null); // Set here a 'Paint' for the contour line.
-        layer.setForeground(null); // Set here a 'Paint' for filling polygon.
-        return layer;
+
+        // add the current layer if not empty
+        if (geometries.getGeometries().size() > 0) {
+            renderedLayers.add(new SLDRenderedGeometries(geometries));
+        }
+
+        RenderedLayer[] layers = new RenderedLayer[renderedLayers.size()];
+
+        return (RenderedLayer[]) renderedLayers.toArray(layers);
+    }
+
+    /**
+     * Process the symbolizers on the current feature. Will update the renderedLayer list and
+     * return the new current JSTGeometries object (the vector layer under construction
+     *
+     * @param feature The feature to be styled
+     * @param symbolizers The symbolizers that will apply styles to the feature
+     * @param scaleRange The scale range in which the styled feature should be rendered
+     * @param renderedLayers The list of renderedLayers
+     * @param geometries The current vector layer under construction
+     *
+     * @return the new vector layer under construction (may be equal to <code>geometries</code>)
+     *
+     * @throws TransformException if a transformation was required and failed.
+     */
+    private JTSGeometries processSymbolizers(Feature feature, Symbolizer[] symbolizers,
+        Range scaleRange, List renderedLayers, JTSGeometries geometries)
+        throws TransformException {
+        for (int i = 0; i < symbolizers.length; i++) {
+            Symbolizer symb = symbolizers[i];
+            Style style = styleFactory.createStyle(feature, symb, scaleRange);
+
+            // if the symbolizer is a raster one we have to create
+            // another rendered layer with the grid alone. 
+            // TODO: do the same for rendered marks
+            if (symb instanceof RasterSymbolizer) {
+                if (geometries.getGeometries().size() > 0) {
+                    renderedLayers.add(new SLDRenderedGeometries(geometries));
+                    geometries = new JTSGeometries();
+                }
+
+                try {
+                    GridCoverage grid = (GridCoverage) feature.getAttribute("grid");
+                    renderedLayers.add(new RenderedGridCoverage(grid));
+                } catch (IllegalFeatureException ife) {
+                    // TODO: should we rethrow the exception or be tolerant?
+                    LOGGER.severe("Grid not found\n" + ife);
+                }
+            } else {
+                SFSGeometry geometry = findGeometry(feature, symb);
+
+                if (geometry != null) {
+                    geometries.add(geometry).setStyle(style);
+                }
+            }
+        }
+
+        return geometries;
+    }
+
+    /**
+     * Checks wheter the feature matches the filter and the type name
+     *
+     * @param feature
+     * @param ftsTypeName The type name that the feature should match
+     * @param filter The filter that the feature should be contained in
+     *
+     * @return true if the feature matches both
+     */
+    private boolean featureMatching(Feature feature, String ftsTypeName, Filter filter) {
+        String typeName = feature.getSchema().getTypeName();
+
+        return ((typeName != null) && typeName.equalsIgnoreCase(ftsTypeName)) &&
+        ((filter == null) || filter.contains(feature));
+    }
+
+    /**
+     * Builds a range from the rule scale specification
+     *
+     * @param r The rule
+     *
+     * @return The range with minimum and maximun scale (will use minimum and maximum double values
+     *         if unbounded
+     */
+    private Range buildRuleRange(Rule r) {
+        double min = r.getMinScaleDenominator();
+        double max = r.getMaxScaleDenominator();
+        Double Min = Double.isInfinite(min) ? new Double(Double.MIN_VALUE) : new Double(min);
+        Double Max = Double.isInfinite(max) ? new Double(Double.MAX_VALUE) : new Double(max);
+
+        return new Range(Double.class, Min, Max);
+    }
+
+    /**
+     * Finds the geometric attribute requested by the symbolizer
+     *
+     * @param f The feature
+     * @param s The symbolizer
+     *
+     * @return The geometry requested in the symbolizer, or the default geometry if none is
+     *         specified
+     */
+    public SFSGeometry findGeometry(Feature f, Symbolizer s) {
+        String geomName = null;
+
+        // TODO: fix the styles, should be the same method name and probably should be moved 
+        // into the a interface...
+        if (s instanceof PolygonSymbolizer) {
+            geomName = ((PolygonSymbolizer) s).geometryPropertyName();
+        } else if (s instanceof PointSymbolizer) {
+            geomName = ((PointSymbolizer) s).geometryPropertyName();
+        } else if (s instanceof LineSymbolizer) {
+            geomName = ((LineSymbolizer) s).geometryPropertyName();
+        } else if (s instanceof TextSymbolizer) {
+            geomName = ((TextSymbolizer) s).getGeometryPropertyName();
+        }
+
+        SFSGeometry geom;
+
+        if (geomName == null) {
+            geom = f.getDefaultGeometry();
+        } else {
+            try {
+                geom = (SFSGeometry) f.getAttribute(geomName);
+            } catch (IllegalFeatureException ife) {
+                // TODO: again, what should we do with this kind of errors?
+                if (LOGGER.isLoggable(Level.FINEST)) {
+                    LOGGER.finest("Geometry " + geomName + " not found " + ife);
+                }
+
+                //hack: not sure if null is the right thing to return at this point
+                geom = null;
+            }
+        }
+
+        return geom;
     }
 }
---
+++ geotools2/geotools-src/renderer/src/org/geotools/renderer/j2d/SLDRenderedGeometries.java
@@ -0,0 +1,111 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
+/*
+ * SLDRenderedGeometries.java
+ *
+ * Created on 11 giugno 2003, 7.35
+ */
+package org.geotools.renderer.j2d;
+
+import org.geotools.ct.*;
+import org.geotools.renderer.geom.GeometryCollection;
+import org.geotools.renderer.style.LineStyle2D;
+import org.geotools.renderer.style.PolygonStyle2D;
+import org.geotools.renderer.style.Style2D;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+
+
+/**
+ * A RenderedGeometries layer that can process styles (will use device space coordinates for  style
+ * rendering)
+ *
+ * @author aaime
+ */
+public class SLDRenderedGeometries extends RenderedGeometries {
+    protected double currentScale;
+
+    /**
+     * Construct a layer for the specified geometry.
+     *
+     * @param geometry The geometry, or <code>null</code> if none.
+     *
+     * @see #setGeometry
+     */
+    public SLDRenderedGeometries(final GeometryCollection geometry) {
+        super(geometry);
+
+        // this layer works directly with device space coordinates
+        setRenderUsingMapCS(false);
+    }
+
+    /**
+     * Draw the geometry.
+     *
+     * @param context The set of transformations needed for transforming geographic coordinates
+     *        (<var>longitude</var>,<var>latitude</var>) into pixels coordinates.
+     *
+     * @throws TransformException If a transformation failed.
+     */
+    protected void paint(final RenderingContext context)
+        throws TransformException {
+        // Overridden to get the current scale even if rendering in device space coordinates.
+        // Use only the scaleX since this is the behaviour of Java2DRendering 
+        // (reference implementation)
+        currentScale = context.getAffineTransform(context.mapCS, context.textCS).getScaleX();
+        System.out.println("Current scale: " + currentScale);
+        super.paint(context);
+    }
+
+    /**
+     * Invoked automatically when a polyline is about to be draw. This implementation paints the
+     * polyline according to the rendered style
+     *
+     * @param graphics The graphics in which to draw.
+     * @param polyline The polyline to draw.
+     * @param style The style to apply, or <code>null</code> if none.
+     */
+    protected void paint(final Graphics2D graphics, final Shape polyline, final Style2D style) {
+        if (style == null) {
+            System.out.println("Null style!"); // TODO: what's going on? Should not be reached...
+
+            return;
+        }
+
+        // Is the current scale within the style scale range? 
+        if (!style.isScaleInRange(currentScale)) {
+            return;
+        }
+
+        // if the style is a polygon one, process it even if the polyline is not
+        // closed (by SLD specification)
+        if (style instanceof PolygonStyle2D) {
+            PolygonStyle2D ps2d = (PolygonStyle2D) style;
+            graphics.setPaint(ps2d.getFill());
+            graphics.setComposite(ps2d.getFillComposite());
+            graphics.fill(polyline);
+        }
+
+        if (style instanceof LineStyle2D) {
+            LineStyle2D ls2d = (LineStyle2D) style;
+            graphics.setPaint(ls2d.getContour());
+            graphics.setStroke(ls2d.getStroke());
+            graphics.setComposite(ls2d.getContourComposite());
+            graphics.draw(polyline);
+        }
+    }
+}
