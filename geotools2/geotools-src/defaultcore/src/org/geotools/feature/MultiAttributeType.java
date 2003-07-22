786c6c3408ff2651fe1ff18123e60d8a365c825e
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-07-22 19:18:32 +0000

    fixes of pmd and checkstyle errors
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3425 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/defaultcore/src/org/geotools/feature/MultiAttributeType.java
+++ geotools2/geotools-src/defaultcore/src/org/geotools/feature/MultiAttributeType.java
@@ -17,153 +17,163 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-
 package org.geotools.feature;
 
-import java.util.*;
-import com.vividsolutions.jts.geom.Geometry;
+import java.util.Iterator;
+import java.util.List;
+
 
 /**
- * Class to handle more than one occurance of an attribute.  There may 
- * be better ways to do this, but this seems to work.  
+ * Class to handle more than one occurance of an attribute.  There may  be
+ * better ways to do this, but this seems to work.
  *
- * @version $Id: MultiAttributeType.java,v 1.2 2003/07/17 07:09:53 ianschneider Exp $
- * @authr Chris Holmes
+ * @author Chris Holmes
+ * @version $Id: MultiAttributeType.java,v 1.3 2003/07/22 19:18:32 cholmesny Exp $
  */
 public class MultiAttributeType extends DefaultAttributeType {
-    
-    /** Name of this attribute. */
-    //private final String name;
-        
-    /** Class type of this attribute. */
-    //private final Class type;
-        
     /** Number of instances of this attribute in the schema. */
     private int maxOccur = 1;
-    
+
+    /** The AttributeType to check each object of the list against. */
     private AttributeType validator;
-    
+
+    /** The minimum number of occurances of this attribute to validate. */
     private int minOccur = 1;
 
     /**
-     * Constructor with name and type.
+     * Constructor with validator.
      *
-     * @param name Name of this attribute.
-     * @param type Class type of this attribute.
+     * @param validator Name of this attribute.
      */
     public MultiAttributeType(AttributeType validator) {
-	super(validator.getName(), validator.getType(),false);
-	this.validator = validator;
+        super(validator.getName(), List.class, false);
+        this.validator = validator;
     }
-    
+
     /**
-     * Constructor with geometry.
+     * Constructor with validator and maxOccurs
      *
-     * @param name Name of this attribute.
-     * @param type Class type of this attribute.
+     * @param validator Name of this attribute.
      * @param maxOccur Number of instances of this attribute in the schema.
      */
     public MultiAttributeType(AttributeType validator, int maxOccur) {
-	this(validator);
+        this(validator);
         this.maxOccur = maxOccur;
     }
-    
-    
-     /**
-     * Constructor with geometry.
+
+    /**
+     * Constructor with validator, minOccurs and maxOccurs
      *
-     * @param name Name of this attribute.
-     * @param type Class type of this attribute.
+     * @param validator Name of this attribute.
      * @param maxOccur Number of instances of this attribute in the schema.
+     * @param minOccur Class type of this attribute.
      */
     public MultiAttributeType(AttributeType validator, int maxOccur,
-			      int minOccur) {
-	this(validator, maxOccur);
-	this.minOccur = minOccur;
+        int minOccur) {
+        this(validator, maxOccur);
+        this.minOccur = minOccur;
     }
 
-
-
-    /**
-     * Gets the name of this attribute.
-     * 
-     * @return Name.
-     */
-    /*public String getName() {
-        return validator.getName();
-	}*/
-    
-    /**
-     * Gets the type of this attribute.
-     * 
-     * @return Type.
-     */
-    //public Class getType() {
-    //  return validator.getType();
-    //}
-    
     /**
      * Gets the maxOccur of this attribute.
-     * 
+     *
      * @return MaxOccur.
      */
     public int getMaxOccurs() {
         return maxOccur;
     }
-    
-     /**
-     * Gets the minimum number of elements that pass the validator
-     * that must be in the list to validate.
-     * 
+
+    /**
+     * Gets the minimum number of elements that pass the validator that must be
+     * in the list to validate.
+     *
      * @return MaxOccur.
      */
     public int getMinOccurs() {
         return minOccur;
     }
 
-
-
-
-
     /**
-     * Returns whether the attribute is a geometry.
-     * Should this be false?  Even if the attributes are geometries?
-     * Because this itself isn't actually a geometry, so it can't be used
-     * as a geometry.
+     * Returns whether the attribute is a geometry. Should this be false?  Even
+     * if the attributes are geometries? Because this itself isn't actually a
+     * geometry, so it can't be used as a geometry.
+     *
      * @return true if the attribute's type is a geometry.
      */
-    public boolean isGeometry(){
-	return false;
-	//return Geometry.class.isAssignableFrom(getType());
+    public boolean isGeometry() {
+        return false;
     }
-    
-    //revisit:
-    public Object clone()
-        throws CloneNotSupportedException {
+
+    /**
+     * Returns a clone of this object.
+     *
+     * @return a copy of this attribute type.
+     *
+     * @throws CloneNotSupportedException if clone is not supported.
+     */
+    public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
 
-    public boolean isValid(Object obj){
-	if ((obj == null) && nillable) {
-	    return true;
-	} else {
-	    if (obj instanceof List) {
-		int i = 0;
-		for (Iterator iter = ((List)obj).iterator(); 
-		     iter.hasNext(); i++){
-           try {
-		    validator.validate(iter.next()); 
-        } catch (IllegalArgumentException iae) {
-          return false;
+    /**
+     * Whether the tested object is a Feature and its attributes validate
+     * against the featureType.   An IllegalArgumentException reporting the
+     * error in validation is thrown if validation fails..
+     *
+     * @param attribute The object to be tested for validity.
+     *
+     * @throws IllegalArgumentException if the object does not validate.
+     */
+    public void validate(Object attribute) throws IllegalArgumentException {
+        super.validate(attribute);
+
+        if (attribute instanceof List) {
+            int occurs = ((List) attribute).size();
+
+            if (occurs < minOccur) {
+                String mesg = "The list of attributes is " + occurs + " long."
+                    + "  It must not be less than minOccurs: " + minOccur;
+                throw new IllegalArgumentException(mesg);
+            }
+
+            if (occurs > maxOccur) {
+                String mesg = "The list of attributes is " + occurs + " long."
+                    + "  It must not be greater than maxOccurs: " + maxOccur;
+                throw new IllegalArgumentException(mesg);
+            }
+
+            for (Iterator iter = ((List) attribute).iterator(); 
+                 iter.hasNext();) {
+                validator.validate(iter.next());
+            }
+        } else {
+            //REVISIT: allow just one if it's not in a list?
+            String msg = attribute.getClass().getName()
+                + " is not an accetable"
+                + " class for a multiAttributeType.  Must be of type List";
+
+            throw new IllegalArgumentException(msg);
         }
-			
-		    }
-		}
-		//TODO: checking occurancs here i < minOccurs, i > maxOccurs.
-		//need to work out default values for them.
-		return true;
-	    } 
-	
+    }
+
+    /**
+     * If a single object is passed in then it is parsed into a list with just
+     * it as the element.  If an array is passed in then it is turned into a
+     * list.
+     *
+     * @param value the object to attempt parsing of.
+     *
+     * @return <code>value</code> converted to the preferred storage of this
+     *         <code>AttributeType</code>.  If no parsing was possible then
+     *         the same object is returned.
+     *
+     * @throws IllegalArgumentException if parsing is attempted and is
+     *         unsuccessful.
+     *
+     * @task REVISIT: implement this method as described in this comment.
+     */
+    public Object parse(Object value) throws IllegalArgumentException {
+        return value;
     }
 
     /**
@@ -173,11 +183,12 @@ public class MultiAttributeType extends DefaultAttributeType {
      */
     public String toString() {
         StringBuffer returnString = new StringBuffer("MultiAttributeType [ ");
-						     
+
         returnString.append("name=").append(name).append(',');
         returnString.append("type=").append(type.getName()).append(',');
         returnString.append("maxOccurs=").append(maxOccur).append(',');
         returnString.append("minOccur=").append(minOccur).append(" ]");
+
         return returnString.toString();
     }
 }
