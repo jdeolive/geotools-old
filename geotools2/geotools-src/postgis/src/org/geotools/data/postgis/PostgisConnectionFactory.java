3dab7542787db669b73529bd0abe7418282a82cd
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-07-24 19:15:41 +0000

    charSet added, code clean up, pmd and checkstyle fixes, cleaned up javadocs
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3478 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/postgis/src/org/geotools/data/postgis/PostgisConnectionFactory.java
+++ geotools2/geotools-src/postgis/src/org/geotools/data/postgis/PostgisConnectionFactory.java
@@ -1,10 +1,11 @@
 /*
- *    Geotools - OpenSource mapping toolkit
- *    (C) 2002, Centre for Computational Geography
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; 
+ *    License as published by the Free Software Foundation;
  *    version 2.1 of the License.
  *
  *    This library is distributed in the hope that it will be useful,
@@ -12,45 +13,46 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *    
  */
-
 package org.geotools.data.postgis;
 
-import java.io.*;
-import java.util.*;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 
 /**
- * Shell for JDBC transactions of all types.
+ * Shell for JDBC transactions of all types. This creates connections for the
+ * postgis datasource to make its  transactions.  To create a
+ * PostgisDataSource, create a  PostgisConnectionFactory, then call
+ * getConnection(), and pass that connection to the PostgisDataSource
+ * constructor.
  *
- * This creates connections for the postgis datasource to make its 
- * transactions.  To create a PostgisDataSource, create a 
- * PostgisConnectionFactory, then call getConnection(), and pass
- * that connection to the PostgisDataSource constructor.
- *
- * @version $Id: PostgisConnectionFactory.java,v 1.2 2003/02/12 17:09:25 cholmesny Exp $
- * @author Rob Hranac, Vision for New York 
+ * @author Rob Hranac, Vision for New York
  * @author Chris Holmes, TOPP
+ * @version $Id: PostgisConnectionFactory.java,v 1.3 2003/07/24 19:15:41 cholmesny Exp $
+ *
+ * @task REVISIT: connection pooling, implementing java.sql.Datasource.  I
+ *       removed the implementing because that class should be provided by the
+ *       vendor, not a hack on top of DriverManager.  The only problem is that
+ *       we can't use the postgres one directly, because it doesn't allow you
+ *       to set the charset, which some of our users need.
  */
-public class PostgisConnectionFactory implements javax.sql.DataSource {
-
+public class PostgisConnectionFactory {
     /** Standard logging instance */
-    private static final Logger LOGGER = 
-        Logger.getLogger("org.geotools.defaultcore");
-    
-    /** Creates PostGIS-specific JDBC driver class. */ 
-    private static final String POSTGIS_DRIVER_CLASS = "org.postgresql.Driver";
-    /** Creates PostGIS-specific JDBC driver path. */ 
-    private static final String POSTGIS_DRIVER_PATH = "jdbc:postgresql";
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.geotools.defaultcore");
+
+    /** Creates PostGIS-specific JDBC driver class. */
+    private static final String DRIVER_CLASS = "org.postgresql.Driver";
+
+    /** Creates PostGIS-specific JDBC driver path. */
+    private static final String DRIVER_PATH = "jdbc:postgresql";
 
     /** The full path used to connect to the database */
-    private String connectionPath;
+    private String connPath;
 
     /** The user to connect with */
     private String user = "test";
@@ -58,112 +60,118 @@ public class PostgisConnectionFactory implements javax.sql.DataSource {
     /** The password of the user */
     private String password = "test";
 
-    
+    /** An alternate character set to use. */
+    private String charSet;
+
     /**
-     * Constructor with all internal database driver classes, driver paths
-     * and database types.
+     * Constructor with all internal database driver classes, driver paths and
+     * database types.
      *
      * @param host The name or ip address of the computer where the postgis
-     * database is installed.
+     *        database is installed.
      * @param port The port to connect on; 5432 is generally the postgres
-     * default.
-     * @param dbName The name of the pgsql database that holds the tables
-     * of the feature types that will be used by PostgisDataSource.
-     */ 
-    public PostgisConnectionFactory (String host, String port, String dbName) {
-        connectionPath = 
-            POSTGIS_DRIVER_PATH + "://" + host + ":" + port + "/" + dbName;
+     *        default.
+     * @param dbName The name of the pgsql database that holds the tables of
+     *        the feature types that will be used by PostgisDataSource.
+     */
+    public PostgisConnectionFactory(String host, String port, String dbName) {
+        connPath = DRIVER_PATH + "://" + host + ":" + port + "/"
+            + dbName;
     }
-    
+
     /**
-     * Constructor with all internal database driver classes, driver paths
-     * and database types.
+     * Constructor with all internal database driver classes, driver paths and
+     * database types.
      *
-     * @param connectionPath The driver class; should be passed from the
-     * database-specific subclass.
-     */ 
-    public PostgisConnectionFactory (String connectionPath) {
-        connectionPath = POSTGIS_DRIVER_PATH + "://" + connectionPath;
+     * @param connPath The driver class; should be passed from the
+     *        database-specific subclass.
+     */
+    public PostgisConnectionFactory(String connPath) {
+        connPath = DRIVER_PATH + "://" + connPath;
     }
-    
-    
+
     /**
-     * Creates a database connection method to initialize a given database
-     * for feature extraction.
+     * Creates a database connection method to initialize a given database for
+     * feature extraction.
      *
      * @param user the name of the user connect to connect to the pgsql db.
      * @param password the password for the user.
-     */ 
-    public void setLogin( String user, String password) {
+     */
+    public void setLogin(String user, String password) {
         this.user = user;
         this.password = password;
     }
 
     /**
-     * Creates a database connection method to initialize a given database
-     * for feature extraction using the set username and password.
+     * Sets a different character set for the postgis driver to use.
+     *
+     * @param charSet the string of a valid charset name.
+     */
+    public void setCharSet(String charSet) {
+        this.charSet = charSet;
+    }
+
+    /**
+     * Creates a database connection method to initialize a given database for
+     * feature extraction using the set username and password.
      *
      * @return the sql Connection object to the database.
-     */ 
-    public Connection getConnection()
-        throws SQLException {
+     *
+     * @throws SQLException if the postgres driver could not be found
+     */
+    public Connection getConnection() throws SQLException {
         return getConnection(user, password);
     }
 
-
     /**
-     * Creates a database connection method to initialize a given database
-     * for feature extraction with the user and password params.
+     * Creates a database connection method to initialize a given database for
+     * feature extraction with the user and password params.
      *
      * @param user the name of the user connect to connect to the pgsql db.
      * @param password the password for the user.
-     */ 
+     *
+     * @return the sql Connection object to the database.
+     *
+     * @throws SQLException if the postgis sql driver could not be found
+     */
     public Connection getConnection(String user, String password)
         throws SQLException {
-	Properties props = new Properties();
-	props.put("user", user);
-	props.put("password", password);
-	return getConnection(props);
+        Properties props = new Properties();
+        props.put("user", user);
+        props.put("password", password);
+
+        if (charSet != null) {
+            props.put("charSet", charSet);
+        }
+
+        return getConnection(props);
     }
-    
+
     /**
-     * Creates a database connection method to initialize a given database
-     * for feature extraction with the given Properties.
+     * Creates a database connection method to initialize a given database for
+     * feature extraction with the given Properties.
      *
-     * @param props Should contain at a minimum the user and password.  
-     * Additional properties, such as charSet, can also be added.
+     * @param props Should contain at a minimum the user and password.
+     *        Additional properties, such as charSet, can also be added.
+     *
+     * @return the sql Connection object to the database.
+     *
+     * @throws SQLException if the postgis sql driver could not be found
      */
     public Connection getConnection(Properties props) throws SQLException {
-	  // makes a new feature type bean to deal with incoming
+        // makes a new feature type bean to deal with incoming
         Connection dbConnection = null;
-        
+
         // Instantiate the driver classes
         try {
-            Class.forName(POSTGIS_DRIVER_CLASS);
-	    dbConnection = DriverManager.getConnection( connectionPath , props);
-        }
-        catch (ClassNotFoundException e) {
+            Class.forName(DRIVER_CLASS);
+            LOGGER.finest("getting connection at " + connPath + "with props: "
+                + props);
+            dbConnection = DriverManager.getConnection(connPath, props);
+        } catch (ClassNotFoundException cnfe) {
             throw new SQLException("Postgis driver was not found.");
         }
 
         return dbConnection;
     }
-    
-
-    public int getLoginTimeout() {
-        return 10;
-    }
-
-    public void setLoginTimeout(int seconds) {
-    }
-
-    public PrintWriter getLogWriter() {
-        return new PrintWriter(System.out);
-    }
-
-    public void setLogWriter(PrintWriter out) {
-    }
-
-
-
 }
--- geotools2/geotools-src/postgis/src/org/geotools/data/postgis/PostgisDataSourceFactory.java
+++ geotools2/geotools-src/postgis/src/org/geotools/data/postgis/PostgisDataSourceFactory.java
@@ -1,83 +1,142 @@
 /*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
+/*
  * PostgisDataSourceFactory.java
  *
  * Created on March 5, 2003, 10:59 AM
  */
-
 package org.geotools.data.postgis;
 
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.sql.SQLException;
-import java.util.Map;
-import java.util.HashMap;
 import org.geotools.data.DataSource;
 import org.geotools.data.DataSourceException;
+import java.sql.SQLException;
+import java.util.Map;
+
 
 /**
+ * Creates a PostgisDataSource with the correct params.  This factory should be
+ * registered in the META-INF/ folder, under services/ in the
+ * DataSourceFactorySpi file.  This will allow DataSourceFinder to create
+ * PostgisDataSources with the correct params.
  *
- * @author  jamesm
+ * @author James Macgill, PSU
+ * @version $Id: PostgisDataSourceFactory.java,v 1.3 2003/07/24 19:15:41 cholmesny Exp $
  */
-public class PostgisDataSourceFactory implements org.geotools.data.DataSourceFactorySpi {
-    
-    /** Creates a new instance of GMLDataSourceFactory */
+public class PostgisDataSourceFactory
+    implements org.geotools.data.DataSourceFactorySpi {
+    /**
+     * Creates a new instance of GMLDataSourceFactory
+     */
     public PostgisDataSourceFactory() {
     }
-    
+
+    /**
+     * Checks to see if all the postgis params are there.  Should have  dbtype
+     * equal to postgis, as well as host, user, passwd, database, and table.
+     *
+     * @param params The full set of information needed to construct a live
+     *        data source.
+     *
+     * @return <code>true</code> if dbtype equals postgis, and contains keys
+     *         for host, user, passwd, database, and table.
+     */
     public boolean canProcess(Map params) {
-        if(!params.containsKey("dbtype")){
+        if (!params.containsKey("dbtype")) {
             return false;
         }
-        if(!((String)params.get("dbtype")).equalsIgnoreCase("postgis")){
+
+        if (!((String) params.get("dbtype")).equalsIgnoreCase("postgis")) {
             return false;
         }
-        if(!params.containsKey("host")){
+
+        if (!params.containsKey("host")) {
             return false;
         }
-        if(!params.containsKey("user")){
+
+        if (!params.containsKey("user")) {
             return false;
         }
-        if(!params.containsKey("passwd")){
+
+        if (!params.containsKey("passwd")) {
             return false;
         }
-        if(!params.containsKey("database")){
+
+        if (!params.containsKey("database")) {
             return false;
         }
-        if(!params.containsKey("table")){
+
+        if (!params.containsKey("table")) {
             return false;
         }
+
         return true;
     }
-    
-    public DataSource createDataSource(Map params)  throws DataSourceException {
-        if(!canProcess(params)){
+
+    /**
+     * Construct a live data source using the params specifed.
+     *
+     * @param params The full set of information needed to construct a live
+     *        data source.  Should have  dbtype equal to postgis, as well as
+     *        host, user, passwd, database, and table.
+     *
+     * @return The created DataSource, this may be null if the required
+     *         resource was not found or if insufficent parameters were given.
+     *         Note that canProcess() should have returned false if the
+     *         problem is to do with insuficent parameters.
+     *
+     * @throws DataSourceException Thrown if there were any problems creating
+     *         or connecting the datasource.
+     */
+    public DataSource createDataSource(Map params) throws DataSourceException {
+        if (!canProcess(params)) {
             return null;
         }
-        String host = (String)params.get("host");
-        String user = (String)params.get("user");
-        String passwd = (String)params.get("passwd");
-        String port = (String)params.get("port");
-        String database = (String)params.get("database");
-        String table = (String)params.get("table");
-        
-        
-        PostgisConnectionFactory db =
-        new PostgisConnectionFactory(host, port, database);
-        try{
-            db.setLogin(user, passwd);            
-            PostgisDataSource ds = new PostgisDataSource(db.getConnection(), table);
-            return ds;
+
+        String host = (String) params.get("host");
+        String user = (String) params.get("user");
+        String passwd = (String) params.get("passwd");
+        String port = (String) params.get("port");
+        String database = (String) params.get("database");
+        String table = (String) params.get("table");
+        String charSet = (String) params.get("charset");
+
+        PostgisConnectionFactory connFact = new PostgisConnectionFactory(host,
+                port, database);
+
+        try {
+            connFact.setLogin(user, passwd);
+            connFact.setCharSet(charSet);
+
+            PostgisDataSource pgds = new PostgisDataSource(connFact
+                    .getConnection(), table);
+
+            return pgds;
+        } catch (SQLException sqle) {
+            throw new DataSourceException("Unable to connect to database", sqle);
         }
-        catch(SQLException sqle){
-            throw new DataSourceException("Unable to connect to database",sqle);
-        }
-        
     }
-    
+
+    /**
+     * Describe the nature of the datasource constructed by this factory.
+     *
+     * @return A human readable description that is suitable for inclusion in a
+     *         list of available datasources.
+     */
     public String getDescription() {
         return "PostGIS spatial database";
     }
-    
 }
-
