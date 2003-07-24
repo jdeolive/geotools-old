e49437a36e389dc66755bcde90bd4c0b09703dd1
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-07-24 21:03:03 +0000

    better coverage for clover, jalopied
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3482 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/postgis/tests/unit/org/geotools/data/postgis/PostgisTestSuite.java
+++ geotools2/geotools-src/postgis/tests/unit/org/geotools/data/postgis/PostgisTestSuite.java
@@ -1,61 +1,49 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
 package org.geotools.data.postgis;
 
-import java.util.*;
-import junit.framework.*;
 import com.vividsolutions.jts.geom.*;
-import java.util.logging.Logger;
+import junit.framework.*;
 import org.geotools.data.*;
 import org.geotools.feature.*;
+import java.util.*;
+import java.util.logging.Logger;
+
 
 public class PostgisTestSuite extends TestCase {
-
-     /**
-     * The logger for the filter module.
-     */
-    private static final Logger LOGGER = Logger.getLogger("org.geotools.postgis");
-
+    /** The logger for the filter module. */
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.geotools.postgis");
     DataSource postgis = null;
-
     FeatureCollection collection = FeatureCollections.newCollection();
 
-    public PostgisTestSuite(String testName){
+    public PostgisTestSuite(String testName) {
         super(testName);
     }
 
     public static void main(String[] args) {
         junit.textui.TestRunner.run(suite());
     }
-    
+
     public static Test suite() {
         TestSuite suite = new TestSuite("All postgis tests");
         suite.addTestSuite(PostgisTest.class);
+        suite.addTestSuite(ServiceTest.class);
+
         return suite;
     }
-
-    /*    
-    public void setUp() {
-        _log.info("creating postgis connection...");
-        PostgisConnection db = new PostgisConnection ("feathers.leeds.ac.uk","5432","postgis_test"); 
-        _log.info("created new db connection");
-        db.setLogin("postgis_ro","postgis_ro");
-        _log.info("set the login");
-        postgis = new DataSourcePostgis(db, "testset");
-        _log.info("created new datasource");
-    }
-
-
-    public void testImport() {
-        _log.info("starting type enforcement tests...");
-        try {
-            postgis.importFeatures(collection, new EnvelopeExtent());
-        }
-        catch(DataSourceException e) {
-            _log.info("...threw data source exception");      
-            this.fail("...threw data source exception");  
-        }
-        assertEquals(18, collection.getFeatures().length);
-        _log.info("...ending type enforcement tests");
-    }
-    */
-
 }
