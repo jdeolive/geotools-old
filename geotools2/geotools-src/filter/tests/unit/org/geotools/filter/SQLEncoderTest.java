35ff9a6ca84c3c1a4caa6f8cb669f55a0da59d61
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-07-30 22:40:06 +0000

    got rid of info logging, used filter.toString for better reporting.  Still need to automate this more, do the actual string comparisons
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3553 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/filter/tests/unit/org/geotools/filter/SQLEncoderTest.java
+++ geotools2/geotools-src/filter/tests/unit/org/geotools/filter/SQLEncoderTest.java
@@ -18,19 +18,20 @@ package org.geotools.filter;
 
 import junit.framework.*;
 import org.w3c.dom.*;
+import java.io.StringWriter;
 import java.util.logging.Logger;
 import javax.xml.parsers.*;
 
 
 /**
- * Unit test for expressions.  This is a complimentary test suite with the
- * filter test suite.
+ * Unit test for sql encoding of filters into where statements.
  *
  * @author Chris Holmes, TOPP
+ *
+ * @task REVISIT: validate these so we know if they break.
  */
 public class SQLEncoderTest extends FilterTestSupport {
-
-
+    private FilterFactory filterFac = FilterFactory.createFilterFactory();
 
     /** Test suite for this test case */
     TestSuite suite = null;
@@ -41,15 +42,15 @@ public class SQLEncoderTest extends FilterTestSupport {
 
     public SQLEncoderTest(String testName) {
         super(testName);
-        LOGGER.info("running SQLEncoderTests");
-        ;
+        LOGGER.finer("running SQLEncoderTests");
+
         dataFolder = System.getProperty("dataFolder");
 
         if (dataFolder == null) {
             //then we are being run by maven
             dataFolder = System.getProperty("basedir");
-            dataFolder = "file:////" + dataFolder + "/tests/unit/testData"; 
-            LOGGER.fine("data folder is " + dataFolder);
+            dataFolder = "file:////" + dataFolder + "/tests/unit/testData";
+            LOGGER.finer("data folder is " + dataFolder);
         }
     }
 
@@ -76,20 +77,25 @@ public class SQLEncoderTest extends FilterTestSupport {
     }
 
     public void test1() throws Exception {
-        Filter test = parseDocument(dataFolder + "/test1.xml");
-        LOGGER.fine("parsed filter is: " + test);
+        Filter test = testEncode(dataFolder + "/test1.xml");
+
+        //LOGGER.fine("parsed filter is: " + test);
     }
 
     public void test2() throws Exception {
-        Filter test = parseDocument(dataFolder + "/test2.xml");
-        LOGGER.fine("parsed filter is: " + test);
+        Filter test = testEncode(dataFolder + "/test2.xml");
+
+        //LOGGER.fine("parsed filter is: " + test);
     }
 
     public void test3a() throws Exception {
         try {
-            Filter test = parseDocument(dataFolder + "/test3a.xml");
-            LOGGER.fine("parsed filter is: " + test);
+            Filter test = testEncode(dataFolder + "/test3a.xml");
+
+            //LOGGER.fine("parsed filter is: " + test);
         } catch (SQLEncoderException e) {
+            LOGGER.fine("successfully caught exception: " + e);
+
             //contains geom, should not be supported
             String expectMessage = "Filter type not supported";
             assertTrue(expectMessage.equals(e.getMessage()));
@@ -97,34 +103,57 @@ public class SQLEncoderTest extends FilterTestSupport {
     }
 
     public void test8() throws Exception {
-        Filter test = parseDocument(dataFolder + "/test8.xml");
-        LOGGER.fine("parsed filter is: " + test);
+        Filter test = testEncode(dataFolder + "/test8.xml");
+
+        //LOGGER.fine("parsed filter is: " + test);
     }
 
     public void test9() throws Exception {
-        Filter test = parseDocument(dataFolder + "/test9.xml");
-        LOGGER.fine("parsed filter is: " + test);
+        Filter test = testEncode(dataFolder + "/test9.xml");
+
+        //LOGGER.fine("parsed filter is: " + test);
     }
 
     /*public void test11() //like filters, uncomment when they are supported
        throws Exception {
-       Filter test = parseDocument(dataFolder+"/test11.xml");
+       Filter test = testEncode(dataFolder+"/test11.xml");
        } */
     public void test13() throws Exception {
-        Filter test = parseDocument(dataFolder + "/test13.xml");
-        LOGGER.fine("parsed filter is: " + test);
+        Filter test = testEncode(dataFolder + "/test13.xml");
+
+        //LOGGER.fine("parsed filter is: " + test);
+    }
+
+    public void testConstructor() throws Exception {
+        NullFilter tFilter = filterFac.createNullFilter();
+
+        Integer testInt = new Integer(5);
+        Expression testAtt = filterFac.createAttributeExpression(null, "test");
+        tFilter.nullCheckValue(testAtt);
+
+        //tFilter.addRightValue(testLiteral);
+        LogicFilter notFilter = filterFac.createLogicFilter(AbstractFilter.LOGIC_NOT);
+        notFilter.addFilter(tFilter);
+
+        StringWriter output = new StringWriter();
+        SQLEncoder encoder = new SQLEncoder(output, notFilter);
+        LOGGER.fine("test filter is " + notFilter + "\n encoding result is "
+            + output.getBuffer());
     }
 
     /* public void test14() contains geom filter, not supported
        throws Exception {
-       Filter test = parseDocument(dataFolder+"/test14.xml");
+       Filter test = testEncode(dataFolder+"/test14.xml");
        } */
-    public Filter parseDocument(String uri) throws Exception {
+
+    //TODO: changes to testEncode(String uri, String expected) so that 
+    //we actually assert things, so tests break if things mess up.
+    public Filter testEncode(String uri) throws Exception {
         Filter filter = null;
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document dom = db.parse(uri);
-        LOGGER.info("exporting " + uri);
+        LOGGER.fine("exporting " + uri);
 
         // first grab a filter node
         NodeList nodes = dom.getElementsByTagName("Filter");
@@ -138,21 +167,21 @@ public class SQLEncoderTest extends FilterTestSupport {
                 child = list.item(i);
 
                 //_log.getLoggerRepository().setThreshold(Level.INFO);
-                if ((child == null) ||
-                        (child.getNodeType() != Node.ELEMENT_NODE)) {
+                if ((child == null)
+                        || (child.getNodeType() != Node.ELEMENT_NODE)) {
                     continue;
                 }
 
                 filter = FilterDOMParser.parseFilter(child);
 
                 //_log.getLoggerRepository().setThreshold(Level.DEBUG);
-                LOGGER.finer("filter: " + filter.getClass().toString());
+                LOGGER.fine("filter: " + filter.toString());
 
                 //StringWriter output = new StringWriter();
                 SQLEncoder encoder = new SQLEncoder();
                 String out = encoder.encode((AbstractFilter) filter);
 
-                LOGGER.finer("Resulting SQL filter is \n" + out);
+                LOGGER.fine("Resulting SQL filter is \n" + out);
             }
         }
 
