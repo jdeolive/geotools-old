96a8c2aac6d23c12a0afb64dd518687ecea965fd
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-07-30 22:27:48 +0000

    updates for pmd and checkstyle
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3550 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/filter/src/org/geotools/filter/XMLEncoder.java
+++ geotools2/geotools-src/filter/src/org/geotools/filter/XMLEncoder.java
@@ -16,29 +16,56 @@
  */
 package org.geotools.filter;
 
-import com.vividsolutions.jts.geom.*;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+
+// J2SE dependencies 
 import java.io.PrintWriter;
 import java.io.Writer;
-
-// J2SE dependencies
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 
 /**
- * Exports a filter as a OGC XML Filter document.
+ * Exports a filter as a OGC XML Filter document.  This class is does not
+ * generate namespace compliant xml, even though it does print gml prefixes.
+ * It was also written before the 1.0 filter spec, so some of it may be not up
+ * to date.
  *
- * @author jamesm
+ * @author James Macgill, PSU
  *
- * @task HACK: Attrrbutes are not yet supported
+ * @task HACK: Logging errors, very bad!  We need a filter visitor exception,
+ *       or have visit methods throw illegal filter exceptions, or io
+ *       exceptions.
  * @task TODO: Support full header information for new XML file
+ * @task REVISIT: make filter utils class so that other encoders (like sql). It
+ *       could also be nice to refactor common code from gml producer, as
+ *       there is basically a GeometryProducer there.
+ * @task REVISIT: make namespace aware.
  */
 public class XMLEncoder implements org.geotools.filter.FilterVisitor {
     /** The logger for the filter module. */
     private static final Logger LOGGER = Logger.getLogger("org.geotools.filter");
-    private static java.util.HashMap comparisions = new java.util.HashMap();
-    private static java.util.HashMap spatial = new java.util.HashMap();
-    private static java.util.HashMap logical = new java.util.HashMap();
-    private static java.util.HashMap expressions = new java.util.HashMap();
+
+    /** Map of comparison types to sql representation */
+    private static Map comparisions = new HashMap();
+
+    /** Map of spatial types to sql representation */
+    private static Map spatial = new HashMap();
+
+    /** Map of logical types to sql representation */
+    private static Map logical = new HashMap();
+
+    /** Map of expression types to sql representation */
+    private static Map expressions = new HashMap();
 
     static {
         comparisions.put(new Integer(AbstractFilter.COMPARE_EQUALS),
@@ -80,8 +107,14 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         logical.put(new Integer(AbstractFilter.LOGIC_NOT), "Not");
     }
 
+    /** To write the xml representations of filters to */
     private Writer out;
 
+    /**
+     * Constructor with writer to write filters to.
+     *
+     * @param out where to write the xml representation of filters.
+     */
     public XMLEncoder(Writer out) {
         this.out = out;
     }
@@ -102,12 +135,24 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * Encodes the filter to the current writer.
+     *
+     * @param filter the filter to encode.
+     *
+     * @throws java.io.IOException if there are problems writing to out.
+     */
     public void encode(Filter filter) throws java.io.IOException {
         out.write("<Filter>\n");
         filter.accept(this);
         out.write("</Filter>\n");
     }
 
+    /**
+     * Encodes the expression to the current writer.
+     *
+     * @param expression the expression to encode.
+     */
     public void encode(Expression expression) {
         expression.accept(this);
     }
@@ -123,6 +168,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         LOGGER.warning("exporting unknown filter type");
     }
 
+    /**
+     * Writes the xml representation of a Between filter.
+     *
+     * @param filter the between filter to encode.
+     */
     public void visit(BetweenFilter filter) {
         LOGGER.finer("exporting BetweenFilter");
 
@@ -150,6 +200,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * Writes the xml representation of a like filter.
+     *
+     * @param filter the like filter to encode.
+     */
     public void visit(LikeFilter filter) {
         LOGGER.finer("exporting like filter");
 
@@ -158,7 +213,7 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
             String wcs = filter.getWildcardSingle();
             String esc = filter.getEscape();
             out.write("<PropertyIsLike wildCard=\"" + wcm + "\" singleChar=\""
-                + wcs + "\" escapeChar=\"" + esc + "\">\n");
+                + wcs + "\" escape=\"" + esc + "\">\n");
             ((Expression) filter.getValue()).accept(this);
             out.write("<Literal>\n" + filter.getPattern() + "\n</Literal>\n");
             out.write("</PropertyIsLike>\n");
@@ -167,6 +222,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * Writes the xml representation of a logic filter.
+     *
+     * @param filter the logic filter to encode.
+     */
     public void visit(LogicFilter filter) {
         LOGGER.finer("exporting LogicFilter");
 
@@ -189,6 +249,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * Writes the xml representation of a compare filter.
+     *
+     * @param filter the compare filter to encode.
+     */
     public void visit(CompareFilter filter) {
         LOGGER.finer("exporting ComparisonFilter");
 
@@ -213,6 +278,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * Writes the xml representation of a geometry filter.
+     *
+     * @param filter the geometry filter to encode.
+     */
     public void visit(GeometryFilter filter) {
         LOGGER.finer("exporting GeometryFilter");
 
@@ -230,6 +300,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * Writes the xml representation of a null filter.
+     *
+     * @param filter the null filter to encode.
+     */
     public void visit(NullFilter filter) {
         LOGGER.finer("exporting NullFilter");
 
@@ -247,6 +322,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * Writes the xml representation of a fid filter.
+     *
+     * @param filter the fid filter to encode.
+     */
     public void visit(FidFilter filter) {
         LOGGER.finer("exporting FidFilter");
 
@@ -255,12 +335,17 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         for (int i = 0; i < fids.length; i++) {
             try {
                 out.write("<FeatureId fid=\"" + fids[i] + "\"/>");
-            } catch (Exception e) {
-                LOGGER.warning("Unable to export filter" + e);
+            } catch (java.io.IOException ioe) {
+                LOGGER.warning("Unable to export filter" + ioe);
             }
         }
     }
 
+    /**
+     * Writes the xml representation of an Attribute expression.
+     *
+     * @param expression the attribute expression to encode.
+     */
     public void visit(AttributeExpression expression) {
         LOGGER.finer("exporting ExpressionAttribute");
 
@@ -272,6 +357,13 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * This should never be called.  This can only happen if a subclass of
+     * DefaultExpression fails to implement its own version of
+     * accept(FilterVisitor);
+     *
+     * @param expression the expression to encode.
+     */
     public void visit(Expression expression) {
         LOGGER.warning("exporting unknown (default) expression");
     }
@@ -301,6 +393,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * Writes the xml representation of a  expression.
+     *
+     * @param expression the expression to encode.
+     */
     public void visit(MathExpression expression) {
         LOGGER.finer("exporting Expression Math");
 
@@ -316,6 +413,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
+    /**
+     * Writes the xml representation of a  expression.
+     *
+     * @param expression the expression to encode.
+     */
     public void visit(FunctionExpression expression) {
         LOGGER.finer("exporting Expression Math");
 
@@ -336,19 +438,44 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
         }
     }
 
-    class GeometryEncoder {
+    /**
+     * Encodes geometries for filters.
+     *
+     * @task REVISIT: combine with gml producer code if possible (may not be,
+     *       but it should be investigated).
+     */
+    static class GeometryEncoder {
+        /** HACK!?!  The srs isn't always 4326, is it? */
         private String srs = "epsg:4326";
+
+        /** The writer where the gml should print to. */
         private PrintWriter out;
 
+        /**
+         * Constructor with writer to write to.
+         *
+         * @param out where to write the geometry.
+         */
         public GeometryEncoder(Writer out) {
             this.out = new PrintWriter(out);
         }
 
+        /**
+         * Constructor with writer to write to and srs.
+         *
+         * @param srs The spatial reference system id of the geometry.
+         * @param out where to write the geometry.
+         */
         public GeometryEncoder(String srs, Writer out) {
             this.out = new PrintWriter(out);
             this.srs = srs;
         }
 
+        /**
+         * Encodes the geometry, delegating to the appropriate sub method.
+         *
+         * @param geom the geometry to encode.
+         */
         public void encode(Geometry geom) {
             Class geomType = geom.getClass();
 
@@ -369,6 +496,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
             }
         }
 
+        /**
+         * Encodes the coordinates to gml
+         *
+         * @param coords the coordinates to encode.
+         */
         public void encode(Coordinate[] coords) {
             out.print("<gml:coordinates>");
 
@@ -380,18 +512,33 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
             out.println("</gml:coordinates>");
         }
 
+        /**
+         * Encodes the point to gml
+         *
+         * @param point the point to encode.
+         */
         public void encode(Point point) {
             out.println("<gml:Point srsName=\"" + srs + "\">");
             encode(point.getCoordinates());
             out.println("</gml:Point>");
         }
 
+        /**
+         * Encodes the LineString to gml
+         *
+         * @param line the LineString to encode.
+         */
         public void encode(LineString line) {
             out.println("<gml:LineString srsName=\"" + srs + "\">");
             encode(line.getCoordinates());
             out.println("</gml:LineString>");
         }
 
+        /**
+         * Encodes the Polygon to gml
+         *
+         * @param polygon the Polygon to encode.
+         */
         public void encode(Polygon polygon) {
             out.println("<gml:Polygon srsName=\"" + srs + "\">");
             out.println("<gml:outerBoundaryIs>");
@@ -407,6 +554,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
             out.println("</gml:Polygon>");
         }
 
+        /**
+         * Encodes the MultiPoint to gml
+         *
+         * @param mpoint the MultiPoint to encode.
+         */
         public void encode(MultiPoint mpoint) {
             out.println("<gml:MultiPoint srsName=\"" + srs + "\">\n");
 
@@ -417,6 +569,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
             out.println("</gml:MultiPoint>\n");
         }
 
+        /**
+         * Encodes the MultiLineString to gml
+         *
+         * @param mline the MultiLineString to encode.
+         */
         public void encode(MultiLineString mline) {
             out.println("<gml:MultiLineString srsName=\"" + srs + "\">\n");
 
@@ -427,6 +584,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
             out.println("</gml:MultiLineString>\n");
         }
 
+        /**
+         * Encodes the MultiPolygon to gml
+         *
+         * @param mpolygon the MultiPolygon to encode.
+         */
         public void encode(MultiPolygon mpolygon) {
             out.println("<gml:MultiPolygon srsName=\"" + srs + "\">\n");
 
@@ -437,6 +599,11 @@ public class XMLEncoder implements org.geotools.filter.FilterVisitor {
             out.println("</gml:MultiPolygon>\n");
         }
 
+        /**
+         * Encodes the Geometry Collection to gml
+         *
+         * @param geomcoll the collection to encode
+         */
         public void encode(GeometryCollection geomcoll) {
             out.println("<gml:MultiGeometry srsName=\"" + srs + "\">\n");
 
