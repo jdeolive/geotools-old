d1e67ba1cb4cebac4ab803f37cce1a3c11f2d196
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-07-31 18:01:03 +0000

    changed to assertEquals, added a few tests - ors should have more though, got rid of info logging
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3562 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/filter/tests/unit/org/geotools/filter/SQLUnpackerTest.java
+++ geotools2/geotools-src/filter/tests/unit/org/geotools/filter/SQLUnpackerTest.java
@@ -1,4 +1,20 @@
 /*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
+/*
  *    Geotools - OpenSource mapping toolkit
  *    (C) 2002, Centre for Computational Geography
  *
@@ -17,28 +33,26 @@
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-
 package org.geotools.filter;
 
-
-import junit.framework.*;
 import com.vividsolutions.jts.geom.*;
-import java.util.logging.Logger;
+import junit.framework.*;
 import org.geotools.feature.IllegalAttributeException;
 import org.geotools.feature.SchemaException;
+import java.util.logging.Logger;
 
 
 /**
- * Unit test for SQLUnpacker.  
+ * Unit test for SQLUnpacker.
  *
  * @author Chris Holmes, TOPP
  */
 public class SQLUnpackerTest extends FilterTestSupport {
-    
     /** Standard logging instance */
+
     //private static final Logger LOGGER = Logger.getLogger("org.geotools.defaultcore");
-    
-    /** Filters on which to perform tests*/
+
+    /** Filters on which to perform tests */
     private BetweenFilterImpl btwnFilter;
     private CompareFilterImpl compFilter;
     private GeometryFilterImpl geomFilter;
@@ -55,242 +69,277 @@ public class SQLUnpackerTest extends FilterTestSupport {
     private String wcMulti = "!";
     private String wcSingle = "_";
     private String escape = "#";
-    
-    /** Schema on which to preform tests */
-    //private static FeatureType testSchema = null;
-    
-    /** Schema on which to preform tests */
-    //private static Feature testFeature = null;
 
     /** capabilities for unpacker */
     private FilterCapabilities capabilities;
 
     //    private SQLEncoder encoder;
-
     private SQLUnpacker unpacker;
-    
+
     /** Test suite for this test case */
     TestSuite suite = null;
 
-    
     /**
      * Constructor with test name.
+     *
+     * @param testName DOCUMENT ME!
      */
-
     public SQLUnpackerTest(String testName) {
         super(testName);
-        LOGGER.info("running SQLUnpackerTests");
-        
+        LOGGER.finer("running SQLUnpackerTests");
     }
-    
+
     /**
      * Main for test runner.
+     *
+     * @param args DOCUMENT ME!
      */
     public static void main(String[] args) {
         junit.textui.TestRunner.run(suite());
     }
-    
+
     /**
      * Required suite builder.
+     *
      * @return A test suite for this unit test.
      */
     public static Test suite() {
-       
         //_log.getLoggerRepository().setThreshold(Level.DEBUG);
-        
         TestSuite suite = new TestSuite(SQLUnpackerTest.class);
-	return suite;
+
+        return suite;
     }
-    
-    
+
     /**
      * Sets up a schema and a test feature.
+     *
      * @throws SchemaException If there is a problem setting up the schema.
-     * @throws IllegalFeatureException If problem setting up the feature.
+     * @throws IllegalAttributeException If problem setting up the feature.
      */
     protected void setUp() throws SchemaException, IllegalAttributeException {
         //if(setup) return;
-	super.setUp();
-        setup=true;
+        super.setUp();
+        setup = true;
+
         //_log.getLoggerRepository().setThreshold(Level.INFO);
-	//Set capabilities for the SQLUnpacker
-	capabilities = new FilterCapabilities();
-	capabilities.addType(AbstractFilter.LOGIC_OR);
-	capabilities.addType(AbstractFilter.LOGIC_AND);
-	capabilities.addType(AbstractFilter.LOGIC_NOT);
-	capabilities.addType(AbstractFilter.COMPARE_EQUALS);
-	capabilities.addType(AbstractFilter.COMPARE_LESS_THAN);
-	capabilities.addType(AbstractFilter.COMPARE_GREATER_THAN);
-	capabilities.addType(AbstractFilter.COMPARE_LESS_THAN_EQUAL);
-	capabilities.addType(AbstractFilter.COMPARE_GREATER_THAN_EQUAL);
-	capabilities.addType(AbstractFilter.NULL);
-	capabilities.addType(AbstractFilter.BETWEEN);	
-	unpacker = new SQLUnpacker(capabilities);
+        //Set capabilities for the SQLUnpacker
+        capabilities = new FilterCapabilities();
+        capabilities.addType(AbstractFilter.LOGIC_OR);
+        capabilities.addType(AbstractFilter.LOGIC_AND);
+        capabilities.addType(AbstractFilter.LOGIC_NOT);
+        capabilities.addType(AbstractFilter.COMPARE_EQUALS);
+        capabilities.addType(AbstractFilter.COMPARE_LESS_THAN);
+        capabilities.addType(AbstractFilter.COMPARE_GREATER_THAN);
+        capabilities.addType(AbstractFilter.COMPARE_LESS_THAN_EQUAL);
+        capabilities.addType(AbstractFilter.COMPARE_GREATER_THAN_EQUAL);
+        capabilities.addType(AbstractFilter.NULL);
+        capabilities.addType(AbstractFilter.BETWEEN);
+        unpacker = new SQLUnpacker(capabilities);
 
+        try {
+            attrExp1 = new AttributeExpressionImpl(testSchema, "testInteger");
+            attrExp2 = new AttributeExpressionImpl(testSchema, "testGeometry");
+            litExp1 = new LiteralExpressionImpl(new Integer(65));
+            litExp2 = new LiteralExpressionImpl(new Integer(35));
+            mathExp1 = new MathExpressionImpl(DefaultExpression.MATH_ADD);
+            mathExp1.addLeftValue(litExp1);
+            mathExp1.addRightValue(litExp2);
 
-	try {
-	attrExp1 = new AttributeExpressionImpl(testSchema, "testInteger");
-	attrExp2 = new AttributeExpressionImpl(testSchema, "testGeometry");
-	litExp1 = new LiteralExpressionImpl(new Integer(65));
-	litExp2 = new LiteralExpressionImpl(new Integer(35));
-	mathExp1 = new MathExpressionImpl(DefaultExpression.MATH_ADD);
-	mathExp1.addLeftValue(litExp1);
-	mathExp1.addRightValue(litExp2);
+            btwnFilter = new BetweenFilterImpl();
+            btwnFilter.addLeftValue(litExp1);
+            btwnFilter.addMiddleValue(attrExp1);
+            btwnFilter.addRightValue(mathExp1);
 
-	btwnFilter = new BetweenFilterImpl();
-	btwnFilter.addLeftValue(litExp1);
-	btwnFilter.addMiddleValue(attrExp1);
-	btwnFilter.addRightValue(mathExp1);
+            compFilter = new CompareFilterImpl(AbstractFilter.COMPARE_LESS_THAN);
+            compFilter.addLeftValue(attrExp1);
+            compFilter.addRightValue(litExp2);
 
-	compFilter = new CompareFilterImpl(AbstractFilter.COMPARE_LESS_THAN);
-	compFilter.addLeftValue(attrExp1);
-	compFilter.addRightValue(litExp2);
+            geomFilter = new GeometryFilterImpl(AbstractFilter.GEOMETRY_TOUCHES);
+            geomFilter.addLeftGeometry(attrExp2);
+            geomFilter.addRightGeometry(litExp2);
 
-	geomFilter = new GeometryFilterImpl(AbstractFilter.GEOMETRY_TOUCHES);
-	geomFilter.addLeftGeometry(attrExp2);
-	geomFilter.addRightGeometry(litExp2);
+            likeFilter = new LikeFilterImpl();
+            likeFilter.setValue(attrExp1);
+            likeFilter.setPattern(pattern, wcMulti, wcSingle, escape);
 
-	likeFilter = new LikeFilterImpl();
-	likeFilter.setValue(attrExp1);
-	likeFilter.setPattern(pattern, wcMulti, wcSingle, escape);
+            nullFilter = new NullFilterImpl();
+            nullFilter.nullCheckValue(attrExp2);
+        } catch (IllegalFilterException e) {
+            //should not happen.
+            fail(e.getMessage());
+        }
 
-	nullFilter = new NullFilterImpl();
-	nullFilter.nullCheckValue(attrExp2);
-	} catch (IllegalFilterException e) {
-	    //should not happen.
-	    fail(e.getMessage());
-	}
-    
-
-	 //    testFeature = factory.create(attributes);
+        //    testFeature = factory.create(attributes);
         LOGGER.finer("...set up complete");
+
         //_log.getLoggerRepository().setThreshold(Level.DEBUG);
     }
-    
 
-    
-    public void testBasics() 
-    throws IllegalFilterException {
-	unpacker.unPack(btwnFilter);
-	//Unsupported should be null, supported btwnFilter
-	assertNull(unpacker.getUnSupported());
-	assertTrue(btwnFilter.equals(unpacker.getSupported()));
-	
-	unpacker.unPack(compFilter);
-	//null, compFilter
-	assertNull(unpacker.getUnSupported());
-	assertTrue(compFilter.equals(unpacker.getSupported()));
+    public void testBasics() throws IllegalFilterException {
+        unpacker.unPackAND(btwnFilter);
 
-	unpacker.unPack(geomFilter);
-	//Unsupported should be geomFilter, supported null
-	assertTrue(geomFilter.equals(unpacker.getUnSupported()));
-	assertNull(unpacker.getSupported());
+        //Unsupported should be null, supported btwnFilter
+        assertNull(unpacker.getUnSupported());
+        assertEquals(btwnFilter, unpacker.getSupported());
 
-	unpacker.unPack(likeFilter);
-	//likeFilter, null
-	assertTrue(likeFilter.equals(unpacker.getUnSupported()));
-	assertNull(unpacker.getSupported());
+        unpacker.unPackAND(compFilter);
 
-	unpacker.unPack(nullFilter);
-	//null, nullFilter
-	assertNull(unpacker.getUnSupported());
-	assertTrue(nullFilter.equals(unpacker.getSupported()));
+        //null, compFilter
+        assertNull(unpacker.getUnSupported());
+        assertEquals(compFilter, unpacker.getSupported());
 
+        unpacker.unPackAND(geomFilter);
 
-    }    
+        //Unsupported should be geomFilter, supported null
+        assertEquals(geomFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
 
-    public void testAnd()
-	throws IllegalFilterException{
+        unpacker.unPackAND(likeFilter);
 
-    //I will use the notation (Unsupported, Supported) to indicate
-    //the filters that should result, that we are testing against.
-	Filter andFilter = btwnFilter.and(compFilter);
-	unpacker.unPack(andFilter);
-	//both supported (null, andFilter)
-	assertNull(unpacker.getUnSupported());
-	assertTrue(andFilter.equals(unpacker.getSupported()));
+        //likeFilter, null
+        assertEquals(likeFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
 
-	andFilter = likeFilter.and(compFilter);
-	unpacker.unPack(andFilter);
-	//Comp supported, Like not: (likeFilter, compFilter)
-	assertTrue(likeFilter.equals(unpacker.getUnSupported()));
-	assertTrue(compFilter.equals(unpacker.getSupported()));
+        unpacker.unPackAND(nullFilter);
 
-	andFilter = likeFilter.and(geomFilter);
-	unpacker.unPack(andFilter);
-	//both unsupported (andFilter, null)
-	assertTrue(andFilter.equals(unpacker.getUnSupported()));
-	assertNull(unpacker.getSupported());
-    }
-		
-    public void testNot()
-	throws IllegalFilterException{
-	Filter notFilter = nullFilter.not();
-	unpacker.unPack(notFilter);
-	//nullFilters supported (null, nullFilter)
-	assertNull(unpacker.getUnSupported());
-	assertTrue(notFilter.equals(unpacker.getSupported()));
-
-	notFilter = geomFilter.not();
-	unpacker.unPack(notFilter);
-	//geomFilters not supported (geomFilter, null);
-	assertTrue(notFilter.equals(unpacker.getUnSupported()));
-	assertNull(unpacker.getSupported());
+        //null, nullFilter
+        assertNull(unpacker.getUnSupported());
+        assertEquals(nullFilter, unpacker.getSupported());
     }
 
-    public void testOr()
-	throws IllegalFilterException{
-	Filter orFilter = btwnFilter.or(compFilter);
-	unpacker.unPack(orFilter);
-	//both supported (null, orFilter)
-	assertNull(unpacker.getUnSupported());
-	assertTrue(orFilter.equals(unpacker.getSupported()));
+    public void testBasicsOR() throws IllegalFilterException {
+        unpacker.unPackOR(btwnFilter);
 
-	orFilter = likeFilter.or(compFilter);
-	unpacker.unPack(orFilter);
-	//both unsupported: (orFilter, null)  
-	assertTrue(orFilter.equals(unpacker.getUnSupported()));
-	assertNull(unpacker.getSupported());
+        //Unsupported should be null, supported btwnFilter
+        assertNull(unpacker.getUnSupported());
+        assertEquals(btwnFilter, unpacker.getSupported());
 
-	orFilter = likeFilter.and(geomFilter);
-	unpacker.unPack(orFilter);
-	//both unsupported (orFilter, null)
-	assertTrue(orFilter.equals(unpacker.getUnSupported()));
-	assertNull(unpacker.getSupported());
-	
+        unpacker.unPackOR(compFilter);
+
+        //null, compFilter
+        assertNull(unpacker.getUnSupported());
+        assertEquals(compFilter, unpacker.getSupported());
+
+        unpacker.unPackOR(geomFilter);
+
+        //Unsupported should be geomFilter, supported null
+        assertEquals(geomFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
+
+        unpacker.unPackOR(likeFilter);
+
+        //likeFilter, null
+        assertEquals(likeFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
+
+        unpacker.unPackOR(nullFilter);
+
+        //null, nullFilter
+        assertNull(unpacker.getUnSupported());
+        assertEquals(nullFilter, unpacker.getSupported());
     }
 
-    public void testComplex()
-	throws IllegalFilterException{
-	Filter orFilter = likeFilter.or(btwnFilter);
-	Filter andFilter = orFilter.and(compFilter);
-	unpacker.unPack(andFilter);
-	//compFilter supported, none of orFilter: (orFilter, compFilter)
-	assertTrue(orFilter.equals(unpacker.getUnSupported()));
-	assertTrue(compFilter.equals(unpacker.getSupported()));
-	
-	Filter bigOrFilter = (andFilter.or(compFilter)).or(nullFilter);
-	Filter biggerOrFilter = bigOrFilter.or(nullFilter);
-	//Top level or with one unsupported (like from orFilter)
-	//makes all unsupported: (bigOrFilter, null)
-	unpacker.unPack(biggerOrFilter);
-	assertTrue(biggerOrFilter.equals(unpacker.getUnSupported()));
-	assertNull(unpacker.getSupported());
+    public void testAnd() throws IllegalFilterException {
+        //I will use the notation (Unsupported, Supported) to indicate
+        //the filters that should result, that we are testing against.
+        Filter andFilter = btwnFilter.and(compFilter);
+        unpacker.unPackAND(andFilter);
 
-	Filter bigAndFilter = (geomFilter.and(compFilter)).and(orFilter);
-	//comp supported, orFilter not, geomFilter not:
-	//(orFilter AND geomFilter, compFilter)
-	unpacker.unPack(bigAndFilter);
-	assertTrue((orFilter.and(geomFilter)).equals(unpacker.getUnSupported()));
-	assertTrue(compFilter.equals(unpacker.getSupported()));
+        //both supported (null, andFilter)
+        assertNull(unpacker.getUnSupported());
+        assertEquals(andFilter, unpacker.getSupported());
 
-	Filter hugeAndFilter = (bigAndFilter.and(andFilter)).and(bigOrFilter);
-	unpacker.unPack(hugeAndFilter);
-	//two comps should be supported; geom, or and bigOr unsupported
-	//(compFilter AND compFilter, geomFilter AND orFilter AND orFilter AND bigOrFilter)
-	assertTrue((compFilter.and(compFilter)).equals(unpacker.getSupported()));
-	assertTrue((((geomFilter.and(orFilter)).and(orFilter)).and(bigOrFilter))
-		   .equals(unpacker.getUnSupported()));
+        andFilter = likeFilter.and(compFilter);
+        unpacker.unPackAND(andFilter);
+
+        //Comp supported, Like not: (likeFilter, compFilter)
+        assertEquals(likeFilter, unpacker.getUnSupported());
+        assertEquals(compFilter, unpacker.getSupported());
+
+        andFilter = likeFilter.and(geomFilter);
+        unpacker.unPackAND(andFilter);
+
+        //both unsupported (andFilter, null)
+        assertEquals(andFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
     }
 
+    public void testNot() throws IllegalFilterException {
+        Filter notFilter = nullFilter.not();
+        unpacker.unPackAND(notFilter);
+
+        //nullFilters supported (null, nullFilter)
+        assertNull(unpacker.getUnSupported());
+        assertEquals(notFilter, unpacker.getSupported());
+
+        notFilter = geomFilter.not();
+        unpacker.unPackAND(notFilter);
+
+        //geomFilters not supported (geomFilter, null);
+        assertEquals(notFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
+    }
+
+    public void testOr() throws IllegalFilterException {
+        Filter orFilter = btwnFilter.or(compFilter);
+        unpacker.unPackAND(orFilter);
+
+        //both supported (null, orFilter)
+        assertNull(unpacker.getUnSupported());
+        assertEquals(orFilter, unpacker.getSupported());
+
+        orFilter = likeFilter.or(compFilter);
+        unpacker.unPackAND(orFilter);
+
+        //both unsupported: (orFilter, null)  
+        assertEquals(orFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
+
+        orFilter = likeFilter.and(geomFilter);
+        unpacker.unPackAND(orFilter);
+
+        //both unsupported (orFilter, null)
+        assertEquals(orFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
+    }
+
+    //put in more unPackOR's
+    public void testComplex() throws IllegalFilterException {
+        Filter orFilter = likeFilter.or(btwnFilter);
+        Filter andFilter = orFilter.and(compFilter);
+        unpacker.unPackAND(andFilter);
+
+        //compFilter supported, none of orFilter: (orFilter, compFilter)
+        assertEquals(orFilter, unpacker.getUnSupported());
+        assertEquals(compFilter, unpacker.getSupported());
+
+        Filter bigOrFilter = (andFilter.or(compFilter)).or(nullFilter);
+        Filter biggerOrFilter = bigOrFilter.or(nullFilter);
+
+        //Top level or with one unsupported (like from orFilter)
+        //makes all unsupported: (bigOrFilter, null)
+        unpacker.unPackAND(biggerOrFilter);
+        assertEquals(biggerOrFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
+
+        Filter bigAndFilter = (geomFilter.and(compFilter)).and(orFilter);
+
+        //comp supported, orFilter not, geomFilter not:
+        //(orFilter AND geomFilter, compFilter)
+        unpacker.unPackAND(bigAndFilter);
+        assertEquals((orFilter.and(geomFilter)), unpacker.getUnSupported());
+        assertEquals(compFilter, unpacker.getSupported());
+        unpacker.unPackOR(bigAndFilter);
+        assertEquals(bigAndFilter, unpacker.getUnSupported());
+        assertNull(unpacker.getSupported());
+
+        Filter hugeAndFilter = (bigAndFilter.and(andFilter)).and(bigOrFilter);
+        unpacker.unPackAND(hugeAndFilter);
+
+        //two comps should be supported; geom, or and bigOr unsupported
+        //(compFilter AND compFilter, geomFilter AND orFilter AND orFilter AND bigOrFilter)
+        assertEquals((compFilter.and(compFilter)), unpacker.getSupported());
+        assertEquals((((geomFilter.and(orFilter)).and(orFilter)).and(
+                bigOrFilter)), unpacker.getUnSupported());
+    }
 }
