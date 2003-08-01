8d302a32b3bdfde45347080f9b78bea4e4aae992
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-08-01 16:49:38 +0000

    got rid of all output sending to System.out.  Added a little class so an output stream can become a string
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3575 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/gmldatasource/tests/unit/org/geotools/gml/ProducerTest.java
+++ geotools2/geotools-src/gmldatasource/tests/unit/org/geotools/gml/ProducerTest.java
@@ -1,4 +1,34 @@
+/*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
 package org.geotools.gml;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.PrecisionModel;
+import junit.framework.*;
 
 /*
  * ProducerTest.java
@@ -6,131 +36,187 @@ package org.geotools.gml;
  *
  */
 import org.geotools.data.*;
-import org.geotools.feature.*;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.LineString;
-import com.vividsolutions.jts.geom.LinearRing;
-import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.geom.PrecisionModel;
-import com.vividsolutions.jts.geom.Point;
-import java.net.URL;
-import java.util.*;
-import java.util.logging.Logger;
-import java.util.logging.Level;
-import org.geotools.gml.producer.*;
 import org.geotools.data.gml.GMLDataSource;
+import org.geotools.feature.*;
+import org.geotools.gml.producer.*;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import junit.framework.*;
 
 /**
+ * DOCUMENT ME!
  *
  * @author Chris Holmes, TOPP
  */
 public class ProducerTest extends TestCase {
-    
-        
     /** The logger for the filter module. */
-    private static final Logger LOGGER = Logger.getLogger(
-            "org.geotools.gml");
-    private Feature testFeature;
-
-    private FeatureType schema;
-
-       private FeatureFactory featureFactory;
-
-
+    private static final Logger LOGGER = Logger.getLogger("org.geotools.gml");
     static int NTests = 7;
-    
+    private Feature testFeature;
+    private FeatureType schema;
+    private FeatureFactory featureFactory;
     FeatureCollection table = null;
-    
+
     public ProducerTest(java.lang.String testName) {
         super(testName);
     }
-    
+
     public static void main(java.lang.String[] args) {
         junit.textui.TestRunner.run(suite());
     }
-    
+
     public static Test suite() {
         TestSuite suite = new TestSuite(ProducerTest.class);
+
         return suite;
     }
 
     /**
-     * This needs to be redone, for now it is just a demo
-     * that will print some sample features.
+     * This needs to be redone, for now it is just a demo that will print some
+     * sample features.
+     *
+     * @throws Exception DOCUMENT ME!
      */
     public void testProducer() throws Exception {
-	
-    System.setProperty("javax.xml.transform.TransformerFactory", "org.apache.xalan.processor.TransformerFactoryImpl");
+        System.setProperty("javax.xml.transform.TransformerFactory",
+            "org.apache.xalan.processor.TransformerFactoryImpl");
 
-	LOGGER.info("testing producer" );
-	    String dataFolder = System.getProperty("dataFolder");
-            if(dataFolder==null){
-                //then we are being run by maven
-                dataFolder = System.getProperty("basedir");
-                dataFolder+="/tests/unit/testData";
-            }
-            URL url = new URL("file:///"+dataFolder+"/testGML7Features.gml");
-            System.out.println("Testing blorg to load "+url+" as Feature datasource");
-            DataSource ds = new GMLDataSource(url);
-            
+        LOGGER.fine("testing producer");
 
-            AttributeType[] atts = {  
-	    AttributeTypeFactory.newAttributeType("geoid", Integer.class),
-	    AttributeTypeFactory.newAttributeType("geom", Geometry.class),
-	    AttributeTypeFactory.newAttributeType("name", String.class)};
-	try {
-	schema = FeatureTypeFactory.newFeatureType(atts,"rail");
-	//schema = ((FeatureTypeFlat)schema).setNamespace("http://www.openplans.org/ch");
-	} catch (SchemaException e) {
-	    LOGGER.finer("problem with creating schema");
-	}
-	LOGGER.info("namespace is " + schema.getNamespace());
-	featureFactory = schema;
+        String dataFolder = System.getProperty("dataFolder");
 
- 
-	Coordinate[] points = { new Coordinate(15, 15),
-				new Coordinate(15, 25),
-				new Coordinate(25, 25),
-				new Coordinate(25, 15),
-				 new Coordinate(15, 15) };
-	PrecisionModel precModel = new PrecisionModel();
-	int srid = 2035;
-	LinearRing shell = new LinearRing(points, precModel, srid);
-	Polygon the_geom = new Polygon(shell, precModel, srid);
+        if (dataFolder == null) {
+            //then we are being run by maven
+            dataFolder = System.getProperty("basedir");
+            dataFolder += "/tests/unit/testData";
+        }
 
-	Point point = new Point(new Coordinate(3, 35), precModel, srid);
-	LineString line = new LineString(points, precModel, srid);
+        URL url = new URL("file:///" + dataFolder + "/testGML7Features.gml");
+        LOGGER.fine("Testing to load " + url + " as Feature datasource");
 
-	Integer featureId = new Integer(32);
-	String name = "inse<rt polygon";
-	Object[] attributes = { featureId, point, name };
-	Feature polygonFeature = null;
-	Feature lineFeature = null;
-	try{
-	    
-	testFeature = featureFactory.create(attributes, null);
-	attributes[1] = line;
-	lineFeature =  featureFactory.create(attributes, null);
-	attributes[1] = the_geom;
-	polygonFeature = featureFactory.create(attributes, null);
-	    
-	} catch (IllegalAttributeException ife) {
-	    LOGGER.warning("problem in setup " + ife);
-	}
+        DataSource ds = new GMLDataSource(url);
 
-	//table = ds.getFeatures();//new FeatureCollectionDefault();
-	table = FeatureCollections.newCollection();
-	table.add(testFeature);
-	table.add(lineFeature);
-	table.add(polygonFeature);
-	    //System.out.println("the feature collection is " + table + ", and " + "the first feat is " + table.features().next());
-	    
-	    FeatureTransformer fr = new FeatureTransformer();
-	    fr.setPrettyPrint(true);
-	    fr.setDefaultNamespace("http://www.openplans.org/ch");
-	    fr.transform(table, System.out);
+        AttributeType[] atts = {
+            AttributeTypeFactory.newAttributeType("geoid", Integer.class),
+            AttributeTypeFactory.newAttributeType("geom", Geometry.class),
+            AttributeTypeFactory.newAttributeType("name", String.class)
+        };
+
+        try {
+            schema = FeatureTypeFactory.newFeatureType(atts, "rail",
+                    "http://www.openplans.org/data");
+        } catch (SchemaException e) {
+            LOGGER.finer("problem with creating schema");
+        }
+
+        LOGGER.fine("namespace is " + schema.getNamespace());
+
+        PrecisionModel precModel = new PrecisionModel();
+        int srid = 2035;
+
+        GeometryFactory geomFactory = new GeometryFactory(precModel, srid);
+
+        Coordinate[] points = {
+            new Coordinate(15, 15), new Coordinate(15, 25),
+            new Coordinate(25, 25), new Coordinate(25, 15),
+            new Coordinate(15, 15)
+        };
+        LinearRing shell = new LinearRing(points, precModel, srid);
+        Polygon the_geom = new Polygon(shell, precModel, srid);
+        Polygon[] polyArr = { the_geom, the_geom };
+        MultiPolygon multiPoly = geomFactory.createMultiPolygon(polyArr);
+        Point point = geomFactory.createPoint(new Coordinate(3, 35));
+        Point[] pointArr = { point, point, point, point };
+        MultiPoint multiPoint = new MultiPoint(pointArr, precModel, srid);
+        LineString line = new LineString(points, precModel, srid);
+        LineString[] lineArr = { line, line, line };
+        MultiLineString multiLine = new MultiLineString(lineArr, precModel, srid);
+        Geometry[] geomArr = { multiPoly, point, line, multiLine };
+        GeometryCollection multiGeom = new GeometryCollection(geomArr,
+                precModel, srid);
+        Integer featureId = new Integer(32);
+        String name = "inse<rt polygon444444";
+        Object[] attributes = { featureId, point, name };
+        Feature polygonFeature = null;
+        Feature lineFeature = null;
+        Feature multiLineFeature = null;
+        Feature mPointFeature = null;
+        Feature mPolyFeature = null;
+        Feature mGeomFeature = null;
+
+        try {
+            testFeature = schema.create(attributes, "rail.1");
+            attributes[1] = line;
+            lineFeature = schema.create(attributes, "rail.2");
+            attributes[1] = the_geom;
+            polygonFeature = schema.create(attributes, "rail.3");
+            attributes[1] = multiLine;
+            multiLineFeature = schema.create(attributes, "rail.4");
+            attributes[1] = multiPoint;
+            mPointFeature = schema.create(attributes, "rail.5");
+            attributes[1] = multiPoly;
+            mPolyFeature = schema.create(attributes, "rail.6");
+            attributes[1] = multiGeom;
+            mGeomFeature = schema.create(attributes, "rail.7");
+        } catch (IllegalAttributeException ife) {
+            LOGGER.warning("problem in setup " + ife);
+        }
+
+        //table = ds.getFeatures();//new FeatureCollectionDefault();
+        table = FeatureCollections.newCollection();
+        table.add(testFeature);
+        table.add(lineFeature);
+        table.add(polygonFeature);
+        table.add(multiLineFeature);
+        table.add(mPointFeature);
+        table.add(mPolyFeature);
+        table.add(mGeomFeature);
+        LOGGER.fine("the feature collection is " + table + ", and "
+            + "the first feat is " + table.features().next());
+
+        FeatureTransformer fr = new FeatureTransformer();
+        fr.setPrettyPrint(true);
+        fr.setDefaultNamespace("http://www.openplans.org/ch");
+
+        //increase the capacity if the output gets longer.
+        LogOutputStream out = new LogOutputStream(10000);
+        fr.transform(table, out);
+        LOGGER.fine("output is " + out.toString());
+    }
+
+    /** 
+     * little class to allow one to use an output stream and just call
+     * toString on it.  I couldn't find any java class that did this easily,
+     * like a StringWriter.  Maybe I just wasn't looking in the right place,
+     * but this seems to work and was easy to implement.  But it allows
+     * us to not use System.out for the transform, so the logging level can
+     * be recorded.
+     */
+    private static class LogOutputStream extends OutputStream {
+        private ByteBuffer bytes;
+
+        public LogOutputStream(int capacity) {
+            bytes = ByteBuffer.allocate(capacity);
+        }
+
+        public void write(int b) {
+            bytes.put(new Integer(b).byteValue());
+        }
+
+        public void write(byte[] b, int off, int len) {
+            bytes.put(b, off, len);
+        }
+
+        public void write(byte[] b) {
+            bytes.put(b);
+        }
+
+        public String toString() {
+            return new String(bytes.array());
+        }
     }
 }
