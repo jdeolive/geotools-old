50e3038508c7afcd43d13a5bc5480348a1774647
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-08-05 23:24:41 +0000

    checkstyle clean-ups
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3634 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/core/src/org/geotools/feature/IllegalAttributeException.java
+++ geotools2/geotools-src/core/src/org/geotools/feature/IllegalAttributeException.java
@@ -20,45 +20,64 @@ package org.geotools.feature;
  * Indicates client class has attempted to create an invalid feature.
  *
  * @author Rob Hranac, Vision for New York
- * @version $Id: IllegalAttributeException.java,v 1.3 2003/07/20 15:58:23 aaime Exp $
+ * @version $Id: IllegalAttributeException.java,v 1.4 2003/08/05 23:24:40 cholmesny Exp $
  */
 public class IllegalAttributeException extends Exception {
-  
-  // I don't believe this is very useful! -IanS
-  //    /**
-  //     * Constructor with no argument.
-  //     */
-  //    public IllegalAttributeException() {
-  //        super();
-  //    }
-  
-  public final AttributeType expected;
-  public final Object invalid;
-  
-  /**
-   * Constructor with message argument.
-   *
-   * @param message Reason for the exception being thrown
-   */
-  public IllegalAttributeException(String message) {
-    super(message);
-    expected = null;
-    invalid = null;
-  }
-  
-  public IllegalAttributeException(AttributeType expected,Object invalid) {
-    this(expected,invalid,null);
-  }
-  
-  public IllegalAttributeException(AttributeType expected,Object invalid,Throwable cause) {
-    super(errorMessage(expected,invalid),cause);
-    this.expected = expected;
-    this.invalid = invalid;
-  }
-  
-  static String errorMessage(AttributeType expected,Object invalid) {
-    String message = "expected " + expected.getType().getName();
-    message += " , but got " + ((invalid == null) ? "null" : invalid.getClass().getName());
-    return message;
-  }
+    /** The expected attribute type. */
+    private final AttributeType expected;
+
+    /** The object that does not match the expected type. */
+    private final Object invalid;
+
+    /**
+     * Constructor with message argument.
+     *
+     * @param message Reason for the exception being thrown
+     */
+    public IllegalAttributeException(String message) {
+        super(message);
+        expected = null;
+        invalid = null;
+    }
+
+    /**
+     * Constructor that makes the message given the expected and invalid.
+     *
+     * @param expected the expected AttributeType.
+     * @param invalid the attribute that does not validate against expected.
+     */
+    public IllegalAttributeException(AttributeType expected, Object invalid) {
+        this(expected, invalid, null);
+    }
+
+    /**
+     * Constructor that makes the message given the expected and invalid,
+     * along with the root cause.
+     *
+     * @param expected the expected AttributeType.
+     * @param invalid the attribute that does not validate against expected.
+     * @param cause the root cause of the error.
+     */
+    public IllegalAttributeException(AttributeType expected, Object invalid,
+        Throwable cause) {
+        super(errorMessage(expected, invalid), cause);
+        this.expected = expected;
+        this.invalid = invalid;
+    }
+
+    /**
+     * Constructs an error message based on expected and invalid.
+     *
+     * @param expected the expected AttributeType.
+     * @param invalid the attribute that does not validate against expected.
+     *
+     * @return an error message reporting the problem.
+     */
+    static String errorMessage(AttributeType expected, Object invalid) {
+        String message = "expected " + expected.getType().getName();
+        message += (" , but got "
+        + ((invalid == null) ? "null" : invalid.getClass().getName()));
+
+        return message;
+    }
 }
