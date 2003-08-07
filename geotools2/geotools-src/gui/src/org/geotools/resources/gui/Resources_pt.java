126fdde088523d5ecc14f411d79a1a7c91eba02c
desruisseaux <desruisseaux@e5c1c795-43da-0310-a71f-fac65c449510>
2003-08-07 13:20:34 +0000

    Added Portuguese language resources
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3655 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ geotools2/geotools-src/gui/src/org/geotools/resources/gui/Resources_pt.java
@@ -0,0 +1,45 @@
+/*
+ * Geotools 2 - OpenSource mapping toolkit
+ * (C) 2003, Geotools Project Management Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ * Contacts:
+ *     UNITED KINDOM: James Macgill
+ *             mailto:j.macgill@geog.leeds.ac.uk
+ */
+package org.geotools.resources.gui;
+
+
+/**
+ * Portuguese language resource.
+ *
+ * @version $Id: Resources_pt.java,v 1.1 2003/08/07 13:20:34 desruisseaux Exp $
+ * @author Martin Desruisseaux
+ */
+public class Resources_pt extends Resources {
+    /**
+     * The resource file, using UTF-8 encoding.
+     */
+    static final String FILEPATH = "org/geotools/resources/gui/resources_pt.utf";
+
+    /**
+     * Construct resources for Spanich.
+     */
+    public Resources_pt() {
+        super(FILEPATH);
+    }
+}
