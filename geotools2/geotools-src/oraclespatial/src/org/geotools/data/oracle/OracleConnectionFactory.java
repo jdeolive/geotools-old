db15cc8b245fb4dd269d289e74d4b1bea70b09ea
seangeo <seangeo@e5c1c795-43da-0310-a71f-fac65c449510>
2003-08-08 07:37:18 +0000

    Connection Pooling added. PMD/CheckStyle clean up.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3674 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/oraclespatial/src/org/geotools/data/oracle/OracleConnectionFactory.java
+++ geotools2/geotools-src/oraclespatial/src/org/geotools/data/oracle/OracleConnectionFactory.java
@@ -1,1 +1,97 @@
-/* *    Geotools2 - OpenSource mapping toolkit *    http://geotools.org *    (C) 2002, Geotools Project Managment Committee (PMC) * *    This library is free software; you can redistribute it and/or *    modify it under the terms of the GNU Lesser General Public *    License as published by the Free Software Foundation; *    version 2.1 of the License. * *    This library is distributed in the hope that it will be useful, *    but WITHOUT ANY WARRANTY; without even the implied warranty of *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU *    Lesser General Public License for more details. * */package org.geotools.data.oracle;import java.sql.DriverManager;import java.sql.SQLException;import oracle.jdbc.OracleConnection;/** * Provides javax.sql.DataSource wrapper around an OracleConnection object. * * @author Sean Geoghegan, Defence Science and Technology Organisation * @author $Author: seangeo $ * @version $Id: OracleConnectionFactory.java,v 1.3 2003/08/02 02:47:06 seangeo Exp $ */public class OracleConnectionFactory {    /** The prefix of an Oracle JDBC url */    private static final String JDBC_PATH = "jdbc:oracle:thin:@";    /** The Oracle driver class name */    private static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";        /** The url to the DB */    private String dbUrl;    /** The username to login with */    private String username = "";    /** The password to login with */     private String passwd = "";    /**     * Creates a new OracleConnection object that wraps a     * oracle.jdbc.driver.OracleConnection.     *     * @param host The host to connect to.     * @param port The port number on the host     * @param instance The instance name on the host     */    public OracleConnectionFactory(String host, String port, String instance) {        dbUrl = JDBC_PATH + host + ":" + port + ":" + instance;    }    /**     * Creates the real OracleConnection. Logs in to the Oracle Database and     * creates the OracleConnection object.     *     * @param user The user name.     * @param pass The password     *     * @return The real OracleConnection object.     *     * @throws SQLException If an error occurs connecting to the DB.     */    public OracleConnection getOracleConnection(String user, String pass)                throws SQLException {        OracleConnection conn = null;        try {            Class.forName(JDBC_DRIVER);            conn = (OracleConnection) DriverManager.getConnection(dbUrl, user,                    pass);        } catch (ClassNotFoundException e) {            throw new SQLException("Oracle Driver Not Found");        }        return conn;    }    /**     * Creates the real OracleConnection.  Logs into the database using the     * credentials provided by setLogin     *     * @return The oracle connection to the data base.     *     * @throws SQLException If an error occurs connecting to the DB.     */    public OracleConnection getOracleConnection() throws SQLException {        return getOracleConnection(username, passwd);    }    /**     * Sets the login credentials.     *     * @param user The username     * @param pass The password     */    public void setLogin(String user, String pass) {        this.username = user;        this.passwd = pass;    }}
+/* *    Geotools2 - OpenSource mapping toolkit *    http://geotools.org *    (C) 2002, Geotools Project Managment Committee (PMC) * *    This library is free software; you can redistribute it and/or *    modify it under the terms of the GNU Lesser General Public *    License as published by the Free Software Foundation; *    version 2.1 of the License. * *    This library is distributed in the hope that it will be useful, *    but WITHOUT ANY WARRANTY; without even the implied warranty of *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU *    Lesser General Public License for more details. * */
+package org.geotools.data.oracle;
+
+import oracle.jdbc.pool.OracleConnectionPoolDataSource;
+import org.geotools.data.jdbc.ConnectionPool;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Provides javax.sql.DataSource wrapper around an OracleConnection object.
+ *
+ * @author Sean Geoghegan, Defence Science and Technology Organisation
+ * @author $Author: seangeo $
+ * @version $Id: OracleConnectionFactory.java,v 1.4 2003/08/08 07:37:18 seangeo Exp $
+ */
+public class OracleConnectionFactory {
+    /** The prefix of an Oracle JDBC url */
+    private static final String JDBC_PATH = "jdbc:oracle:thin:@";
+    private static Map connectionPools = Collections.synchronizedMap(new HashMap());
+
+    /** The url to the DB */
+    private String dbUrl;
+
+    /** The username to login with */
+    private String username = "";
+
+    /** The password to login with */
+    private String passwd = "";
+
+    /**
+     * Creates a new OracleConnection object that wraps a oracle.jdbc.driver.OracleConnection.
+     *
+     * @param host The host to connect to.
+     * @param port The port number on the host
+     * @param instance The instance name on the host
+     */
+    public OracleConnectionFactory(String host, String port, String instance) {
+        dbUrl = JDBC_PATH + host + ":" + port + ":" + instance;
+    }
+
+    /**
+     * Creates the real OracleConnection. Logs in to the Oracle Database and creates the
+     * OracleConnection object.
+     *
+     * @param user The user name.
+     * @param pass The password
+     *
+     * @return The real OracleConnection object.
+     *
+     * @throws SQLException If an error occurs connecting to the DB.
+     */
+    public ConnectionPool getConnectionPool(String user, String pass)
+        throws SQLException {
+        ConnectionPool pool = null;
+        String poolKey = dbUrl + user + pass;
+
+        if ((pool = (ConnectionPool) connectionPools.get(poolKey)) == null) {
+            OracleConnectionPoolDataSource cpDataSource = new OracleConnectionPoolDataSource();
+
+            cpDataSource.setURL(dbUrl);
+            cpDataSource.setUser(user);
+            cpDataSource.setPassword(pass);
+
+            pool = new ConnectionPool(cpDataSource);
+            connectionPools.put(poolKey, pool);
+        }
+
+        return pool;
+    }
+
+    /**
+     * Creates the real OracleConnection.  Logs into the database using the credentials provided by
+     * setLogin
+     *
+     * @return The oracle connection to the data base.
+     *
+     * @throws SQLException If an error occurs connecting to the DB.
+     */
+    public ConnectionPool getConnectionPool() throws SQLException {
+        return getConnectionPool(username, passwd);
+    }
+
+    /**
+     * Sets the login credentials.
+     *
+     * @param user The username
+     * @param pass The password
+     */
+    public void setLogin(String user, String pass) {
+        this.username = user;
+
+        this.passwd = pass;
+    }
+}
