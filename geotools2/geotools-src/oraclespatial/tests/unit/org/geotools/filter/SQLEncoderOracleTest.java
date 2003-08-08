fe3a675e258d635a52a3827e77a64da9b7b7715b
seangeo <seangeo@e5c1c795-43da-0310-a71f-fac65c449510>
2003-08-08 07:33:17 +0000

    lots more test
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3669 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/oraclespatial/tests/unit/org/geotools/filter/SQLEncoderOracleTest.java
+++ geotools2/geotools-src/oraclespatial/tests/unit/org/geotools/filter/SQLEncoderOracleTest.java
@@ -1,32 +1,32 @@
-/* $Id: SQLEncoderOracleTest.java,v 1.1 2003/08/02 02:34:29 seangeo Exp $
- * 
+/* $Id: SQLEncoderOracleTest.java,v 1.2 2003/08/08 07:33:17 seangeo Exp $
+ *
  * Created on 31/07/2003
  */
 package org.geotools.filter;
 
-import java.util.logging.Logger;
-
-import junit.framework.TestCase;
-
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import junit.framework.TestCase;
+import java.util.logging.Logger;
+
 
 /**
+ * DOCUMENT ME!
+ *
  * @author Sean Geoghegan, Defence Science and Technology Organisation
  * @author $Author: seangeo $
- * @version $Id: SQLEncoderOracleTest.java,v 1.1 2003/08/02 02:34:29 seangeo Exp $
- * Last Modified: $Date: 2003/08/02 02:34:29 $ 
+ * @version $Id: SQLEncoderOracleTest.java,v 1.2 2003/08/08 07:33:17 seangeo Exp $ Last Modified: $Date: 2003/08/08 07:33:17 $
  */
 public class SQLEncoderOracleTest extends TestCase {
     private static final Logger LOGGER = Logger.getLogger("org.geotools.filter");
     private static final FilterFactory filterFactory = FilterFactory.createFilterFactory();
     private static final GeometryFactory geometryFactory = new GeometryFactory();
-
     private SQLEncoder encoder;
-    
+
     /**
      * Constructor for SQLEncoderOracleTest.
+     *
      * @param arg0
      */
     public SQLEncoderOracleTest(String arg0) {
@@ -48,32 +48,36 @@ public class SQLEncoderOracleTest extends TestCase {
     }
 
     public void testGeometryFilterEncoder() throws Exception {
-        
         encoder = new SQLEncoderOracle(-1);
-        
+
         GeometryFilter filter = filterFactory.createGeometryFilter(AbstractFilter.GEOMETRY_BBOX);
-        filter.addLeftGeometry(filterFactory.createAttributeExpression(null,"GEOM"));
-        filter.addRightGeometry(filterFactory.createBBoxExpression(new Envelope(-180.0,180.0,-90.0,90.0)));
+        filter.addLeftGeometry(filterFactory.createAttributeExpression(null, "GEOM"));
+        filter.addRightGeometry(filterFactory.createBBoxExpression(
+                new Envelope(-180.0, 180.0, -90.0, 90.0)));
         String value = encoder.encode(filter);
-        assertEquals("WHERE SDO_GEOM.RELATE(\"GEOM\",'disjoint'," +            "MDSYS.SDO_GEOMETRY(2003,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1)," +            "MDSYS.SDO_ORDINATE_ARRAY(-180.0,-90.0,-180.0,90.0,180.0,90.0,180.0,-90.0,-180.0,-90.0)),0.001)" +            " = 'FALSE' ",value);
-        
+        assertEquals("WHERE SDO_GEOM.RELATE(\"GEOM\",'disjoint'," +
+            "MDSYS.SDO_GEOMETRY(2003,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1)," +
+            "MDSYS.SDO_ORDINATE_ARRAY(-180.0,-90.0,-180.0,90.0,180.0,90.0,180.0,-90.0,-180.0,-90.0)),0.001)" +
+            " = 'FALSE' ", value);
+
         filter = filterFactory.createGeometryFilter(AbstractFilter.GEOMETRY_CONTAINS);
-        filter.addLeftGeometry(filterFactory.createAttributeExpression(null,"SHAPE"));
-        filter.addRightGeometry(filterFactory.createLiteralExpression(
-                                 geometryFactory.createPoint(new Coordinate(10.0,-10.0))));                                
+        filter.addLeftGeometry(filterFactory.createAttributeExpression(null, "SHAPE"));
+        filter.addRightGeometry(filterFactory.createLiteralExpression(geometryFactory.createPoint(
+                    new Coordinate(10.0, -10.0))));
         value = encoder.encode(filter);
-        LOGGER.info(value); 
-        assertEquals("WHERE SDO_RELATE(\"SHAPE\",MDSYS.SDO_GEOMETRY(0001,NULL," +            "MDSYS.SDO_POINT_TYPE(10.0,-10.0,NULL),NULL,NULL),'mask=contains querytype=WINDOW') = 'TRUE' ",            value) ;
-            
+        LOGGER.fine(value);
+        assertEquals("WHERE SDO_RELATE(\"SHAPE\",MDSYS.SDO_GEOMETRY(0001,NULL," +
+            "MDSYS.SDO_POINT_TYPE(10.0,-10.0,NULL),NULL,NULL),'mask=contains querytype=WINDOW') = 'TRUE' ",
+            value);
+
         filter = filterFactory.createGeometryFilter(AbstractFilter.GEOMETRY_CROSSES);
-        filter.addLeftGeometry(filterFactory.createAttributeExpression(null,"GEOM"));
+        filter.addLeftGeometry(filterFactory.createAttributeExpression(null, "GEOM"));
         filter.addRightGeometry(filterFactory.createLiteralExpression(
-                geometryFactory.createLineString(new Coordinate[]{
-                        new Coordinate(-10.0d,-10.0d),new Coordinate(10d,10d)
-                })));
+                geometryFactory.createLineString(
+                    new Coordinate[] { new Coordinate(-10.0d, -10.0d), new Coordinate(10d, 10d) })));
         value = encoder.encode(filter);
         assertEquals("WHERE SDO_RELATE(\"GEOM\",MDSYS.SDO_GEOMETRY(1002,NULL," +
-            "NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),MDSYS.SDO_ORDINATE_ARRAY(" +            "-10.0,-10.0,10.0,10.0)),'mask=overlapbydisjoint querytype=WINDOW') = 'TRUE' ",
-            value);
+            "NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),MDSYS.SDO_ORDINATE_ARRAY(" +
+            "-10.0,-10.0,10.0,10.0)),'mask=overlapbydisjoint querytype=WINDOW') = 'TRUE' ", value);
     }
 }
