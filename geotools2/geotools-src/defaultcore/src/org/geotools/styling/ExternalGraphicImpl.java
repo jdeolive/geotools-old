2036dcfed44c5111844ddd351108006de2289e5c
seangeo <seangeo@e5c1c795-43da-0310-a71f-fac65c449510>
2003-08-10 08:38:58 +0000

    Made getLocation throw a malformed url exception.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@3691 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/defaultcore/src/org/geotools/styling/ExternalGraphicImpl.java
+++ geotools2/geotools-src/defaultcore/src/org/geotools/styling/ExternalGraphicImpl.java
@@ -19,26 +19,25 @@
  */
 package org.geotools.styling;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
- * @version $Id: ExternalGraphicImpl.java,v 1.6 2003/08/01 16:54:49 ianturton Exp $
+ * @version $Id: ExternalGraphicImpl.java,v 1.7 2003/08/10 08:38:58 seangeo Exp $
  * @author Ian Turton, CCG
  */
-public class ExternalGraphicImpl implements ExternalGraphic, Symbol {
+public class ExternalGraphicImpl implements ExternalGraphic, Symbol, Cloneable {
     /**
      * The logger for the default core module.
      */
     private static final java.util.logging.Logger LOGGER = 
             java.util.logging.Logger.getLogger("org.geotools.core");
-    private java.net.URL location = null;
-    private String format = "";
+    private URL location = null;
+    private String format = null;
+    private String uri = null;
 
     public void setURI(String uri) {
-        try {
-            setLocation(new java.net.URL(uri));
-        } catch (java.net.MalformedURLException mfue) {
-            LOGGER.info("Exception setting uri: " + uri + "\n" + mfue);
-            mfue.printStackTrace();
-        }
+        this.uri = uri;   
     }
 
     /**
@@ -54,7 +53,11 @@ public class ExternalGraphicImpl implements ExternalGraphic, Symbol {
      * Provides the URL for where the external graphic resouce can be located.
      * @return The URL of the ExternalGraphic
      */
-    public java.net.URL getLocation() {
+    public java.net.URL getLocation() throws MalformedURLException {
+        if (location == null) {
+            location = new URL(uri);            
+        }
+
         return location;
     }
 
@@ -71,6 +74,7 @@ public class ExternalGraphicImpl implements ExternalGraphic, Symbol {
      * @param location New value of property location.
      */
     public void setLocation(java.net.URL location) {
+        this.uri = location.toString();
         this.location = location;
     }
     
@@ -78,4 +82,78 @@ public class ExternalGraphicImpl implements ExternalGraphic, Symbol {
         visitor.visit(this); 
     }
     
+    /** Returns a clone of the ExternalGraphic
+     * 
+     * @see org.geotools.styling.ExternalGraphic#clone()
+     */
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            // This will never happen
+            throw new RuntimeException("External Graphic Clone failed"); 
+        }
+    }
+
+    /** Generates a hashcode for the ExternalGraphic
+     * 
+     *  @return The hash code.
+     */
+    public int hashCode() {
+        final int PRIME = 1000003;
+        int result = 0;
+        if (format != null) {
+            result = PRIME * result + format.hashCode();
+        }
+        if (uri != null) {
+            result = PRIME * result + uri.hashCode();
+        }
+
+        return result;
+    }
+
+    /** Compares this ExternalGraphi with another.
+     * 
+     *  <p>Two external graphics are equal if they have the
+     *  same uri and format.
+     * 
+     *  @param oth The other External graphic.
+     *  @return True if this and the other external graphic are equal. 
+     */
+    public boolean equals(Object oth) {
+        if (this == oth) {
+            return true;
+        }
+
+        if (oth == null) {
+            return false;
+        }
+
+        if (oth.getClass() != getClass()) {
+            return false;
+        }
+
+        ExternalGraphicImpl other = (ExternalGraphicImpl) oth;
+        if (this.format == null) {
+            if (other.format != null) {
+                return false;
+            }
+        } else {
+            if (!this.format.equals(other.format)) {
+                return false;
+            }
+        }
+        if (this.uri == null) {
+            if (other.uri != null) {
+                return false;
+            }
+        } else {
+            if (!this.uri.equals(other.uri)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
 }
