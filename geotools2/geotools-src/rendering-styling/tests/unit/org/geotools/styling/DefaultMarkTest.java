4455f22f4f8f30d5f08362c1c5bd34a754d69f98
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2003-11-02 21:24:05 +0000

    Changed to test both lite rendering and j2d rendering
    git-svn-id: http://svn.osgeo.org/geotools/trunk@4399 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/rendering-styling/tests/unit/org/geotools/styling/DefaultMarkTest.java
+++ geotools2/geotools-src/rendering-styling/tests/unit/org/geotools/styling/DefaultMarkTest.java
@@ -1,4 +1,20 @@
 /*
+ *    Geotools2 - OpenSource mapping toolkit
+ *    http://geotools.org
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ */
+/*
  * RenderStyleTest.java
  *
  * Created on 27 May 2002, 15:40
@@ -7,6 +23,17 @@ package org.geotools.styling;
 
 import com.vividsolutions.jts.geom.Point;
 import org.geotools.feature.AttributeTypeFactory;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.map.Context;
+import org.geotools.map.ContextFactory;
+import org.geotools.map.Layer;
+import org.geotools.renderer.Renderer;
+import org.geotools.renderer.Renderer2D;
+import org.geotools.renderer.j2d.StyledRenderer;
+import org.geotools.renderer.lite.LiteRenderer;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
 import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
 import javax.imageio.ImageIO;
@@ -14,65 +41,65 @@ import javax.media.jai.JAI;
 
 
 /**
- * DOCUMENT ME!
+ * Test class for marks and text
  *
  * @author jamesm
+ * @author Andrea Aime
  */
 public class DefaultMarkTest extends junit.framework.TestCase {
+    /**
+     * Creates a new DefaultMarkTest object.
+     */
     public DefaultMarkTest(java.lang.String testName) {
         super(testName);
     }
 
+    /**
+     * Main
+     */
     public static void main(java.lang.String[] args) {
         junit.textui.TestRunner.run(suite());
     }
 
+    /**
+     * Suite
+     */
     public static junit.framework.Test suite() {
         junit.framework.TestSuite suite = new junit.framework.TestSuite(DefaultMarkTest.class);
 
         return suite;
     }
 
-    public void testSimpleRender() throws Exception {
-        //same as the datasource test, load in some features into a table
-        System.out.println("\n\nMark Test\n");
-
+    /**
+     * Builds and returns the features used to perform this test
+     */
+    private FeatureCollection buildFeatureCollection()
+        throws Exception {
         // Request extent
-        com.vividsolutions.jts.geom.Envelope ex = new com.vividsolutions.jts.geom.Envelope(0,
-                45, 0, 45);
+        com.vividsolutions.jts.geom.Envelope ex = new com.vividsolutions.jts.geom.Envelope(0, 45,
+                0, 45);
 
-        //EnvelopeExtent ex = new EnvelopeExtent(0, 45, 0, 45);
         com.vividsolutions.jts.geom.GeometryFactory geomFac = new com.vividsolutions.jts.geom.GeometryFactory();
         java.util.ArrayList features = new java.util.ArrayList();
 
         org.geotools.feature.AttributeType[] pointAttribute = new org.geotools.feature.AttributeType[4];
-        pointAttribute[0] = AttributeTypeFactory.newAttributeType("centre",
-                Point.class);
-        pointAttribute[1] = AttributeTypeFactory.newAttributeType("size",
-                Double.class);
-        pointAttribute[2] = AttributeTypeFactory.newAttributeType("rotation",
-                Double.class);
-        pointAttribute[3] = AttributeTypeFactory.newAttributeType("name",
-                String.class);
+        pointAttribute[0] = AttributeTypeFactory.newAttributeType("centre", Point.class);
+        pointAttribute[1] = AttributeTypeFactory.newAttributeType("size", Double.class);
+        pointAttribute[2] = AttributeTypeFactory.newAttributeType("rotation", Double.class);
+        pointAttribute[3] = AttributeTypeFactory.newAttributeType("name", String.class);
 
         org.geotools.feature.FeatureType pointType = org.geotools.feature.FeatureTypeFactory
             .newFeatureType(pointAttribute, "testPoint");
 
         org.geotools.feature.AttributeType[] labelAttribute = new org.geotools.feature.AttributeType[4];
-        labelAttribute[0] = AttributeTypeFactory.newAttributeType("centre",
-                Point.class);
-        labelAttribute[1] = AttributeTypeFactory.newAttributeType("name",
-                String.class);
-        labelAttribute[2] = AttributeTypeFactory.newAttributeType("X",
-                Double.class);
-        labelAttribute[3] = AttributeTypeFactory.newAttributeType("Y",
-                Double.class);
+        labelAttribute[0] = AttributeTypeFactory.newAttributeType("centre", Point.class);
+        labelAttribute[1] = AttributeTypeFactory.newAttributeType("name", String.class);
+        labelAttribute[2] = AttributeTypeFactory.newAttributeType("X", Double.class);
+        labelAttribute[3] = AttributeTypeFactory.newAttributeType("Y", Double.class);
 
         org.geotools.feature.FeatureType labelType = org.geotools.feature.FeatureTypeFactory
             .newFeatureType(labelAttribute, "labelPoint");
-        String[] marks = {
-            "Circle", "Triangle", "Cross", "Star", "X", "Square", "Arrow"
-        };
+        String[] marks = { "Circle", "Triangle", "Cross", "Star", "X", "Square", "Arrow" };
         double size = 6;
         double rotation = 0;
         int rows = 7;
@@ -80,17 +107,14 @@ public class DefaultMarkTest extends junit.framework.TestCase {
         for (int j = 0; j < rows; j++) {
             Point point = makeSamplePoint(geomFac, 2, 5.0 + (j * 5));
             org.geotools.feature.Feature pointFeature = labelType.create(new Object[] {
-                        point, "" + size + "/" + rotation, new Double(0.3),
-                        new Double(.5)
+                        point, "" + size + "/" + rotation, new Double(0.3), new Double(.5)
                     });
             features.add(pointFeature);
 
             for (int i = 0; i < marks.length; i++) {
-                point = makeSamplePoint(geomFac, ((double) i * 5.0) + 10.0,
-                        5.0 + (j * 5));
+                point = makeSamplePoint(geomFac, ((double) i * 5.0) + 10.0, 5.0 + (j * 5));
                 pointFeature = pointType.create(new Object[] {
-                            point, new Double(size), new Double(rotation),
-                            marks[i]
+                            point, new Double(size), new Double(rotation), marks[i]
                         });
                 features.add(pointFeature);
             }
@@ -100,42 +124,66 @@ public class DefaultMarkTest extends junit.framework.TestCase {
         }
 
         for (int i = 0; i < marks.length; i++) {
-            Point point = makeSamplePoint(geomFac, ((double) i * 5.0) + 10.0,
-                    5.0 + (rows * 5));
+            Point point = makeSamplePoint(geomFac, ((double) i * 5.0) + 10.0, 5.0 + (rows * 5));
             org.geotools.feature.Feature pointFeature = labelType.create(new Object[] {
                         point, marks[i], new Double(.5), new Double(0)
                     });
             features.add(pointFeature);
         }
 
-        //        System.out.println("got "+features.size()+" features");
         org.geotools.feature.FeatureCollection ft = org.geotools.feature.FeatureCollections
             .newCollection();
         ft.addAll(features);
 
-        org.geotools.map.Map map = new org.geotools.map.DefaultMap();
+        return ft;
+    }
+
+    private Style buildStyle() throws Exception {
         java.net.URL base = getClass().getResource("testData/");
 
-        //        String dataFolder = System.getProperty("dataFolder");
-        //        if(dataFolder==null){
-        //            //then we are being run by maven
-        //            dataFolder = System.getProperty("basedir");
-        //            if(dataFolder == null) dataFolder = ".";
-        //            dataFolder+="/tests/unit/testData";
-        //        }
-        //        File f = new File(dataFolder,"markTest.sld");
-        //        System.out.println("testing reader using "+ url);
         StyleFactory factory = StyleFactory.createStyleFactory();
         java.net.URL surl = new java.net.URL(base + "/markTest.sld");
         SLDStyle stylereader = new SLDStyle(factory, surl);
         Style[] style = stylereader.readXML();
 
-        map.addFeatureTable(ft, style[0]);
+        return style[0];
+    }
 
-        org.geotools.renderer.lite.LiteRenderer renderer = new org.geotools.renderer.lite.LiteRenderer();
+    private Context buildContext() throws Exception {
+        ContextFactory cf = ContextFactory.createFactory();
+        Context ctx = cf.createContext();
+        Layer layer = cf.createLayer(buildFeatureCollection(), buildStyle());
+        ctx.getLayerList().addLayer(layer);
 
-        //        org.geotools.renderer.Java2DRenderer renderer = new org.geotools.renderer.Java2DRenderer();
-        java.awt.Frame frame = new java.awt.Frame("default mark test");
+        return ctx;
+    }
+
+    /**
+     * Test lite renderer
+     */
+    public void testLiteRenderer() throws Exception {
+        Context ctx = buildContext();
+        performTestOnRenderer(new LiteRenderer(ctx));
+    }
+
+    /**
+     * Test j2d renderer
+     */
+    public void testJ2DRenderer() throws Exception {
+        Context ctx = buildContext();
+        StyledRenderer sr = new StyledRenderer(null);
+        sr.setContext(ctx);
+        performTestOnRenderer(sr);
+    }
+
+    /**
+     * Perform test on the passed renderer, which must be already configured with its context
+     */
+    private void performTestOnRenderer(Renderer2D renderer)
+        throws Exception {
+        java.net.URL base = getClass().getResource("testData/");
+
+        java.awt.Frame frame = new java.awt.Frame("Mark test (" + renderer.getClass().getName());
         frame.addWindowListener(new java.awt.event.WindowAdapter() {
                 public void windowClosing(java.awt.event.WindowEvent e) {
                     e.getWindow().dispose();
@@ -144,24 +192,29 @@ public class DefaultMarkTest extends junit.framework.TestCase {
 
         java.awt.Panel p = new java.awt.Panel();
         frame.add(p);
-        frame.setSize(300, 300);
-        frame.setLocation(300, 0);
+        frame.setSize(400, 400);
+        frame.setLocation(0, 0);
         frame.setVisible(true);
-        renderer.setOutput(p.getGraphics(), p.getBounds());
-        map.render(renderer, ex); //and finaly try and draw it!
+
+        AffineTransform at = new AffineTransform();
+        at.translate(0, 400);
+        at.scale(9, -9);
+        renderer.paint((Graphics2D) p.getGraphics(), p.getBounds(), at);
 
         int w = 400;
         int h = 400;
-        java.awt.image.BufferedImage image = new java.awt.image.BufferedImage(w,
-                h, java.awt.image.BufferedImage.TYPE_INT_RGB);
-        java.awt.Graphics g = image.getGraphics();
+        java.awt.image.BufferedImage image = new java.awt.image.BufferedImage(w, h,
+                java.awt.image.BufferedImage.TYPE_INT_RGB);
+        java.awt.Graphics2D g = (Graphics2D) image.getGraphics();
         g.setColor(java.awt.Color.white);
         g.fillRect(0, 0, w, h);
-        renderer.setOutput(g, new java.awt.Rectangle(0, 0, w, h));
-        map.render(renderer, ex); //and finaly try and draw it!
+        renderer.paint(g, new java.awt.Rectangle(0, 0, w, h), at);
+
+        // Thread.sleep(5000);
+        frame.dispose();
 
         java.io.File file = new java.io.File(base.getPath(),
-                "DefaultMarkTest.png");
+                "DefaultMarkTest_" + renderer.getClass().getName().replace('.', '_') + ".png");
         java.io.FileOutputStream out = new java.io.FileOutputStream(file);
         boolean fred = javax.imageio.ImageIO.write(image, "PNG", out);
 
@@ -170,11 +223,9 @@ public class DefaultMarkTest extends junit.framework.TestCase {
         }
 
         java.io.File file2 = new java.io.File(base.getPath() + "/exemplars/",
-                "DefaultMarkTest.png");
-        System.out.println("about to load " + file2 + " to carry out checks");
+                "DefaultMarkTest_" + renderer.getClass().getName().replace('.', '_') + ".png");
 
-        RenderedImage image2 = (RenderedImage) JAI.create("fileload",
-                file2.toString());
+        RenderedImage image2 = (RenderedImage) JAI.create("fileload", file2.toString());
 
         assertNotNull("Failed to load exemplar image", image2);
 
@@ -192,23 +243,12 @@ public class DefaultMarkTest extends junit.framework.TestCase {
                 if ((notBlack(pixel1)) && (notBlack(pixel2))) { //Since text is black and fonts are not stable across platforms, ignore pixels where at least one is black.
 
                     for (int band = 0; band < data2.getNumBands(); band++) {
-                        assertEquals("mismatch in image comparison at (x: " + x
-                            + " y: " + y + " band: " + band + ")",
-                            pixel1[band], pixel2[band]);
+                        assertEquals("mismatch in image comparison at (x: " + x + " y: " + y
+                            + " band: " + band + ")", pixel1[band], pixel2[band]);
                     }
                 }
             }
         }
-
-        //        for(int band=0;band<data2.getNumBands();band++){
-        //            pix=data.getSamples(0,0,image.getWidth(),image.getHeight(),band,pix);
-        //            pix2=data2.getSamples(0,0,image2.getWidth(),image2.getHeight(),band,pix2);
-        //            for(int i=0;i<pix.length;i++){
-        //                assertEquals("mismatch in image comparision at ("+i+")",pix[i],pix2[i]);
-        //            }
-        //        }
-        //Thread.sleep(5000);
-        frame.dispose();
     }
 
     private boolean notBlack(int[] pixel) {
@@ -223,11 +263,9 @@ public class DefaultMarkTest extends junit.framework.TestCase {
         return !(isBlack);
     }
 
-    private Point makeSamplePoint(
-        final com.vividsolutions.jts.geom.GeometryFactory geomFac, double x,
-        double y) {
-        com.vividsolutions.jts.geom.Coordinate c = new com.vividsolutions.jts.geom.Coordinate(x,
-                y);
+    private Point makeSamplePoint(final com.vividsolutions.jts.geom.GeometryFactory geomFac,
+        double x, double y) {
+        com.vividsolutions.jts.geom.Coordinate c = new com.vividsolutions.jts.geom.Coordinate(x, y);
         Point point = geomFac.createPoint(c);
 
         return point;
