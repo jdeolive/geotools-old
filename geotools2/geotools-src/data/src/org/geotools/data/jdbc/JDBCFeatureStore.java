7a3e752d429b11a312f6de77db2c20863f982d8b
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-11-21 00:10:18 +0000

    added comment about atomic with auto-commit
    git-svn-id: http://svn.osgeo.org/geotools/trunk@4663 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/data/src/org/geotools/data/jdbc/JDBCFeatureStore.java
+++ geotools2/geotools-src/data/src/org/geotools/data/jdbc/JDBCFeatureStore.java
@@ -1,7 +1,7 @@
 /*
  *    Geotools2 - OpenSource mapping toolkit
  *    http://geotools.org
- *    (C) 2003, Geotools Project Managment Committee (PMC)
+ *    (C) 2002, Geotools Project Managment Committee (PMC)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
@@ -12,6 +12,7 @@
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
+ *
  */
 package org.geotools.data.jdbc;
 
@@ -29,29 +30,41 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+
 /**
- * This is a starting point for providing your own FeatureStore
- * implementation.
- * 
+ * This is a starting point for providing your own FeatureStore implementation.
+ *
  * @author Jody Garnett, Refractions Research
+ *
+ * @task REVISIT: Make modify/add/remove atomic if the transaction is
+ *       AUTO_COMMIT.  This is done by the start of each of those method
+ *       checking to see if the transaction is auto-commit, if it is then they
+ *       make a new transaction and pass that to the writer.  The writer does
+ *       its thing, and then at the end of the method you just commit the
+ *       transaction.  This way if the writer messes up its changes are rolled
+ *       back.  The old jdbc datasources supported this, and it'd be nice to
+ *       do here as well.
  */
 public class JDBCFeatureStore extends JDBCFeatureSource implements FeatureStore {
     /** Current Transaction this FeatureSource is opperating against */
     protected Transaction transaction = Transaction.AUTO_COMMIT;
-    
-    public JDBCFeatureStore(JDBCDataStore jdbcDataStore, FeatureType featureType){
-        super( jdbcDataStore, featureType );        
-    }        
-    
-    /* Retrieve the Transaction this FeatureSource is opperating against. */     
+
+    public JDBCFeatureStore(JDBCDataStore jdbcDataStore, FeatureType featureType) {
+        super(jdbcDataStore, featureType);
+    }
+
+    /* Retrieve the Transaction this FeatureSource is opperating against. */
     public Transaction getTransaction() {
         return transaction;
     }
+
     // 
     // FeatureStore implementation against DataStore API
     // 
+
     /**
      * Modifies features matching <code>filter</code>.
+     * 
      * <p>
      * Equivelent to:
      * </p>
@@ -59,23 +72,30 @@ public class JDBCFeatureStore extends JDBCFeatureSource implements FeatureStore 
      * modifyFeatures( new AttributeType[]{ type, }, new Object[]{ value, }, filter );
      * </code>
      * </pre>
+     * 
      * <p>
      * Subclasses may override this method to perform the appropriate
      * optimization for this result.
      * </p>
-     * 
-     * @see org.geotools.data.FeatureStore#modifyFeatures(org.geotools.feature.AttributeType, java.lang.Object, org.geotools.filter.Filter)
+     *
      * @param type Attribute to modify
      * @param value Modification being made to type
      * @param filter Identifies features to modify
+     *
      * @throws IOException
+     *
+     * @see org.geotools.data.FeatureStore#modifyFeatures(org.geotools.feature.AttributeType,
+     *      java.lang.Object, org.geotools.filter.Filter)
      */
     public void modifyFeatures(AttributeType type, Object value, Filter filter)
         throws IOException {
-        modifyFeatures( new AttributeType[]{ type, }, new Object[]{ value, }, filter );        
+        modifyFeatures(new AttributeType[] { type, }, new Object[] { value, },
+            filter);
     }
+
     /**
      * Modifies features matching <code>filter</code>.
+     * 
      * <p>
      * Equivelent to:
      * </p>
@@ -93,44 +113,52 @@ public class JDBCFeatureStore extends JDBCFeatureSource implements FeatureStore 
      * writer.close();
      * </code>
      * </pre>
+     * 
      * <p>
      * Subclasses may override this method to perform the appropriate
      * optimization for this result.
      * </p>
-     * 
-     * @see org.geotools.data.FeatureStore#modifyFeatures(org.geotools.feature.AttributeType, java.lang.Object, org.geotools.filter.Filter)
+     *
      * @param type Attributes to modify
      * @param value Modifications being made to type
      * @param filter Identifies features to modify
+     *
      * @throws IOException
+     *
+     * @see org.geotools.data.FeatureStore#modifyFeatures(org.geotools.feature.AttributeType,
+     *      java.lang.Object, org.geotools.filter.Filter)
      */
-    public void modifyFeatures(AttributeType[] type, Object[] value, Filter filter)
-        throws IOException {
+    public void modifyFeatures(AttributeType[] type, Object[] value,
+        Filter filter) throws IOException {
         String typeName = getSchema().getTypeName();
-        FeatureWriter writer =
-            getDataStore().getFeatureWriter( typeName, filter, getTransaction() );
+        FeatureWriter writer = getDataStore().getFeatureWriter(typeName,
+                filter, getTransaction());
         Feature feature;
+
         try {
-            while( writer.hasNext() ){
+            while (writer.hasNext()) {
                 feature = writer.next();
-                for( int i=0; i<type.length; i++){
+
+                for (int i = 0; i < type.length; i++) {
                     try {
-                        feature.setAttribute( type[i].getName(), value[i] );
-                    }
-                    catch (IllegalAttributeException e) {
+                        feature.setAttribute(type[i].getName(), value[i]);
+                    } catch (IllegalAttributeException e) {
                         throw new DataSourceException(
-                            "Could not update feature "+feature.getID() + " with "+type[i].getName()+"="+value[i], e );
+                            "Could not update feature " + feature.getID()
+                            + " with " + type[i].getName() + "=" + value[i], e);
                     }
-                }        
-                writer.write();                                
+                }
+
+                writer.write();
             }
         } finally {
             writer.close();
         }
-    }    
-    
+    }
+
     /**
      * Add Features from reader to this FeatureStore.
+     * 
      * <p>
      * Equivelent to:
      * </p>
@@ -151,50 +179,66 @@ public class JDBCFeatureStore extends JDBCFeatureSource implements FeatureStore 
      * return set;
      * </code>
      * </pre>
+     * 
      * <p>
      * (If you don't have a FeatureReader handy DataUtilities.reader() may be
      * able to help out)
      * </p>
+     * 
      * <p>
      * Subclasses may override this method to perform the appropriate
      * optimization for this result.
      * </p>
-     * @see org.geotools.data.FeatureStore#addFeatures(org.geotools.data.FeatureReader)
+     *
      * @param reader
+     *
      * @return The Set of FeatureIDs added
+     *
      * @throws IOException
+     *
+     * @see org.geotools.data.FeatureStore#addFeatures(org.geotools.data.FeatureReader)
      */
     public Set addFeatures(FeatureReader reader) throws IOException {
         Set addedFids = new HashSet();
         String typeName = getSchema().getTypeName();
         Feature feature = null;
         Feature newFeature;
-        FeatureWriter writer = getDataStore().getFeatureWriterAppend( typeName, getTransaction() );
+        FeatureWriter writer = getDataStore().getFeatureWriterAppend(typeName,
+                getTransaction());
+
         try {
-            while( reader.hasNext() ){
+            while (reader.hasNext()) {
                 try {
                     feature = reader.next();
                 } catch (Exception e) {
-                    throw new DataSourceException( "Could not add Features, problem with provided reader", e );
-                }                                     
-                newFeature = writer.next();
-                try {
-                    newFeature.setAttributes( feature.getAttributes( null ));
-                } catch (IllegalAttributeException writeProblem) {
-                    throw new DataSourceException( "Could not create "+typeName+" out of provided feature: "+feature.getID(), writeProblem );
+                    throw new DataSourceException("Could not add Features, problem with provided reader",
+                        e);
                 }
+
+                newFeature = writer.next();
+
+                try {
+                    newFeature.setAttributes(feature.getAttributes(null));
+                } catch (IllegalAttributeException writeProblem) {
+                    throw new DataSourceException("Could not create "
+                        + typeName + " out of provided feature: "
+                        + feature.getID(), writeProblem);
+                }
+
                 writer.write();
-                addedFids.add( newFeature.getID() );                         
+                addedFids.add(newFeature.getID());
             }
-        }
-        finally {
+        } finally {
             reader.close();
             writer.close();
         }
+
         return addedFids;
     }
+
     /**
      * Removes features indicated by provided filter.
+     * 
      * <p>
      * Equivelent to:
      * </p>
@@ -208,32 +252,39 @@ public class JDBCFeatureStore extends JDBCFeatureSource implements FeatureStore 
      * writer.close();
      * </code>
      * </pre>
+     * 
      * <p>
      * Subclasses may override this method to perform the appropriate
      * optimization for this result.
      * </p>
-     * 
-     * @see org.geotools.data.FeatureStore#modifyFeatures(org.geotools.feature.AttributeType, java.lang.Object, org.geotools.filter.Filter)
+     *
      * @param filter Identifies features to remove
+     *
      * @throws IOException
+     *
+     * @see org.geotools.data.FeatureStore#modifyFeatures(org.geotools.feature.AttributeType,
+     *      java.lang.Object, org.geotools.filter.Filter)
      */
-    public void removeFeatures( Filter filter) throws IOException {
+    public void removeFeatures(Filter filter) throws IOException {
         String typeName = getSchema().getTypeName();
-        FeatureWriter writer =
-            getDataStore().getFeatureWriter( typeName, filter, getTransaction() );
+        FeatureWriter writer = getDataStore().getFeatureWriter(typeName,
+                filter, getTransaction());
         Feature feature;
+
         try {
-            while( writer.hasNext() ){
+            while (writer.hasNext()) {
                 feature = writer.next();
-                writer.remove();                                
+                writer.remove();
             }
         } finally {
             writer.close();
         }
     }
+
     /**
-     * Replace with contents of reader.  
-     * * <p>
+     * Replace with contents of reader.
+     * 
+     * <p>
      * Equivelent to:
      * </p>
      * <pre><code>
@@ -253,48 +304,64 @@ public class JDBCFeatureStore extends JDBCFeatureSource implements FeatureStore 
      * writer.close();
      * </code>
      * </pre>
+     * 
      * <p>
      * Subclasses may override this method to perform the appropriate
      * optimization for this result.
      * </p>
-     * 
-     * @see org.geotools.data.FeatureStore#modifyFeatures(org.geotools.feature.AttributeType, java.lang.Object, org.geotools.filter.Filter)
+     *
      * @param reader Contents to replace with
+     *
      * @throws IOException
+     *
+     * @see org.geotools.data.FeatureStore#modifyFeatures(org.geotools.feature.AttributeType,
+     *      java.lang.Object, org.geotools.filter.Filter)
      */
     public void setFeatures(FeatureReader reader) throws IOException {
         String typeName = getSchema().getTypeName();
-        FeatureWriter writer =
-            getDataStore().getFeatureWriter( typeName, getTransaction() );
-        Feature feature, newFeature;
+        FeatureWriter writer = getDataStore().getFeatureWriter(typeName,
+                getTransaction());
+        Feature feature;
+        Feature newFeature;
+
         try {
-            while( writer.hasNext() ){
+            while (writer.hasNext()) {
                 feature = writer.next();
-                writer.remove();                                
+                writer.remove();
             }
-            while( reader.hasNext() ){
+
+            while (reader.hasNext()) {
                 try {
                     feature = reader.next();
                 } catch (Exception readProblem) {
-                    throw new DataSourceException( "Could not add Features, problem with provided reader", readProblem );
-                }                                     
-                newFeature = writer.next();
-                try {
-                    newFeature.setAttributes( feature.getAttributes( null ));
-                } catch (IllegalAttributeException writeProblem) {
-                    throw new DataSourceException( "Could not create "+typeName+" out of provided feature: "+feature.getID(), writeProblem );
+                    throw new DataSourceException("Could not add Features, problem with provided reader",
+                        readProblem);
                 }
+
+                newFeature = writer.next();
+
+                try {
+                    newFeature.setAttributes(feature.getAttributes(null));
+                } catch (IllegalAttributeException writeProblem) {
+                    throw new DataSourceException("Could not create "
+                        + typeName + " out of provided feature: "
+                        + feature.getID(), writeProblem);
+                }
+
                 writer.write();
-            }                       
+            }
         } finally {
             reader.close();
             writer.close();
         }
     }
-    public void setTransaction( Transaction transaction ){
-        if( transaction == null ){
-            throw new IllegalArgumentException( "Transaction cannot be null, did you mean Transaction.AUTO_COMMIT?");
+
+    public void setTransaction(Transaction transaction) {
+        if (transaction == null) {
+            throw new IllegalArgumentException(
+                "Transaction cannot be null, did you mean Transaction.AUTO_COMMIT?");
         }
+
         this.transaction = transaction;
-    }        
+    }
 }
