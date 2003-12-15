f60a5179be70ea67c5cc532167de47a9e5310edf
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-12-15 23:41:10 +0000

    added comment on what should be technically correct but introduces to much overhead for too little special case gain, imho
    git-svn-id: http://svn.osgeo.org/geotools/trunk@4803 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/data/src/org/geotools/data/jdbc/ResultSetFIDReader.java
+++ geotools2/geotools-src/data/src/org/geotools/data/jdbc/ResultSetFIDReader.java
@@ -16,44 +16,47 @@
  */
 package org.geotools.data.jdbc;
 
+import org.geotools.data.DataSourceException;
+import org.geotools.data.FIDReader;
+import org.geotools.data.jdbc.QueryData.RowData;
+
 //geotools imports
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.geotools.data.DataSourceException;
-import org.geotools.data.FIDReader;
-import org.geotools.data.jdbc.QueryData.RowData;
 
 /**
- * FIDReader for JDBC.  Assumes that the column being passed in is
- * a number, as the typeName is always appended on.  This is because
- * id's must start with a letter or an underscore.  If needed we
- * could only append the typeName if the column is an int.   
+ * FIDReader for JDBC.  Assumes that the column being passed in is a number, as
+ * the typeName is always appended on.  This is because id's must start with a
+ * letter or an underscore.  If needed we could only append the typeName if
+ * the column is an int.
+ *
+ * @author Chris Holmes
  *
  * @task TODO: factory construction methods?
- *
- * @author  Chris Holmes
  */
-public class ResultSetFIDReader implements FIDReader, QueryDataListener, QueryDataObserver {
-
+public class ResultSetFIDReader implements FIDReader, QueryDataListener,
+    QueryDataObserver {
+    /** The logger for the jdbc module. */
+    private static final Logger LOGGER = Logger.getLogger(
+            "org.geotools.data.jdbc");
     private QueryData queryData;
     private int column;
-    /** The logger for the jdbc module. */
-    private static final Logger LOGGER = Logger.getLogger("org.geotools.data.jdbc");
     private String typeName;
     private boolean isClosed = false;
 
-    /** Constructor that takes a QueryData object instead of a ResultSet.
-     * 
+    /**
+     * Constructor that takes a QueryData object instead of a ResultSet.
+     *
      * @param queryData The queryData object
-     * @param typename  The typename to preppend.
-     * @param column the offset at which the fid column is.
-     * Starts at 1, corresponding to the java ResultSet way of doing things.
+     * @param typename The typename to preppend.
+     * @param column the offset at which the fid column is. Starts at 1,
+     *        corresponding to the java ResultSet way of doing things.
      */
     public ResultSetFIDReader(QueryData queryData, String typename, int column) {
-        this.typeName = typename;       
+        this.typeName = typename;
         this.column = column;
         this.queryData = queryData;
         queryData.attachObserver(this);
@@ -63,16 +66,18 @@ public class ResultSetFIDReader implements FIDReader, QueryDataListener, QueryDa
         if (isClosed) {
             throw new IOException("This FIDReader has already been closed");
         }
-        
+
         try {
-           boolean hasNext = queryData.hasNext(this);
-           queryData.next(this); // need to call next now because of API differences.
-           return hasNext;
+            boolean hasNext = queryData.hasNext(this);
+            queryData.next(this); // need to call next now because of API differences.
+
+            return hasNext;
         } catch (SQLException sqlException) {
-            queryData.close( sqlException, this );
-            String msg = "Error checking for more content"; 
-            LOGGER.log(Level.SEVERE,msg,sqlException);
-            throw new DataSourceException(msg, sqlException);                         
+            queryData.close(sqlException, this);
+
+            String msg = "Error checking for more content";
+            LOGGER.log(Level.SEVERE, msg, sqlException);
+            throw new DataSourceException(msg, sqlException);
         }
     }
 
@@ -80,32 +85,39 @@ public class ResultSetFIDReader implements FIDReader, QueryDataListener, QueryDa
         if (isClosed) {
             throw new IOException("This FIDReader has already been closed");
         }
-        
+
         if (!(hasNext())) {
-            throw new IOException("No more fids, check hasNext() before " + "calling next()");
+            throw new IOException("No more fids, check hasNext() before "
+                + "calling next()");
         }
-        
-        try {            
+
+        try {
             RowData rd = queryData.getRowData(this);
-            return typeName + "." + rd.read(1);            
+            String id = rd.read(1).toString();
+            //this overhead kinda sucks.  Especially because it's just for 
+            //lame special cases.  Perhaps just check once?
+            //return id.startsWith(typeName) ? id : (typeName + "." + id);
+            return typeName + "." + id;
         } catch (SQLException sqlException) {
-            queryData.close( sqlException, this );
-            String msg = "Error obtaining more content"; 
-            LOGGER.log(Level.SEVERE,msg,sqlException);
-            throw new DataSourceException(msg, sqlException);                        
+            queryData.close(sqlException, this);
+
+            String msg = "Error obtaining more content";
+            LOGGER.log(Level.SEVERE, msg, sqlException);
+            throw new DataSourceException(msg, sqlException);
         }
     }
 
     public void close() throws IOException {
-        if (!isClosed)  {
+        if (!isClosed) {
             isClosed = true;
-            if (queryData != null)  {
-                queryData.close( null, this );
+
+            if (queryData != null) {
+                queryData.close(null, this);
                 queryData.removeObserver(this);
             }
         }
     }
-    
+
     /* (non-Javadoc)
      * @see org.geotools.data.jdbc.QueryDataListener#queryDataClosed(org.geotools.data.jdbc.JDBCDataStore.QueryData)
      */
@@ -114,8 +126,12 @@ public class ResultSetFIDReader implements FIDReader, QueryDataListener, QueryDa
     }
 
     /**
-     * @see org.geotools.data.jdbc.QueryDataListener#rowDeleted(org.geotools.data.jdbc.JDBCDataStore.QueryData)
+     * DOCUMENT ME!
+     *
      * @param queryData
+     *
+     * @see org.geotools.data.jdbc.QueryDataListener#rowDeleted(org.geotools.data.jdbc.JDBCDataStore.QueryData)
      */
-    public void rowDeleted(QueryData queryData) {}
+    public void rowDeleted(QueryData queryData) {
+    }
 }
