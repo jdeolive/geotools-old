61eaf8bb5a7fefc96687b6eaf6c7227d7a3e26f3
cholmesny <cholmesny@e5c1c795-43da-0310-a71f-fac65c449510>
2003-12-31 19:01:34 +0000

    fix to fid encoding to handle DUMMYFEATURE fids that cite does
    git-svn-id: http://svn.osgeo.org/geotools/trunk@4842 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/postgis/src/org/geotools/filter/SQLEncoderPostgis.java
+++ geotools2/geotools-src/postgis/src/org/geotools/filter/SQLEncoderPostgis.java
@@ -18,8 +18,9 @@ package org.geotools.filter;
 
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.io.WKTWriter;
-import java.util.logging.Logger;
 import java.util.Arrays;
+import java.util.logging.Logger;
+
 
 /**
  * Encodes a filter into a SQL WHERE statement for postgis.  This class adds
@@ -28,27 +29,25 @@ import java.util.Arrays;
  *
  * @author Chris Holmes, TOPP
  *
- * @task TODO: integrated with SQLEncoderPostgisGeos.java, as there no 
- * real reason to have two different classes.  We just need to do testing
- * to make sure both handle everything.  At the very least have the geos
- * one extend more intelligently.
+ * @task TODO: integrated with SQLEncoderPostgisGeos.java, as there no  real
+ *       reason to have two different classes.  We just need to do testing to
+ *       make sure both handle everything.  At the very least have the geos
+ *       one extend more intelligently.
  */
 public class SQLEncoderPostgis extends SQLEncoder
     implements org.geotools.filter.FilterVisitor {
-        
-    /**
-     * The filters that this encoder can processed.
-     * (Note this value shadows private capabilities in superclass)
-     */
-    private FilterCapabilities capabilities = new FilterCapabilities();
-
     /** Standard java logger */
     private static Logger LOGGER = Logger.getLogger("org.geotools.filter");
 
     /** To write geometry so postgis can read it. */
     private static WKTWriter wkt = new WKTWriter();
 
-    
+    /**
+     * The filters that this encoder can processed. (Note this value shadows
+     * private capabilities in superclass)
+     */
+    private FilterCapabilities capabilities = new FilterCapabilities();
+
     /**
      * The srid of the schema, so the bbox conforms.  Could be better to have
      * it in the bbox filter itself, but this works for now.
@@ -57,15 +56,13 @@ public class SQLEncoderPostgis extends SQLEncoder
 
     /** The geometry attribute to use if none is specified. */
     private String defaultGeom;
-
     private boolean useGeos;
-
     private String fidColumn;
 
     /**
      * Empty constructor TODO: rethink empty constructor, as BBOXes _need_ an
      * SRID, must make client set it somehow.  Maybe detect when encode is
-     * called? 
+     * called?
      */
     public SQLEncoderPostgis() {
         capabilities.addType(AbstractFilter.LOGIC_OR);
@@ -80,26 +77,17 @@ public class SQLEncoderPostgis extends SQLEncoder
         capabilities.addType(AbstractFilter.NULL);
         capabilities.addType(AbstractFilter.BETWEEN);
         capabilities.addType((short) 12345);
-        capabilities.addType((short) -12345);        
-	capabilities.addType(AbstractFilter.GEOMETRY_BBOX);
+        capabilities.addType((short) -12345);
+        capabilities.addType(AbstractFilter.GEOMETRY_BBOX);
         capabilities.addType(AbstractFilter.FID);
     }
 
     public SQLEncoderPostgis(boolean looseBbox) {
-	this();
-	if (!looseBbox) {
-	    this.useGeos = true;
-	}
-    }
-	    
-    /**
-     * Capabilities of this encoder.
-     * 
-     * @see org.geotools.filter.SQLEncoder#getCapabilities()
-     * @return
-     */
-    public FilterCapabilities getCapabilities() {
-        return capabilities;
+        this();
+
+        if (!looseBbox) {
+            this.useGeos = true;
+        }
     }
 
     /**
@@ -108,8 +96,19 @@ public class SQLEncoderPostgis extends SQLEncoder
      * @param srid spatial reference id to encode geometries with.
      */
     public SQLEncoderPostgis(int srid) {
-	this(true);
-	this.srid = srid;
+        this(true);
+        this.srid = srid;
+    }
+
+    /**
+     * Capabilities of this encoder.
+     *
+     * @return
+     *
+     * @see org.geotools.filter.SQLEncoder#getCapabilities()
+     */
+    public FilterCapabilities getCapabilities() {
+        return capabilities;
     }
 
     /**
@@ -124,13 +123,16 @@ public class SQLEncoderPostgis extends SQLEncoder
     }
 
     /**
-     * Sets the fid column to use for fid filters.  This _must_ be set if
-     * fid filters are used.  
+     * Sets the fid column to use for fid filters.  This _must_ be set if fid
+     * filters are used.
+     *
+     * @param fidColumnName DOCUMENT ME!
+     *
      * @task TODO: put in constructor.
      * @task TODO: if set to null don't report in capabilities.
      */
-    public void setFidColumn(String fidColumnName){
-	this.fidColumn = fidColumnName;
+    public void setFidColumn(String fidColumnName) {
+        this.fidColumn = fidColumnName;
     }
 
     /**
@@ -150,7 +152,7 @@ public class SQLEncoderPostgis extends SQLEncoder
         this.defaultGeom = name;
     }
 
-     /**
+    /**
      * This should never be called. This can only happen if a subclass of
      * AbstractFilter failes to implement its own version of
      * accept(FilterVisitor);
@@ -179,10 +181,23 @@ public class SQLEncoderPostgis extends SQLEncoder
         }
     }
 
-       /**
-     * DOCUMENT ME!
+    /**
+     * Encodes an FidFilter.
      *
      * @param filter
+     *
+     * @task TODO: We're getting problems when the fidColumn is an int but  the
+     *       fidFilter passes in something that is not an int.  Ideally we
+     *       should figure out if the fidColumn type matches that passed in
+     *       but for now we are going to make an assumption based on the fact
+     *       that all fids returned by postgis will be of form rail.342.  So
+     *       if we don't have a '.' in the fid, we will just pass -1 for the
+     *       column name, which should gaurantee it won't match anything.  The
+     *       one place where this _could_ mess us up is if users pass in
+     *       something like 'road.rail3'.  But all should really use fids that
+     *       they are returned, and this is really just a fix to pass the cite
+     *       tests.  We can revisit once we figure out fid encoding in
+     *       general.
      *
      * @see org.geotools.filter.SQLEncoder#visit(org.geotools.filter.FidFilter)
      */
@@ -194,15 +209,20 @@ public class SQLEncoderPostgis extends SQLEncoder
             try {
                 out.write(fidColumn);
                 out.write(" = '");
-                
+
                 int pos;
-                
+
                 if ((pos = fids[i].indexOf('.')) != -1) {
-                	out.write(fids[i].substring(pos + 1));
+                    out.write(fids[i].substring(pos + 1));
                 } else {
-					out.write(fids[i]);
+                    //not what we're expecting, postgis always returns in 
+                    //rail.3 form, so just write out something that will not
+                    //match but that will also parse (not causing errors).
+                    out.write("-1");
+
+                    //out.write(fids[i]);
                 }
-                
+
                 out.write("'");
 
                 if (i < (fids.length - 1)) {
@@ -213,7 +233,6 @@ public class SQLEncoderPostgis extends SQLEncoder
             }
         }
     }
-
 
     /**
      * Turns a geometry filter into the postgis sql bbox statement.
@@ -230,31 +249,33 @@ public class SQLEncoderPostgis extends SQLEncoder
             DefaultExpression right = (DefaultExpression) filter
                 .getRightGeometry();
 
-	    try {	    
-		if (useGeos) {
-		    out.write("NOT disjoint(");
-		}
-		if (left == null) {
-		    out.write("\"" + defaultGeom + "\"");
-		} else {
-		    left.accept(this);
-		}
-		
-		if (useGeos) {
-		    out.write(", ");
-		} else {
-		    out.write(" && ");
-		}		    
-		if (right == null) {
-		    out.write("\"" + defaultGeom + "\"");
-		} else {
-		    right.accept(this);
-		}
-		if (useGeos) {
-		    out.write(")");
-		}
+            try {
+                if (useGeos) {
+                    out.write("NOT disjoint(");
+                }
 
-	    } catch (java.io.IOException ioe) {
+                if (left == null) {
+                    out.write("\"" + defaultGeom + "\"");
+                } else {
+                    left.accept(this);
+                }
+
+                if (useGeos) {
+                    out.write(", ");
+                } else {
+                    out.write(" && ");
+                }
+
+                if (right == null) {
+                    out.write("\"" + defaultGeom + "\"");
+                } else {
+                    right.accept(this);
+                }
+
+                if (useGeos) {
+                    out.write(")");
+                }
+            } catch (java.io.IOException ioe) {
                 LOGGER.warning("Unable to export filter" + ioe);
                 throw new RuntimeException("io error while writing", ioe);
             }
