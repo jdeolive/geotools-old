9d5c50aa4aab712928f89cc56dfb96d87e45c891
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2004-01-14 20:25:10 +0000

    updated datastore param useage
    git-svn-id: http://svn.osgeo.org/geotools/trunk@4926 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/data/src/org/geotools/data/DataStoreFactorySpi.java
+++ geotools2/geotools-src/data/src/org/geotools/data/DataStoreFactorySpi.java
@@ -18,6 +18,7 @@ package org.geotools.data;
 
 import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
 
@@ -176,6 +177,14 @@ public interface DataStoreFactorySpi extends org.geotools.factory.Factory {
         final public String description;
 
         /**
+         * Sampel value provided as an example for user input.
+         * <p>
+         * May be passed to getAsText( sample ) for inital text based
+         * user interface default.
+         * </p>
+         */
+        final public Object sample;
+        /**
          * Provides support for text representations
          * 
          * <p>
@@ -225,18 +234,114 @@ public interface DataStoreFactorySpi extends org.geotools.factory.Factory {
          * @param required <code>true</code> is param is required
          */
         public Param(String key, Class type, String description, boolean required) {
+            this( key, type, description, required, null );
+        }
+        /**
+         * Provides support for text representations
+         *
+         * @param key Key used to file this Param in the Parameter Map for
+         *        createDataStore
+         * @param type Class type intended for this Param
+         * @param description User description of Param (40 chars or less)
+         * @param required <code>true</code> is param is required
+         * @param sampleValue Sample value as an example for user input
+         */
+        public Param(String key, Class type, String description, boolean required, Object sample ) {
             this.key = key;
             this.type = type;
             this.description = description;
             this.required = required;
+            this.sample = sample;
         }
-        public String getAsText(Object value) {
+        
+        /**
+         * Lookup Param in a user supplied map.
+         * <p>
+         * Type conversion will occur if required, this may result in an
+         * IOException. An IOException will be throw in the Param is required
+         * and the Map does not contain the Map.
+         * </p>
+         * <p>
+         * The handle method is used to process the user's value. 
+         * </p>
+         * @param map Map of user input
+         * @return Parameter as specified in map
+         * @throws IOException if parse could not handle value
+         */
+        public Object lookUp( Map map ) throws IOException{
+            if( !map.containsKey( key ) ){
+                if( required ){
+                    throw new IOException("Parameter "+key+" is required:"+description );
+                }
+                else {
+                    return null;
+                }
+            }
+            Object value = map.get( key );
+            
+            if( value == null ){
+                return null;
+            }
+            if( value instanceof String && type != String.class ){
+                value = handle( (String) value );                
+            }
+            if( value == null ){
+                return null;
+            }
+            if( !type.isInstance(value)){
+                throw new IOException(
+                        type.getName()+ " required for parameter "+key+": not "+value.getClass().getName()                    
+                );
+            }
+            return value;
+        }
+        /** Convert value to text representation for this Parameter */
+        public String text(Object value) {
             return value.toString();
         }
-
+        
+        /**
+         * Handle text in a sensible manner.
+         * <p>
+         * Performs the most common way of handling text value:
+         * </p>
+         * <ul>
+         * <li>null: If text is null</li>
+         * <li>origional text: if type == String.class</li>
+         * <li>null: if type != String.class and text.getLength == 0</li>
+         * <li>parse( text ): if type != String.class</li>
+         * </ul>
+         * 
+         * @param text
+         * @return Value as processed by text
+         * @throws IOException If text could not be parsed
+         */
+        public Object handle( String text ) throws IOException {
+            if( text == null ){
+                return null;
+            }
+            if( type == String.class ){
+                return text;
+            }
+            if( text.length() == 0 ){
+                return null;
+            }
+            try {
+                return parse( text );
+            }
+            catch( IOException ioException ){
+                throw ioException;
+            }
+            catch (Throwable throwable) {
+                throw new DataSourceException(
+                        "Problem creating "+type.getName()+" from '"+text+"'",
+                        throwable
+                );
+            }            
+        }
+        
         /**
          * Provides support for text representations
-         * 
          * <p>
          * Provides basic support for common types using reflection.
          * </p>
@@ -246,22 +351,47 @@ public interface DataStoreFactorySpi extends org.geotools.factory.Factory {
          * types.
          * </p>
          *
-         * @param text Text representation
+         * @param text Text representation of type should not be null or empty
          *
-         * @return Object converted from text representation, or
-         *         <code>null</code> if unsuccessful
+         * @return Object converted from text representation
+         * @throws IOException If text could not be parsed
          */
-        public Object setAsText(String text) {
+        public Object parse(String text) throws Throwable {
             Constructor constructor;
-
             try {
                 constructor = type.getConstructor(new Class[] { String.class });
-
-                return constructor.newInstance(new Object[] { text, });
-            } catch (Exception e) {
-                // fail to construct
-                return null;
+            } catch (SecurityException e) {
+                //  type( String ) constructor is not public
+                throw new IOException("Could not create "+type.getName()+" from text" );
             }
-        }
+            catch (NoSuchMethodException e) {
+                // No type( String ) constructor
+                throw new IOException("Could not create "+type.getName()+" from text" );
+            }
+            try {
+               return constructor.newInstance(new Object[] { text, });               
+            }
+            catch (IllegalArgumentException illegalArgumentException) {
+                throw new DataSourceException(
+                    "Could not create "+type.getName()+": from '"+text+"'",
+                    illegalArgumentException
+                );
+            }
+            catch (InstantiationException instantiaionException) {
+                throw new DataSourceException(
+                        "Could not create "+type.getName()+": from '"+text+"'",
+                        instantiaionException
+                );
+            }
+            catch (IllegalAccessException illegalAccessException) {
+                throw new DataSourceException(
+                        "Could not create "+type.getName()+": from '"+text+"'",
+                        illegalAccessException
+                );                
+            }
+            catch (InvocationTargetException targetException) { 
+                throw targetException.getCause();        
+            }
+        }        
     }
 }
