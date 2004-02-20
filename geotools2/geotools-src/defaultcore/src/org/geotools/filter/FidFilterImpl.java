d937a4b391546f8a5cfa562fe64221a0754d5409
seangeo <seangeo@e5c1c795-43da-0310-a71f-fac65c449510>
2004-02-20 00:18:31 +0000

    Remove fids methods implemented. See GEOT-64.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@5069 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/defaultcore/src/org/geotools/filter/FidFilterImpl.java
+++ geotools2/geotools-src/defaultcore/src/org/geotools/filter/FidFilterImpl.java
@@ -21,6 +21,7 @@ package org.geotools.filter;
 import org.geotools.feature.Feature;
 
 // J2SE dependencies
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -33,7 +34,7 @@ import java.util.logging.Logger;
  * uniquely.
  *
  * @author Rob Hranac, TOPP
- * @version $Id: FidFilterImpl.java,v 1.12 2003/08/11 18:48:25 cholmesny Exp $
+ * @version $Id: FidFilterImpl.java,v 1.13 2004/02/20 00:18:31 seangeo Exp $
  */
 public class FidFilterImpl extends AbstractFilterImpl implements FidFilter {
     /** Logger for the default core module. */
@@ -152,11 +153,39 @@ public class FidFilterImpl extends AbstractFilterImpl implements FidFilter {
      *
      * @return the internally stored fids.
      */
-    Set getFidsSet() {
+    public Set getFidsSet() {
         return fids;
     }
+    
+	/** 
+	 * Removes a collection of feature IDs from the filter. 
+	 * 
+	 * @param fids A collection of feature IDs. 
+	 */ 
+	public void removeAllFids(Collection fidsToRemove) { 	    
+	   fids.removeAll(fidsToRemove); 
+	} 
 
-    /**
+	/** 
+	 * Adds a collection of feature IDs to the filter. 
+	 * 
+	 * @param fids A collection of feature IDs. 
+	 */ 
+	public void addAllFids(Collection fidsToAdd) { 
+	   fids.addAll(fidsToAdd); 
+	} 
+
+	/** 
+	 * Removes a feature ID from the filter. 
+	 * 
+	 * @param fid A single feature ID. 
+	 */ 
+	public final void removeFid(String fid) {		 
+		fids.remove(fid); 
+	} 
+
+	 
+	/**
      * Used by FilterVisitors to perform some action on this filter instance.
      * Typicaly used by Filter decoders, but may also be used by any thing
      * which needs infomration from filter structure. Implementations should
