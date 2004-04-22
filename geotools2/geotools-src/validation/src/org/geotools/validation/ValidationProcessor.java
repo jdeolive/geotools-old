a9f9bff1957fe1297d9fab823d0918dfb1af8e89
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2004-04-22 08:44:51 +0000

    Update to log messages
    git-svn-id: http://svn.osgeo.org/geotools/trunk@5348 e5c1c795-43da-0310-a71f-fac65c449510

--- geotools2/geotools-src/validation/src/org/geotools/validation/ValidationProcessor.java
+++ geotools2/geotools-src/validation/src/org/geotools/validation/ValidationProcessor.java
@@ -96,7 +96,7 @@ import java.util.logging.Logger;
  *
  * @author bowens, Refractions Research, Inc.
  * @author $Author: jive $ (last modification)
- * @version $Id: ValidationProcessor.java,v 1.9 2004/04/22 08:22:15 jive Exp $
+ * @version $Id: ValidationProcessor.java,v 1.10 2004/04/22 08:44:51 jive Exp $
  */
 public class ValidationProcessor {
 	private static final Logger LOGGER = Logger.getLogger("org.geotools.validation");
@@ -372,6 +372,9 @@ public class ValidationProcessor {
      */
     public void runIntegrityTests(Set typeRefs, Map stores, Envelope envelope,
         ValidationResults results) throws Exception {
+    	if( integrityLookup == null || integrityLookup.size() == 0){
+    		return;
+    	}
     	LOGGER.fine("Starting validation tests for:"+ typeRefs );
     	LOGGER.fine("Marshalled "+stores.size()+" FeatureSources for testing" );
     	LOGGER.fine("Testing limited to "+envelope );
