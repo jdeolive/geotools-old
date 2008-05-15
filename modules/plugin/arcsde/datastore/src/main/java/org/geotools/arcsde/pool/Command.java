6e91db5faaf25291026d4c4b626eaafe55931485
groldan <groldan@e5c1c795-43da-0310-a71f-fac65c449510>
2008-05-15 12:50:21 +0000

    javadoc fix
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30291 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/plugin/arcsde/datastore/src/main/java/org/geotools/arcsde/pool/Command.java
+++ modules/plugin/arcsde/datastore/src/main/java/org/geotools/arcsde/pool/Command.java
@@ -7,22 +7,20 @@ import com.esri.sde.sdk.client.SeConnection;
 /**
  * Runnable used to interact with an ArcSDEConnection.
  * <p>
- * Instances of this class can the sent to ArcSDEDataStore.getConnection( ArcSDERunnable )
- * in order to be executed. An ArcSDERunnable has exclusive access to the Connection for the
- * duration. This facility is used to prevent a series of complicated locks and try/catch/finally
- * code.
+ * Instances of this class can the sent to {@link Session#execute(Command)} in order to be executed.
+ * An ArcSDERunnable has exclusive access to the Connection for the duration. This facility is used
+ * to prevent a series of complicated locks and try/catch/finally code.
  * 
  * @author Jody Garnett
  */
 public abstract class Command {
     /**
-     * Executed to operate on an SeConnection, a Command is scheduled for execution
-     * on a Session.
+     * Executed to operate on an SeConnection, a Command is scheduled for execution on a Session.
      * <p>
      * Please keep in mind that a Command should be short in duration; you are sharing this
      * SeConnection with other threads.
      * 
      * @param connection connection used to interact with ArcSDE
      */
-    public abstract void execute( SeConnection connection ) throws IOException;
+    public abstract void execute(SeConnection connection) throws IOException;
 }
