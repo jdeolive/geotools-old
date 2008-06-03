7b02e90f4328611647a0e50ffa443d1fcb5be3d5
desruisseaux <desruisseaux@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-03 13:38:38 +0000

    Added an org.geotools.console package as a home for utilities to be launch from the command-line. Those utilities will migrate progressively. This package will probably move to a "gt-util" module together with outher metadata packages later.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30489 e5c1c795-43da-0310-a71f-fac65c449510

---
+++ modules/library/metadata/src/main/java/org/geotools/console/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * A set of tools to be invoked from the {@code java} command in a shell.
+ * <p>
+ * <b>TODO</b> Some commands defined in random places in the GeoTools code base may move here.
+ */
+package org.geotools.console;
