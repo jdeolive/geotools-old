e88cfc3bc2f461d39ad51d63bd0c47b3a4bc658a
acuster <acuster@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-04 18:31:27 +0000

    Header cleanup 1 -- prep for Cedric's script --- modules/library: add missing
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30512 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/metadata/src/main/java/org/geotools/resources/jaxb/uom/package-info.java
+++ modules/library/metadata/src/main/java/org/geotools/resources/jaxb/uom/package-info.java
@@ -1,3 +1,18 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 /**
  * <p>JAXB designed package for adapters. </p>
  * <p>Regroups all adapters designed to handle <i>unit of measure</i> as the
