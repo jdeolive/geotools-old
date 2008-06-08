a7e2bb50deb0a06f55174cbd3ec74fc40987a8c5
acuster <acuster@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-08 11:26:56 +0000

    Copyright headers: build/maven
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30567 e5c1c795-43da-0310-a71f-fac65c449510

--- build/maven/archetype/src/main/resources/archetype-resources/src/main/java/DemoData.java
+++ build/maven/archetype/src/main/resources/archetype-resources/src/main/java/DemoData.java
@@ -1,8 +1,8 @@
 /*
- *    GeoTools - OpenSource mapping toolkit
+ *    GeoTools - The Open Source Java GIS Tookit
  *    http://geotools.org
- *    (C) 2006, GeoTools Project Managment Committee (PMC)
- *    (C) 2006, Adrian Custer, assigned to the PMC.
+ * 
+ *    (C) 2006-2008, Open Source Geospatial Foundation (OSGeo)
  *
  *    This file is hereby placed into the Public Domain. This means anyone is
  *    free to do whatever they wish with this file. Use it well and enjoy!
