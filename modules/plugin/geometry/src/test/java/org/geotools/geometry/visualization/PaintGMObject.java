778d64ef7149d42634e904013e1d623409a0e3e9
acuster <acuster@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-08 14:10:07 +0000

    Geometry: fix typo; add LICENSE.txt
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30586 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/plugin/geometry/src/test/java/org/geotools/geometry/visualization/PaintGMObject.java
+++ modules/plugin/geometry/src/test/java/org/geotools/geometry/visualization/PaintGMObject.java
@@ -14,7 +14,7 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-ackage org.geotools.geometry.visualization;
+package org.geotools.geometry.visualization;
 
 
 
