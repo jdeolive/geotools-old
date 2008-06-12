83b7f407372f788b7b992934d1087cebc9fde735
acuster <acuster@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-12 17:15:49 +0000

    Copyright headers: cleanup library/referencing
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30639 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/referencing/src/main/java/org/geotools/referencing/operation/builder/TINTriangle.java
+++ modules/library/referencing/src/main/java/org/geotools/referencing/operation/builder/TINTriangle.java
@@ -1,5 +1,5 @@
 /*
- *    Geotools2 - OpenSource mapping toolkit
+ *    GeoTools - The Open Source Java GIS Tookit
  *    http://geotools.org
  * 
  *    (C) 2002-2008, Open Source Geospatial Foundation (OSGeo)
--- modules/library/referencing/src/main/java/org/geotools/referencing/wkt/Formatter.java
+++ modules/library/referencing/src/main/java/org/geotools/referencing/wkt/Formatter.java
@@ -2,13 +2,12 @@
  *    GeoTools - The Open Source Java GIS Tookit
  *    http://geotools.org
  *
- * 
  *    (C) 2004-2008, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
