01f4e095dc10a31138781aefe89990a8a1f85986
acuster <acuster@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-12 21:56:34 +0000

    Copyright headers: extn/shapefile-renderer, this time with feeling (and the el in Toolkit)
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30663 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/extension/shapefile-renderer/perf/org/geotools/renderer/shape/Timing.java
+++ modules/extension/shapefile-renderer/perf/org/geotools/renderer/shape/Timing.java
@@ -1,18 +1,18 @@
 /*
- *    Geotools2 - OpenSource mapping toolkit
+ *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
- *    (C) 2002, Geotools Project Managment Committee (PMC)
+ *
+ *    (C) 2002-2008, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation;
  *    version 2.1 of the License.
  *
- *    This library is distributed in the hryope that it will be useful,
+ *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
- *
  */
 package org.geotools.renderer.shape;
 
