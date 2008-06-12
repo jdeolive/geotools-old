42a0c040b8ef2fd490bd57cf575da518ef2408cd
acuster <acuster@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-12 21:44:16 +0000

    Copyright headers: extn/validation, this time with feeling (and the el in Toolkit)
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30662 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/extension/validation/src/main/java/org/geotools/validation/spatial/LineCoveredByPolygonBoundaryValidationBeanInfo.java
+++ modules/extension/validation/src/main/java/org/geotools/validation/spatial/LineCoveredByPolygonBoundaryValidationBeanInfo.java
@@ -1,19 +1,18 @@
 /*
- *    GeoTools - OpenSource mapping toolkit
+ *    GeoTools - The Open Source Java GIS Toolkit
  *    http://geotools.org
- *    (C) 2004-2006, Geotools Project Managment Committee (PMC)
+ *
+ *    (C) 2004-2008, Open Source Geospatial Foundation (OSGeo)
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
- *
- *    Created on Jan 22, 2004
  */
 package org.geotools.validation.spatial;
 
