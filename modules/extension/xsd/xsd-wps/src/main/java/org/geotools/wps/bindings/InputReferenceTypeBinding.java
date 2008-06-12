2caaa629d7ad5567944c885f71c3351c637f41f7
acuster <acuster@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-12 22:48:07 +0000

    Copyright headers: extn/xsd, headers for all of Justin's work
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30665 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/extension/xsd/xsd-wps/src/main/java/org/geotools/wps/bindings/InputReferenceTypeBinding.java
+++ modules/extension/xsd/xsd-wps/src/main/java/org/geotools/wps/bindings/InputReferenceTypeBinding.java
@@ -1,7 +1,19 @@
-/**
-    @author lreed@refractions.net
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
-
 package org.geotools.wps.bindings;
 
 import java.util.List;
