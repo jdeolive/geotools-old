ef692cd66fbc6a842ecb4f5591204ea0dddfe39e
johann.sorel <johann.sorel@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-17 15:42:09 +0000

    some fixes in CategorizeFunction.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30751 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/render/src/main/java/org/geotools/filter/CategorizeFunction.java
+++ modules/library/render/src/main/java/org/geotools/filter/CategorizeFunction.java
@@ -29,6 +29,7 @@ import org.opengis.filter.expression.ExpressionVisitor;
 import org.opengis.filter.expression.Function;
 import org.opengis.filter.expression.Literal;
 import org.opengis.filter.Filter;
+
 /**
  * Implementation of "Categorize" as a normal function.
  * <p>
@@ -47,149 +48,179 @@ import org.opengis.filter.Filter;
  * <li>Literal: (Optional) succeeding or preceding
  * </ol>
  * In reality any expression will do.
- * @author Jody  
+ * @author Jody Garnett
+ * @author Johann Sorel (Geomatys)
  */
-public class CategorizeFunction implements Function {	
-	/** Use as a literal value to indicate behaviour of threshold boundary */
-	public String SUCCEEDING = "succeeding";
-	
-	/** Use as a literal value to indicate behaviour of threshold boundary */	
-	public String PRECEDING = "preceding";
-	
-	/**
-	 * Use as a PropertyName when defining a color map.
-	 * The "Raterdata" is expected to apply to only a single band; if multiple
-	 * bands are provided it is probably a mistake; but we will use the maximum
-	 * value (since we are working against a threshold).
-	 */
-	public String RASTER_DATA = "Rasterdata";
-	
-	private List<Expression> parameters;
-	private Literal fallback;
-	
-	/**
-	 * Make the instance of FunctionName available in
-	 * a consistent spot.
-	 */
-	public static FunctionName NAME = new Name();
-	
-	/**
-	 * Describe how this function works.
-	 * (should be available via FactoryFinder lookup...)
-	 */
-	public static class Name implements FunctionName {
-		public int getArgumentCount() {
-			return -2; // indicating unbounded, 2 minimum
-		}
-		public List<String> getArgumentNames() {
-			return Arrays.asList( new String[]{
-					"LookupValue",
-					"Value",
-					"Threshold 1", "Value 1",
-					"Threshold 2", "Value 2",
-					"succeeding or preceding"
-			});
-		}
-		public String getName() {
-			return "Categorize";
-		}		
-	};
-	
-	public CategorizeFunction(){
-		this.parameters = new ArrayList<Expression>();
-		this.fallback = null;
-	}
-	public CategorizeFunction( List<Expression> parameters, Literal fallback ){
-		this.parameters = parameters;
-		this.fallback = fallback;
-	}
-	
-	public String getName() {
-		return "Categorize";
-	}
-	
-	public List<Expression> getParameters() {
-		return Collections.unmodifiableList( parameters );
-	}
+public class CategorizeFunction implements Function {
 
-	public Object accept(ExpressionVisitor visitor, Object extraData) {
-		return visitor.visit(this, extraData);
-	}
+    /** Use as a literal value to indicate behaviour of threshold boundary */
+    public static final String SUCCEEDING = "succeeding";
+    
+    /** Use as a literal value to indicate behaviour of threshold boundary */
+    public static final String PRECEDING = "preceding"; 
+    
+    /**
+     * Use as a PropertyName when defining a color map.
+     * The "Raterdata" is expected to apply to only a single band; if multiple
+     * bands are provided it is probably a mistake; but we will use the maximum
+     * value (since we are working against a threshold).
+     */
+    public static final String RASTER_DATA = "Rasterdata";
+    
+    private final List<Expression> parameters;
+    private final Literal fallback;
+    
+    
+    /**
+     * Make the instance of FunctionName available in
+     * a consistent spot.
+     */
+    public static final FunctionName NAME = new Name();
 
-	public Object evaluate(Object object) {
-		return evaluate( object, Object.class );
-	}
+    /**
+     * Describe how this function works.
+     * (should be available via FactoryFinder lookup...)
+     */
+    public static class Name implements FunctionName {
 
-	public <T> T evaluate(Object object, Class<T> context) {
-		Expression lookupValue = parameters.get(0);
-		String belongsTo = queryThreshdoldBelongsTo( object );		
-		Expression currentValue = parameters.get(1);
-		
-		List<Expression> splits;
-		if( parameters.size() == 2 ){
-			return currentValue.evaluate( object, context );
-		}
-		else if ( parameters.size() % 2 == 0 ){
-			splits = parameters.subList(2, parameters.size());
-		}
-		else {
-			splits = parameters.subList(2, parameters.size()-1);
-		}
-		FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
-		for( int i = 0; i < splits.size(); i+=2){
-			Expression threshhold = splits.get(i);
-			if( i+1 == splits.size() ) {
-				// we must be past the limit - but it does not make sense to end on a threshold!
-				break;
-			}			
-			Expression value = splits.get(i+1);
-			Filter isIncludedInThreshold;
-			if( PRECEDING.equals( belongsTo ) ){
-				isIncludedInThreshold = ff.greater( lookupValue, threshhold );
-			}
-			else {
-				isIncludedInThreshold = ff.greaterOrEqual(lookupValue, threshhold );
-			}
-			if( isIncludedInThreshold.evaluate( object ) ){
-				currentValue = value;
-			}
-			else {
-				break;
-			}
-		}
-		return currentValue.evaluate( object, context );
-	}
-	
-	/**
-	 * There is no great way to check if the last parameter
-	 * is intended to be the optional ThreshholdBelongsTo
-	 * or not (since it is an optional parameter).
-	 * <p>
-	 * We can figure out if "succeeding" or "preceding" is
-	 * being provided by checking the number of parameters.
-	 * @param object
-	 * @return PRECEDING or SUCCEEDING after checking if any information was provided.
-	 */
-	private String queryThreshdoldBelongsTo( Object object ){
-		if( parameters.size() <= 2 && parameters.size() % 2 == 0 ){
-			return SUCCEEDING; // user has not specified anything
-		}
-		Expression lastParameter = parameters.get( parameters.size()-1 );
-		String lastValue = lastParameter.evaluate(object, String.class);
-		if( PRECEDING.equalsIgnoreCase( lastValue)){
-			return PRECEDING;
-		}
-		else if( SUCCEEDING.equalsIgnoreCase( lastValue)){
-			return SUCCEEDING;
-		}
-		else {
-			// warning?
-			return SUCCEEDING; // default
-		}		
-	}
-	
-	public Literal getFallbackValue() {
-		return fallback;
-	}
+        public int getArgumentCount() {
+            return -2; // indicating unbounded, 2 minimum
+        }
 
+        public List<String> getArgumentNames() {
+            return Arrays.asList(new String[]{
+                        "LookupValue",
+                        "Value",
+                        "Threshold 1", "Value 1",
+                        "Threshold 2", "Value 2",
+                        "succeeding or preceding"
+                    });
+        }
+
+        public String getName() {
+            return "Categorize";
+        }
+    };
+
+    public CategorizeFunction() {
+        this( new ArrayList<Expression>(), null);
+    }
+
+    public CategorizeFunction(List<Expression> parameters, Literal fallback) {
+        if(parameters == null || parameters.size() <2){
+            throw new IllegalArgumentException("Categorize function must have at least 2 parameters : lookup value and first value");
+        }
+        
+        this.parameters = parameters;
+        this.fallback = fallback;
+    }
+
+    public String getName() {
+        return "Categorize";
+    }
+
+    public List<Expression> getParameters() {
+        return Collections.unmodifiableList(parameters);
+    }
+
+    public Object accept(ExpressionVisitor visitor, Object extraData) {
+        return visitor.visit(this, extraData);
+    }
+
+    public Object evaluate(Object object) {
+        return evaluate(object, Object.class);
+    }
+
+    public <T> T evaluate(Object object, Class<T> context) {
+        final Expression lookupExp = parameters.get(0);
+        final String belongsTo = queryThreshdoldBelongsTo(object);
+        Expression currentExp = parameters.get(1);
+
+        final List<Expression> splits;
+        if (parameters.size() == 2) {
+            return currentExp.evaluate(object, context);
+        } else if (parameters.size() % 2 == 0) {
+            splits = parameters.subList(2, parameters.size());
+        } else {
+            splits = parameters.subList(2, parameters.size() - 1);
+        }
+        
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
+        for (int i = 0; i < splits.size(); i += 2) {
+            Expression threshholdExp = splits.get(i);
+            Expression rangedExp = splits.get(i + 1);
+            
+            
+            String lookupValue = lookupExp.evaluate(object, String.class);
+            
+            //we deal with a raster data
+            if(lookupValue.equalsIgnoreCase(RASTER_DATA)){
+                Double bandValue = new Double(object.toString());
+                Double threshholdValue = threshholdExp.evaluate(object, Double.class);
+                
+                if (PRECEDING.equals(belongsTo)) {
+                    if(bandValue > threshholdValue){
+                        currentExp = rangedExp;
+                    }else{
+                        break;
+                    }
+                } else {
+                    if(bandValue >= threshholdValue){
+                        currentExp = rangedExp;
+                    }else{
+                        break;
+                    }
+                }
+            }
+            //we deal with something else, a mistake ? can it happen ?
+            else{
+                Filter isIncludedInThreshold;
+                if (PRECEDING.equals(belongsTo)) {
+                    isIncludedInThreshold = ff.greater(lookupExp, threshholdExp);
+                } else {
+                    isIncludedInThreshold = ff.greaterOrEqual(lookupExp, threshholdExp);
+                }
+                if (isIncludedInThreshold.evaluate(object)) {
+                    currentExp = rangedExp;
+                } else {
+                    break;
+                }
+            }
+            
+        }
+        return currentExp.evaluate(object, context);
+    }
+
+    /**
+     * There is no great way to check if the last parameter
+     * is intended to be the optional ThreshholdBelongsTo
+     * or not (since it is an optional parameter).
+     * <p>
+     * We can figure out if "succeeding" or "preceding" is
+     * being provided by checking the number of parameters.
+     * @param object
+     * @return PRECEDING or SUCCEEDING after checking if any information was provided.
+     */
+    private String queryThreshdoldBelongsTo(Object object) {
+        if (parameters.size() == 2 && parameters.size() % 2 == 0) {
+            return SUCCEEDING; // user has not specified anything
+        }
+        
+        Expression lastParameter = parameters.get(parameters.size() - 1);
+        String lastValue = lastParameter.evaluate(object, String.class);
+        
+        if (PRECEDING.equalsIgnoreCase(lastValue)) {
+            return PRECEDING;
+        } else if (SUCCEEDING.equalsIgnoreCase(lastValue)) {
+            return SUCCEEDING;
+        } else {
+            // warning?
+            return SUCCEEDING; // default
+        }
+    }
+
+    public Literal getFallbackValue() {
+        return fallback;
+    }
+    
 }
