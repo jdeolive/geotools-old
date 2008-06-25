c02241b13bf998eda2b834ba8d79f8c5fdcaa54e
jdeolive <jdeolive@e5c1c795-43da-0310-a71f-fac65c449510>
2008-06-25 17:29:43 +0000

    added data to ComplexDataType
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30810 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/ogc/net.opengis.wps/src/net/opengis/wps/ComplexDataType.java
+++ modules/unsupported/ogc/net.opengis.wps/src/net/opengis/wps/ComplexDataType.java
@@ -6,6 +6,9 @@
  */
 package net.opengis.wps;
 
+import org.eclipse.emf.common.util.EList;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
 
 import org.eclipse.emf.ecore.util.FeatureMap;
@@ -26,6 +29,7 @@ import org.eclipse.emf.ecore.util.FeatureMap;
  *   <li>{@link net.opengis.wps.ComplexDataType#getEncoding <em>Encoding</em>}</li>
  *   <li>{@link net.opengis.wps.ComplexDataType#getMimeType <em>Mime Type</em>}</li>
  *   <li>{@link net.opengis.wps.ComplexDataType#getSchema <em>Schema</em>}</li>
+ *   <li>{@link net.opengis.wps.ComplexDataType#getData <em>Data</em>}</li>
  * </ul>
  * </p>
  *
@@ -34,100 +38,107 @@ import org.eclipse.emf.ecore.util.FeatureMap;
  * @generated
  */
 public interface ComplexDataType extends EObject {
+    /**
+     * The contents of the data type.
+     * 
+     * @model type="java.lang.Object"
+     */
+    EList getData();
+    
 	/**
-	 * Returns the value of the '<em><b>Mixed</b></em>' attribute list.
-	 * The list contents are of type {@link org.eclipse.emf.ecore.util.FeatureMap.Entry}.
-	 * <!-- begin-user-doc -->
+     * Returns the value of the '<em><b>Mixed</b></em>' attribute list.
+     * The list contents are of type {@link org.eclipse.emf.ecore.util.FeatureMap.Entry}.
+     * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Mixed</em>' attribute list isn't clear,
 	 * there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Mixed</em>' attribute list.
-	 * @see net.opengis.wps.WpsPackage#getComplexDataType_Mixed()
-	 * @model unique="false" dataType="org.eclipse.emf.ecore.EFeatureMapEntry" many="true"
-	 *        extendedMetaData="kind='elementWildcard' name=':mixed'"
-	 * @generated
-	 */
+     * @return the value of the '<em>Mixed</em>' attribute list.
+     * @see net.opengis.wps.WpsPackage#getComplexDataType_Mixed()
+     * @model unique="false" dataType="org.eclipse.emf.ecore.EFeatureMapEntry" many="true"
+     *        extendedMetaData="kind='elementWildcard' name=':mixed'"
+     * @generated
+     */
 	FeatureMap getMixed();
 
 	/**
-	 * Returns the value of the '<em><b>Encoding</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+     * Returns the value of the '<em><b>Encoding</b></em>' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * <!-- begin-model-doc -->
-	 * The encoding of this input or requested for this output (e.g., UTF-8). This "encoding" shall be included whenever the encoding required is not the default encoding indicated in the Process full description. When included, this encoding shall be one published for this output or input in the Process full description.
-	 * <!-- end-model-doc -->
-	 * @return the value of the '<em>Encoding</em>' attribute.
-	 * @see #setEncoding(String)
-	 * @see net.opengis.wps.WpsPackage#getComplexDataType_Encoding()
-	 * @model dataType="org.eclipse.emf.ecore.xml.type.AnyURI"
-	 *        extendedMetaData="kind='attribute' name='encoding'"
-	 * @generated
-	 */
+     * <!-- begin-model-doc -->
+     * The encoding of this input or requested for this output (e.g., UTF-8). This "encoding" shall be included whenever the encoding required is not the default encoding indicated in the Process full description. When included, this encoding shall be one published for this output or input in the Process full description.
+     * <!-- end-model-doc -->
+     * @return the value of the '<em>Encoding</em>' attribute.
+     * @see #setEncoding(String)
+     * @see net.opengis.wps.WpsPackage#getComplexDataType_Encoding()
+     * @model dataType="org.eclipse.emf.ecore.xml.type.AnyURI"
+     *        extendedMetaData="kind='attribute' name='encoding'"
+     * @generated
+     */
 	String getEncoding();
 
 	/**
-	 * Sets the value of the '{@link net.opengis.wps.ComplexDataType#getEncoding <em>Encoding</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * Sets the value of the '{@link net.opengis.wps.ComplexDataType#getEncoding <em>Encoding</em>}' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Encoding</em>' attribute.
-	 * @see #getEncoding()
-	 * @generated
-	 */
+     * @param value the new value of the '<em>Encoding</em>' attribute.
+     * @see #getEncoding()
+     * @generated
+     */
 	void setEncoding(String value);
 
 	/**
-	 * Returns the value of the '<em><b>Mime Type</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+     * Returns the value of the '<em><b>Mime Type</b></em>' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * <!-- begin-model-doc -->
-	 * The Format of this input or requested for this output (e.g., text/xml). This element shall be omitted when the Format is indicated in the http header of the output. When included, this format shall be one published for this output or input in the Process full description.
-	 * <!-- end-model-doc -->
-	 * @return the value of the '<em>Mime Type</em>' attribute.
-	 * @see #setMimeType(String)
-	 * @see net.opengis.wps.WpsPackage#getComplexDataType_MimeType()
-	 * @model dataType="net.opengis.ows11.MimeType"
-	 *        extendedMetaData="kind='attribute' name='mimeType'"
-	 * @generated
-	 */
+     * <!-- begin-model-doc -->
+     * The Format of this input or requested for this output (e.g., text/xml). This element shall be omitted when the Format is indicated in the http header of the output. When included, this format shall be one published for this output or input in the Process full description.
+     * <!-- end-model-doc -->
+     * @return the value of the '<em>Mime Type</em>' attribute.
+     * @see #setMimeType(String)
+     * @see net.opengis.wps.WpsPackage#getComplexDataType_MimeType()
+     * @model dataType="net.opengis.ows11.MimeType"
+     *        extendedMetaData="kind='attribute' name='mimeType'"
+     * @generated
+     */
 	String getMimeType();
 
 	/**
-	 * Sets the value of the '{@link net.opengis.wps.ComplexDataType#getMimeType <em>Mime Type</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * Sets the value of the '{@link net.opengis.wps.ComplexDataType#getMimeType <em>Mime Type</em>}' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Mime Type</em>' attribute.
-	 * @see #getMimeType()
-	 * @generated
-	 */
+     * @param value the new value of the '<em>Mime Type</em>' attribute.
+     * @see #getMimeType()
+     * @generated
+     */
 	void setMimeType(String value);
 
 	/**
-	 * Returns the value of the '<em><b>Schema</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+     * Returns the value of the '<em><b>Schema</b></em>' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * <!-- begin-model-doc -->
-	 * Web-accessible XML Schema Document that defines the content model of this complex resource (e.g., encoded using GML 2.2 Application Schema).  This reference should be included for XML encoded complex resources to facilitate validation.
-	 * PS I changed the name of this attribute to be consistent with the ProcessDescription.  The original was giving me validation troubles in XMLSpy.
-	 * <!-- end-model-doc -->
-	 * @return the value of the '<em>Schema</em>' attribute.
-	 * @see #setSchema(String)
-	 * @see net.opengis.wps.WpsPackage#getComplexDataType_Schema()
-	 * @model dataType="org.eclipse.emf.ecore.xml.type.AnyURI"
-	 *        extendedMetaData="kind='attribute' name='schema'"
-	 * @generated
-	 */
+     * <!-- begin-model-doc -->
+     * Web-accessible XML Schema Document that defines the content model of this complex resource (e.g., encoded using GML 2.2 Application Schema).  This reference should be included for XML encoded complex resources to facilitate validation.
+     * PS I changed the name of this attribute to be consistent with the ProcessDescription.  The original was giving me validation troubles in XMLSpy.
+     * <!-- end-model-doc -->
+     * @return the value of the '<em>Schema</em>' attribute.
+     * @see #setSchema(String)
+     * @see net.opengis.wps.WpsPackage#getComplexDataType_Schema()
+     * @model dataType="org.eclipse.emf.ecore.xml.type.AnyURI"
+     *        extendedMetaData="kind='attribute' name='schema'"
+     * @generated
+     */
 	String getSchema();
 
 	/**
-	 * Sets the value of the '{@link net.opengis.wps.ComplexDataType#getSchema <em>Schema</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * Sets the value of the '{@link net.opengis.wps.ComplexDataType#getSchema <em>Schema</em>}' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Schema</em>' attribute.
-	 * @see #getSchema()
-	 * @generated
-	 */
+     * @param value the new value of the '<em>Schema</em>' attribute.
+     * @see #getSchema()
+     * @generated
+     */
 	void setSchema(String value);
 
 } // ComplexDataType
--- modules/unsupported/ogc/net.opengis.wps/src/net/opengis/wps/impl/ComplexDataTypeImpl.java
+++ modules/unsupported/ogc/net.opengis.wps/src/net/opengis/wps/impl/ComplexDataTypeImpl.java
@@ -6,12 +6,14 @@
  */
 package net.opengis.wps.impl;
 
+import java.util.Collection;
 import net.opengis.wps.ComplexDataType;
 import net.opengis.wps.WpsPackage;
 
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
 
@@ -19,6 +21,7 @@ import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
 
 import org.eclipse.emf.ecore.util.BasicFeatureMap;
+import org.eclipse.emf.ecore.util.EDataTypeUniqueEList;
 import org.eclipse.emf.ecore.util.FeatureMap;
 import org.eclipse.emf.ecore.util.InternalEList;
 
@@ -33,6 +36,7 @@ import org.eclipse.emf.ecore.util.InternalEList;
  *   <li>{@link net.opengis.wps.impl.ComplexDataTypeImpl#getEncoding <em>Encoding</em>}</li>
  *   <li>{@link net.opengis.wps.impl.ComplexDataTypeImpl#getMimeType <em>Mime Type</em>}</li>
  *   <li>{@link net.opengis.wps.impl.ComplexDataTypeImpl#getSchema <em>Schema</em>}</li>
+ *   <li>{@link net.opengis.wps.impl.ComplexDataTypeImpl#getData <em>Data</em>}</li>
  * </ul>
  * </p>
  *
@@ -40,285 +44,320 @@ import org.eclipse.emf.ecore.util.InternalEList;
  */
 public class ComplexDataTypeImpl extends EObjectImpl implements ComplexDataType {
 	/**
-	 * The cached value of the '{@link #getMixed() <em>Mixed</em>}' attribute list.
-	 * <!-- begin-user-doc -->
+     * The cached value of the '{@link #getMixed() <em>Mixed</em>}' attribute list.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @see #getMixed()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getMixed()
+     * @generated
+     * @ordered
+     */
 	protected FeatureMap mixed;
 
 	/**
-	 * The default value of the '{@link #getEncoding() <em>Encoding</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * The default value of the '{@link #getEncoding() <em>Encoding</em>}' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @see #getEncoding()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getEncoding()
+     * @generated
+     * @ordered
+     */
 	protected static final String ENCODING_EDEFAULT = null;
 
 	/**
-	 * The cached value of the '{@link #getEncoding() <em>Encoding</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * The cached value of the '{@link #getEncoding() <em>Encoding</em>}' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @see #getEncoding()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getEncoding()
+     * @generated
+     * @ordered
+     */
 	protected String encoding = ENCODING_EDEFAULT;
 
 	/**
-	 * The default value of the '{@link #getMimeType() <em>Mime Type</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * The default value of the '{@link #getMimeType() <em>Mime Type</em>}' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @see #getMimeType()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getMimeType()
+     * @generated
+     * @ordered
+     */
 	protected static final String MIME_TYPE_EDEFAULT = null;
 
 	/**
-	 * The cached value of the '{@link #getMimeType() <em>Mime Type</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * The cached value of the '{@link #getMimeType() <em>Mime Type</em>}' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @see #getMimeType()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getMimeType()
+     * @generated
+     * @ordered
+     */
 	protected String mimeType = MIME_TYPE_EDEFAULT;
 
 	/**
-	 * The default value of the '{@link #getSchema() <em>Schema</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * The default value of the '{@link #getSchema() <em>Schema</em>}' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @see #getSchema()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getSchema()
+     * @generated
+     * @ordered
+     */
 	protected static final String SCHEMA_EDEFAULT = null;
 
 	/**
-	 * The cached value of the '{@link #getSchema() <em>Schema</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * The cached value of the '{@link #getSchema() <em>Schema</em>}' attribute.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @see #getSchema()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getSchema()
+     * @generated
+     * @ordered
+     */
 	protected String schema = SCHEMA_EDEFAULT;
 
 	/**
-	 * <!-- begin-user-doc -->
+     * The cached value of the '{@link #getData() <em>Data</em>}' attribute list.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getData()
+     * @generated
+     * @ordered
+     */
+    protected EList data;
+
+    /**
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	protected ComplexDataTypeImpl() {
-		super();
-	}
+        super();
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	protected EClass eStaticClass() {
-		return WpsPackage.eINSTANCE.getComplexDataType();
-	}
+        return WpsPackage.eINSTANCE.getComplexDataType();
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public FeatureMap getMixed() {
-		if (mixed == null) {
-			mixed = new BasicFeatureMap(this, WpsPackage.COMPLEX_DATA_TYPE__MIXED);
-		}
-		return mixed;
-	}
+        if (mixed == null) {
+            mixed = new BasicFeatureMap(this, WpsPackage.COMPLEX_DATA_TYPE__MIXED);
+        }
+        return mixed;
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public String getEncoding() {
-		return encoding;
-	}
+        return encoding;
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public void setEncoding(String newEncoding) {
-		String oldEncoding = encoding;
-		encoding = newEncoding;
-		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, WpsPackage.COMPLEX_DATA_TYPE__ENCODING, oldEncoding, encoding));
-	}
+        String oldEncoding = encoding;
+        encoding = newEncoding;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, WpsPackage.COMPLEX_DATA_TYPE__ENCODING, oldEncoding, encoding));
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public String getMimeType() {
-		return mimeType;
-	}
+        return mimeType;
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public void setMimeType(String newMimeType) {
-		String oldMimeType = mimeType;
-		mimeType = newMimeType;
-		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE, oldMimeType, mimeType));
-	}
+        String oldMimeType = mimeType;
+        mimeType = newMimeType;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE, oldMimeType, mimeType));
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public String getSchema() {
-		return schema;
-	}
+        return schema;
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public void setSchema(String newSchema) {
-		String oldSchema = schema;
-		schema = newSchema;
-		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, WpsPackage.COMPLEX_DATA_TYPE__SCHEMA, oldSchema, schema));
-	}
+        String oldSchema = schema;
+        schema = newSchema;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, WpsPackage.COMPLEX_DATA_TYPE__SCHEMA, oldSchema, schema));
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EList getData() {
+        if (data == null) {
+            data = new EDataTypeUniqueEList(Object.class, this, WpsPackage.COMPLEX_DATA_TYPE__DATA);
+        }
+        return data;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
-		switch (featureID) {
-			case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
-				return ((InternalEList)getMixed()).basicRemove(otherEnd, msgs);
-		}
-		return super.eInverseRemove(otherEnd, featureID, msgs);
-	}
+        switch (featureID) {
+            case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
+                return ((InternalEList)getMixed()).basicRemove(otherEnd, msgs);
+        }
+        return super.eInverseRemove(otherEnd, featureID, msgs);
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
-		switch (featureID) {
-			case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
-				if (coreType) return getMixed();
-				return ((FeatureMap.Internal)getMixed()).getWrapper();
-			case WpsPackage.COMPLEX_DATA_TYPE__ENCODING:
-				return getEncoding();
-			case WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE:
-				return getMimeType();
-			case WpsPackage.COMPLEX_DATA_TYPE__SCHEMA:
-				return getSchema();
-		}
-		return super.eGet(featureID, resolve, coreType);
-	}
+        switch (featureID) {
+            case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
+                if (coreType) return getMixed();
+                return ((FeatureMap.Internal)getMixed()).getWrapper();
+            case WpsPackage.COMPLEX_DATA_TYPE__ENCODING:
+                return getEncoding();
+            case WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE:
+                return getMimeType();
+            case WpsPackage.COMPLEX_DATA_TYPE__SCHEMA:
+                return getSchema();
+            case WpsPackage.COMPLEX_DATA_TYPE__DATA:
+                return getData();
+        }
+        return super.eGet(featureID, resolve, coreType);
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public void eSet(int featureID, Object newValue) {
-		switch (featureID) {
-			case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
-				((FeatureMap.Internal)getMixed()).set(newValue);
-				return;
-			case WpsPackage.COMPLEX_DATA_TYPE__ENCODING:
-				setEncoding((String)newValue);
-				return;
-			case WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE:
-				setMimeType((String)newValue);
-				return;
-			case WpsPackage.COMPLEX_DATA_TYPE__SCHEMA:
-				setSchema((String)newValue);
-				return;
-		}
-		super.eSet(featureID, newValue);
-	}
+        switch (featureID) {
+            case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
+                ((FeatureMap.Internal)getMixed()).set(newValue);
+                return;
+            case WpsPackage.COMPLEX_DATA_TYPE__ENCODING:
+                setEncoding((String)newValue);
+                return;
+            case WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE:
+                setMimeType((String)newValue);
+                return;
+            case WpsPackage.COMPLEX_DATA_TYPE__SCHEMA:
+                setSchema((String)newValue);
+                return;
+            case WpsPackage.COMPLEX_DATA_TYPE__DATA:
+                getData().clear();
+                getData().addAll((Collection)newValue);
+                return;
+        }
+        super.eSet(featureID, newValue);
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public void eUnset(int featureID) {
-		switch (featureID) {
-			case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
-				getMixed().clear();
-				return;
-			case WpsPackage.COMPLEX_DATA_TYPE__ENCODING:
-				setEncoding(ENCODING_EDEFAULT);
-				return;
-			case WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE:
-				setMimeType(MIME_TYPE_EDEFAULT);
-				return;
-			case WpsPackage.COMPLEX_DATA_TYPE__SCHEMA:
-				setSchema(SCHEMA_EDEFAULT);
-				return;
-		}
-		super.eUnset(featureID);
-	}
+        switch (featureID) {
+            case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
+                getMixed().clear();
+                return;
+            case WpsPackage.COMPLEX_DATA_TYPE__ENCODING:
+                setEncoding(ENCODING_EDEFAULT);
+                return;
+            case WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE:
+                setMimeType(MIME_TYPE_EDEFAULT);
+                return;
+            case WpsPackage.COMPLEX_DATA_TYPE__SCHEMA:
+                setSchema(SCHEMA_EDEFAULT);
+                return;
+            case WpsPackage.COMPLEX_DATA_TYPE__DATA:
+                getData().clear();
+                return;
+        }
+        super.eUnset(featureID);
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public boolean eIsSet(int featureID) {
-		switch (featureID) {
-			case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
-				return mixed != null && !mixed.isEmpty();
-			case WpsPackage.COMPLEX_DATA_TYPE__ENCODING:
-				return ENCODING_EDEFAULT == null ? encoding != null : !ENCODING_EDEFAULT.equals(encoding);
-			case WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE:
-				return MIME_TYPE_EDEFAULT == null ? mimeType != null : !MIME_TYPE_EDEFAULT.equals(mimeType);
-			case WpsPackage.COMPLEX_DATA_TYPE__SCHEMA:
-				return SCHEMA_EDEFAULT == null ? schema != null : !SCHEMA_EDEFAULT.equals(schema);
-		}
-		return super.eIsSet(featureID);
-	}
+        switch (featureID) {
+            case WpsPackage.COMPLEX_DATA_TYPE__MIXED:
+                return mixed != null && !mixed.isEmpty();
+            case WpsPackage.COMPLEX_DATA_TYPE__ENCODING:
+                return ENCODING_EDEFAULT == null ? encoding != null : !ENCODING_EDEFAULT.equals(encoding);
+            case WpsPackage.COMPLEX_DATA_TYPE__MIME_TYPE:
+                return MIME_TYPE_EDEFAULT == null ? mimeType != null : !MIME_TYPE_EDEFAULT.equals(mimeType);
+            case WpsPackage.COMPLEX_DATA_TYPE__SCHEMA:
+                return SCHEMA_EDEFAULT == null ? schema != null : !SCHEMA_EDEFAULT.equals(schema);
+            case WpsPackage.COMPLEX_DATA_TYPE__DATA:
+                return data != null && !data.isEmpty();
+        }
+        return super.eIsSet(featureID);
+    }
 
 	/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public String toString() {
-		if (eIsProxy()) return super.toString();
+        if (eIsProxy()) return super.toString();
 
-		StringBuffer result = new StringBuffer(super.toString());
-		result.append(" (mixed: ");
-		result.append(mixed);
-		result.append(", encoding: ");
-		result.append(encoding);
-		result.append(", mimeType: ");
-		result.append(mimeType);
-		result.append(", schema: ");
-		result.append(schema);
-		result.append(')');
-		return result.toString();
-	}
+        StringBuffer result = new StringBuffer(super.toString());
+        result.append(" (mixed: ");
+        result.append(mixed);
+        result.append(", encoding: ");
+        result.append(encoding);
+        result.append(", mimeType: ");
+        result.append(mimeType);
+        result.append(", schema: ");
+        result.append(schema);
+        result.append(", data: ");
+        result.append(data);
+        result.append(')');
+        return result.toString();
+    }
 
 } //ComplexDataTypeImpl
