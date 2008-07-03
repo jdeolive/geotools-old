ac596f6b60a7b1e40fd19c213fa6ce8413e6539f
johann.sorel <johann.sorel@e5c1c795-43da-0310-a71f-fac65c449510>
2008-07-03 09:23:16 +0000

    Added Implementation of OGC Symboly Encoding 1.1 Functions : Interpolation and Recode.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@30847 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/render/src/main/java/org/geotools/filter/RecodeFunction.java
+++ modules/library/render/src/main/java/org/geotools/filter/RecodeFunction.java
@@ -28,10 +28,9 @@ import org.opengis.filter.expression.Expression;
 import org.opengis.filter.expression.ExpressionVisitor;
 import org.opengis.filter.expression.Function;
 import org.opengis.filter.expression.Literal;
-import org.opengis.filter.Filter;
 
 /**
- * Implementation of "Categorize" as a normal function.
+ * Implementation of "Recode" as a normal function.
  * <p>
  * This implementation is compatible with the Function
  * interface; the parameter list can be used to set the
@@ -41,29 +40,20 @@ import org.opengis.filter.Filter;
  * <ol>
  * <li>PropertyName; use "Rasterdata" to indicate this is a colour map
  * <li>Literal: lookup value
- * <li>Literal: threshold 1
- * <li>Literal: value 1
- * <li>Literal: threshold 2
- * <li>Literal: value 2
- * <li>Literal: (Optional) succeeding or preceding
+ * <li>Literal: MapItem : data 1
+ * <li>Literal: MapItem : value 1
+ * <li>Literal: MapItem : data 2
+ * <li>Literal: MapItem : value 2
  * </ol>
  * In reality any expression will do.
- * @author Jody Garnett
+ * 
  * @author Johann Sorel (Geomatys)
  */
-public class CategorizeFunction implements Function {
-
-    /** Use as a literal value to indicate behaviour of threshold boundary */
-    public static final String SUCCEEDING = "succeeding";
-    
-    /** Use as a literal value to indicate behaviour of threshold boundary */
-    public static final String PRECEDING = "preceding"; 
+public class RecodeFunction implements Function {
     
     /**
      * Use as a PropertyName when defining a color map.
-     * The "Raterdata" is expected to apply to only a single band; if multiple
-     * bands are provided it is probably a mistake; but we will use the maximum
-     * value (since we are working against a threshold).
+     * The "Raterdata" is expected to apply to only a single band;
      */
     public static final String RASTER_DATA = "Rasterdata";
     
@@ -90,29 +80,27 @@ public class CategorizeFunction implements Function {
         public List<String> getArgumentNames() {
             return Arrays.asList(new String[]{
                         "LookupValue",
-                        "Value",
-                        "Threshold 1", "Value 1",
-                        "Threshold 2", "Value 2",
-                        "succeeding or preceding"
+                        "Data 1", "Value 1",
+                        "Data 2", "Value 2"
                     });
         }
 
         public String getName() {
-            return "Categorize";
+            return "Recode";
         }
     };
 
-    public CategorizeFunction() {
+    public RecodeFunction() {
         this( new ArrayList<Expression>(), null);
     }
 
-    public CategorizeFunction(List<Expression> parameters, Literal fallback) {
+    public RecodeFunction(List<Expression> parameters, Literal fallback) {
         this.parameters = parameters;
         this.fallback = fallback;
     }
 
     public String getName() {
-        return "Categorize";
+        return "Recode";
     }
 
     public List<Expression> getParameters() {
@@ -129,22 +117,22 @@ public class CategorizeFunction implements Function {
 
     public <T> T evaluate(Object object, Class<T> context) {
         final Expression lookupExp = parameters.get(0);
-        final String belongsTo = queryThreshdoldBelongsTo(object);
         Expression currentExp = parameters.get(1);
 
         final List<Expression> splits;
         if (parameters.size() == 2) {
             return currentExp.evaluate(object, context);
-        } else if (parameters.size() % 2 == 0) {
-            splits = parameters.subList(2, parameters.size());
+        } else if (parameters.size() % 2 == 1) {
+            splits = parameters.subList(1, parameters.size());
         } else {
-            splits = parameters.subList(2, parameters.size() - 1);
+            //this should not happen
+            splits = parameters.subList(1, parameters.size() - 1);
         }
         
         FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
         for (int i = 0; i < splits.size(); i += 2) {
-            Expression threshholdExp = splits.get(i);
-            Expression rangedExp = splits.get(i + 1);
+            Expression dataExp = splits.get(i);
+            Expression resultExp = splits.get(i + 1);
             
             
             String lookupValue = lookupExp.evaluate(object, String.class);
@@ -152,68 +140,21 @@ public class CategorizeFunction implements Function {
             //we deal with a raster data
             if(lookupValue.equalsIgnoreCase(RASTER_DATA)){
                 Double bandValue = new Double(object.toString());
-                Double threshholdValue = threshholdExp.evaluate(object, Double.class);
+                Double compareValue = dataExp.evaluate(object, Double.class);
                 
-                if (PRECEDING.equals(belongsTo)) {
-                    if(bandValue > threshholdValue){
-                        currentExp = rangedExp;
-                    }else{
-                        break;
-                    }
-                } else {
-                    if(bandValue >= threshholdValue){
-                        currentExp = rangedExp;
-                    }else{
-                        break;
-                    }
+                if(bandValue == compareValue){
+                    return resultExp.evaluate(object, context);
                 }
+                
             }
             //we deal with something else, a mistake ? can it happen ?
             else{
-                Filter isIncludedInThreshold;
-                if (PRECEDING.equals(belongsTo)) {
-                    isIncludedInThreshold = ff.greater(lookupExp, threshholdExp);
-                } else {
-                    isIncludedInThreshold = ff.greaterOrEqual(lookupExp, threshholdExp);
-                }
-                if (isIncludedInThreshold.evaluate(object)) {
-                    currentExp = rangedExp;
-                } else {
-                    break;
-                }
             }
             
         }
         return currentExp.evaluate(object, context);
     }
 
-    /**
-     * There is no great way to check if the last parameter
-     * is intended to be the optional ThreshholdBelongsTo
-     * or not (since it is an optional parameter).
-     * <p>
-     * We can figure out if "succeeding" or "preceding" is
-     * being provided by checking the number of parameters.
-     * @param object
-     * @return PRECEDING or SUCCEEDING after checking if any information was provided.
-     */
-    private String queryThreshdoldBelongsTo(Object object) {
-        if (parameters.size() == 2 && parameters.size() % 2 == 0) {
-            return SUCCEEDING; // user has not specified anything
-        }
-        
-        Expression lastParameter = parameters.get(parameters.size() - 1);
-        String lastValue = lastParameter.evaluate(object, String.class);
-        
-        if (PRECEDING.equalsIgnoreCase(lastValue)) {
-            return PRECEDING;
-        } else if (SUCCEEDING.equalsIgnoreCase(lastValue)) {
-            return SUCCEEDING;
-        } else {
-            // warning?
-            return SUCCEEDING; // default
-        }
-    }
 
     public Literal getFallbackValue() {
         return fallback;
