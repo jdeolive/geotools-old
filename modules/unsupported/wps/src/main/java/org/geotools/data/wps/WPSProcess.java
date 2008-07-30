ce3b00f8c8fd6b466399ad620d257a335db41d77
lreed <lreed@e5c1c795-43da-0310-a71f-fac65c449510>
2008-07-30 19:12:43 +0000

    Fixed WPSProcess import
    git-svn-id: http://svn.osgeo.org/geotools/trunk@31114 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/wps/src/main/java/org/geotools/data/wps/WPSProcess.java
+++ modules/unsupported/wps/src/main/java/org/geotools/data/wps/WPSProcess.java
@@ -19,6 +19,7 @@ package org.geotools.data.wps;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
