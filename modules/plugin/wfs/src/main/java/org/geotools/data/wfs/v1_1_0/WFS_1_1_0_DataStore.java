a5b5992cdfa25db1c6aa46dd8fc1b8113b736d5e
groldan <groldan@e5c1c795-43da-0310-a71f-fac65c449510>
2008-11-07 21:33:30 +0000

    better exception reporting form wfs exceptions, returning FilterCapabilities from WFSProtocol
    git-svn-id: http://svn.osgeo.org/geotools/trunk@31807 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/plugin/wfs/src/main/java/org/geotools/data/wfs/v1_1_0/WFS_1_1_0_DataStore.java
+++ modules/plugin/wfs/src/main/java/org/geotools/data/wfs/v1_1_0/WFS_1_1_0_DataStore.java
@@ -181,7 +181,7 @@ public final class WFS_1_1_0_DataStore implements WFSDataStore {
             }
 
             final URL describeUrl = wfs.getDescribeFeatureTypeURLGet(prefixedTypeName);
-            //@TODO remove this
+            // @TODO remove this
             System.err.println("DecribeFT URL for " + prefixedTypeName + ": " + describeUrl);
 
             final SimpleFeatureType featureType;
@@ -289,6 +289,7 @@ public final class WFS_1_1_0_DataStore implements WFSDataStore {
 
         GetFeatureParser parser;
         if (result instanceof WFSException) {
+            // try to recover from common server implementation errors
             throw (WFSException) result;
         } else if (result instanceof GetFeatureParser) {
             parser = (GetFeatureParser) result;
@@ -783,5 +784,10 @@ public final class WFS_1_1_0_DataStore implements WFSDataStore {
         }
         throw new IllegalArgumentException("Neither POST nor GET method is supported for the "
                 + operation + " operation by the server");
+    }
+    
+    
+    private static interface WFSProtocolStrategy extends WFSProtocol{
+        
     }
 }
