de76b0acd0bf3e054ff2523429623f714b36295c
bencaradocdavies <bencaradocdavies@e5c1c795-43da-0310-a71f-fac65c449510>
2008-12-22 02:03:01 +0000

    Made createSchemaLocator() public to be compatible with increased visibility of superclass.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@32055 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/app-schema/app-schema/src/main/java/org/geotools/data/complex/config/CatalogApplicationSchemaXSD.java
+++ modules/unsupported/app-schema/app-schema/src/main/java/org/geotools/data/complex/config/CatalogApplicationSchemaXSD.java
@@ -26,7 +26,9 @@ import org.geotools.xml.SchemaLocationResolver;
  * 
  * @author Ben Caradoc-Davies, CSIRO Exploration and Mining
  * @version $Id$
- * @source $URL$
+ * @source $URL:
+ *         http://svn.geotools.org/trunk/modules/unsupported/app-schema/app-schema/src/main/java
+ *         /org/geotools/data/complex/config/CatalogApplicationSchemaXSD.java $
  * @since 2.6
  */
 public class CatalogApplicationSchemaXSD extends ApplicationSchemaXSD {
@@ -35,11 +37,11 @@ public class CatalogApplicationSchemaXSD extends ApplicationSchemaXSD {
 
     /**
      * @param namespaceURI
-     *                namespace of the schema
+     *            namespace of the schema
      * @param schemaLocation
-     *                the schema location
+     *            the schema location
      * @param catalog
-     *                OASIS Catalog for lookup, or null if none
+     *            OASIS Catalog for lookup, or null if none
      */
     public CatalogApplicationSchemaXSD(String namespaceURI, String schemaLocation, Catalog catalog) {
         super(namespaceURI, resolveSchemaLocation(catalog, schemaLocation));
@@ -50,7 +52,7 @@ public class CatalogApplicationSchemaXSD extends ApplicationSchemaXSD {
      * @see org.geotools.gml3.ApplicationSchemaXSD#createSchemaLocationResolver()
      */
     @Override
-    protected SchemaLocationResolver createSchemaLocationResolver() {
+    public SchemaLocationResolver createSchemaLocationResolver() {
         return new CatalogSchemaLocationResolverWrapper(catalog, super
                 .createSchemaLocationResolver());
     }
