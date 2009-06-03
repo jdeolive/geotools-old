0e7027120596d56fffc90b698110b0fbd2dffef5
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2009-06-03 07:25:11 +0000

    GEOT-2527,  gt-jdbc-oracle OracleDateConverterFactory static initialiser throws if ojdbc5.jar is absent
    git-svn-id: http://svn.osgeo.org/geotools/trunk@33204 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/jdbc-ng/jdbc-oracle/src/main/java/org/geotools/data/oracle/OracleDateConverterFactory.java
+++ modules/unsupported/jdbc-ng/jdbc-oracle/src/main/java/org/geotools/data/oracle/OracleDateConverterFactory.java
@@ -27,28 +27,47 @@ import org.geotools.util.ConverterFactory;
 public class OracleDateConverterFactory implements ConverterFactory {
 	
 	OracleDateConverter converter = new OracleDateConverter();
-	static final Class ORA_DATE;
-	static final Class ORA_TIMESTAMP;
+	static final Class<?> ORA_DATE;
+	static final Class<?> ORA_TIMESTAMP;
 	static final Method ORA_DATE_DVALUE;
 	static final Method ORA_DATE_TSVALUE;
 	static final Method ORA_TS_DVALUE;
 	static final Method ORA_TS_TSVALUE;
 	
 	static {
-		try {
-			ORA_DATE = Class.forName("oracle.sql.DATE");
-			ORA_DATE_DVALUE = ORA_DATE.getMethod("dateValue");
-			ORA_DATE_TSVALUE = ORA_DATE.getMethod("timestampValue");
-			ORA_TIMESTAMP = Class.forName("oracle.sql.TIMESTAMP");
-			ORA_TS_DVALUE = ORA_TIMESTAMP.getMethod("dateValue");
-			ORA_TS_TSVALUE = ORA_TIMESTAMP.getMethod("timestampValue");
-		} catch(Exception e) {
-			throw new RuntimeException("Could not initialize the oracle date converter", e);
-		}
+	    Class<?> oracleDateClass = null;
+	    try {
+	        oracleDateClass = Class.forName("oracle.sql.DATE");
+	    } catch (ClassNotFoundException e) {
+	        // ojdbc*.jar not on the path
+	    }
+	    if (oracleDateClass == null) {
+	        ORA_DATE = null;
+	        ORA_DATE_DVALUE = null;
+	        ORA_DATE_TSVALUE = null;
+	        ORA_TIMESTAMP = null;
+	        ORA_TS_DVALUE = null;
+	        ORA_TS_TSVALUE = null;
+	    } else {
+	        try {
+	            ORA_DATE = oracleDateClass;
+	            ORA_DATE_DVALUE = ORA_DATE.getMethod("dateValue");
+	            ORA_DATE_TSVALUE = ORA_DATE.getMethod("timestampValue");
+	            ORA_TIMESTAMP = Class.forName("oracle.sql.TIMESTAMP");
+	            ORA_TS_DVALUE = ORA_TIMESTAMP.getMethod("dateValue");
+	            ORA_TS_TSVALUE = ORA_TIMESTAMP.getMethod("timestampValue");
+	        } catch(Exception e) {
+	            throw new RuntimeException("Could not initialize the oracle date converter", e);
+	        }
+	    }
 	}
 
 	public Converter createConverter(Class<?> source, Class<?> target,
 			Hints hints) {
+	    // if the jdbc driver is not in the classpath don't bother trying to convert
+	    if(ORA_DATE == null)
+	        return null;
+	    
 		// can only convert towards java.util.Date && subclasses
 		if (!(Date.class.isAssignableFrom(target)))
 			return null;
