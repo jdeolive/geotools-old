06b01bd27ed3033c22a269031baf49823f029433
bencaradocdavies <bencaradocdavies@e5c1c795-43da-0310-a71f-fac65c449510>
2009-06-11 08:11:49 +0000

    Added an unregisterAll method to DataAccessRegistry.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@33268 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/app-schema/app-schema/src/main/java/org/geotools/data/complex/DataAccessRegistry.java
+++ modules/unsupported/app-schema/app-schema/src/main/java/org/geotools/data/complex/DataAccessRegistry.java
@@ -88,6 +88,16 @@ public class DataAccessRegistry extends ArrayList<DataAccess<FeatureType, Featur
         registry.remove(dataAccess);
     }
 
+    /**
+     * Unregister all data accesses in the registry. This is may be needed to prevent unit tests
+     * from conflicting with data accesses with the same type name registered for other tests.
+     */
+    public static synchronized void unregisterAll() {
+        if (registry != null) {
+            registry.clear();
+        }
+    }
+    
     public static boolean hasName(Name featureTypeName) throws IOException {
         if (registry == null) {
             // nothing's been registered, but it's OK, return false
