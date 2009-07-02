c4ceb979dedad4d0019d4aa550fee78283d0d983
robatkinson <robatkinson@e5c1c795-43da-0310-a71f-fac65c449510>
2009-07-02 04:48:06 +0000

    changed cardinality of resources.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@33431 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/app-schema/app-schema/src/test/java/org/geotools/data/complex/config/XMLConfigReaderTest.java
+++ modules/unsupported/app-schema/app-schema/src/test/java/org/geotools/data/complex/config/XMLConfigReaderTest.java
@@ -56,7 +56,7 @@ public class XMLConfigReaderTest extends TestCase {
         assertEquals(1, mappings.size());
         FeatureTypeMapping mapping = (FeatureTypeMapping) mappings.iterator().next();
 
-        assertEquals(6, mapping.getAttributeMappings().size());
+        assertEquals(7, mapping.getAttributeMappings().size());
         assertNotNull(mapping.getTargetFeature());
         assertNotNull(mapping.getSource());
 
