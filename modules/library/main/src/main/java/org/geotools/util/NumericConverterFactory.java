84d44aef57289416822e66100c33e881401e1ab5
jdeolive <jdeolive@e5c1c795-43da-0310-a71f-fac65c449510>
2009-07-19 05:36:48 +0000

    fixing indentation
    git-svn-id: http://svn.osgeo.org/geotools/trunk@33588 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/main/src/main/java/org/geotools/util/NumericConverterFactory.java
+++ modules/library/main/src/main/java/org/geotools/util/NumericConverterFactory.java
@@ -25,7 +25,7 @@ import org.geotools.factory.Hints;
 /**
  * ConverterFactory which converts between the "standard" numeric types.
  * <p>
- * 	Supported types:
+ * Supported types:
  * <ul>
  * <li>{@link java.lang.Long}
  * <li>{@link java.lang.Integer}
@@ -43,179 +43,169 @@ import org.geotools.factory.Hints;
  */
 public class NumericConverterFactory implements ConverterFactory {
 
-	public Converter createConverter(Class source, Class target, Hints hints) {
-	
-		//check if source is a number or a string.  We can't convert to a number
-	    // from anything else.
-		if ( !(Number.class.isAssignableFrom( source )) &&
-		     !(String.class.isAssignableFrom( source )) ) 
-			return null;
-		
-		//check if target is one of supported
-		if ( 
-			Long.class.equals( target ) || 
-			Integer.class.equals( target ) || 
-			Short.class.equals( target ) || 
-			Byte.class.equals( target ) || 
-			BigInteger.class.equals( target ) || 
-			BigDecimal.class.equals( target ) || 
-			Double.class.equals( target ) || 
-			Float.class.equals( target ) ||
-			Number.class.equals( target )
-		) {
-			return new NumericConverter();
-		}
-		
-		return null;
-	}
-	
-	class NumericConverter implements Converter {
+    public Converter createConverter(Class source, Class target, Hints hints) {
 
-		public Object convert(Object source, Class target) throws Exception {
-		    if (source instanceof Number) {
-    			Number s = (Number) source;
-    			
-    			//integral
-    			if ( Long.class.equals( target ) ) {
-    				return new Long( s.longValue() );
-    			}
-    			if ( Integer.class.equals( target ) ) {
-    				return new Integer( s.intValue() );
-    			}
-    			if ( Short.class.equals( target ) ) {
-    				return new Short( s.shortValue() );
-    			}
-    			if ( Byte.class.equals( target ) ) {
-    				return new Byte( s.byteValue() );
-    			}
-    			if ( BigInteger.class.equals( target ) ) {
-    				return BigInteger.valueOf( s.longValue() );
-    			}
-    		
-    			//floating point
-                        // JD: we use the string reprensentation to avoid coordinate
-                        // drift due to precision issues, there could be some 
-                        // performance issues with this.
-    			if ( Double.class.equals( target ) ) {
-    				return new Double( s.toString() );
-    			}
-    			if ( Float.class.equals( target ) ) {
-    				return new Float( s.toString() );
-    			}
-    			if ( BigDecimal.class.equals( target ) ) {
-    				return new BigDecimal( s.toString() );
-    			}
-    			
-    			if (Number.class.equals( target )) {
-    			    try {
+        // check if source is a number or a string. We can't convert to a number
+        // from anything else.
+        if (!(Number.class.isAssignableFrom(source)) && !(String.class.isAssignableFrom(source)))
+            return null;
+
+        // check if target is one of supported
+        if (Long.class.equals(target) || Integer.class.equals(target) || Short.class.equals(target)
+                || Byte.class.equals(target) || BigInteger.class.equals(target)
+                || BigDecimal.class.equals(target) || Double.class.equals(target)
+                || Float.class.equals(target) || Number.class.equals(target)) {
+            return new NumericConverter();
+        }
+
+        return null;
+    }
+
+    class NumericConverter implements Converter {
+
+        public Object convert(Object source, Class target) throws Exception {
+            if (source instanceof Number) {
+                Number s = (Number) source;
+
+                // integral
+                if (Long.class.equals(target)) {
+                    return new Long(s.longValue());
+                }
+                if (Integer.class.equals(target)) {
+                    return new Integer(s.intValue());
+                }
+                if (Short.class.equals(target)) {
+                    return new Short(s.shortValue());
+                }
+                if (Byte.class.equals(target)) {
+                    return new Byte(s.byteValue());
+                }
+                if (BigInteger.class.equals(target)) {
+                    return BigInteger.valueOf(s.longValue());
+                }
+
+                // floating point
+                // JD: we use the string reprensentation to avoid coordinate
+                // drift due to precision issues, there could be some
+                // performance issues with this.
+                if (Double.class.equals(target)) {
+                    return new Double(s.toString());
+                }
+                if (Float.class.equals(target)) {
+                    return new Float(s.toString());
+                }
+                if (BigDecimal.class.equals(target)) {
+                    return new BigDecimal(s.toString());
+                }
+
+                if (Number.class.equals(target)) {
+                    try {
                         return new Integer(s.toString());
                     } catch (Exception e) {
                     }
-                    
+
                     try {
                         return new BigInteger(s.toString());
                     } catch (Exception e) {
                     }
-                    
+
                     try {
                         return new Double(s.toString());
                     } catch (Exception e) {
                     }
-                    
+
                     try {
                         return new BigDecimal(s.toString());
                     } catch (Exception e) {
                     }
-    			}
-		    } else if (source instanceof String) {
-		        String s = (String) source;		        
-		        //ensure we trim any space off the string
-		        s = s.trim();
+                }
+            } else if (source instanceof String) {
+                String s = (String) source;
+                // ensure we trim any space off the string
+                s = s.trim();
 
                 String integral = toIntegral(s);
-                
-                //floating point
-                if ( Double.class.equals( target ) ) {
+
+                // floating point
+                if (Double.class.equals(target)) {
                     return new Double(s);
                 }
-                if ( Float.class.equals( target ) ) {
+                if (Float.class.equals(target)) {
                     return new Float(s);
                 }
-                if ( BigDecimal.class.equals( target ) ) {
+                if (BigDecimal.class.equals(target)) {
                     return new BigDecimal(s);
                 }
-                
 
-                
-		        //textual
-                if ( Long.class.equals( target ) ) {
+                // textual
+                if (Long.class.equals(target)) {
                     return new Long(integral);
                 }
-                if ( Integer.class.equals( target ) ) {
+                if (Integer.class.equals(target)) {
                     return new Integer(integral);
                 }
-                if ( Short.class.equals( target ) ) {
+                if (Short.class.equals(target)) {
                     return new Short(integral);
                 }
-                if ( Byte.class.equals( target ) ) {
+                if (Byte.class.equals(target)) {
                     return new Byte(integral);
                 }
-                if ( BigInteger.class.equals( target ) ) {
+                if (BigInteger.class.equals(target)) {
                     return new BigInteger(integral);
                 }
-                            
-                // fallback.  If you ask for Number, you get our 'best guess'
-                if (Number.class.equals( target )) {
-                    if( integral.equals(s)){
+
+                // fallback. If you ask for Number, you get our 'best guess'
+                if (Number.class.equals(target)) {
+                    if (integral.equals(s)) {
                         // we think this is an integer of some sort
                         try {
                             return new Integer(integral);
                         } catch (Exception e) {
                         }
-                        
+
                         try {
                             return new BigInteger(integral);
                         } catch (Exception e) {
-                        }                            
-                    }                    
+                        }
+                    }
                     try {
                         return new Double(s);
                     } catch (Exception e) {
                     }
-                    
+
                     try {
                         return new BigDecimal(s);
                     } catch (Exception e) {
                     }
                 }
-		    }
-		    //nothing matched.  Return null.
-		    return null;
-		}
+            }
+            // nothing matched. Return null.
+            return null;
+        }
 
-	}
+    }
 
-	/**
-	 * Extract the integral part out of a decimal format string. 
-	 * @param s
-	 * @return integral component of decimal representation
-	 */
-	static String toIntegral( String s ) {
-	    //NumberFormat format = NumberFormat.getInstance();
-	    
-	    int radex = -1; // last non numeric character to account for "." vs "," seperators
-        for( int i=s.length()-1; i>0; i--){
+    /**
+     * Extract the integral part out of a decimal format string.
+     * 
+     * @param s
+     * @return integral component of decimal representation
+     */
+    static String toIntegral(String s) {
+        // NumberFormat format = NumberFormat.getInstance();
+
+        int radex = -1; // last non numeric character to account for "." vs "," seperators
+        for (int i = s.length() - 1; i > 0; i--) {
             char ch = s.charAt(i);
-            if( !Character.isDigit(ch) && '-' != ch){
+            if (!Character.isDigit(ch) && '-' != ch) {
                 radex = i;
                 break;
             }
         }
-        if( radex != -1 ){
-           // text is formatted in decimal but floating point format supplied
-            return s.substring(0, radex );                    
-        }
-        else {
+        if (radex != -1) {
+            // text is formatted in decimal but floating point format supplied
+            return s.substring(0, radex);
+        } else {
             return s;
         }
     }
--- modules/library/main/src/main/java/org/geotools/util/TemporalConverterFactory.java
+++ modules/library/main/src/main/java/org/geotools/util/TemporalConverterFactory.java
@@ -27,168 +27,168 @@ import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.geotools.factory.Hints;
+
 /**
  * Converter factory which created converting between the various temporal types.
  * <p>
  * Supported converstions:
  * <ul>
- * 	<li>{@link java.util.Date} to {@link Calendar}
- *  <li>{@link java.sql.Timestamp} to {@link java.util.Calendar} 
- *  <li>{@link java.sql.Time} to {@link java.util.Calendar}
- *  <li>{@link java.util.Date} to {@link java.sql.Timestamp}
- *  <li>{@link java.util.Date} to {@link java.sql.Time}
- *  <li>{@link java.util.Date} to {@link java.sql.Date}
- *  <li>{@link java.util.Calendar} to {@link java.util.Date}
- *  <li>{@link java.util.Calendar} to {@link java.sql.Timestamp}
- *  <li>{@link java.util.Calendar} to {@link java.sql.Time}
- *  <li>{@link XMLGregorianCalendar} to {@link Calendar}
- *  <li>{@link Calendar} to {@link XMLGregorianCalendar}
- *  <li>{@link XMLGregorianCalendar} to {@link Date}
- *  <li>{@link Date} to {@link XMLGregorianCalendar}
+ * <li>{@link java.util.Date} to {@link Calendar}
+ * <li>{@link java.sql.Timestamp} to {@link java.util.Calendar}
+ * <li>{@link java.sql.Time} to {@link java.util.Calendar}
+ * <li>{@link java.util.Date} to {@link java.sql.Timestamp}
+ * <li>{@link java.util.Date} to {@link java.sql.Time}
+ * <li>{@link java.util.Date} to {@link java.sql.Date}
+ * <li>{@link java.util.Calendar} to {@link java.util.Date}
+ * <li>{@link java.util.Calendar} to {@link java.sql.Timestamp}
+ * <li>{@link java.util.Calendar} to {@link java.sql.Time}
+ * <li>{@link XMLGregorianCalendar} to {@link Calendar}
+ * <li>{@link Calendar} to {@link XMLGregorianCalendar}
+ * <li>{@link XMLGregorianCalendar} to {@link Date}
+ * <li>{@link Date} to {@link XMLGregorianCalendar}
  * </ul>
  * </p>
  * <p>
- * The hint {@link #DATE_FORMAT} can be used to control the format of converting a temporal value
- * to a String.
+ * The hint {@link #DATE_FORMAT} can be used to control the format of converting a temporal value to
+ * a String.
  * </p>
+ * 
  * @author Justin Deoliveira, The Open Planning Project
  * @since 2.4
  */
 public class TemporalConverterFactory implements ConverterFactory {
 
-	public Converter createConverter(Class source, Class target, Hints hints) {
-		
-		if ( Date.class.isAssignableFrom( source ) ) {
-			// handle all of (java.util.Date,java.sql.Timestamp,and java.sql.Time) -> java.util.Calendar 
-			if ( Calendar.class.isAssignableFrom( target ) ) {
-				return new Converter() {
-					public Object convert(Object source, Class target) throws Exception {
-						Calendar calendar = Calendar.getInstance();
-						calendar.setTime( (Date) source );
-						
-						return calendar;
-					}
-				};
-			}
-			
-			//handle all of (java.util.Date) -> (java.sql.Timestamp,java.sql.Time)
-			if ( Timestamp.class.isAssignableFrom( target ) || Time.class.isAssignableFrom( target ) || 
-                                java.sql.Date.class.isAssignableFrom( target ) ) {
-                            
-				return new Converter() {
+    public Converter createConverter(Class source, Class target, Hints hints) {
 
-					public Object convert(Object source, Class target) throws Exception {
-						Date date = (Date) source;
-						return target.getConstructor( new Class[] { Long.TYPE } )
-							.newInstance( new Object[]{ new Long( date.getTime() ) } );
-					}
-					
-				};
-			}
-			
-			if ( XMLGregorianCalendar.class.isAssignableFrom( target ) ) {
-                            return new Converter() {
-                                public <T> T convert(Object source, Class<T> target)
-                                        throws Exception {
-                                    Date date = (Date) source;
-                                    Calendar calendar = createConverter(Date.class, Calendar.class,null)
-                                        .convert( date, Calendar.class);
-                                    
-                                    return (T) createConverter( Calendar.class, XMLGregorianCalendar.class, null )
-                                        .convert( calendar, XMLGregorianCalendar.class );
-                                        
-                                }
-                            };
+        if (Date.class.isAssignableFrom(source)) {
+            // handle all of (java.util.Date,java.sql.Timestamp,and java.sql.Time) ->
+            // java.util.Calendar
+            if (Calendar.class.isAssignableFrom(target)) {
+                return new Converter() {
+                    public Object convert(Object source, Class target) throws Exception {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.setTime((Date) source);
+
+                        return calendar;
+                    }
+                };
+            }
+
+            // handle all of (java.util.Date) -> (java.sql.Timestamp,java.sql.Time)
+            if (Timestamp.class.isAssignableFrom(target) || Time.class.isAssignableFrom(target)
+                    || java.sql.Date.class.isAssignableFrom(target)) {
+
+                return new Converter() {
+
+                    public Object convert(Object source, Class target) throws Exception {
+                        Date date = (Date) source;
+                        return target.getConstructor(new Class[] { Long.TYPE }).newInstance(
+                                new Object[] { new Long(date.getTime()) });
+                    }
+
+                };
+            }
+
+            if (XMLGregorianCalendar.class.isAssignableFrom(target)) {
+                return new Converter() {
+                    public <T> T convert(Object source, Class<T> target) throws Exception {
+                        Date date = (Date) source;
+                        Calendar calendar = createConverter(Date.class, Calendar.class, null)
+                                .convert(date, Calendar.class);
+
+                        return (T) createConverter(Calendar.class, XMLGregorianCalendar.class, null)
+                                .convert(calendar, XMLGregorianCalendar.class);
+
+                    }
+                };
+            }
+            // if ( String.class.equals( target ) ) {
+            // final DateFormat fFormat;
+            // if ( dateFormat != null ) {
+            // fFormat = dateFormat;
+            // }
+            // else {
+            // //create a default
+            // fFormat = DateFormat.getDateInstance();
+            // }
+            //				
+            // return new Converter() {
+            // public Object convert(Object source, Class target) throws Exception {
+            // return fFormat.format( (Date)source );
+            // }
+            // };
+            // }
+        }
+
+        // this should handle java.util.Calendar to
+        // (java.util.Date,java.sql.Timestamp,java.util.Time}
+        if (Calendar.class.isAssignableFrom(source)) {
+            if (Date.class.isAssignableFrom(target)) {
+                final Class fTarget = target;
+                return new Converter() {
+                    public Object convert(Object source, Class target) throws Exception {
+                        Calendar calendar = (Calendar) source;
+
+                        return target.getConstructor(new Class[] { Long.TYPE }).newInstance(
+                                new Object[] { new Long(calendar.getTimeInMillis()) });
+                    }
+                };
+            }
+            if (XMLGregorianCalendar.class.isAssignableFrom(target)) {
+                return new Converter() {
+                    public <T> T convert(Object source, Class<T> target) throws Exception {
+                        if (source instanceof GregorianCalendar) {
+                            return (T) DatatypeFactory.newInstance().newXMLGregorianCalendar(
+                                    (GregorianCalendar) source);
                         }
-//			if ( String.class.equals( target ) ) {
-//				final DateFormat fFormat;
-//				if ( dateFormat != null ) {
-//					fFormat = dateFormat;
-//				}
-//				else {
-//					//create a default
-//					fFormat = DateFormat.getDateInstance();
-//				}
-//				
-//				return new Converter() {
-//					public Object convert(Object source, Class target) throws Exception {
-//						return fFormat.format( (Date)source );
-//					}
-//				};
-//			}
-		}
-		
-		//this should handle java.util.Calendar to (java.util.Date,java.sql.Timestamp,java.util.Time}
-		if ( Calendar.class.isAssignableFrom( source ) ) {
-			if ( Date.class.isAssignableFrom( target ) ) {
-				final Class fTarget = target;
-				return new Converter() {
-					public Object convert(Object source, Class target) throws Exception {
-						Calendar calendar = (Calendar) source;
-						
-						return target.getConstructor( new Class[] { Long.TYPE } ).newInstance( 
-							new Object[]{ new Long( calendar.getTimeInMillis() ) }
-						);
-					}
-				};
-			}
-			if ( XMLGregorianCalendar.class.isAssignableFrom( target ) ) {
-			    return new Converter() {
-			        public <T> T convert(Object source, Class<T> target)
-                                        throws Exception {
-			            if( source instanceof GregorianCalendar ) {
-			                return (T) DatatypeFactory.newInstance().newXMLGregorianCalendar( (GregorianCalendar) source );
-			            }
-			            
-			            return null;
-			        }
-			    };
-			}
-//			if ( String.class.equals( target ) ) {
-//				final DateFormat fFormat;
-//				if ( dateTimeFormat != null ) {
-//					fFormat = dateTimeFormat;
-//				}
-//				else {
-//					//create a default
-//					fFormat = DateFormat.getDateTimeInstance();
-//				}
-//				
-//				return new Converter() {
-//					public Object convert(Object source, Class target) throws Exception {
-//						Date date = ((Calendar)source).getTime();
-//						return fFormat.format( date );
-//					}
-//				};
-//			}
-		}
-		
-		if ( XMLGregorianCalendar.class.isAssignableFrom( source ) ) {
-		    if ( Calendar.class.isAssignableFrom( target ) ) {
-		        return new Converter() {
-                            public <T> T convert(Object source, Class<T> target)
-                                    throws Exception {
-                                XMLGregorianCalendar calendar = (XMLGregorianCalendar) source;
-                                return (T) calendar.toGregorianCalendar();
-                            }
-		        };
-		    }
-		    if ( Date.class.isAssignableFrom( target ) ) {
-		        return new Converter() {
-		            public <T> T convert(Object source, Class<T> target)
-                                    throws Exception {
-		                Calendar calendar = createConverter(XMLGregorianCalendar.class, Calendar.class, null)
-	                            .convert( source, Calendar.class );
-	                        if ( calendar != null ) {
-	                            return (T) createConverter( Calendar.class, Date.class, null )
-	                                .convert( calendar, Date.class );
-	                        }
-	                        return null;
-                            }
-		        };
-		    }
-		}
-		return null;
-	}
+
+                        return null;
+                    }
+                };
+            }
+            // if ( String.class.equals( target ) ) {
+            // final DateFormat fFormat;
+            // if ( dateTimeFormat != null ) {
+            // fFormat = dateTimeFormat;
+            // }
+            // else {
+            // //create a default
+            // fFormat = DateFormat.getDateTimeInstance();
+            // }
+            //				
+            // return new Converter() {
+            // public Object convert(Object source, Class target) throws Exception {
+            // Date date = ((Calendar)source).getTime();
+            // return fFormat.format( date );
+            // }
+            // };
+            // }
+        }
+
+        if (XMLGregorianCalendar.class.isAssignableFrom(source)) {
+            if (Calendar.class.isAssignableFrom(target)) {
+                return new Converter() {
+                    public <T> T convert(Object source, Class<T> target) throws Exception {
+                        XMLGregorianCalendar calendar = (XMLGregorianCalendar) source;
+                        return (T) calendar.toGregorianCalendar();
+                    }
+                };
+            }
+            if (Date.class.isAssignableFrom(target)) {
+                return new Converter() {
+                    public <T> T convert(Object source, Class<T> target) throws Exception {
+                        Calendar calendar = createConverter(XMLGregorianCalendar.class,
+                                Calendar.class, null).convert(source, Calendar.class);
+                        if (calendar != null) {
+                            return (T) createConverter(Calendar.class, Date.class, null).convert(
+                                    calendar, Date.class);
+                        }
+                        return null;
+                    }
+                };
+            }
+        }
+        return null;
+    }
 
 }
