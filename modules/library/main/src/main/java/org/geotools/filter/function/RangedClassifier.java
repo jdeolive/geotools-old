db606383eb21b28040703408d17d6dc04bc2d62a
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2009-08-03 08:18:42 +0000

    Apply patch for GEOT-2555 asking quantile function to check <= and < allowing it to handle single value ranges
    git-svn-id: http://svn.osgeo.org/geotools/trunk@33665 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/main/src/main/java/org/geotools/filter/function/RangedClassifier.java
+++ modules/library/main/src/main/java/org/geotools/filter/function/RangedClassifier.java
@@ -19,6 +19,20 @@ package org.geotools.filter.function;
 
 /**
  * Classifies into ranges of minimum and maximum values.
+ * <p>
+ * The buckets are defined such that:<br>
+ * min <= x < max
+ * </p>
+ * <p>
+ * So if you provide the following min/max values:<br>
+ * min = {3, 6, 9}<br>
+ * max = {4, 10, 30}<br>
+ * </p>
+ * <p>
+ * The classify function will classify items based on:<br>
+ * 3 <= x < 4<br>
+ * 6 <= x < 10<br>
+ * 9 <= x <= 30<br>
  * 
  * @author Cory Horner, Refractions Research
  *
