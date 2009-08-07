d3e8a4e3bc9c8e77b62bddf2030fe046f6090924
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2009-08-07 08:06:50 +0000

    expanded the docs
    git-svn-id: http://svn.osgeo.org/geotools/trunk@33713 e5c1c795-43da-0310-a71f-fac65c449510

--- demo/example/src/main/java/org/geotools/demo/FirstProject.java
+++ demo/example/src/main/java/org/geotools/demo/FirstProject.java
@@ -28,6 +28,8 @@ import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 
 import com.vividsolutions.jts.geom.Geometry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * The example code for the "FirstProject" in the GeoTools wiki.
@@ -38,96 +40,174 @@ import com.vividsolutions.jts.geom.Geometry;
  * <li><a href="http://docs.codehaus.org/display/GEOTDOC/04+How+to+Read+a+Shapefile">How to Read a Shapefile</a>
  * </ul>
  * 
+ * The code illustrates how to open a shapefile and collect data about the features
+ * contained within it.
+ *
  * @author Jody Garnett
  */
 public class FirstProject {
-	
-public static void main(String[] args) throws Exception {
-    System.out.println("Welcome to GeoTools:" + GeoTools.getVersion());
-    
-    File file = promptShapeFile(args);
-    try {
-    	// Connection parameters
-        Map<String,Serializable>
-        	connectParameters = new HashMap<String,Serializable>();
-        
-        connectParameters.put("url", file.toURI().toURL());
-        connectParameters.put("create spatial index", true );
-        DataStore dataStore = DataStoreFinder.getDataStore(connectParameters);
-        
-        // we are now connected
-        String[] typeNames = dataStore.getTypeNames();
-        String typeName = typeNames[0];
 
-        System.out.println("Reading content " + typeName);
+    /**
+     * Opens a shapefile provided by the user (that should contain line
+     * or polygon features), then iterates over the features within it
+     * to calculate the sum of their line or boundary length. The user
+     * can specify the path/name of the shapefile as an argument on the
+     * command line; otherwise a dialog is displayed to prompt for the
+     * file.
+     * <p>
+     * <b>Note:</b> If specifying the shapefile at the command line, don't
+     * forget the quotes around your path if there are spaces!
+     * <p>
+     * The code for this method illustrates:
+     * <ul>
+     * <li> connecting to the shapefile using a {@code DataStore} object
+     * <li> accessing features in the shapefile
+     * <li> getting spatial data for each feature
+     * </ul>
+     *
+     * @param args Optionally, the path and name of the shapefile
+     *
+     * @throws java.lang.Exception if the shapefile cannot be opened
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to GeoTools:" + GeoTools.getVersion());
 
-        FeatureSource<SimpleFeatureType, SimpleFeature> featureSource;
-        FeatureCollection<SimpleFeatureType, SimpleFeature> collection;
-        FeatureIterator<SimpleFeature> iterator;
-        
-        featureSource = dataStore.getFeatureSource(typeName);
-        collection = featureSource.getFeatures();
-        iterator = collection.features();
-        
-        double totalLength=0.0;
+        /*
+         * Get the shapefile, prompting for its path/name if
+         * this was not provided on the command line
+         */
+        File file = promptShapeFile(args);
+
         try {
-            while (iterator.hasNext()) {
-                SimpleFeature feature = iterator.next();
-                
-                Geometry geometry = (Geometry) feature.getDefaultGeometry();
-                totalLength += geometry.getLength();
+            /*
+             * Attmpt to find a GeoTools DataStore that can handle the shapefile
+             */
+            Map<String, Serializable> connectParameters = new HashMap<String, Serializable>();
+
+            connectParameters.put("url", file.toURI().toURL());
+            connectParameters.put("create spatial index", true);
+
+            DataStore dataStore = DataStoreFinder.getDataStore(connectParameters);
+            if (dataStore == null) {
+                Logger.getLogger(FirstProject.class.getName()).log(
+                        Level.WARNING, "No DataStore found to handle" + file.getPath());
+                System.exit(1);
             }
+
+            /*
+             * We are now connected to the shapefile. Get the type name of
+             * the features within it
+             */
+            String[] typeNames = dataStore.getTypeNames();
+            String typeName = typeNames[0];
+
+            System.out.println("Reading content " + typeName);
+
+            /*
+             * Iterate through the features, collecting some spatial
+             * data (line or boundary length) on each one
+             */
+            FeatureSource<SimpleFeatureType, SimpleFeature> featureSource;
+            FeatureCollection<SimpleFeatureType, SimpleFeature> collection;
+            FeatureIterator<SimpleFeature> iterator;
+
+            featureSource = dataStore.getFeatureSource(typeName);
+            collection = featureSource.getFeatures();
+            iterator = collection.features();
+
+            double totalLength = 0.0;
+            try {
+                while (iterator.hasNext()) {
+                    SimpleFeature feature = iterator.next();
+
+                    /*
+                     * The spatial portion of the feature is represented by
+                     * a Geometry object
+                     */
+                    Geometry geometry = (Geometry) feature.getDefaultGeometry();
+                    totalLength += geometry.getLength();
+                }
+            } finally {
+                /*
+                 * You MUST explicitly close the feature iterator
+                 * otherwise terrible things will happen !!!
+                 */
+                if (iterator != null) {
+                    iterator.close();
+                }
+            }
+
+            System.out.println("Total Length " + totalLength);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.exit(1);
         }
-        finally {
-        	if( iterator != null ){
-        		// YOU MUST CLOSE THE ITERATOR!
-                iterator.close();	
-        	}                
-        }
-        System.out.println("Total Length " + totalLength);
-    } catch (Exception ex) {
-        ex.printStackTrace();
-        System.exit(1);
+
+        System.exit(0);
     }
-    System.exit(0);
-}
 
-/** 
- * Prompt for File if not provided on the command line.
- * Don't forget the quotes around your path if there are spaces!
- * 
- * @throws FileNotFoundException 
- */
-private static File promptShapeFile(String[] args)
-		throws FileNotFoundException {
-	File file;
-	if (args.length == 0) {
-		JFileChooser chooser = new JFileChooser();
-		chooser.setDialogTitle("Open Shapefile for Reprojection");
-		chooser.setFileFilter(new FileFilter() {
-			public boolean accept(File f) {
-				return f.isDirectory() || f.getPath().endsWith("shp")
-						|| f.getPath().endsWith("SHP");
-			}
+    /**
+     * This method takes the command line argument from the main method which
+     * will either be empty or the name of a shapefile to open (only the first
+     * argument is examined). If empty, a dialog is displayed to prompt the
+     * user for the shapefile.
+     * <p>
+     * @return The shapefile as a new {@code File} object
+     * @throws FileNotFoundException if the specified file does not exist
+     */
+    private static File promptShapeFile(String[] args)
+            throws FileNotFoundException {
 
-			public String getDescription() {
-				return "Shapefiles";
-			}
-		});
-		int returnVal = chooser.showOpenDialog(null);
+        File file = null;
+        
+        if (args.length == 0) {
+            /*
+             * The shapefile was not specified on the command line.
+             * Display a Swing dialog to prompt the user for the file.
+             */
+            JFileChooser chooser = new JFileChooser();
+            chooser.setDialogTitle("Open Shapefile for Reprojection");
+            chooser.setFileFilter(new FileFilter() {
 
-		if (returnVal != JFileChooser.APPROVE_OPTION) {
-			System.exit(0);
-		}
-		file = chooser.getSelectedFile();
+                /*
+                 * Note: we check for lower or upper case shp extension
+                 */
+                public boolean accept(File f) {
+                    return f.isDirectory() || f.getPath().endsWith("shp") || f.getPath().endsWith("SHP");
+                }
 
-		System.out.println("You chose to open this file: " + file.getName());
-	} else {
-		file = new File(args[0]);
-	}
-	if (!file.exists()) {
-		throw new FileNotFoundException(file.getAbsolutePath());
-	}
-	return file;
-}
+                public String getDescription() {
+                    return "Shapefiles";
+                }
+            });
+            int returnVal = chooser.showOpenDialog(null);
+
+            if (returnVal != JFileChooser.APPROVE_OPTION) {
+                /*
+                 * If the user cancels just exit the program
+                 */
+                System.exit(0);
+            }
+
+            file = chooser.getSelectedFile();
+            System.out.println("You chose to open this file: " + file.getName());
+
+        } else {
+            /*
+             * The shapefile's path/name was provided on the
+             * command line
+             */
+            file = new File(args[0]);
+        }
+
+        /*
+         * Check that the file actually exists
+         */
+        if (!file.exists()) {
+            throw new FileNotFoundException(file.getAbsolutePath());
+        }
+
+        return file;
+    }
 }
