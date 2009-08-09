c1f75450c5abdbe9d6762f1dcfcfad9dd5fe7215
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2009-08-09 14:11:30 +0000

    don't test ui widgets when headless
    git-svn-id: http://svn.osgeo.org/geotools/trunk@33725 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/swing/src/test/java/org/geotools/gui/swing/dndlist/DnDListTest.java
+++ modules/unsupported/swing/src/test/java/org/geotools/gui/swing/dndlist/DnDListTest.java
@@ -1,21 +1,31 @@
 package org.geotools.gui.swing.dndlist;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
 
-import java.awt.dnd.DragGestureEvent;
+import java.awt.GraphicsEnvironment;
 
 import org.junit.Test;
 
 public class DnDListTest {
 
+    boolean isHeadless(){
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment(); 
+        return ge.isHeadless();
+    }
     @Test
     public void testDnDList() {
+        if( isHeadless() ) return;
+
         DnDList<String> list = new DnDList<String>();
         assertNotNull( list );
     }
 
     @Test
     public void testDnDListDnDListModelOfT() {
+        if( isHeadless() ) return;
+
         DnDListModel<String> model = new DnDListModel<String>();
         model.addItem("one");
         model.addItem("two");
@@ -33,6 +43,8 @@ public class DnDListTest {
 
     @Test
     public void testGetModel() {
+        if( isHeadless() ) return;
+
         DnDListModel<String> model = new DnDListModel<String>();
         model.addItem("one");
         model.addItem("two");
