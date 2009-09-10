a9473d1c32717d12d140f40aa7e8f25fe7c5d34a
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2009-09-10 03:23:49 +0000

    new style snippet: polygon fill colors based on feature data
    git-svn-id: http://svn.osgeo.org/geotools/trunk@33879 e5c1c795-43da-0310-a71f-fac65c449510

--- demo/example/src/main/java/org/geotools/demo/style/StyleSnippets.java
+++ demo/example/src/main/java/org/geotools/demo/style/StyleSnippets.java
@@ -1,17 +1,23 @@
 package org.geotools.demo.style;
 
+import java.awt.Color;
 import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.factory.GeoTools;
 import org.geotools.styling.ExternalGraphic;
 import org.geotools.styling.FeatureTypeConstraint;
 import org.geotools.styling.FeatureTypeStyle;
+import org.geotools.styling.Fill;
 import org.geotools.styling.PointSymbolizer;
+import org.geotools.styling.PolygonSymbolizer;
 import org.geotools.styling.Rule;
+import org.geotools.styling.Stroke;
 import org.geotools.styling.Style;
 import org.geotools.styling.StyleBuilder;
 import org.geotools.styling.StyleFactory;
 import org.geotools.styling.StyledLayerDescriptor;
 import org.geotools.styling.UserLayer;
 import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory;
 import org.opengis.style.Graphic;
 
 
@@ -51,8 +57,12 @@ public class StyleSnippets {
             sld.layers().add(layer);
         }        
     }
-    
-    public void featureTypeStyle(){
+
+    /**
+     * This snippet illustrates creating a feature type style that uses
+     * an external graphic
+     */
+    public void externalGraphicStyle(){
         StyleBuilder styleBuilder = new StyleBuilder();
         Style style = styleBuilder.createStyle();        
         {
@@ -72,4 +82,62 @@ public class StyleSnippets {
         }
     }
     
+    /**
+     * This snippet illustrates creating a feature type style with different
+     * color fills based on the feature's data. 
+     */
+    public void fillFromFeatureAttribute() {
+        StyleBuilder styleBuilder = new StyleBuilder();
+        Style style = styleBuilder.createStyle();
+
+        /*
+         * Assume that we are working with a feature type that has
+         * an attribute PERCENT with integer values between 0 and 100
+         */
+        final String typeName = "pretend"; // feature type name
+        final String attrName = "PERCENT"; // data attribute name
+
+        // breaks for PERCENT value ranges
+        final int[] breaks = {10, 25, 50, 75, 100};
+
+        // corresponding fill colors
+        final Color[] colors = {
+            Color.BLUE,
+            Color.GREEN,
+            Color.YELLOW,
+            Color.ORANGE,
+            Color.RED
+        };
+
+        FilterFactory ff = CommonFactoryFinder.getFilterFactory(GeoTools.getDefaultHints());
+        Rule[] rules = new Rule[breaks.length];
+
+        for (int i = 0; i < breaks.length; i++) {
+            // create a fill for this value range
+            Fill fill = styleBuilder.createFill(colors[i], 0.5d);  // partially transparent
+
+            // create the polygon outline stroke
+            Stroke stroke = styleBuilder.createStroke(colors[i], 1.0d); // line width 1.0
+
+            // create the symbolizer
+            PolygonSymbolizer symbolizer = styleBuilder.createPolygonSymbolizer(stroke, fill);
+
+            // create a rule and set the condition (value range) for which features
+            // it will apply to
+            Rule rule = styleBuilder.createRule(symbolizer);
+            Filter filter = ff.lessOrEqual(ff.property("PERCENT"), ff.literal(breaks[i]));
+            rule.setFilter(filter);
+
+            // if this is not the first rule it is an 'else' rule
+            rule.setIsElseFilter(i > 0);
+
+            rules[i] = rule;
+        }
+
+        // package our rules as a feature type style
+        FeatureTypeStyle fts = styleBuilder.createFeatureTypeStyle(typeName, rules);
+
+        // package that in our Style and we're ready to render !
+        style.featureTypeStyles().add(fts);
+    }
 }
