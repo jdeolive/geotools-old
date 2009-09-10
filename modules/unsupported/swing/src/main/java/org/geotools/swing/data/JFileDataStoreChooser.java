12db4327aebc71cfbaa210366babdee946f510f7
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2009-09-10 08:49:21 +0000

    minor fixes plus docs
    git-svn-id: http://svn.osgeo.org/geotools/trunk@33881 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/swing/src/main/java/org/geotools/swing/data/JFileDataStoreChooser.java
+++ modules/unsupported/swing/src/main/java/org/geotools/swing/data/JFileDataStoreChooser.java
@@ -21,67 +21,152 @@ import java.awt.HeadlessException;
 import java.io.File;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileFilter;
 
 import org.geotools.data.FileDataStoreFactorySpi;
 
+/**
+ * A file chooser dialog to get user choices for data stores.
+ * <p>
+ * Examples of use:
+ * <pre>{@code
+ * // prompt the user for a shapefile
+ * File file = JFileDataStoreChooser.showOpenFile("shp", parentFrame);
+ * if (file != null) {
+ *    ...
+ * }
+ *
+ * // prompt the user for a given data format
+ *
+ * }</pre>
+ *
+ * @author Jody Garnett
+ * @since 2.6
+ * @source $URL$
+ * @version $Id$
+ */
 public class JFileDataStoreChooser extends JFileChooser {
-    
-    public JFileDataStoreChooser( final String extension ){
-        setFileFilter( new FileFilter(){
-            public boolean accept( File f ) {
-                return f.isDirectory() || f.getPath().endsWith("shp") || f.getPath().endsWith("SHP");
+
+    /**
+     * Create a dialog that filters for files with the specified extension.
+     * Normally client code will not need to call this
+     * directly but will use the static method:
+     * {@linkplain #showOpenFile(java.lang.String, java.awt.Component) }
+     * @param extension the file extension, with or without the leading '.'
+     */
+    public JFileDataStoreChooser(final String extension) {
+        final String lowerExt;
+
+        if (extension.startsWith(".")) {
+            lowerExt = extension.toLowerCase();
+        } else {
+            lowerExt = "." + extension.toLowerCase();
+        }
+
+        setFileFilter(new FileFilter() {
+
+            public boolean accept(File f) {
+                return f.isDirectory() ||
+                        f.getPath().endsWith(lowerExt) ||
+                        f.getPath().endsWith(lowerExt.toUpperCase());
             }
+
             public String getDescription() {
-                return "Shapefiles";
-            }               
+                if (".shp".equals(lowerExt)) {
+                    return "Shapefiles";
+
+                } else {
+                    return lowerExt + " files";
+                }
+            }
         });
-        //int returnVal = showOpenDialog( null );
     }
-    public JFileDataStoreChooser( final FileDataStoreFactorySpi format ){
-        setFileFilter( new FileFilter(){
-            public boolean accept( File f ) {
-                if( f.isDirectory() ){
+
+    /**
+     * Creates a dialog that filters for files matching the specified
+     * data format. Normally client code will not need to call this
+     * directly but will use the static method:
+     * {@linkplain #showOpenFile(org.geotools.data.FileDataStoreFactorySpi, java.awt.Component)}
+     *
+     * @param format data file format
+     */
+    public JFileDataStoreChooser(final FileDataStoreFactorySpi format) {
+
+        setFileFilter(new FileFilter() {
+
+            public boolean accept(File f) {
+                if (f.isDirectory()) {
                     return true;
                 }
-                for( String ext : format.getFileExtensions()){
-                    if( f.getPath().endsWith(ext) ){
+
+                for (String ext : format.getFileExtensions()) {
+                    if (f.getPath().endsWith(ext)) {
                         return true;
                     }
-                    if( f.getPath().endsWith(ext.toUpperCase()) ){
+                    if (f.getPath().endsWith(ext.toUpperCase())) {
                         return true;
                     }
                 }
                 return false;
             }
+
             public String getDescription() {
-                return "Shapefiles";
-            }               
+                return format.getDescription();
+            }
         });
-        //int returnVal = showOpenDialog( null );
     }
-    
-    
-    public static File showOpenFile( String extension, Component parent ) throws HeadlessException {
-        JFileDataStoreChooser dialog = new JFileDataStoreChooser( extension );
-        int returnVal = dialog.showOpenDialog(parent);
-        if(returnVal != JFileChooser.APPROVE_OPTION) {
-            return null;
+
+    /**
+     * Show a file open dialog that filters for files with the given extension.
+     *
+     * @param extension file extension, with or without leading '.'
+     * @param parent parent GUI component (may be null)
+     *
+     * @return the selected file or null if the user cancelled the selection
+     * @throws java.awt.HeadlessException if run in an unsupported environment
+     */
+    public static File showOpenFile(String extension, Component parent) throws HeadlessException {
+        JFileDataStoreChooser dialog = new JFileDataStoreChooser(extension);
+        
+        if (dialog.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
+            return dialog.getSelectedFile();
         }
-        File file = dialog.getSelectedFile();
-        return file;
+        
+        return null;
     }
-    public static File showOpenFile( FileDataStoreFactorySpi format, Component parent ) throws HeadlessException {
-        JFileDataStoreChooser dialog = new JFileDataStoreChooser( format );
-        int returnVal = dialog.showOpenDialog(parent);
-        if(returnVal != JFileChooser.APPROVE_OPTION) {
-            return null;
+
+    /**
+     * Show a file open dialog that filters for files that match a given file
+     * data store format
+     *
+     * @param format the file data store format
+     * @param parent parent GUI component (may be null)
+     *
+     * @return the selected file or null if the user cancelled the selection
+     * @throws java.awt.HeadlessException if run in an unsupported environment
+     */
+    public static File showOpenFile(FileDataStoreFactorySpi format, Component parent) throws HeadlessException {
+        JFileDataStoreChooser dialog = new JFileDataStoreChooser(format);
+
+        if (dialog.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
+            return dialog.getSelectedFile();
         }
-        File file = dialog.getSelectedFile();
-        return file;
+
+        return null;
     }
-    
-    public static void main( String arg[] ){
-        File file = JFileDataStoreChooser.showOpenFile("shp", null );
+
+    /**
+     * Deomonstrates the file data store dialog by prompting for a shapefile
+     *
+     * @param arg ignored
+     */
+    public static void main(String arg[]) {
+        File file = JFileDataStoreChooser.showOpenFile("shp", null);
+        if (file != null) {
+            JOptionPane.showMessageDialog(null, "Selected " + file.getPath());
+        } else {
+            JOptionPane.showMessageDialog(null, "Selection cancelled");
+        }
     }
 }
