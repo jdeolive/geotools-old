8e08a05a775274a6776516aa63a07db6c172b23d
groldan <groldan@e5c1c795-43da-0310-a71f-fac65c449510>
2009-11-06 19:42:43 +0000

    added dummy api method
    git-svn-id: http://svn.osgeo.org/geotools/trunk@34338 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/plugin/arcsde/sde-dummy/src/main/java/com/esri/sde/sdk/client/SeRasterTile.java
+++ modules/plugin/arcsde/sde-dummy/src/main/java/com/esri/sde/sdk/client/SeRasterTile.java
@@ -5,6 +5,7 @@ public class SeRasterTile {
     public SeObjectId getBandId() { return null; }
     public byte[] getBitMaskData() { return null; }
     public byte[] getPixels(byte[] b) { return null; }
+    public byte[] getPixels(double[] b) { return null; }
     public float[] getPixels(float[] f) { return null; }
     public byte[] getPixelData() { return null; }
     public int getNumPixels() { return -1; }
