ee2dc9592ea28c088e31bfc78da58387758d9d08
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2009-12-03 04:56:26 +0000

    Unit tests for RasterToVectorProcess
    git-svn-id: http://svn.osgeo.org/geotools/trunk@34604 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/process/src/test/java/org/geotools/process/raster/RasterToVectorProcessTest.java
+++ modules/unsupported/process/src/test/java/org/geotools/process/raster/RasterToVectorProcessTest.java
@@ -14,125 +14,85 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-
 package org.geotools.process.raster;
 
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Polygon;
-import java.awt.image.BandedSampleModel;
-import java.awt.image.DataBuffer;
-import java.awt.image.DataBufferInt;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
-import java.awt.image.SampleModel;
-import java.awt.image.WritableRaster;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import javax.imageio.ImageIO;
 import org.geotools.coverage.CoverageFactoryFinder;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridCoverageFactory;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureIterator;
-import org.geotools.geometry.Envelope2D;
-import org.geotools.referencing.CRS;
-import org.junit.Before;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.process.ProcessException;
 import org.junit.Test;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.opengis.util.ProgressListener;
 import static org.junit.Assert.*;
 
 /**
- * TODO: Rewrite and extend tests
+ * Unit tests for {@code JMapPaneModel}.
  *
- * @author Michael Bedward <michael.bedward@gmail.com>
- *
+ * @author Michael Bedward
+ * @since 2.6
  * @source $URL$
+ * @version $Id$
  */
 public class RasterToVectorProcessTest {
-    private GridCoverage2D grid = null;
-    private static final int[] DATA = {
-            1, 1, 0, 1,
-            0, 1, 0, 0,
-            0, 1, 1, 1,
-            1, 0, 0, 1
-        };
 
-    private static final int CELL_WIDTH = 100;
-    private static final int PERIMETER = 2400;
-    private static final int AREA = 90000;
-
-    private static final String PROJ_WKT =
-            "PROJCS[\"WGS 84 / UTM zone 55S\", " +
-            "GEOGCS[\"WGS 84\", " +
-            "DATUM[\"World Geodetic System 1984\", " +
-            "SPHEROID[\"WGS 84\", 6378137.0, 298.257223563, AUTHORITY[\"EPSG\",\"7030\"]], " +
-            "AUTHORITY[\"EPSG\",\"6326\"]], " +
-            "PRIMEM[\"Greenwich\", 0.0, AUTHORITY[\"EPSG\",\"8901\"]], " +
-            "UNIT[\"degree\", 0.017453292519943295], " +
-            "AXIS[\"Geodetic latitude\", NORTH], " +
-            "AXIS[\"Geodetic longitude\", EAST], " +
-            "AUTHORITY[\"EPSG\",\"4326\"]], " +
-            "PROJECTION[\"Transverse Mercator\", AUTHORITY[\"EPSG\",\"9807\"]], " +
-            "PARAMETER[\"central_meridian\", 147.0], " +
-            "PARAMETER[\"latitude_of_origin\", 0.0], " +
-            "PARAMETER[\"scale_factor\", 0.9996], " +
-            "PARAMETER[\"false_easting\", 500000.0], " +
-            "PARAMETER[\"false_northing\", 10000000.0], " +
-            "UNIT[\"m\", 1.0], " +
-            "AXIS[\"Easting\", EAST], " +
-            "AXIS[\"Northing\", NORTH], " +
-            "AUTHORITY[\"EPSG\",\"32755\"]]";
-
-    public RasterToVectorProcessTest() {
-    }
-
-    @Before
-    public void setUp() {
-        int rows = 4, cols = 4;
-
-        /*
-         * small raster with 3 regions, two which touch at a corner
-         */
-        double minX =  700000;
-        double maxX =  700400;
-        double minY = 5500000;
-        double maxY = 5500400;
-        CoordinateReferenceSystem crs;
-        try {
-            crs = CRS.parseWKT(PROJ_WKT);
-        } catch(Exception ex) {
-            throw new IllegalStateException(ex);
-        }
-
-        DataBuffer buffer = new DataBufferInt(DATA, rows * cols);
-        SampleModel sample = new BandedSampleModel(DataBuffer.TYPE_INT, cols, rows, 1);
-        WritableRaster raster = Raster.createWritableRaster(sample, buffer, null);
-        Envelope2D envelope = new Envelope2D(crs, minX, minY, maxX - minX, maxY - minY);
-
-        GridCoverageFactory factory = CoverageFactoryFinder.getGridCoverageFactory(null);
-        grid = factory.create("testcov", raster, envelope);
-    }
+    private static final GridCoverageFactory covFactory = CoverageFactoryFinder.getGridCoverageFactory(null);
 
     /**
-     * Test of convert method, of class RasterToVectorProcess.
+     * Test conversion with a tiny coverage
      */
     @Test
     public void testConvert() throws Exception {
-        System.out.println("Vectorizing test grid coverage...");
+        System.out.println("   convert simple raster");
+
+        // small raster with 3 regions, two which touch at a corner
+        final float[][] DATA = {
+            {1, 1, 0, 1},
+            {0, 1, 0, 0},
+            {0, 1, 1, 1},
+            {1, 0, 0, 1}
+        };
+
+        final int DATA_WIDTH = 4;
+        final int CELL_WIDTH = 100;
+        final int PERIMETER = 2400;
+        final int AREA = 90000;
+
+        GridCoverage2D cov = covFactory.create(
+                "coverage",
+                DATA,
+                new ReferencedEnvelope(0, DATA_WIDTH * CELL_WIDTH, 0, DATA_WIDTH * CELL_WIDTH, null));
+
         int band = 0;
         Set<Double> outsideValues = new HashSet<Double>();
         outsideValues.add(0d);
-        
+
         ProgressListener progress = null;
         FeatureCollection<SimpleFeatureType, SimpleFeature> fc =
-                RasterToVectorProcess.process(grid, 0, null, outsideValues, progress);
-        
+                RasterToVectorProcess.process(cov, band, null, outsideValues, progress);
+
         double perimeter = 0;
         double area = 0;
         FeatureIterator iter = fc.features();
         try {
             while (iter.hasNext()) {
-                SimpleFeature feature = (SimpleFeature)iter.next();
+                SimpleFeature feature = (SimpleFeature) iter.next();
                 Polygon poly = (Polygon) feature.getDefaultGeometry();
                 perimeter += poly.getLength();
                 area += poly.getArea();
@@ -140,11 +100,81 @@ public class RasterToVectorProcessTest {
         } finally {
             iter.close();
         }
-        
-        System.out.println("Total feature area: expected " + AREA + " got " + (int)Math.round(area));
-        System.out.println("Total feature perimeter: expected " + PERIMETER + " got " + (int)Math.round(perimeter));
 
-        assertTrue(AREA == (int)Math.round(area) && PERIMETER == (int)Math.round(perimeter));
+        assertEquals(AREA, (int) Math.round(area));
+        assertEquals(PERIMETER, (int) Math.round(perimeter));
     }
 
+    /**
+     * Test conversion with an image that gave problems at one stage
+     */
+    @Test
+    public void testProblemTiff() throws IOException, ProcessException {
+        System.out.println("   convert problem image");
+
+        final double ROUND_OFF_TOLERANCE = 1.0e-4D;
+
+        URL url = getClass().getResource("data/viewshed.tif");
+        BufferedImage img = ImageIO.read(url);
+
+        ReferencedEnvelope env = new ReferencedEnvelope(
+                new Rectangle2D.Double(img.getMinX(), img.getMinY(), img.getWidth(), img.getHeight()),
+                null);
+
+        GridCoverage2D cov = covFactory.create("coverage", img, env);
+
+        int band = 0;
+        int outside = -1;
+        Set<Double> outsideValues = Collections.singleton(Double.valueOf(outside));
+
+        ProgressListener progress = null;
+        FeatureCollection<SimpleFeatureType, SimpleFeature> fc =
+                RasterToVectorProcess.process(cov, band, null, outsideValues, progress);
+
+        // validate geometries and sum areas
+        FeatureIterator<SimpleFeature> iter = fc.features();
+        Map<Integer, Double> areas = new HashMap<Integer, Double>();
+        try {
+            while (iter.hasNext()) {
+                SimpleFeature feature = iter.next();
+                Geometry geom = (Geometry) feature.getDefaultGeometry();
+                assertTrue(geom.isValid());
+
+                int code = ((Number) feature.getAttribute("code")).intValue();
+                if (code != outside) {
+                    Double sum = areas.get(code);
+                    if (sum == null) {
+                        sum = 0.0d;
+                    }
+                    sum += geom.getArea();
+                    areas.put(code, sum);
+                }
+            }
+        } finally {
+            iter.close();
+        }
+
+        // compare summed areas to image data
+        Map<Integer, Double> imgAreas = new HashMap<Integer, Double>();
+        Raster tile = img.getTile(0, 0);
+        for (int y = img.getMinY(), ny = 0; ny < img.getHeight(); y++, ny++) {
+            for (int x = img.getMinX(), nx = 0; nx < img.getWidth(); x++, nx++) {
+                int code = tile.getSample(x, y, 0);
+                if (code != outside) {
+                    Double sum = areas.get(code);
+                    if (sum == null) {
+                        sum = 1.0D;
+                    } else {
+                        sum += 1.0D;
+                    }
+                    areas.put(code, sum);
+                }
+            }
+        }
+
+        for (Integer code : imgAreas.keySet()) {
+            double ratio = areas.get(code) / imgAreas.get(code);
+            assertTrue(Math.abs(1.0D - ratio) < ROUND_OFF_TOLERANCE);
+        }
+    }
 }
