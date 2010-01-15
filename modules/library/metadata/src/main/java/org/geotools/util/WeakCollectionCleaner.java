6b6803c50c89b4f1897f0c205f61cffd51e8eb9b
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2010-01-15 18:24:21 +0000

    GEOT-2742, Need to be able to shut down the WeakCollectionCleaner
    git-svn-id: http://svn.osgeo.org/geotools/trunk@34806 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/metadata/src/main/java/org/geotools/util/WeakCollectionCleaner.java
+++ modules/library/metadata/src/main/java/org/geotools/util/WeakCollectionCleaner.java
@@ -35,7 +35,7 @@ import org.geotools.util.logging.Logging;
  * @version $Id$
  * @author Martin Desruisseaux (IRD)
  */
-final class WeakCollectionCleaner extends Thread {
+public final class WeakCollectionCleaner extends Thread {
     /**
      * The default thread.
      */
@@ -45,8 +45,8 @@ final class WeakCollectionCleaner extends Thread {
      * List of reference collected by the garbage collector.
      * Those elements must be removed from {@link #table}.
      */
-    final ReferenceQueue<Object> referenceQueue = new ReferenceQueue<Object>();
-
+    ReferenceQueue<Object> referenceQueue = new ReferenceQueue<Object>();
+    
     /**
      * Constructs and starts a new thread as a daemon. This thread will be sleeping
      * most of the time.  It will run only some few nanoseconds each time a new
@@ -58,15 +58,16 @@ final class WeakCollectionCleaner extends Thread {
         setDaemon(true);
         start();
     }
+    
+    public ReferenceQueue<Object> getReferenceQueue() {
+        return referenceQueue;
+    }
 
     /**
      * Loop to be run during the virtual machine lifetime.
      */
     @Override
     public void run() {
-        // The reference queue should never be null.  However some strange cases (maybe caused
-        // by an anormal JVM state) have been reported on the mailing list. In such case, stop
-        // the daemon instead of writting 50 Mb of log messages.
         while (referenceQueue != null) {
             try {
                 // Block until a reference is enqueded.
@@ -97,6 +98,24 @@ final class WeakCollectionCleaner extends Thread {
                 // keep the same behaviour as if assertions were turned off.
             }
         }
-        Logging.getLogger(WeakCollectionCleaner.class).severe("Daemon stopped."); // Should never happen.
+        Logging.getLogger(WeakCollectionCleaner.class).info("Weak collection cleaner stopped");
+    }
+    
+    /**
+     * Stops the cleaner thread. Calling this method is recommended in all long running applications
+     * with custom class loaders (e.g., web applications).
+     */
+    public void exit() {
+        // try to stop it gracefully
+        referenceQueue = null;
+        this.interrupt();
+        try {
+            this.join(500);
+        } catch(InterruptedException e) {
+            
+        }
+        // last resort tentative to kill the cleaner thread
+        if(this.isAlive())
+            this.stop();
     }
 }
