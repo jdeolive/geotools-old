6c1f2abc3d7659b4e6417123e545bbde3444c125
hkchow <hkchow@e5c1c795-43da-0310-a71f-fac65c449510>
2010-02-16 21:17:58 +0000

    - added more methods needed to be subclassed for IngresTestSetup.java - created folder path for db.properties file
    git-svn-id: http://svn.osgeo.org/geotools/trunk@34903 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/jdbc-ng/jdbc-ingres/src/test/java/org/geotools/data/ingres/IngresTestSetup.java
+++ modules/unsupported/jdbc-ng/jdbc-ingres/src/test/java/org/geotools/data/ingres/IngresTestSetup.java
@@ -1,7 +1,9 @@
 package org.geotools.data.ingres;
 
+import org.geotools.jdbc.JDBCDataStore;
 import org.geotools.jdbc.JDBCDataStoreFactory;
 import org.geotools.jdbc.JDBCTestSetup;
+import org.geotools.jdbc.SQLDialect;
 
 public class IngresTestSetup extends JDBCTestSetup {
 
@@ -9,5 +11,18 @@ public class IngresTestSetup extends JDBCTestSetup {
     protected JDBCDataStoreFactory createDataStoreFactory() {
         return null;
     }
+    
+    @Override
+    protected void setUpDataStore(JDBCDataStore dataStore) {
+        super.setUpDataStore(dataStore);
+        
+        
+    }
+    
+    @Override
+    protected void setUpData() throws Exception {
+       
+    }
 
+    
 }
