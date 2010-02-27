4d1b59d964ef2926b2b143fe2218bec17a530c95
hkchow <hkchow@e5c1c795-43da-0310-a71f-fac65c449510>
2010-02-27 12:33:53 +0000

    Added 2 new stubs
    git-svn-id: http://svn.osgeo.org/geotools/trunk@34958 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/jdbc-ng/jdbc-ingres/src/main/java/org/geotools/data/ingres/IngresDialect.java
+++ modules/unsupported/jdbc-ng/jdbc-ingres/src/main/java/org/geotools/data/ingres/IngresDialect.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
 
 import org.geotools.jdbc.BasicSQLDialect;
 import org.geotools.jdbc.JDBCDataStore;
@@ -40,4 +41,21 @@ public class IngresDialect extends BasicSQLDialect {
     public void encodeGeometryEnvelope(String tableName, String geometryColumn, StringBuffer sql) {
     }
 
+    /**Determines the class mapping for a particular column of a table.*/
+    @Override
+    public Class<?> getMapping(ResultSet columnMetaData, Connection cx)
+            throws SQLException {
+    	
+    	return null;
+    }
+    
+    
+    @Override
+    //to register additional mappings if necessary
+    public void registerSqlTypeNameToClassMappings(Map<String, Class<?>> mappings) {
+        super.registerSqlTypeNameToClassMappings(mappings);
+
+    }
+    
+    
 }
