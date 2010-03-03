ad8a1fbddb9695323495b5af22c028f42cc63baa
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2010-03-03 18:44:50 +0000

    Adding the GEOT-2947 test case that should have covered regressions against automatically built pyramids
    git-svn-id: http://svn.osgeo.org/geotools/trunk@34971 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/plugin/imagepyramid/src/test/java/org/geotools/gce/imagepyramid/ImagePyramidReaderTest.java
+++ modules/plugin/imagepyramid/src/test/java/org/geotools/gce/imagepyramid/ImagePyramidReaderTest.java
@@ -19,6 +19,7 @@ package org.geotools.gce.imagepyramid;
 import java.awt.Color;
 import java.awt.Rectangle;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -28,11 +29,14 @@ import javax.media.jai.PlanarImage;
 
 import junit.framework.Assert;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridEnvelope2D;
 import org.geotools.coverage.grid.GridGeometry2D;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.data.DataSourceException;
+import org.geotools.data.DataUtilities;
 import org.geotools.factory.Hints;
 import org.geotools.gce.imagemosaic.ImageMosaicFormat;
 import org.geotools.geometry.GeneralEnvelope;
@@ -61,6 +65,88 @@ public class ImagePyramidReaderTest extends Assert {
 	 */
 	private final static String TEST_FILE = "pyramid.properties";
 	private final static String TEST_JAR_FILE = "pyramid.jar";
+	
+	  
+    /**
+     * Tests automatic building of all the mosaic and pyramid files
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testAutomaticBuild() throws IOException {
+        final URL testFile = TestData.getResource(this, TEST_FILE);
+        File sourceDir = DataUtilities.urlToFile(testFile).getParentFile();
+        File targetDir = File.createTempFile("pyramid", "tst", new File("./target"));
+        targetDir.delete();
+        targetDir.mkdir();
+        try {
+            prepareEmptyMosaic(sourceDir, targetDir);
+
+            // now make sure we can actually rebuild the mosaic
+            final AbstractGridFormat format = new ImagePyramidFormat();
+            assertTrue(format.accepts(targetDir));
+            final ImagePyramidReader reader = (ImagePyramidReader) format.getReader(targetDir);
+            assertNotNull(reader);
+        } finally {
+            // cleanup
+            FileUtils.deleteDirectory(targetDir);
+        }
+    }
+
+    /**
+     * Tests automatic building of all the mosaic and pyramid files from a gdal_retile like
+     * directory structure
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testAutomaticBuildGdalRetile() throws IOException {
+        final URL testFile = TestData.getResource(this, TEST_FILE);
+        File sourceDir = DataUtilities.urlToFile(testFile).getParentFile();
+        File targetDir = File.createTempFile("pyramid", "tst", new File("./target"));
+        targetDir.delete();
+        targetDir.mkdir();
+        try {
+            prepareEmptyMosaic(sourceDir, targetDir);
+
+            // move the files so that it looks like a gdal_retile created directory
+            File zeroDir = new File(targetDir, "0");
+            assertTrue(zeroDir.isDirectory());
+            FileUtils.copyDirectory(zeroDir, targetDir);
+            FileUtils.deleteDirectory(zeroDir);
+
+            // now make sure we can actually rebuild the mosaic
+            final AbstractGridFormat format = new ImagePyramidFormat();
+            assertTrue(format.accepts(targetDir));
+            final ImagePyramidReader reader = (ImagePyramidReader) format.getReader(targetDir);
+            assertNotNull(reader);
+        } finally {
+            // cleanup
+            FileUtils.deleteDirectory(targetDir);
+        }
+    }
+
+   /**
+    * Copies the mosaic from the source dir to the target dir and removes all
+    * metadata files from it
+    * @param sourceDir
+    * @param targetDir
+    * @throws IOException
+    */
+    void prepareEmptyMosaic(File sourceDir, File targetDir) throws IOException {
+        FileUtils.copyDirectory(sourceDir, targetDir);
+        
+        // remove the files we want to recreate
+        File[] dirs = new File[] {targetDir, new File(targetDir, "0"),
+                new File(targetDir, "2"), new File(targetDir, "4"), new File(targetDir, "8")};
+        FileFilter metadataFilter = FileFilterUtils.prefixFileFilter("pyramid.");
+        for (File dir : dirs) {
+            for (File file : dir.listFiles(metadataFilter)) {
+                file.delete();
+            }
+        }
+    }
+	
 
 
 	@Test
