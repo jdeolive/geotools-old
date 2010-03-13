612a3487ce36519244e4c6f62ec817f8930b1dce
anthony87 <anthony87@e5c1c795-43da-0310-a71f-fac65c449510>
2010-03-13 21:34:35 +0000

    updating ingres test class IngresTestSetup.java
    git-svn-id: http://svn.osgeo.org/geotools/trunk@35028 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/jdbc-ng/jdbc-ingres/src/test/java/org/geotools/data/ingres/IngresTestSetup.java
+++ modules/unsupported/jdbc-ng/jdbc-ingres/src/test/java/org/geotools/data/ingres/IngresTestSetup.java
@@ -20,7 +20,19 @@ public class IngresTestSetup extends JDBCTestSetup {
     
     @Override
     protected void setUpData() throws Exception {
-       
+    	
+    	runSafe("DROP TABLE ft1");
+    	
+    	run("CREATE TABLE ft1 " +
+    			"(geometry geometry, " +
+    			"intProperty integer, " +
+    			"doubleProperty float, " +
+    			"stringProperty varchar(255))");
+    	
+    	run("INSERT INTO ft1 VALUES(0, GeometryFromText('POINT(0 0)', 4326), 0, 0.0, 'zero')");
+    	run("INSERT INTO ft1 VALUES(1, GeometryFromText('POINT(1 1)', 4326), 1, 1.1, 'one')");
+    	run("INSERT INTO ft1 VALUES(2, GeometryFromText('POINT(2 2)', 4326), 2, 2.2, 'two')");
+    	
     }
 
     
