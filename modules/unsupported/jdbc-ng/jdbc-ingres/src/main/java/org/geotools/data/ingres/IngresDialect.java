4d33b3e1dca9498a193db4099b194bd4f0d28576
hkchow <hkchow@e5c1c795-43da-0310-a71f-fac65c449510>
2010-03-15 07:58:11 +0000

    Implemented getMapping method in IngresDialect.  6 JUnit tests now pass: testCount, testCountWithOffsetLimit, testGetFeatures, testGetFeaturesWithInvalidFilter, testGetFeaturesWithInvalidQuery, testQueryCapabilitiesReliableFid.
    git-svn-id: http://svn.osgeo.org/geotools/trunk@35048 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/jdbc-ng/jdbc-ingres/src/main/java/org/geotools/data/ingres/IngresDialect.java
+++ modules/unsupported/jdbc-ng/jdbc-ingres/src/main/java/org/geotools/data/ingres/IngresDialect.java
@@ -5,8 +5,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
+import java.util.logging.Level;
 
 import org.geotools.jdbc.BasicSQLDialect;
 import org.geotools.jdbc.JDBCDataStore;
@@ -14,13 +16,42 @@ import org.opengis.feature.type.GeometryDescriptor;
 
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 
 public class IngresDialect extends BasicSQLDialect {
 
+    final static Map<String, Class> TYPE_TO_CLASS_MAP = new HashMap<String, Class>() {
+        {
+            put("GEOMETRY", Geometry.class);
+            put("POINT", Point.class);
+            put("POLYGON", Polygon.class);
+            put("MULTIPOINT", MultiPoint.class);
+            put("MULTILINESTRING", MultiLineString.class);
+            put("MULTIPOLYGON", MultiPolygon.class);
+            put("GEOMCOLLECTION", GeometryCollection.class);
+        }
+    };
+	
+    /** Whether to use only primary filters for BBOX filters */
+    boolean looseBBOXEnabled = false;
+	
     public IngresDialect(JDBCDataStore dataStore) {
         super(dataStore);
         
+    }
+    
+    public boolean isLooseBBOXEnabled() {
+        return looseBBOXEnabled;
+    }
+
+    public void setLooseBBOXEnabled(boolean looseBBOXEnabled) {
+        this.looseBBOXEnabled = looseBBOXEnabled;
     }
 
     @Override
@@ -63,7 +94,45 @@ public class IngresDialect extends BasicSQLDialect {
     public Class<?> getMapping(ResultSet columnMetaData, Connection cx)
             throws SQLException {
     	
-    	return null;
+    	final int SCHEMA_NAME = 2;
+        final int TABLE_NAME = 3;
+        final int COLUMN_NAME = 4;
+        final int TYPE_NAME = 6;
+    	
+        // grab the information we need to proceed
+        String tableName = columnMetaData.getString(TABLE_NAME);
+        String columnName = columnMetaData.getString(COLUMN_NAME);
+        String schemaName = columnMetaData.getString(SCHEMA_NAME);
+        
+        Statement statement = null;
+        ResultSet result = null;
+        String gType = null;
+        try {
+            String sqlStatement = "SELECT GEOMETRY_TYPE FROM GEOMETRY_COLUMNS WHERE " //
+                    + "SCHEMA_NAME = '" + schemaName + "' " //
+                    + "AND TABLE_NAME = '" + tableName + "' " //
+                    + "AND COLUMN_NAME = '" + columnName + "'";
+
+            LOGGER.log(Level.FINE, "Geometry type check; {0} ", sqlStatement);
+            statement = cx.createStatement();
+            result = statement.executeQuery(sqlStatement);
+       
+            
+            if (result.next()) {
+                gType = result.getString(1);  
+            }
+
+        } finally {
+            dataStore.closeSafe(result);
+            dataStore.closeSafe(statement);
+        }
+
+        // decode the type into
+        Class geometryClass = (Class) TYPE_TO_CLASS_MAP.get(gType);
+        if (geometryClass == null)
+            geometryClass = Geometry.class;
+
+        return geometryClass;
     }
     
     
@@ -102,15 +171,18 @@ public class IngresDialect extends BasicSQLDialect {
     public boolean includeTable(String schemaName, String tableName, Connection cx)
             throws SQLException {
         if (tableName.equals("geometry_columns")) {
-            return false;
+            return true;
         } else if (tableName.startsWith("spatial_ref_sys")) {
-            return false;
+            return true;
         } else if (tableName.equals("geography_columns")) {
-            return false;
+            return true;
         }
+          else if (tableName.equals("ft1")) {
+                return true;
+          }
 
         // others?
-        return true;
+        return false;
     }
 
     @Override   
