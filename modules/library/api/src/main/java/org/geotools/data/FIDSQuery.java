6a03f45eb0f2b59621ccd3a897288e8c77ee1dde
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2010-05-01 08:34:34 +0000

    added javadocs
    git-svn-id: http://svn.osgeo.org/geotools/trunk@35321 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/api/src/main/java/org/geotools/data/FIDSQuery.java
+++ modules/library/api/src/main/java/org/geotools/data/FIDSQuery.java
@@ -50,40 +50,120 @@ import org.geotools.factory.Hints;
  * @source $URL$
  */
 class FIDSQuery extends Query {
-    static final String[] NO_PROPERTIES = new String[0];
+    /** Empty String array */
+    public static final String[] NO_PROPERTIES = new String[0];
+    
+    /** The string "Request Feature IDs" */
+    public static final String FIDS_HANDLE = "Request Feature IDs";
 
+    /** For the toString method: "Query.FIDS" */
+    public static final String FIDS_NAME = "Query.FIDS";
+
+    /**
+     * Returns the value {@linkplain #NO_PROPERTIES} which is an empty String array.
+     *
+     * @return an empty array
+     *
+     * @see Query#getPropertyNames
+     */
+    @Override
     public String[] getPropertyNames() {
         return NO_PROPERTIES;
     }
 
+    /**
+     * Always returns false since FIDSQuery does not retrieve any properties.
+     *
+     * @return false
+     *
+     * @see Query#retrieveAllProperties
+     */
+    @Override
     public boolean retrieveAllProperties() {
         return false;
     }
 
+    /**
+     * Returns {@linkplain #DEFAULT_MAX} to indicate no max features limit is set.
+     *
+     * @return {@linkplain #DEFAULT_MAX}
+     *
+     * @see Query#getMaxFeatures
+     */
+    @Override
     public int getMaxFeatures() {
         return DEFAULT_MAX; // consider Integer.MAX_VALUE
     }
-    
+
+    /**
+     * Returns {@code null} to indicate no start index applies.
+     *
+     * @return {@code null}
+     *
+     * @see Query#getStartIndex
+     */
+    @Override
     public Integer getStartIndex(){
         return null;
     }
 
+    /**
+     * Always returns {@linkplain Filter#INCLUDE} to indicate that all features
+     * will be processed.
+     *
+     * @return {@linkplain Filter#INCLUDE}
+     *
+     * @see Query#getFilter
+     */
+    @Override
     public Filter getFilter() {
         return Filter.INCLUDE;
     }
 
+    /**
+     * Always returns {@code null} to indicate no feature type applies.
+     *
+     * @return {@code null}
+     *
+     * @see Query#getTypeName()
+     */
+    @Override
     public String getTypeName() {
         return null;
     }
 
+    /**
+     * Always returns {@linkplain #NO_NAMESPACE}.
+     *
+     * @return {@linkplain #NO_NAMESPACE}
+     *
+     * @see Query#getNamespace()
+     */
+    @Override
     public URI getNamespace() {
         return NO_NAMESPACE;
     }
 
+    /**
+     * Returns the constant {@linkplain #FIDS_HANDLE}.
+     *
+     * @return {@linkplain #FIDS_HANDLE}
+     *
+     * @see Query#getHandle()
+     */
+    @Override
     public String getHandle() {
-        return "Request Feature IDs";
+        return FIDS_HANDLE;
     }
 
+    /**
+     * Always returns {@code null} since version don't apply to FIDSQuery.
+     *
+     * @return {@code null}
+     *
+     * @see Query#getVersion()
+     */
+    @Override
     public String getVersion() {
         return null;
     }
@@ -93,6 +173,7 @@ class FIDSQuery extends Query {
      *
      * @return hascode for filter
      */
+    @Override
     public int hashCode() {
         String[] n = getPropertyNames();
 
@@ -116,6 +197,7 @@ class FIDSQuery extends Query {
      *
      * @return <code>true</code> if <code>obj</code> retrieves only FIDS
      */
+    @Override
     public boolean equals(Object obj) {
         if ((obj == null) || !(obj instanceof Query)) {
             return false;
@@ -145,8 +227,14 @@ class FIDSQuery extends Query {
             .getCoordinateSystemReproject()));
     }
 
+    /**
+     * Returns the constant {@linkplain #FIDS_NAME}.
+     *
+     * @return {@linkplain #FIDS_NAME}
+     */
+    @Override
     public String toString() {
-        return "Query.FIDS";
+        return FIDS_NAME;
     }
 
     /**
@@ -156,6 +244,7 @@ class FIDSQuery extends Query {
      *
      * @see org.geotools.data.Query#getCoordinateSystem()
      */
+    @Override
     public CoordinateReferenceSystem getCoordinateSystem() {
         return null;
     }
@@ -167,19 +256,29 @@ class FIDSQuery extends Query {
      *
      * @see org.geotools.data.Query#getCoordinateSystemReproject()
      */
+    @Override
     public CoordinateReferenceSystem getCoordinateSystemReproject() {
         return null;
     }
 
     /**
-     * @return {@link SortBy#UNSORTED}.
+     * Always returns {@link SortBy#UNSORTED}.
+     *
+     * @return {@link SortBy#UNSORTED}
+     *
+     * @see Query#getSortBy()
      */
+    @Override
     public SortBy[] getSortBy() {
         return SortBy.UNSORTED;
     }
 
     /**
      * Returns the GeoTools default hints {@link GeoTools#getDefaultHints()}
+     *
+     * @return {@link GeoTools#getDefaultHints()}
+     *
+     * @see Query#getHints()
      */
     public Hints getHints() {
         return GeoTools.getDefaultHints();
@@ -188,54 +287,106 @@ class FIDSQuery extends Query {
     //
     // Not mutable; all values hard coded
     //
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setCoordinateSystem(CoordinateReferenceSystem system) {
         new UnsupportedOperationException("Query.ALL cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setCoordinateSystemReproject(CoordinateReferenceSystem system) {
         new UnsupportedOperationException("Query.ALL cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setFilter(Filter filter) {
         new UnsupportedOperationException("Query.ALL cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setHandle(String handle) {
         new UnsupportedOperationException("Query.ALL cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setHints(Hints hints) {
         new UnsupportedOperationException("Query.ALL cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setMaxFeatures(int maxFeatures) {
         new UnsupportedOperationException("Query.ALL cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setNamespace(URI namespace) {
         new UnsupportedOperationException("Query.ALL cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setPropertyNames(List<String> propNames) {
         new UnsupportedOperationException("Query.FIDS cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setPropertyNames(String[] propNames) {
         new UnsupportedOperationException("Query.FIDS cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setSortBy(SortBy[] sortBy) {
         new UnsupportedOperationException("Query.FIDS cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setStartIndex(Integer startIndex) {
         new UnsupportedOperationException("Query.FIDS cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setTypeName(String typeName) {
         new UnsupportedOperationException("Query.FIDS cannot be changed, please just use as a default.");
     }
+    /**
+     * Not applicable to FIDSQuery.
+     * @throws UnsupportedOperationException if called
+     */
     @Override
     public void setVersion(String version) {
         new UnsupportedOperationException("Query.FIDS cannot be changed, please just use as a default.");
