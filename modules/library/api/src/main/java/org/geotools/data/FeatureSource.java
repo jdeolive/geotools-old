93a64695bf4dcbbca81576581210c1c690c98768
mbedward <mbedward@e5c1c795-43da-0310-a71f-fac65c449510>
2010-05-05 07:35:58 +0000

    javadoc edits
    git-svn-id: http://svn.osgeo.org/geotools/trunk@35363 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/api/src/main/java/org/geotools/data/FeatureSource.java
+++ modules/library/api/src/main/java/org/geotools/data/FeatureSource.java
@@ -17,48 +17,30 @@
 package org.geotools.data;
 
 import java.awt.RenderingHints;
-import java.beans.PropertyDescriptor;
 import java.io.IOException;
 import java.util.Set;
 
 import org.geotools.feature.FeatureCollection;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.opengis.feature.Feature;
-import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.FeatureType;
 import org.opengis.feature.type.Name;
 import org.opengis.filter.Filter;
 
 
 /**
- * Highlevel API for Features from a specific location.
- *
- * <p>
- * Individual Shapefiles, databases tables , etc. are referenced through this
- * interface. Compare and constrast with DataStore.
- * </p>
- *
- * <p>
- * Differences from DataStore:
- * </p>
- *
- * <ul>
- * <li>
- * This is a prototype DataSource replacement, the Transaction methods have
- * been moved to an external object, and the locking api has been intergrated.
- * </li>
- * <li>
- * SimpleFeatureCollection has been replaced with FeatureResult as we do not wish to
- * indicate that results can be stored in memory.
- * </li>
- * <li>
- * SimpleFeatureSource has been split into three interfaces, the intention is to use
- * the instanceof opperator to check capabilities rather than the previous
- * DataSourceMetaData.
- * </li>
- * </ul>
- *
+ * This class provides a high-level API for operations on feature data. Typically,
+ * when working with a data source such as a shapefile or database table you will
+ * initially create a {@code DataStore} object to connect
+ * to the physical source and then retrieve a {@code FeatureSource} to work with
+ * the feature data, as in this excerpt from the GeoTools Quickstart example
+ * (<a href="http://geotools.org/quickstart.html">http://geotools.org/quickstart.html</a>)
+ * <pre><code>
+ *     File file = ...
+ *     FileDataStore store = FileDataStoreFinder.getDataStore(file);
+ *     FeatureSource featureSource = store.getFeatureSource();
+ * </code></pre>
+ * @see DataStore
  *
  * @author Jody Garnett
  * @author Ray Gallagher
@@ -70,233 +52,189 @@ import org.opengis.filter.Filter;
 public interface FeatureSource<T extends FeatureType, F extends Feature>{
     
     /**
-     * Returns the qualified name for the Features this FeatureSource serves.
+     * Returns the name of the features (strictly, the name of the 
+     * {@code AttributeDescriptor} for the features) accessible through this
+     * {@code FeatureSource}.
      * <p>
-     * Note this is different from {@code getSchema().getType().getName()} (that
-     * is, the feature type name), this name specifies the
-     * {@link PropertyDescriptor#getName() AttributeDescriptor name} for the
-     * Features served by this source. So,
-     * {@code FeatureSoruce.getName() ==  FeatureSource.getFeatures().next().getAttributeDescriptor().getName()}.
-     * </p>
-     * <p>
-     * Though it's a common practice when dealing with {@link SimpleFeatureType}
-     * and {@link SimpleFeature} to assume they're equal. There's no conflict
-     * (as per the dynamic typing system the {@code org.opengis.feature} package
-     * defines) in a Feature and its type sharing the same name, as well as in a
-     * GML schema an element declaration and a type definition may be named the
-     * same. Yet, the distinction becomes important as we get rid of that
-     * assumption and thus allow to reuse a type definition for different
-     * FeatureSoruces, decoupling the descriptor (homologous to the Feature
-     * element declaration in a GML schema) from its type definition.
-     * </p>
-     * <p>
-     * So, even if implementors are allowed to delegate to
-     * {@code getSchema().getName()} if they want to call the fatures and their
-     * type the same, client code asking a
-     * {@link DataAccess#getFeatureSource(Name)} shall use this name to request
-     * for a FeatureSource, rather than the type name, as used in pre 2.5
-     * versions of GeoTools. For example, if we have a FeatureSource named
-     * {@code Roads} and its type is named {@code Roads_Type}, the
-     * {@code DataAccess} shall be queried through {@code Roads}, not
-     * {@code Roads_Type}.
-     * </p>
-     * 
+     * The value returned by this method can be different to that returned by
+     * {@code featureSource.getSchema().getType().getName()}.
+     * This is because there is a distinction between the name applied to features
+     * and the name of a feature type. When working with {@code SimpleFeature} and
+     * {@code SimpleFeatureType}, for example with a shapefile data source, it is
+     * common practice for feature and feature type names to be the same. However, this
+     * is not the case more generally. For instance, a database can contain two
+     * tables with the same structure. The feature name will refer to the table
+     * while the feature type name refers to the schema (table structure).
+     *
      * @since 2.5
-     * @return the name of the AttributeDescriptor for the Features served by
-     *         this FeatureSource
+     * @return the name of the features accessible through this {@code FeatureSource}
      */
     Name getName();
     
     /**
-     * Information describing the contents of this resource.
-     * <p>
-     * Please note that for FeatureContent:
-     * <ul>
-     * <li>name - unique with in the context of a Service
-     * <li>schema - used to identify the type of resource; usually gml schema; although it may be more specific
-     * <ul>
+     * Returns information describing this {@code FeatureSource} which may
+     * include title, description and spatial parameters. Note that in the
+     * returned {@code ResourceInfo} object, the distinction between feature
+     * name and schema (feature type) name applies as discussed for
+     * {@linkplain #getName()}.
      */
     ResourceInfo getInfo();
     
     /**
-     * Access to the DataStore implementing this FeatureStore.
+     * Returns the data source, as a {@code DataAccess} object, providing
+     * this {@code FeatureSource}.
      *
-     * @return DataStore implementing this FeatureStore
+     * @return the data source providing this {@code FeatureSource}
      */
     DataAccess<T, F> getDataStore();
 
     /**
-     * Returns and indication of what query capabilities this FeatureSource
-     * supports natively.
+     * Enquire what what query capabilities this {@code FeatureSource}
+     * natively supports. For example, whether queries can return sorted
+     * results.
      * 
-     * @return a QueryCapabilities object containing the native query capabilities.
+     * @return the native query capabilities of this {@code FeatureSource}
      * @since 2.5
      */
     QueryCapabilities getQueryCapabilities();
     
     /**
-     * Adds a listener to the list that's notified each time a change to the
-     * FeatureStore occurs.
+     * Registers a listening object that will be notified of changes to this
+     * {@code FeatureSource}.
      *
-     * @param listener FeatureListener
+     * @param listener the new listener
      */
     void addFeatureListener(FeatureListener listener);
 
     /**
-     * Removes a listener from the list that's notified each time a change to
-     * the FeatureStore occurs.
+     * Removes an object from this {@code FeatureSource's} listeners.
      *
-     * @param listener FeatureListener
+     * @param listener the listener to remove
      */
     void removeFeatureListener(FeatureListener listener);
 
     /**
-     * Loads features from the DataAccess into the returned FeatureCollection,
-     * based on the passed query.
+     * Retrieves features, in the form of a {@code FeatureCollection}, based
+     * on an OGC {@code Filter}.
+     *
+     * @param filter the filter to select features; must not be {@code null}
+     *        (use {@linkplain Filter#INCLUDE} instead)
+     *
+     * @return features retrieved by the {@code Filter}
+     *
+     * @throws IOException if the underlying data source cannot be accessed.
+     *
+     * @see Filter
+     */
+    FeatureCollection<T, F> getFeatures(Filter filter) throws IOException;
+
+    /**
+     * Retrieves features, in the form of a {@code FeatureCollection}, based
+     * on a {@code Query}.
      *
      * @param query DataAccess query for requested information, such as typeName,
      *        maxFeatures and filter.
      *
-     * @return Collection The collection to put the features into.
+     * @return features retrieved by the {@code Query}
      *
-     * @throws IOException For all data source errors.
+     * @throws IOException if the underlying data source cannot be accessed.
      *
      * @see Query
      */
     FeatureCollection<T, F> getFeatures(Query query) throws IOException;
 
     /**
-     * Loads features from the DataAccess into the returned FeatureCollection,
-     * based on the passed filter.
-     *
-     * @param filter An OpenGIS filter; specifies which features to retrieve.
-     *        <tt>null</tt> is not allowed, use Filter.INCLUDE instead.
-     *
-     * @return Collection The collection to put the features into.
-     *
-     * @throws IOException For all data source errors.
-     */
-    FeatureCollection<T, F> getFeatures(Filter filter) throws IOException;
-
-    /**
-     * Loads all features from the datasource into the return FeatureCollection.
-     *
+     * Retrieves all features in the form of a {@code FeatureCollection}.
      * <p>
-     * Filter.INCLUDE can also be used to get all features.  Calling this function
-     * is equivalent to using {@link Query#ALL}
-     * </p>
+     * The following statements are equivalent:
+     * <pre><code>
+     *     featureSource.getFeatures();
+     *     featureSource.getFeatures(Filter.INCLUDE);
+     *     featureSource.getFeatures(Query.ALL);
+     * </code></pre>
      *
-     * @return Collection The collection to put the features into.
+     * @return features retrieved by the {@code Query}
      *
-     * @throws IOException For all data source errors.
+     * @throws IOException if the underlying data source cannot be accessed.
      */
     FeatureCollection<T, F> getFeatures() throws IOException;
 
     /**
-     * Retrieves the featureType that features extracted from this datasource
-     * will be created with.
-     *
+     * Retrieves the schema (feature type) that will apply to features retrieved
+     * from this {@code FeatureSource}.
      * <p>
-     * The schema returned is the LCD supported by all available Features. In
-     * the common case of shapfiles and database table this schema will match
-     * that of every feature available. In the degenerate GML case this will
-     * simply reflect the gml:AbstractFeatureType.
-     * </p>
+     * For a homogeneous data source such as a shapefile or a database table,
+     * this schema be that of all features. For a heterogeneous data source,
+     * e.g. a GML document, the schema returned is the lowest common denominator
+     * across all features.
      *
-     * @return the schema of features created by this datasource.
-     *
-     * @task REVISIT: Our current FeatureType model is not yet advanced enough
-     *       to handle multiple featureTypes.  Should getSchema take a
-     *       typeName now that a query takes a typeName, and thus DataSources
-     *       can now support multiple types? Or just wait until we can
-     *       programmatically make powerful enough schemas?
-     * @task REVISIT: we could also just use DataStore to capture multi
-     *       FeatureTypes?
+     * @return the schema that will apply to features retrieved from this
+     *         {@code FeatureSource}
      */
     T getSchema();
 
     /**
-     * Gets the bounding box of this datasource.
-     *
+     * Get the spatial bounds of the feature data. This is equivalent
+     * to calling <code>getBounds(Query.ALL)</code>.
      * <p>
-     * With getBounds(Query) this becomes a convenience method for
-     * getBounds(Query.ALL), that is the bounds for all features contained
-     * here.
-     * </p>
+     * It is possible that this method will return null if the calculation
+     * of bounds is judged to be too costly by the implementing class. 
+     * In this case, you might call <code>getFeatures().getBounds()</code>
+     * instead.
      *
-     * <p>
-     * If getBounds() returns <code>null</code> due to expense consider using
-     * <code>getFeatures().getBounds()</code> as a an alternative.
-     * </p>
+     * @return The bounding envelope of the feature data; or {@code null}
+     *         if the bounds are unknown or too costly to calculate.
      *
-     * @return The bounding box of the datasource or null if unknown and too
-     *         expensive for the method to calculate.
-     *
-     * @throws IOException if there are errors getting the bounding box.
-     *
-     * @task REVISIT: Do we need this or can we use getBounds( Query.ALL )?
+     * @throws IOException on any errors calculating the bounds
      */
     ReferencedEnvelope getBounds() throws IOException;
 
     /**
-     * Gets the bounding box of the features that would be returned by this
-     * query.
-     *
+     * Get the spatial bounds of the features that would be returned by
+     * the given {@code Query}.
      * <p>
-     * To retrieve the bounds of the DataSource please use <code>getBounds(
-     * Query.ALL )</code>.
-     * </p>
+     * It is possible that this method will return null if the calculation
+     * of bounds is judged to be too costly by the implementing class.
+     * In this case, you might call <code>getFeatures(query).getBounds()</code>
+     * instead.
      *
-     * <p>
-     * This method is needed if we are to stream features to a gml out, since a
-     * SimpleFeatureCollection must have a boundedBy element.
-     * </p>
+     * @param query the query to select features
      *
-     * <p>
-     * If getBounds(Query) returns <code>null</code> due to expense consider
-     * using <code>getFeatures(Query).getBounds()</code> as a an alternative.
-     * </p>
+     * @return The bounding envelope of the feature data; or {@code null}
+     *         if the bounds are unknown or too costly to calculate.
      *
-     * @param query Contains the Filter, and optionally MaxFeatures and StartIndex to 
-     *  find the bounds for.
-     *
-     * @return The bounding box of the datasource or null if unknown and too
-     *         expensive for the method to calculate or any errors occur.
-     *
-     * @throws IOException DOCUMENT ME!
+     * @throws IOException on any errors calculating the bounds
      */
     ReferencedEnvelope getBounds(Query query) throws IOException;
 
     /**
-     * Gets the number of the features that would be returned by this query.
-     * 
+     * Gets the number of the features that would be returned by the given
+     * {@code Query}, taking into account any settings for max features and
+     * start index set on the {@code Query}.
      * <p>
-     * If getBounds(Query) returns <code>-1</code> due to expense consider
-     * using <code>getFeatures(Query).getCount()</code> as a an alternative.
-     * </p>
-     * <p>
-     * This method should take into account the Query's {@link Query#getMaxFeatures() maxFeatures}
-     * and {@link Query#getStartIndex() startIndex}, if present, in order to 
-     * consistently return the number of features the query would return.
-     * </p>
+     * It is possible that this method will return {@code -1} if the calculation
+     * of number of features is judged to be too costly by the implementing class.
+     * In this case, you might call <code>getFeatures(query).getBounds()</code>
+     * instead.
      *
-     * @param query Contains the Filter, and optionally MaxFeatures and StartIndex to 
-     *  find the count for.
+     * @param query the query to select features
      *
-     * @return The number of Features provided by the Query or <code>-1</code>
-     *         if count is too expensive to calculate or any errors or occur.
+     * @return the numer of features that would be returned by the {@code Query};
+     *         or {@code -1} if this cannot be calculated.
      *
      * @throws IOException if there are errors getting the count
      */
     int getCount(Query query) throws IOException;
 
     /**
-     * Returns the set of hints this {@link FeatureSource} is able to support.<p>
-     * Hints are to be specified in the {@link Query}, for each data access where they
-     * may be required.<br>
-     * Depending on the actual value provide by the user, the {@link FeatureSource}
-     * may decide not to honor the hint.
-     * @return a set of {@link RenderingHints#Key} objects (eventually empty, never null).
+     * Returns the set of hints that this {@code FeatureSource} supports via
+     * {@code Query} requests.
+     * <p>
+     * Note: the existence of a specific hint does not guarantee that it will
+     * always be honored by the implementing class.
+     *
+     * @return a set of {@code RenderingHints#Key} objects; may be empty but
+     *         never {@code null}
      */
     public Set<RenderingHints.Key> getSupportedHints();
     
