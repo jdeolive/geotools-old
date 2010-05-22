3eca174d775a0e2ebbb83fb2beaaaa214eb79a48
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2010-05-22 12:21:57 +0000

    Fix formatting on IsNullImpl as a seperate commit to keep jdeolive happy
    git-svn-id: http://svn.osgeo.org/geotools/trunk@35545 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/main/src/main/java/org/geotools/filter/IsNullImpl.java
+++ modules/library/main/src/main/java/org/geotools/filter/IsNullImpl.java
@@ -19,38 +19,39 @@ package org.geotools.filter;
 import org.opengis.filter.FilterVisitor;
 import org.opengis.filter.expression.Expression;
 
-public class IsNullImpl extends CompareFilterImpl implements
-		NullFilter {
+public class IsNullImpl extends CompareFilterImpl implements NullFilter {
 
-	public IsNullImpl(org.opengis.filter.FilterFactory factory, org.opengis.filter.expression.Expression expression) {
-		super( factory , expression , null );
-		
-		filterType = FilterType.NULL;
-	}
+    public IsNullImpl(org.opengis.filter.FilterFactory factory,
+            org.opengis.filter.expression.Expression expression) {
+        super(factory, expression, null);
 
-	public boolean evaluate(Object feature) {
-	    Expression expr = getExpression();
+        filterType = FilterType.NULL;
+    }
+
+    public boolean evaluate(Object feature) {
+        Expression expr = getExpression();
         Object value = eval(expr, feature);
-		return value == null;
-	}
+        return value == null;
+    }
 
-	public Object accept(FilterVisitor visitor, Object extraData) {
-		return visitor.visit( this, extraData );
-	}
+    public Object accept(FilterVisitor visitor, Object extraData) {
+        return visitor.visit(this, extraData);
+    }
 
-	public void nullCheckValue(org.geotools.filter.Expression nullCheck) throws IllegalFilterException {
-		setExpression( nullCheck );
-	}
+    public void nullCheckValue(org.geotools.filter.Expression nullCheck)
+            throws IllegalFilterException {
+        setExpression(nullCheck);
+    }
 
-	public org.geotools.filter.Expression getNullCheckValue() {
-		return (org.geotools.filter.Expression) getExpression();
-	}
+    public org.geotools.filter.Expression getNullCheckValue() {
+        return (org.geotools.filter.Expression) getExpression();
+    }
 
-	public Expression getExpression() {
-		return getExpression1();
-	}
+    public Expression getExpression() {
+        return getExpression1();
+    }
 
-	public void setExpression(Expression expression) {
-		super.setExpression1( expression );
-	}
+    public void setExpression(Expression expression) {
+        super.setExpression1(expression);
+    }
 }
