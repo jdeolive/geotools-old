acc6f0668d6fd44227a3fb8a5fb265268a15d305
jive <jive@e5c1c795-43da-0310-a71f-fac65c449510>
2010-07-13 08:37:23 +0000

    ignore rst2pdf extension
    git-svn-id: http://svn.osgeo.org/geotools/trunk@35898 e5c1c795-43da-0310-a71f-fac65c449510

--- docs/tutorial/conf.py
+++ docs/tutorial/conf.py
@@ -9,7 +9,7 @@ html_title='GeoTools %s Tutorial' % release
 html_theme = 'geotools-tutorial'
 
 # extension for pdf
-extensions = ['sphinx.ext.autodoc','rst2pdf.pdfbuilder']
+# extensions = ['sphinx.ext.autodoc','rst2pdf.pdfbuilder']
 
 # options for pdf
 pdf_documents = [
