b9964640e99b2cddf8429f3f86139b527f6300d2
victortey <victortey@e5c1c795-43da-0310-a71f-fac65c449510>
2010-07-28 03:38:25 +0000

    GEOT-2679: Improve app-schema handling of nillable properties: add test cases to handle simple content of type integer
    git-svn-id: http://svn.osgeo.org/geotools/trunk@35957 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/unsupported/app-schema/app-schema/src/main/java/org/geotools/data/complex/DataAccessMappingFeatureIterator.java
+++ modules/unsupported/app-schema/app-schema/src/main/java/org/geotools/data/complex/DataAccessMappingFeatureIterator.java
@@ -706,7 +706,8 @@ public class DataAccessMappingFeatureIterator extends AbstractMappingFeatureIter
         if (p.getValue() instanceof Collection) {
 
             Collection c = (Collection) p.getValue();
-
+            // Will need to add && this.getClientProperties(p).get(XLINK_HREF_NAME)!=null if we
+            // intend to skip empty xlink href eg <gsml:samplingFrame/>             
             if (c.size() == 0 && this.getClientProperties(p).containsKey(XLINK_HREF_NAME)) {
                 return true;
             }
