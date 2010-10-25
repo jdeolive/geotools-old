79fb6b0aa6512926c37623ca125ee656fcbb06fc
aaime <aaime@e5c1c795-43da-0310-a71f-fac65c449510>
2010-10-25 08:47:20 +0000

    [GEOT-3299] FontCache should expose a list of the fonts its managing
    git-svn-id: http://svn.osgeo.org/geotools/trunk@36292 e5c1c795-43da-0310-a71f-fac65c449510

--- modules/library/render/src/main/java/org/geotools/renderer/style/FontCache.java
+++ modules/library/render/src/main/java/org/geotools/renderer/style/FontCache.java
@@ -19,7 +19,6 @@ package org.geotools.renderer.style;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.GraphicsEnvironment;
-import java.awt.Toolkit;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -38,8 +37,6 @@ import java.util.logging.Logger;
  * Lookup and caches font definitions for faster retrieval
  * 
  * @author Andrea Aime - TOPP
- * 
- *
  * @source $URL$
  */
 public class FontCache {
@@ -50,21 +47,23 @@ public class FontCache {
     static FontCache defaultInstance;
 
     /** Set containing the font families known of this machine */
-    Set<String> systemFonts = null;
+    Set<String> systemFonts = new HashSet<String>();
 
     /** Fonts already loaded */
     Map<String, Font> loadedFonts = new ConcurrentHashMap<String, Font>();
 
     /**
      * Returns the default, system wide font cache
+     * 
      * @deprecated Use {@link #getDefaultInstance()} instead
      */
     public static FontCache getDefaultInsance() {
         return getDefaultInstance();
     }
-    
+
     /**
      * Returns the default, system wide font cache
+     * 
      * @since 2.6
      */
     public static FontCache getDefaultInstance() {
@@ -75,23 +74,6 @@ public class FontCache {
     }
 
     public synchronized Font getFont(String requestedFont) {
-        // make sure we load the known font families once
-        if (systemFonts == null) {
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            systemFonts = new HashSet<String>();
-
-            // register both faces and families
-            Font[] fonts = ge.getAllFonts();
-            for (Font font : fonts) {
-                systemFonts.add(font.getName());
-                systemFonts.add(font.getFamily());
-            }
-
-            if (LOGGER.isLoggable(Level.FINEST)) {
-                LOGGER.finest("there are " + systemFonts.size() + " fonts available");
-            }
-        }
-
         // see if the font has already been loaded
         java.awt.Font javaFont = null;
         if (LOGGER.isLoggable(Level.FINEST)) {
@@ -107,18 +89,18 @@ public class FontCache {
         }
 
         // if not, try to load from the java runtime or as an URL
-        if (systemFonts.contains(requestedFont)) {
+        if (getSystemFonts().contains(requestedFont)) {
             javaFont = new java.awt.Font(requestedFont, Font.PLAIN, 12);
         } else {
             if (LOGGER.isLoggable(Level.FINEST)) {
                 LOGGER.finest("not a system font");
             }
-            
-            javaFont = loadFromUrl(requestedFont); 
+
+            javaFont = loadFromUrl(requestedFont);
         }
-        
+
         // log the result and exit
-        if(javaFont == null) {
+        if (javaFont == null) {
             if (LOGGER.isLoggable(Level.WARNING)) {
                 LOGGER.fine("Could not load font " + requestedFont);
             }
@@ -130,6 +112,7 @@ public class FontCache {
 
     /**
      * Tries to load the specified font name as a URL
+     * 
      * @param fontUrl
      * @return
      */
@@ -199,11 +182,11 @@ public class FontCache {
             return null;
         }
     }
-    
+
     /**
-     * Adds the specified font in the font cache. Useful if you want to load
-     * fonts that are not installed in the Operating System and cannot provide
-     * a full path to fonts either.
+     * Adds the specified font in the font cache. Useful if you want to load fonts that are not
+     * installed in the Operating System and cannot provide a full path to fonts either.
+     * 
      * @param f
      */
     public void registerFont(Font f) {
@@ -211,14 +194,63 @@ public class FontCache {
     }
 
     /**
-     * Resets the font loading cache. If any font was manually registered, it will have to be registered again
+     * Resets the font loading cache. If any font was manually registered, it will have to be
+     * registered again
      */
     public synchronized void resetCache() {
-        if(systemFonts != null) {
+        if (systemFonts != null) {
             systemFonts.clear();
         }
-        if(loadedFonts != null) {
+        if (loadedFonts != null) {
             loadedFonts.clear();
         }
     }
+
+    /**
+     * Lazily loads up the system fonts cache
+     * 
+     * @return
+     */
+    private Set<String> getSystemFonts() {
+        // make sure we load the known font families once.
+        if (systemFonts.size() == 0) {
+            synchronized (systemFonts) {
+                if (systemFonts.size() == 0) {
+                    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+                    Set<String> fontset = new HashSet<String>();
+
+                    // register both faces and families
+                    Font[] fonts = ge.getAllFonts();
+                    for (Font font : fonts) {
+                        fontset.add(font.getName());
+                        fontset.add(font.getFamily());
+                    }
+
+                    if (LOGGER.isLoggable(Level.FINEST)) {
+                        LOGGER.finest("there are " + fontset.size() + " fonts available");
+                    }
+
+                    systemFonts.addAll(fontset);
+                }
+            }
+        }
+
+        return systemFonts;
+    }
+
+    /**
+     * Returns the set of font families and font faces available in the system and those manually
+     * loaded into the cache
+     * 
+     * @return
+     */
+    public Set<String> getAvailableFonts() {
+        Set<String> availableFonts = new HashSet<String>();
+
+        availableFonts.addAll(getSystemFonts());
+        availableFonts.addAll(loadedFonts.keySet());
+
+        return availableFonts;
+    }
+
 }
