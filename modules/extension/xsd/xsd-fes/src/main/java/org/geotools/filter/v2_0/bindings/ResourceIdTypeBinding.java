5a6804af89485c6619de64da0f50826e7a0ccc7d
jdeolive <jdeolive@gmail.com>
2011-05-04 14:28:14 -0600

    ResourceIdTypeBinding, declaring type and implementing getProperty

--- modules/extension/xsd/xsd-fes/src/main/java/org/geotools/filter/v2_0/bindings/ResourceIdTypeBinding.java
+++ modules/extension/xsd/xsd-fes/src/main/java/org/geotools/filter/v2_0/bindings/ResourceIdTypeBinding.java
@@ -3,17 +3,23 @@ package org.geotools.filter.v2_0.bindings;
 import javax.xml.namespace.QName;
 
 import org.geotools.filter.v2_0.FES;
+import org.geotools.xml.AbstractComplexBinding;
 import org.geotools.xml.AbstractComplexEMFBinding;
 import org.geotools.xml.ElementInstance;
 import org.geotools.xml.Node;
 import org.opengis.filter.FilterFactory;
+import org.opengis.filter.identity.FeatureId;
 
-public class ResourceIdTypeBinding extends AbstractComplexEMFBinding {
+public class ResourceIdTypeBinding extends AbstractComplexBinding {
 
     FilterFactory factory;
     
     public ResourceIdTypeBinding(FilterFactory factory) {
         this.factory = factory;
+    }
+    
+    public Class getType() {
+        return FeatureId.class;
     }
     
     public QName getTarget() {
@@ -23,5 +29,14 @@ public class ResourceIdTypeBinding extends AbstractComplexEMFBinding {
     @Override
     public Object parse(ElementInstance instance, Node node, Object value) throws Exception {
         return factory.featureId((String)node.getAttributeValue("rid"));
+    }
+    
+    @Override
+    public Object getProperty(Object object, QName name) throws Exception {
+        if ("rid".equals(name.getLocalPart())) {
+            FeatureId fid = (FeatureId) object;
+            return fid.getID();
+        }
+        return null;
     }
 }
